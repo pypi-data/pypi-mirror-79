5ca67c032f5bc9bd18c852f7ad97c9fdcf1f6d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:33 +0000

    Add package muses-msdb/0.1.1599736395/muses_msdb-0.1.1599736395-py3-none-any.whl

---
+++ packages/muses-msdb/muses_msdb-0.1.1599736395-py3-none-any.whl/msdb/models/__init__.py
@@ -0,0 +1,8 @@
+from .base_model import BaseModel
+from .member import MemberManager, Member
+from .message import MessageManager, Message
+from .site_param import SiteParamManager, SiteParam
+from .section import SectionManager, Section
+from .tag import TagManager, Tag
+from .license import LicenseManager, License
+from .book import BookManager, Book
