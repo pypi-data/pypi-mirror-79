6316eccfa02d46e753b33dedd7b38ddfbef79a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:36 +0000

    Add package lyncs-utils/0.0.2/lyncs_utils-0.0.2.tar.gz

---
+++ packages/lyncs-utils/lyncs_utils-0.0.2.tar.gz/lyncs_utils-0.0.2/lyncs_utils/__init__.py
@@ -0,0 +1,9 @@
+"Collection of generic-purpose and stand-alone functions"
+
+__version__ = "0.0.2"
+
+from .factors import *
+from .logical import *
+from .class_utils import *
+from .extensions import *
+from .indeces import *
