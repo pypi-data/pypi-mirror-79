b8f400ba41d0588d35c6162d815b275656d51bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:00 +0000

    Add package django-balloon-block-editor/0.0.3/django_balloon_block_editor-0.0.3.tar.gz

---
+++ packages/django-balloon-block-editor/django_balloon_block_editor-0.0.3.tar.gz/django_balloon_block_editor-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open('VERSION.txt') as f:
+    version = f.readline()
+
+
+setup(
+    name='django_balloon_block_editor',
+    version=version,
+    url='https://github.com/matix-io/django-balloon-block-editor',
+    license='MIT',
+    description='Implementation of CKEditor\'s Balloon Block Editor, for Django.',
+    long_description='',
+    author='Connor Bode',
+    author_email='connor@matix.io',
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False,
+    classifiers=[],
+)
