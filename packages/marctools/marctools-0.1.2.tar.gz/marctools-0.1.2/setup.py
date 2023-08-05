39d0659350d7232d390e330113537bb0b57031a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:57 +0000

    Add package marctools/0.1.2/marctools-0.1.2.tar.gz

---
+++ packages/marctools/marctools-0.1.2.tar.gz/marctools-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="marctools",
+    version="0.1.2",
+    packages=find_packages(),
+    url="",
+    license="MIT",
+    author="Geoffrey Spear",
+    author_email="speargh@pitt.edu",
+    description="Command line tools for MARC21 records",
+    install_requires=["pymarc", "click"],
+    entry_points={
+        "console_scripts": [
+            "marccount = marctools.marccount:marccount",
+            "marcpager = marctools.marcpager:marcpager"
+        ]
+    },
+)
