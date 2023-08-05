ae5a4669c235c6bd25f522419cac03943e5afee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:59 +0000

    Add package pylodstorage/0.0.4/pylodstorage-0.0.4.tar.gz

---
+++ packages/pylodstorage/pylodstorage-0.0.4.tar.gz/pylodstorage-0.0.4/setup.py
@@ -0,0 +1,37 @@
+# ! important
+# see https://stackoverflow.com/a/27868004/1497139
+from setuptools import setup
+from collections import OrderedDict
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pylodstorage',
+    version='0.0.4',
+
+    packages=['lodstorage',],
+    author='Wolfgang Fahl',
+    author_email='wf@bitplan.com',
+    maintainer='Wolfgang Fahl',
+    url='https://github.com/WolfgangFahl/pyLodStorage',
+    project_urls=OrderedDict(
+        (
+            ("Documentation", "http://wiki.bitplan.com/index.php/Special:FormEdit/OsProject/PyLoDStorage"),
+            ("Code", "https://github.com/WolfgangFahl/pyLodStorage"),
+            ("Issue tracker", "https://github.com/WolfgangFahl/pyLodStorage"),
+        )
+    ),
+    license='Apache License',
+    description='python List of Dict (Table) Storage library',
+    classifiers=[
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
