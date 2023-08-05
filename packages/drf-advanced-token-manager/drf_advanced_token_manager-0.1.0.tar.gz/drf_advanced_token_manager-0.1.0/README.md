ad94f510d1b3e9748ceb5e77138ee69e68fa790e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:56 +0000

    Add package drf-advanced-token-manager/0.1.0/drf_advanced_token_manager-0.1.0.tar.gz

---
+++ packages/drf-advanced-token-manager/drf_advanced_token_manager-0.1.0.tar.gz/drf_advanced_token_manager-0.1.0/README.md
@@ -0,0 +1,30 @@
+# DRF Advanced Token Manager
+
+https://pypi.org/project/drf-advanced-token-manager/
+
+A UI for users to manage their own tokens with the DRF Advanced Token Package (https://github.com/amp89/drf_advanced_token)
+
+# Install:
+pip install --upgrade drf-advanced-token-manager
+
+# Setup:
+1. make sure the DRF Advanced Token Package is installed (https://github.com/amp89/drf_advanced_token)
+2. Add `drf_advanced_token_manager` under `drf_advanced_token` in INSTALLED_APPS settings:
+```
+...
+'drf_advanced_token',
+'drf_advanced_token_manager',
+...
+```
+3. Set `SITE_HOME_URL_NAME` in settings to the __name__ of the url that is the root / base site of your project
+4. [OPTIONAL] If you don't want the user to be able to change their own token, add `PREVENT_TOKEN_UI_CHANGE=True`
+5. Add `path('token_manager/', include('drf_advanced_token_manager.urls')),` to your base `urls.py` `urlpatterns`
+
+# Usage:
+
+The added urls are now:
+View token: `token_manager/` (`name='drf_advanced_token_manager.view'`) (This is the UI where the user can manage their token.)
+Change token: `token_manager/change/` (`name='drf_advanced_token_manager.change'`) (Note: There is a link to this in the view url above!)
+
+# More:
+- Create an `UserUIKeyLock` object in the admin panel to lock a user's API key from being changed from the UI (Can still be changed from the API)
---
+++ packages/drf-advanced-token-manager/drf_advanced_token_manager-0.1.0.tar.gz/drf_advanced_token_manager-0.1.0/drf_advanced_token_manager/views.py
@@ -0,0 +1,62 @@
+
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import reverse
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib import messages
+from django.conf import settings
+from django.contrib.auth.models import User, Group
+# Create your views here.
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+from django.http import Http404, HttpResponse, HttpResponseForbidden
+from django.core.exceptions import ObjectDoesNotExist
+import uuid
+from drf_advanced_token_manager.models import UserUIKeyLock
+
+class TokenView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        try:
+            token = Token.objects.get(user=request.user)
+        except ObjectDoesNotExist as odne:
+            token = Token.objects.create(user=request.user, key=str(uuid.uuid4()))
+
+        home_url = reverse(settings.SITE_HOME_URL_NAME) if hasattr(settings,"SITE_HOME_URL_NAME") else "/"
+
+        if UserUIKeyLock.objects.filter(user=request.user).exists():
+            change_lock = True
+        else:
+            change_lock = False
+
+        return render(request, "drf_token_management_view_token.html",
+         {"token":token,"site_home":home_url, "change_lock":change_lock})
+        
+
+    def post(self, request, *args, **kwargs):
+        raise NotImplementedError
+
+
+class ChangeTokenView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        # are you sure page
+        if UserUIKeyLock.objects.filter(user=request.user).exists():
+            return HttpResponseForbidden("You are not allowed to change your token :P")
+        if not hasattr(settings,"PREVENT_TOKEN_UI_CHANGE") or not settings.PREVENT_TOKEN_UI_CHANGE:
+            home_url = reverse(settings.SITE_HOME_URL_NAME) if hasattr(settings,"SITE_HOME_URL_NAME") else "/"
+            return render(request, "drf_token_management_change_token.html", 
+              {"site_home":home_url})
+        else:
+            return HttpResponseForbidden("You are not allowed to change your token :P")
+
+    def post(self, request, *args, **kwargs):
+        if UserUIKeyLock.objects.filter(user=request.user).exists():
+            return HttpResponseForbidden("You are not allowed to change your token :P")
+        token = Token.objects.get(user=request.user)
+        token.delete()
+        token = Token.objects.create(user=request.user, key=str(uuid.uuid4()))
+        return redirect(reverse('drf_advanced_token_manager.view'))
+        
+
+
