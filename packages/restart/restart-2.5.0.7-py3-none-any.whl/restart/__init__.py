f381b8700a1a15a2dc46bc234617154a8968b8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:42 +0000

    Add package restart/2.5.0.7/restart-2.5.0.7-py3-none-any.whl

---
+++ packages/restart/restart-2.5.0.7-py3-none-any.whl/restart/__init__.py
@@ -0,0 +1,9 @@
+"""Initialization script for restart package."""
+
+# Version
+__version__ = "2.5.0.7"
+
+# Public interface for the model
+from restart.data import Data
+
+from restart.restart import RestartModel
