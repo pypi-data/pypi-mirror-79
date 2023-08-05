484687a8019743eab76841f3d0b0ef63c9237233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package pykulgap/0.0.10/pykulgap-0.0.10.tar.gz

---
+++ packages/pykulgap/pykulgap-0.0.10.tar.gz/pykulgap-0.0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as fh:
+    readme = fh.read()
+
+setup(name='pykulgap',
+      version='0.0.10',
+      description="Functions for statistical analysis of treatment response curves in patient derived xenograph (PDX) models of cancer.",
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      url='https://github.com/bhklab/pyKuLGaP/',
+      author='Janosch Ortmann, Christopher Eeles, Benjamin Haibe-Kains',
+      author_email='janosch.ortmann@gmail.com, christopher.eeles@uhnresearch.ca, benjamin.haibe.kains@utoronto.ca',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False
+      )
