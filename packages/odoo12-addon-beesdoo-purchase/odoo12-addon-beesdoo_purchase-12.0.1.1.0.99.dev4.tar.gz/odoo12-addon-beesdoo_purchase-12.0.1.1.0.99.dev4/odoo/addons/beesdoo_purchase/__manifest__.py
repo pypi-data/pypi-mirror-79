7f59a0aaa9328722a37edf836c33f4be04413795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:33 +0000

    Add package odoo12-addon-beesdoo-purchase/12.0.1.1.0.99.dev4/odoo12-addon-beesdoo_purchase-12.0.1.1.0.99.dev4.tar.gz

---
+++ packages/odoo12-addon-beesdoo-purchase/odoo12-addon-beesdoo_purchase-12.0.1.1.0.99.dev4.tar.gz/odoo12-addon-beesdoo_purchase-12.0.1.1.0.99.dev4/odoo/addons/beesdoo_purchase/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Bees Purchase",
+    "summary": """
+        - Adds a 'Responsible' field to purchase orders,
+        - A filter w.r.t. the mail sellers is placed on the products field of a
+        purchase order.
+    """,
+    "author": "Beescoop - Cellule IT, " "Coop IT Easy SCRLfs",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Purchase",
+    "version": "12.0.1.1.0",
+    "depends": ["base", "purchase", "beesdoo_product"],
+    "data": ["views/purchase_order.xml", "report/report_purchaseorder.xml"],
+    "license": "AGPL-3",
+}
