dab63ded5b5b8c1b912761c6cb61d413be0b5963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:22 +0000

    Add package python-dataschema/0.1.3/python-dataschema-0.1.3.tar.gz

---
+++ packages/python-dataschema/python-dataschema-0.1.3.tar.gz/python-dataschema-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-dataschema',
+    version='0.1.3',
+    author='Alex Shafer',
+    author_email='ashafer@pm.me',
+    url='https://github.com/ashafer01/python-dataschema',
+    license='MIT',
+    description='DataSchema schema checking for arbitrary data',
+    long_description='Define schemas for your data or configurations, and validate and normalize them in one step.',
+    packages=['dataschema'],
+)
