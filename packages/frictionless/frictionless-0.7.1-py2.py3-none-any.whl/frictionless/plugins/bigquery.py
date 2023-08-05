9a751f318c4215879848f6f838e688223bd0a377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:59 +0000

    Add package frictionless/0.7.1/frictionless-0.7.1-py2.py3-none-any.whl

---
+++ packages/frictionless/frictionless-0.7.1-py2.py3-none-any.whl/frictionless/plugins/bigquery.py
@@ -0,0 +1,17 @@
+from ..plugin import Plugin
+
+
+# Plugin
+
+
+class BigqueryPlugin(Plugin):
+    """Plugin for BigQuery
+
+    API      | Usage
+    -------- | --------
+    Public   | `from frictionless.plugins.bigquery import BigqueryPlugin`
+
+    """
+
+    def create_storage(self, source):
+        pass
---
+++ packages/frictionless/frictionless-0.7.1-py2.py3-none-any.whl/frictionless/plugins/ckan.py
@@ -0,0 +1,17 @@
+from ..plugin import Plugin
+
+
+# Plugin
+
+
+class CkanPlugin(Plugin):
+    """Plugin for CKAN
+
+    API      | Usage
+    -------- | --------
+    Public   | `from frictionless.plugins.bigquery import CkanPlugin`
+
+    """
+
+    def create_storage(self, source):
+        pass
