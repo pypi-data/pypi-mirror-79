f73f7d9c8b22e22268f01928e6684868c904ba20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package delayed/0.10.0b1/delayed-0.10.0b1-py2.py3-none-any.whl

---
+++ packages/delayed/delayed-0.10.0b1-py2.py3-none-any.whl/delayed/constants.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+class Status(object):
+    (
+        STOPPED,
+        RUNNING,
+        STOPPING
+    ) = range(3)
+
+
+BUF_SIZE = 65536  # Same as the default pipe capacity of Linux and macOS.
+SIGNAL_MASK = 0xff
+SEP = ':'
