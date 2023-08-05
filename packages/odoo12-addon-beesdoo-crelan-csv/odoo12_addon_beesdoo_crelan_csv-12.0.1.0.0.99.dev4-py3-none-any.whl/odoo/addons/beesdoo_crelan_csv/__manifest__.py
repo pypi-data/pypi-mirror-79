feecdf5275fc6a31677d0eaa47dfcb6114b8e48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:05 +0000

    Add package odoo12-addon-beesdoo-crelan-csv/12.0.1.0.0.99.dev4/odoo12_addon_beesdoo_crelan_csv-12.0.1.0.0.99.dev4-py3-none-any.whl

---
+++ packages/odoo12-addon-beesdoo-crelan-csv/odoo12_addon_beesdoo_crelan_csv-12.0.1.0.0.99.dev4-py3-none-any.whl/odoo/addons/beesdoo_crelan_csv/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2016 - 2020 BEES coop SCRLfs
+#   - Thibault Francois (thibault@françois.be)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Beescoop Crelan Import module",
+    "summary": """
+        Import Crelan CSV Wizard
+    """,
+    "author": "Beescoop - Cellule IT, Coop IT Easy SCRLfs",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Accounting & Finance",
+    "version": "12.0.1.0.0",
+    "depends": ["account_bank_statement_import"],
+    "installable": True,
+    "license": "AGPL-3",
+}
