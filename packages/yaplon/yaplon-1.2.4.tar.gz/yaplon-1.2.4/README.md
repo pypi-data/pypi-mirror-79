2c62fa54df1f3738455b47370018c34d2cff415e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:58 +0000

    Add package yaplon/1.2.4/yaplon-1.2.4.tar.gz

---
+++ packages/yaplon/yaplon-1.2.4.tar.gz/yaplon-1.2.4/README.md
@@ -0,0 +1,92 @@
+# yaplon
+
+Convert between JSON, YAML and PLIST (binary and XML) in the commandline.
+Can be used in piping. Written in Python 3.7 (not 2.7 compatible).
+
+- Copyright (c) 2019 Adam Twardoch <adam+github@twardoch.com>
+- Copyright (c) 2012-2015 Isaac Muse <isaacmuse@gmail.com>
+- [MIT license](./LICENSE)
+- Based on [Serialized Data Converter for Sublime Text](https://github.com/facelessuser/SerializedDataConverter)
+
+## Installation
+
+- Install the [release version](https://pypi.org/project/yaplon/):
+
+```
+pip3 install --user --upgrade yaplon
+```
+
+- Install the [development version](https://github.com/twardoch/yaplon):
+
+```
+pip3 install --user --upgrade git+https://github.com/twardoch/yaplon
+```
+
+## Usage
+
+```
+yaplon j2p|j2y|p2j|p2y|y2j|y2p -i input -o output [options]
+```
+
+### Commands:
+
+```
+j2p  -i JSON  -o PLIST [-b] (make binary PLIST)
+j2y  -i JSON  -o YAML  [-m] (minify)
+p2j  -i PLIST -o JSON  [-m] (minify) [-b] (keep binary)
+p2y  -i PLIST -o YAML  [-m] (minify)
+y2j  -i YAML  -o JSON  [-m] (minify) [-b] (keep binary)
+y2p  -i YAML  -o PLIST [-b] (make binary PLIST)
+```
+
+Also installs direct CLI tools: `json22plist`, `json22yaml`, `plist22json`, `plist22yaml`, `yaml22json`, `yaml22plist` that correspond to the commands. Note that they have `22` rather than `2` in the filenames, so they don’t conflict with other similar (often single-purpose) tools that you may have.
+
+## Examples
+
+### JSON to YAML
+
+File to file via the dedicated CLI tool:
+
+```
+$ json22yaml -i input.json -o output.yaml
+```
+
+Using pipe redirects, via the yaplon tool with j2y command:
+
+```
+$ yaplon j2y < input.json > output.yaml
+```
+
+Read file, output minified to stdout, via the Python 3 module
+
+```
+$ python3 -m yaplon j2y -m -i input.json
+```
+
+### PLIST to JSON
+
+Read PLIST file, output minified JSON file, via the dedicated CLI tool.
+
+```
+$ plist22json -m -i input.plist > output.json
+```
+
+Read plist file, output minified JSON to stdout, via the yaplon tool with p2j command.
+
+```
+$ yaplon p2j -m -i input.plist
+```
+
+## Changelog
+
+- 1.2.3: bugfix
+- 1.2.1: added support for orderedattrdict.AttrDict
+- 1.1.0: added -s for sorting data
+- 1.0.8: initial public release
+
+## Links
+
+- Project homepage: [https://twardoch.github.io/yaplon/](https://twardoch.github.io/yaplon/)
+- Python package on PyPi: [https://pypi.org/project/yaplon/](https://pypi.org/project/yaplon/)
+- Source on Github: [https://github.com/twardoch/yaplon](https://github.com/twardoch/yaplon)
+- Donate via [https://www.paypal.me/adamtwar](https://www.paypal.me/adamtwar)
---
+++ packages/yaplon/yaplon-1.2.4.tar.gz/yaplon-1.2.4/setup.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import os
+import re
+
+readme_file = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'README.md')
+try:
+    from m2r import parse_from_file
+    readme = parse_from_file(readme_file)
+except ImportError:
+    # m2r may not be installed in user environment
+    with open(readme_file) as f:
+        readme = f.read()
+
+
+def get_version(*args):
+    verstrline = open("yaplon/__init__.py", "rt").read()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        return "undefined"
+
+
+def get_requirements(*args):
+    """Get requirements from pip requirement files."""
+    requirements = set()
+    with open(get_absolute_path(*args)) as handle:
+        for line in handle:
+            # Strip comments.
+            line = re.sub(r'^#.*|\s#.*', '', line)
+            # Ignore empty lines
+            if line and not line.isspace():
+                requirements.add(re.sub(r'\s+', '', line))
+    return sorted(requirements)
+
+
+def get_absolute_path(*args):
+    """Transform relative pathnames into absolute pathnames."""
+    directory = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(directory, *args)
+
+
+setup(
+    name='yaplon',
+    author='Adam Twardoch',
+    author_email='adam+github@twardoch.com',
+    url='https://twardoch.github.io/yaplon/',
+    project_urls={
+        'Source': "https://github.com/twardoch/yaplon"
+    },
+    version=get_version(),
+    license="MIT",
+    description="Python 3-based commandline converter YAML ↔ JSON ↔ PLIST",
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    python_requires='>=3.7',
+    install_requires=get_requirements('requirements.txt'),
+    extras_require={
+        'dev': [
+            'setuptools',
+            'wheel',
+            'pip',
+            'twine>=3.2.0',
+            'pyinstaller>=4.0',
+            'm2r>=0.2.1'
+        ]
+    },
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='yaml json plist convert cli',
+    entry_points='''
+        [console_scripts]
+        yaplon=yaplon.__main__:cli
+        json22plist=yaplon.__main__:json2plist
+        json22yaml=yaplon.__main__:json2yaml
+        plist22json=yaplon.__main__:plist2json
+        plist22yaml=yaplon.__main__:plist2yaml
+        yaml22json=yaplon.__main__:yaml2json
+        yaml22plist=yaplon.__main__:yaml2plist
+    '''
+
+)
