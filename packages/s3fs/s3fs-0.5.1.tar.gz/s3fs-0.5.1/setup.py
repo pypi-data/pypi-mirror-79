1cf5cc26dbedbf0ced53b5c2a5ceb04294bd4d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package s3fs/0.5.1/s3fs-0.5.1.tar.gz

---
+++ packages/s3fs/s3fs-0.5.1.tar.gz/s3fs-0.5.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import versioneer
+
+setup(name='s3fs',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
+      description='Convenient Filesystem interface over S3',
+      url='http://github.com/dask/s3fs/',
+      maintainer='Martin Durant',
+      maintainer_email='mdurant@continuum.io',
+      license='BSD',
+      keywords='s3, boto',
+      packages=['s3fs'],
+      python_requires='>= 3.6',
+      install_requires=[open('requirements.txt').read().strip().split('\n')],
+      zip_safe=False)
