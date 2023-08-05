680d6c013f98a43734c1d64a3484a71f19bbcc45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package mySelfSumHy/1.1/mySelfSumHy-1.1.tar.gz

---
+++ packages/mySelfSumHy/mySelfSumHy-1.1.tar.gz/mySelfSumHy-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mySelfSumHy',# 需要打包的名字,即本模块要发布的名字
+    version='v1.1',#版本
+    description='A  module for test', # 简要描述
+    py_modules=['mySelfSum'],   #  需要打包的模块
+    author='Squidward', # 作者名
+    author_email='vzhyu@foxmail.com',   # 作者邮件
+    url='https://github.com/vfrtgb158/email', # 项目地址,一般是代码托管的网站
+    # requires=['requests','urllib3'], # 依赖包,如果没有,可以不要
+    license='MIT'
+)
