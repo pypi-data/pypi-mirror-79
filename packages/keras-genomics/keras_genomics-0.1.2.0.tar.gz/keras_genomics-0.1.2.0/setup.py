f09b2148ebe611e4b20941a0a6a83644525c465b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package keras-genomics/0.1.2.0/keras_genomics-0.1.2.0.tar.gz

---
+++ packages/keras-genomics/keras_genomics-0.1.2.0.tar.gz/keras_genomics-0.1.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+if __name__== '__main__':
+    setup(include_package_data=True,
+          name='keras_genomics',
+          version='0.1.2.0',
+          description='Keras Deep Learning for Genomics layers',
+          author='Kundaje Lab',
+          author_email='avanti.shrikumar@gmail.com',
+          url='https://github.com/kundajelab/keras-genomics',
+          license='MIT',
+          install_requires=['keras'],
+          classifiers=[
+              'Development Status :: 3 - Alpha',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Education',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: MIT License',
+              'Programming Language :: Python :: 2',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.6',
+              'Topic :: Software Development :: Libraries',
+              'Topic :: Software Development :: Libraries :: Python Modules'
+          ],
+          packages=find_packages())
