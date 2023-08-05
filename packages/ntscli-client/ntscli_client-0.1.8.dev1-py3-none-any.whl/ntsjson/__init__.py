a97bb0b1fa95f21318c720291f2c0f6fe91a6fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package ntscli-client/0.1.8.dev1/ntscli_client-0.1.8.dev1-py3-none-any.whl

---
+++ packages/ntscli-client/ntscli_client-0.1.8.dev1-py3-none-any.whl/ntsjson/__init__.py
@@ -0,0 +1,3 @@
+VERBOSE_HELP: str = "One of CRITICAL, ERROR, WARNING, INFO or DEBUG, defaults to ERROR"
+VERBOSE_DEFAULT_LEVEL: str = "ERROR"
+SAVE_TO_HELP: str = "Where to send output to. Defaults to stdout."
---
+++ packages/ntscli-client/ntscli_client-0.1.8.dev1-py3-none-any.whl/ntsjson/get_plan_impl.py
@@ -0,0 +1,14 @@
+from ntscli_cloud_lib.stateful_session import stateful_session_mgr
+
+# Implementation libs
+from ntsjson.functions import make_basic_options_dict, nonblock_target_write, set_log_levels_of_libs
+
+
+def get_plan_impl(rae, esn, ip, serial, configuration, testplan):
+    set_log_levels_of_libs()
+    options = make_basic_options_dict(esn, ip, rae, serial, configuration)
+
+    with stateful_session_mgr(**options) as session:
+        session.get_test_plan()  # reminder: stored in the session object at session.plan_request
+        plan_as_str: str = session.plan_request.to_json(indent=4)
+        nonblock_target_write(testplan, plan_as_str)
