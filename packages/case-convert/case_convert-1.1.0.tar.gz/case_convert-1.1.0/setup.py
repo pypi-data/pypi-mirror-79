ab6342fb79114f056dd1a1b9ebd18fd129a3102e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package case-convert/1.1.0/case_convert-1.1.0.tar.gz

---
+++ packages/case-convert/case_convert-1.1.0.tar.gz/case_convert-1.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="case_convert",
+    version="1.1.0",
+    author="SECRET Olivier",
+    author_email="pypi-package-case_convert@devo.live",
+    description="Cross library to convert case with permissive input",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/olive007/case-convert",
+    packages=["case_convert"],
+    package_data={
+        "": ["*.txt"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        "Operating System :: OS Independent",
+    ]
+)
