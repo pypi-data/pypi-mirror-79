314cff6982d2a30d9b05071f5c14560037aa57b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:21 +0000

    Add package gadgetron/1.2.3/gadgetron-1.2.3.tar.gz

---
+++ packages/gadgetron/gadgetron-1.2.3.tar.gz/gadgetron-1.2.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+import gadgetron
+
+setup(
+    name='gadgetron',
+    version=gadgetron.__version__,
+    packages=[
+        'gadgetron',
+        'gadgetron.util',
+        'gadgetron.external',
+        'gadgetron.examples',
+        'gadgetron.legacy',
+        'gadgetron.types'
+    ],
+    url='',
+    license='MIT',
+    author='Kristoffer Langeland Knudsen',
+    author_email='kristofferlknudsen@gradientsoftware.net',
+    description='',
+    install_requires=[
+        'numpy>=1.15.1',
+        'ismrmrd>=1.6.2',
+        'pyFFTW>=0.11',
+        'multimethod'
+    ]
+)
