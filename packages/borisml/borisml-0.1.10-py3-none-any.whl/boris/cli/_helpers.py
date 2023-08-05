3e0ae35d2199b4f5ca8fed63457b531e003679ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:58 +0000

    Add package borisml/0.1.10/borisml-0.1.10-py3-none-any.whl

---
+++ packages/borisml/borisml-0.1.10-py3-none-any.whl/boris/cli/_helpers.py
@@ -0,0 +1,86 @@
+import os
+import torch
+
+from hydra import utils
+
+
+def fix_input_path(path):
+    """Fix broken relative paths.
+
+    """
+    if not os.path.isabs(path):
+        path = utils.to_absolute_path(path)
+    return path
+
+
+def filter_state_dict(state_dict):
+    """Prevent unexpected key error when loading PyTorch-Lightning checkpoints
+       by removing the unnecessary prefix model. from each key.
+
+    """
+    new_state_dict = {}
+    for key, item in state_dict.items():
+        new_key = '.'.join(key.split('.')[1:])
+        new_state_dict[new_key] = item
+    return new_state_dict
+
+
+def _is_url(checkpoint):
+    is_url = ('https://storage.googleapis.com' in checkpoint)
+    return is_url
+
+
+def _model_zoo(model):
+
+    zoo = {
+
+        'resnet-9/simclr/d16/w0.0625':
+        'https://storage.googleapis.com/models_boris/whattolabel-resnet9-simclr-d16-w0.0625-i-ce0d6bd9.pth',
+
+        'resnet-9/simclr/d16/w0.125':
+        'https://storage.googleapis.com/models_boris/whattolabel-resnet9-simclr-d16-w0.125-i-7269c38d.pth',
+
+        'resnet-18/simclr/d16/w1.0':
+        'https://storage.googleapis.com/models_boris/whattolabel-resnet18-simclr-d16-w1.0-i-58852cb9.pth',
+
+        'resnet-18/simclr/d32/w1.0':
+        'https://storage.googleapis.com/models_boris/whattolabel-resnet18-simclr-d32-w1.0-i-085d0693.pth',
+
+        'resnet-34/simclr/d16/w1.0':
+        'https://storage.googleapis.com/models_boris/whattolabel-resnet34-simclr-d16-w1.0-i-6e80d963.pth',
+
+    }
+
+    key = model['name']
+    key += '/simclr'
+    key += '/d' + str(model['num_ftrs'])
+    key += '/w' + str(float(model['width']))
+
+    if key in zoo.keys():
+        return zoo[key], key
+    else:
+        return '', key
+
+
+def load_state_dict_from_url(url, map_location=None):
+
+    try:
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, map_location=map_location
+        )
+        return state_dict
+    except Exception:
+        print('Not able to load state dict from %s' % (url))
+        print('Retrying with http:// prefix')
+    try:
+        url = url.replace('https', 'http')
+        state_dict = torch.hub.load_state_dict_from_url(
+            url, map_location=map_location
+        )
+        return state_dict
+    except Exception:
+        print('Not able to load state dict from %s' % (url))
+
+    # in this case downloading the pre-trained model was not possible
+    # notify the user and return
+    return {'state_dict': None}
---
+++ packages/borisml/borisml-0.1.10-py3-none-any.whl/boris/data/__init__.py
@@ -0,0 +1,11 @@
+""" boris.data
+
+    The boris.data module provides a dataset wrapper
+    and useful collate functions for self-supervised learning
+    (see torch.utils.data.DataLoader for details on collate_fn)
+
+"""
+
+from ._dataset import BorisDataset
+from ._collate import BaseCollateFunction
+from ._collate import ImageCollateFunction
