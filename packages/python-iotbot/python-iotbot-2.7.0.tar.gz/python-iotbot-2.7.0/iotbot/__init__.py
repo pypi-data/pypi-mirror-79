ba26956a0d8d01c85f1e02eec9f8e24e63cfab5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:55 +0000

    Add package python-iotbot/2.7.0/python-iotbot-2.7.0.tar.gz

---
+++ packages/python-iotbot/python-iotbot-2.7.0.tar.gz/python-iotbot-2.7.0/iotbot/__init__.py
@@ -0,0 +1,30 @@
+"""Author: wongxy
+
+github.com/xiyaowong/python-iotbot
+
+>>> from iotbot import IOTBOT, GroupMsg, FriendMsg, EventMsg
+
+>>> bot = IOTBOT(your_bot_qq)
+
+>>> @bot.on_group_msg
+>>> def group(ctx: GroupMsg):
+>>>    pass
+
+>>> @bot.on_friend_msg
+>>> def friend(ctx: FriendMsg):
+>>>    pass
+
+>>> @bot.on_event
+>>> def event(message: EventMsg):
+>>>     pass
+
+>>> if __name__ == '__main__':
+        bot.run()
+"""
+
+from . import refine as refine_message  # 兼容旧版本，以后可能删除
+from .action import Action
+from .client import IOTBOT
+from .model import EventMsg, FriendMsg, GroupMsg
+
+__author__ = 'wongxy'
