dbceb88a485dce6f97a6412c55ec7b41975e63ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package kess/0.1.1/kess-0.1.1.tar.gz

---
+++ packages/kess/kess-0.1.1.tar.gz/kess-0.1.1/kess/imports.py
@@ -0,0 +1,15 @@
+import importlib
+import importlib.util
+import uuid
+from types import ModuleType
+
+import_module = importlib.import_module
+reload = importlib.reload
+
+
+def import_file(file_path: str, module_name: str = None) -> ModuleType:
+    module_name = module_name or uuid.uuid4().hex
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
