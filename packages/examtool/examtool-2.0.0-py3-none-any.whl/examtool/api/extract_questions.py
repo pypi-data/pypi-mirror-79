960eb3cb115c1ca7c2084373ab75b0b265e63a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package examtool/2.0.0/examtool-2.0.0-py3-none-any.whl

---
+++ packages/examtool/examtool-2.0.0-py3-none-any.whl/examtool/api/extract_questions.py
@@ -0,0 +1,42 @@
+from examtool.api.scramble import is_compressible_group
+
+def extract_questions(exam, extract_public_bool: bool=True, top_level: bool=True):
+    if extract_public_bool:
+        yield from extract_public(exam)
+    if top_level:
+        for group in exam["groups"]:
+            yield from group_questions(group)
+    else:
+        yield from group_questions(exam)
+
+
+def group_questions(group):
+    out = _group_questions(group)
+    try:
+        first = next(out)
+    except StopIteration:
+        return
+    first["text"] = group["name"] + "\n\n" + group["text"] + "\n\n" + first["text"]
+    yield first
+    yield from out
+
+
+def _group_questions(group):
+    for element in group.get("elements", []) + group.get("questions", []):
+        if element.get("type") == "group":
+            out = group_questions(element)
+            yield from out
+        else:
+            yield element
+
+def extract_public(exam):
+    if exam.get("public"):
+        yield from group_questions(exam["public"])
+
+def extract_groups(group):
+    for g in group["groups"]:
+        if is_compressible_group(g):
+            for g2 in g["elements"]:
+                yield g2
+        else:
+            yield g
