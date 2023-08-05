8d07b19820ecf6375caadb2a8475e00a1c4ee57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package igel/0.1.5/igel-0.1.5.tar.gz

---
+++ packages/igel/igel-0.1.5.tar.gz/igel-0.1.5/HISTORY.rst
@@ -0,0 +1,36 @@
+=======
+History
+=======
+
+
+0.1.5 (2020-09-10)
+------------------
+* implemented encoding and scaling methods
+
+0.1.4 (2020-09-08)
+------------------
+* support for all sklearn models
+
+0.1.3 (2020-09-07)
+------------------
+* implemented basic dataset operations
+
+0.0.1 (2020-09-05)
+------------------
+* stable release with an end to end pipeline
+
+0.0.6 (2020-09-01)
+------------------
+* Added validation on arguments and provided an example
+
+0.0.5 (2020-08-31)
+------------------
+* Added logging and changed file keyword to yaml_file
+
+0.0.3 (2020-08-30)
+------------------
+* First functional package
+
+0.0.1 (2020-08-27)
+------------------
+* First release on PyPI.
---
+++ packages/igel/igel-0.1.5.tar.gz/igel-0.1.5/igel/preprocessing.py
@@ -0,0 +1,65 @@
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import numpy as np
+import pandas as pd
+import logging
+
+logging.basicConfig(format='%(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def update_dataset_props(dataset_props: dict, default_dataset_props: dict):
+    for key1 in default_dataset_props.keys():
+        if key1 in dataset_props.keys():
+            for key2 in default_dataset_props[key1].keys():
+                if key2 in dataset_props[key1].keys():
+                    default_dataset_props[key1][key2] = dataset_props[key1][key2]
+
+    return default_dataset_props
+
+
+def handle_missing_values(df, fill_value=np.nan, strategy="mean"):
+    logger.info(f"Check for missing values in the dataset ...  \n"
+                f"{df.isna().sum()}  \n "
+                f"{'-'*100}")
+
+    if strategy.lower() == "drop":
+        return df.dropna()
+
+    cleaner = SimpleImputer(fill_value=fill_value, strategy=strategy)
+    cleaned = cleaner.fit_transform(df)
+
+    return pd.DataFrame(cleaned, columns=df.columns)
+
+
+def encode(df, encoding_type="onehotencoding", column=None):
+    if not encoding_type:
+        raise Exception(f"encoding type should be -> oneHotEncoding or labelEncoding")
+
+    if encoding_type == "onehotencoding":
+        logger.info(f"performing a one hot encoding ...")
+        return pd.get_dummies(df)
+
+    elif encoding_type == "labelencoding":
+        logger.info(f"performing a label encoding ...")
+        encoder = LabelEncoder()
+        encoder.fit(df[column])
+        df[column] = encoder.transform(df[column])
+        return df
+
+    else:
+        raise Exception(f"encoding type should be -> oneHotEncoding or labelEncoding")
+
+
+def normalize(x, y=None, method='standard'):
+    methods = ('minmax', 'standard')
+
+    if method not in methods:
+        raise Exception(f"Please choose one of the available scaling methods => {methods}")
+    logger.info(f"performing a {method} scaling ...")
+    scaler = MinMaxScaler() if method == 'minmax' else StandardScaler()
+    if not y:
+        return scaler.fit_transform(X=x)
+    else:
+        return scaler.fit_transform(X=x, y=y)
