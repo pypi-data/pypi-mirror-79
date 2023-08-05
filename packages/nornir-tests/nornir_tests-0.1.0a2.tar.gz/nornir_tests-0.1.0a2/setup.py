7f67747df40b6f38150611fec934d0c885ba551b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:12 +0000

    Add package nornir-tests/0.1.0a2/nornir_tests-0.1.0a2.tar.gz

---
+++ packages/nornir-tests/nornir_tests-0.1.0a2.tar.gz/nornir_tests-0.1.0a2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nornir_tests',
+ 'nornir_tests.plugins',
+ 'nornir_tests.plugins.processors',
+ 'nornir_tests.plugins.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['jsonpath-ng>=1.5.2,<2.0.0', 'lxml>=4.5.2,<5.0.0', 'nornir>=3.0.0b1,<3.1.0']
+
+extras_require = \
+{':python_version >= "3.6" and python_version < "3.7"': ['dataclasses>=0.7,<0.8']}
+
+setup_kwargs = {
+    'name': 'nornir-tests',
+    'version': '0.1.0a2',
+    'description': '',
+    'long_description': None,
+    'author': 'Patrick Avery',
+    'author_email': 'patrickdaj@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
