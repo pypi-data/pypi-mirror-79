7901858ca3b42499264871cae81117cecd1dbc8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:44 +0000

    Add package streamlit-vega-lite/0.0.1/streamlit_vega_lite-0.0.1-py3-none-any.whl

---
+++ packages/streamlit-vega-lite/streamlit_vega_lite-0.0.1-py3-none-any.whl/streamlit-vega-lite/frontend/build/precache-manifest.e2fcf042424217c5590940fb297361b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f8c057910a39020cea524f6e552490",
+    "url": "./index.html"
+  },
+  {
+    "revision": "580631bcb2ad96f35ea1",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "580631bcb2ad96f35ea1",
+    "url": "./static/js/2.eb63a505.chunk.js"
+  },
+  {
+    "revision": "7fd9a81b3ae1e9f05e8eb6467a631a58",
+    "url": "./static/js/2.eb63a505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5025d3bb958fba53de5",
+    "url": "./static/js/main.9422f109.chunk.js"
+  },
+  {
+    "revision": "f16f00b19cc478c2620c",
+    "url": "./static/js/runtime-main.5e376684.js"
+  }
+]);
