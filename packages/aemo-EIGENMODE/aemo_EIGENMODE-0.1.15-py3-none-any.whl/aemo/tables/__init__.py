46546040511a9dadf0edf991954ea8158684eee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:02 +0000

    Add package aemo-EIGENMODE/0.1.15/aemo_EIGENMODE-0.1.15-py3-none-any.whl

---
+++ packages/aemo-EIGENMODE/aemo_EIGENMODE-0.1.15-py3-none-any.whl/aemo/tables/__init__.py
@@ -0,0 +1,18 @@
+import aemo.tables.settlements as settlements
+from typing import Callable, Any, List
+import aemo.key as key
+
+
+# this needs to be updated as move modules are added above
+def mapping(key: key.TableKey) -> Callable[[List[str]], Any]:
+    to_fn = {
+        # start settlements
+        settlements.DayTrack.key(): settlements.DayTrack.from_row,
+        settlements.Cpdata.key(): settlements.Cpdata.from_row,
+        settlements.FcasRecovery.key(): settlements.FcasRecovery.from_row,
+        settlements.Marketfees.key(): settlements.Marketfees.from_row,
+        settlements.NmasRecovery.key(): settlements.NmasRecovery.from_row,
+        settlements.Reallocations.key(): settlements.Reallocations.from_row,
+        # end settlements
+    }
+    return to_fn[key]
