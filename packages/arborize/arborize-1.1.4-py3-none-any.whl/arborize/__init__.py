448acbaad588be770340cd3b3fa346d4f7fd428d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package arborize/1.1.4/arborize-1.1.4-py3-none-any.whl

---
+++ packages/arborize/arborize-1.1.4-py3-none-any.whl/arborize/__init__.py
@@ -0,0 +1,13 @@
+from .core import *
+from .synapse import Synapse
+
+__version__ = "1.1.4"
+
+_morphology_dirs = []
+
+def add_directory(path):
+    """
+        Add a path that Arborize should look through for morphology files.
+    """
+    global _morphology_dirs
+    _morphology_dirs.append(path)
