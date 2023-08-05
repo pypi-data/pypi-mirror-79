ec5ff6804725992d47574b56629ce7aaa0a07407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package GusPI/0.0.47/GusPI-0.0.47.tar.gz

---
+++ packages/GusPI/GusPI-0.0.47.tar.gz/GusPI-0.0.47/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GusPI",
+    version="0.0.47",
+    author="Randy Geszvain",
+    author_email="randy.geszvain@gmail.com",
+    description="This open-source python package aims to provide statistical support in supply chain analytics and finance/accounting analytics. We welcome everyone to use this python package for personal or professional projects. Please let us know any feedback you have. We'd love to improve the package and add feature enhancements to benefit researchers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ygeszvain/GusPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
