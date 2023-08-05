5b3fce50e1adc737672d7d188421cbc114a96374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:26 +0000

    Add package jnt-django-graphene-toolbox/0.1.15/jnt-django-graphene-toolbox-0.1.15.tar.gz

---
+++ packages/jnt-django-graphene-toolbox/jnt-django-graphene-toolbox-0.1.15.tar.gz/jnt-django-graphene-toolbox-0.1.15/src/jnt_django_graphene_toolbox/connection_fields/data_source_allow_any.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from jnt_django_graphene_toolbox.connection_fields import (
+    DataSourceConnectionField,
+)
+from jnt_django_graphene_toolbox.security.permissions import AllowAny
+
+
+class DataSourceAllowAnyConnectionField(DataSourceConnectionField):
+    """Data source connection field for any users."""
+
+    permission_classes = (AllowAny,)
