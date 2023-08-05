9a61de6ad172c03f02195e1c32cad3488f5568db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:05 +0000

    Add package xToolkit/0.0.54/xToolkit-0.0.54.tar.gz

---
+++ packages/xToolkit/xToolkit-0.0.54.tar.gz/xToolkit-0.0.54/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: 熊利宏
+@email:xionglihong@163.com
+@phone：15172383635
+@project: xToolkit
+@file: setup.py
+@time: 2019-05-19 下午9:18
+"""
+import io
+from setuptools import find_packages, setup
+
+# 版本号
+from xToolkit import VERSION
+
+# 导入信息说明文档
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setup(
+    # pip项目发布的名称
+    name="xToolkit",
+    # 版本号
+    version=VERSION,
+    # 模块的关键词，使用空格分割
+    keywords=["xToolkit", "xtoolkit", "x", "tool", "toolkit"],
+    # 项目的简单描述：
+    description="xtoolkit从新封装了python下常用的数据类型，在内置模块的基础上扩展了部分功能.",
+    # 单文件模块写法
+    py_modules=[],
+    # 项目详细描述
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # 许可证
+    license="MIT Licence",
+    # 项目相关文件地址，一般是github
+    url="https://github.com/xionglihong/xToolkit",
+    # 作者姓名
+    author="xionglihong",
+    # 作者邮箱
+    author_email="xionglihong@163.com",
+    # 维护人员的名字
+    maintainer='xionglihong',
+    # 维护人员的邮箱
+    maintainer_email='xionglihong@163.com',
+    # 注意,这个地方参数要用英文,如果用中文会出现上传识别，不断重试之后还是失败
+    classifiers=[
+        # 当前开发进度等级的选项
+        # 3 - Alpha
+        # 4 - Beta
+        # 5 - Production/Stable
+        'Development Status :: 3 - Alpha',  # 当前开发进度等级（测试版，正式版等）
+        'Intended Audience :: Developers',  # 模块适用人群
+        'Topic :: Software Development :: Build Tools',  # 给模块加话题标签
+        'License :: OSI Approved :: MIT License',  # 模块的license
+        'Programming Language :: Python :: 3',  # 模块支持的Python版本
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    # 这个参数是导入目录下的所有__init__.py包
+    packages=["xToolkit"],
+    include_package_data=True,
+    platforms="any",
+    # 依赖库，格式为 arrow 或者 arrow>=0.13.2
+    install_requires=["python-dateutil>=2.8.0", "jieba>=0.42.1", "numpy>=1.19.1", "pandas>=1.1.1", "emoji>=0.6.0"]
+)
