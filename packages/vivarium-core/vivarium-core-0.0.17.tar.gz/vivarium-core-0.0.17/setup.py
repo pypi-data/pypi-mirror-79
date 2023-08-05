b955170580972fec98f140acd10d7d8df2cdca2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package vivarium-core/0.0.17/vivarium-core-0.0.17.tar.gz

---
+++ packages/vivarium-core/vivarium-core-0.0.17.tar.gz/vivarium-core-0.0.17/setup.py
@@ -0,0 +1,36 @@
+import os
+import glob
+import setuptools
+from distutils.core import setup
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name='vivarium-core',
+    version='0.0.17',
+    packages=[
+        'vivarium',
+        'vivarium.core',
+        'vivarium.processes',
+        'vivarium.library'
+    ],
+    author='Eran Agmon, Ryan Spangler',
+    author_email='eagmon@stanford.edu, ryan.spangler@gmail.com',
+    url='https://github.com/vivarium-collective/vivarium-core',
+    license='MIT',
+    entry_points={
+        'console_scripts': []},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_data={},
+    include_package_data=True,
+    install_requires=[
+        'confluent-kafka',
+        'matplotlib',
+        'networkx',
+        'numpy',
+        'Pint',
+        'pymongo',
+        'pytest',
+        'scipy'])
