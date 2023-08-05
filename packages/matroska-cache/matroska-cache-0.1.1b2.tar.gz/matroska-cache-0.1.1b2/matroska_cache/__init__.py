6fe328cf1ab31786bc9df4060e183122e0e54eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:39 +0000

    Add package matroska-cache/0.1.1b2/matroska-cache-0.1.1b2.tar.gz

---
+++ packages/matroska-cache/matroska-cache-0.1.1b2.tar.gz/matroska-cache-0.1.1b2/matroska_cache/__init__.py
@@ -0,0 +1,10 @@
+__version__ = __import__('pkg_resources').get_distribution('matroska_cache').version
+
+
+from .cache import MatroskaCache
+from . import dependency as dep
+
+try:
+    from .sa_dependencies import sa_dependencies
+except ImportError:
+    pass
