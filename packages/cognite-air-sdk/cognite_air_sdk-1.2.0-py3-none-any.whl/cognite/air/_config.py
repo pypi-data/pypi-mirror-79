0e775b9fadff80d637257b0ff917b7b4276fa010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package cognite-air-sdk/1.2.0/cognite_air_sdk-1.2.0-py3-none-any.whl

---
+++ packages/cognite-air-sdk/cognite_air_sdk-1.2.0-py3-none-any.whl/cognite/air/_config.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+from cognite.air.utils import strip_patch_from_version
+from cognite.client import CogniteClient
+
+
+@dataclass(frozen=True)
+class AIRClientConfig:
+    client: CogniteClient
+    data_set_id: int
+    schedule_asset_id: int
+    schedule_asset_ext_id: str
+    data_fields: dict
+    data_fields_defs: list
+    model_name: str
+    model_version: str
+
+    @property
+    def model_version_stripped(self):
+        return strip_patch_from_version(self.model_version)
+
+    def __post_init__(self):
+        for name, exp_type in self.__annotations__.items():
+            var = self.__dict__[name]
+            if not isinstance(var, exp_type):
+                raise TypeError(f"Expected field '{name}' to be of type '{exp_type}' not '{type(var)}'")
