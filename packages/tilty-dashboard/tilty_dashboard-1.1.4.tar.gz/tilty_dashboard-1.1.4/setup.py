34fea9c016ab72343163532de2209c7e8fc0edb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package tilty-dashboard/1.1.4/tilty_dashboard-1.1.4.tar.gz

---
+++ packages/tilty-dashboard/tilty_dashboard-1.1.4.tar.gz/tilty_dashboard-1.1.4/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tilty_dashboard',
+    description='A live dashboard for the tilty based on Flask-SocketIO',  # noqa
+    author='Marcus Young',
+    author_email='3vilpenguin@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='1.1.4',
+    package_data={
+       "": ["*.html", "*.png", "*.css", "*.js"],
+    },
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'Flask',
+        'Flask-Bootstrap',
+        'Flask-Cors',
+        'flask-session',
+        'Flask-SQLAlchemy',
+        'Flask-SocketIO',
+        'Flask-Script',
+        'backoff',
+        'configobj',
+        'gunicorn',
+        'Werkzeug==0.15.3',
+        'eventlet',
+    ]
+)
