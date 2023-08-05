67c695a2bfaff9a19972fd44ce80d272ab51d886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:43 +0000

    Add package nctoolkit/0.2.0/nctoolkit-0.2.0.tar.gz

---
+++ packages/nctoolkit/nctoolkit-0.2.0.tar.gz/nctoolkit-0.2.0/nctoolkit/cdo_command.py
@@ -0,0 +1,52 @@
+import subprocess
+
+from nctoolkit.runthis import run_this
+
+
+def cdo_command(self, command=None):
+    """
+    Apply a cdo command
+
+    Parameters
+    -------------
+    command : string
+        cdo command to call. This command must be such that "cdo {command} infile outfile" will run.
+    """
+
+    # First, check that the command is valid
+    if command is None:
+        raise ValueError("Please supply a command")
+
+    if type(command) is not str:
+        raise TypeError("Command supplied is not a str")
+
+    if command.startswith("cdo "):
+        command = command.replace("cdo ", " ").strip()
+    read = subprocess.run(
+        "cdo --operators", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    ).stdout
+
+    cdo_methods = [x.split(" ")[0].replace("b'", "") for x in str(read).split("\\n")]
+
+    cdo_methods = [mm for mm in cdo_methods if len(mm) > 0]
+
+    for x in command.split(" "):
+        y = x.split(",")[0].replace("-", "")
+        if y not in cdo_methods:
+            raise ValueError("You have supplied an invalid cdo method:" + y)
+
+    # remove cdo from the command
+
+    output = "ensemble"
+    cdo_command = "cdo " + command + " "
+
+    if ("merge " in cdo_command) or ("mergetime " in cdo_command):
+        output = "one"
+        self._merged = True
+
+    for mm in cdo_methods:
+
+        if " " + mm + "," in cdo_command:
+            cdo_command = cdo_command.replace(" " + mm + ",", " -" + mm + ",")
+
+    run_this(cdo_command, self, output=output)
---
+++ packages/nctoolkit/nctoolkit-0.2.0.tar.gz/nctoolkit-0.2.0/nctoolkit/inttime.py
@@ -0,0 +1,48 @@
+from nctoolkit.runthis import run_this
+
+# to do. Some padding needs to be added in case something like 2000/1/1 is provided.
+
+
+def time_interp(self, start=None, end=None, resolution="monthly"):
+    """
+    Temporally interpolate variables based on date range and time resolution
+
+    Parameters
+    -------------
+    start : str
+        Start date for interpolation. Needs to be of the form YYYY/MM/DD or YYYY-MM-DD.
+    end : str
+        End date for interpolation. Needs to be of the form YYYY/MM/DD or YYYY-MM-DD. If end is not given interpolation will be to the final available time in the dataset.
+    resolution : str
+        Time steps used for interpolation. Needs to be "daily", "weekly", "monthly" or "yearly". Defaults to monthly.
+    """
+
+    if resolution not in ["daily", "weekly", "monthly", "yearly"]:
+        raise ValueError("Please supply a valid time resolution!")
+
+    if resolution == "daily":
+        resolution = "1day"
+
+    if resolution == "weekly":
+        resolution = "7day"
+
+    if resolution == "monthly":
+        resolution = "1month"
+
+    if resolution == "yearly":
+        resolution = "1year"
+
+    if start is None:
+        raise ValueError("No start data supplied")
+
+    start = start.replace("/", "-")
+
+    cdo_command = f"-inttime,{start},12:00:00,{resolution}"
+
+    if end is None:
+        cdo_command = f"cdo {cdo_command}"
+    else:
+        end = end.replace("/", "-")
+        cdo_command = f"cdo -seldate,{start},{end} {cdo_command}"
+
+    run_this(cdo_command, self, output="ensemble")
