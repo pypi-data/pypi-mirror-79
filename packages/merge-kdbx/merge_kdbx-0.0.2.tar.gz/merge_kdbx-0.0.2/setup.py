dec406f9fb36a658a97578faf1819d960953eeb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package merge-kdbx/0.0.2/merge_kdbx-0.0.2.tar.gz

---
+++ packages/merge-kdbx/merge_kdbx-0.0.2.tar.gz/merge_kdbx-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="merge_kdbx",
+    version="0.0.2",
+    author="rollman054",
+    author_email="qwpmb554@gmail.com",
+    description="A mergetool for Keepass 2.x databases",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RollMan/merge_kdbx",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts':[
+            'merge_kdbx = merge_kdbx.merge_kdbx:main'
+        ]
+    },
+)
