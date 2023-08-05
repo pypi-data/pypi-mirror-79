7440294f143aaeac0c9899b787157365b1b0d46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:00 +0000

    Add package mlchain/0.1.7/mlchain-0.1.7.tar.gz

---
+++ packages/mlchain/mlchain-0.1.7.tar.gz/mlchain-0.1.7/mlchain/__init__.py
@@ -0,0 +1,21 @@
+# Parameters of MLchain
+__version__ = "0.1.6"
+HOST = "https://www.api.mlchain.ml"
+WEB_HOST = HOST
+API_ADDRESS = HOST
+MODEL_ID = None
+from os import environ
+
+environ['OBJC_DISABLE_INITIALIZE_FORK_SAFETY'] = 'YES'
+from mlchain.base.log import logger
+from .context import mlchain_context
+
+from .base.exceptions import *
+from .config import mlconfig
+
+try:
+    import torch
+
+    torch.set_num_thread(1)
+except Exception as e:
+    pass
