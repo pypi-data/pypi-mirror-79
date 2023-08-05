490aa07070f7c3e3d565b15db7b6dfad604b6c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:58 +0000

    Add package jict/2.5.6/jict-2.5.6-py3-none-any.whl

---
+++ packages/jict/jict-2.5.6-py3-none-any.whl/jict/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""`jict` provides dictionaries with multiple levels of nested-ness."""
+
+__version__ = '2.5.6'
+from .jict import jict, sqlconnect
+from .helpers import evaluate
+from collections import deque
+
+try:
+    import urwid
+    from .menu import jictmt
+except Exception as e:
+    class jictmt:
+        def __init__(self):
+            super(jictmt, self).__init__()
+            print( 'jictmt requires urwid, install by pip3 install urwid' )
+
+__all__ = ( 'jict', 'sqlconnect', 'evaluate', 'deque', 'jictmt' )
