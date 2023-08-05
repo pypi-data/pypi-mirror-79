017ab704c1f1cf1e80ab55720b387c94de1990e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:10 +0000

    Add package pycromanager/0.4.6/pycromanager-0.4.6.tar.gz

---
+++ packages/pycromanager/pycromanager-0.4.6.tar.gz/pycromanager-0.4.6/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycromanager",
+    version="0.4.6",
+    author="Henry Pinkard",
+    author_email="henry.pinkard@gmail.com",
+    description="Open source microscope control using python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/henrypinkard/pycro-manager",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'dask[array]>=2.4.0', 'zmq'],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",      
+        "Operating System :: OS Independent",
+    ],
+)
