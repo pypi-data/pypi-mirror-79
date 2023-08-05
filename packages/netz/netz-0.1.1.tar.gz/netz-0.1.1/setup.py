fea5374575f58e51cbb94890ab2cfbdb62c549bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:59 +0000

    Add package netz/0.1.1/netz-0.1.1.tar.gz

---
+++ packages/netz/netz-0.1.1.tar.gz/netz-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['netz']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['chardet>=3.0.4,<4.0.0', 'pandas>=1.1.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'netz',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'mjaquier',
+    'author_email': 'mjaquier@admin.xyz',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
