cf0b238d9d39e11ce0257687d7c4080a1b4e3f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:58 +0000

    Add package stockie/0.0.1/stockie-0.0.1.tar.gz

---
+++ packages/stockie/stockie-0.0.1.tar.gz/stockie-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stockie", 
+    version="0.0.1",
+    author="Suparjo Tamin",
+    author_email="suparjo.tamin@gmail.com",
+    description="A package to identify stock candlestick pattern",
+    long_description=long_description,
+    url="https://github.com/suparjotamin/stocky",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
