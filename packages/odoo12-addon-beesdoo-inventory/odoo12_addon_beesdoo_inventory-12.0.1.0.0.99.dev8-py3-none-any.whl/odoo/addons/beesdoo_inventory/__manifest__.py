7b069ac142318219f89943a18e9bd2cb9ec9e2fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package odoo12-addon-beesdoo-inventory/12.0.1.0.0.99.dev8/odoo12_addon_beesdoo_inventory-12.0.1.0.0.99.dev8-py3-none-any.whl

---
+++ packages/odoo12-addon-beesdoo-inventory/odoo12_addon_beesdoo_inventory-12.0.1.0.0.99.dev8-py3-none-any.whl/odoo/addons/beesdoo_inventory/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2017 - 2020 BEES coop SCRLfs
+#   - Augustin Borsu
+#   - Elise Dupont
+#   - Thibault François
+#   - Jean-Marc François
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Beesdoo Inventory",
+    "summary": """
+        Adds a responsible, a max shipping date and a button to copy quantity to
+        stock pickings.""",
+    "author": "Beescoop - Cellule IT, Coop IT Easy SCRLfs",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Inventory",
+    "version": "12.0.1.0.0",
+    "depends": ["delivery", "beesdoo_base", "beesdoo_product"],
+    "data": ["views/stock.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
