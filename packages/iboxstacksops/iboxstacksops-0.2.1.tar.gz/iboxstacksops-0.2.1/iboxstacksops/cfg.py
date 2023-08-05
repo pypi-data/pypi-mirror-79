9f5a8b4d9675dd3d9b458b7e184f8b21a5e1748d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:35 +0000

    Add package iboxstacksops/0.2.1/iboxstacksops-0.2.1.tar.gz

---
+++ packages/iboxstacksops/iboxstacksops-0.2.1.tar.gz/iboxstacksops-0.2.1/iboxstacksops/cfg.py
@@ -0,0 +1,72 @@
+# parser default cfg
+role = type = jobs = pause = version = template = nowait = None
+max_retry_ecs_service_running_count = 0
+#
+
+MAX_SINGLE_STACKS = 5
+
+STACK_BASE_DATA = [
+    'StackName',
+    'Description',
+    'StackStatus',
+    'CreationTime',
+    'LastUpdatedTime',
+]
+
+RESOURCES_MAP = {
+    'AutoScalingGroup': 'AutoScalingGroupName',
+    'AutoScalingGroupSpot': 'AutoScalingGroupSpotName',
+    'TargetGroup': 'TargetGroup',
+    'TargetGroupExternal': 'TargetGroupExternal',
+    'TargetGroupInternal': 'TargetGroupInternal',
+    'Service': 'ServiceName',
+    'ServiceExternal': 'ServiceName',
+    'ServiceInternal': 'ServiceName',
+    'LoadBalancerClassicExternal': 'LoadBalancerNameExternal',
+    'LoadBalancerClassicInternal': 'LoadBalancerNameInternal',
+    'LoadBalancerApplicationExternal': 'LoadBalancerExternal',
+    'LoadBalancerApplicationInternal': 'LoadBalancerInternal',
+    'Cluster': 'ClusterName',
+    'ScalableTarget': 'ClusterName',
+    'ListenerHttpsExternalRules1': 'LoadBalancerExternal',
+    'ListenerHttpsExternalRules2': 'LoadBalancerExternal',
+    'ListenerHttpInternalRules1': 'LoadBalancerInternal',
+    'AlarmCPUHigh': None,
+    'AlarmCPULow': None,
+}
+SCALING_POLICY_TRACKINGS_NAMES = {
+    'ScalingPolicyTrackings1': None,
+    'ScalingPolicyTrackingsASCpu': 'ScalingPolicyTrackings1',
+    'ScalingPolicyTrackingsASCustom': 'ScalingPolicyTrackings1',
+    'ScalingPolicyTrackingsAPPCpu': 'ScalingPolicyTrackings1',
+    'ScalingPolicyTrackingsAPPCustom': 'ScalingPolicyTrackings1',
+}
+RESOURCES_MAP.update(SCALING_POLICY_TRACKINGS_NAMES)
+
+STACK_COMPLETE_STATUS = [
+    'UPDATE_COMPLETE',
+    'CREATE_COMPLETE',
+    'ROLLBACK_COMPLETE',
+    'UPDATE_ROLLBACK_COMPLETE',
+    'UPDATE_ROLLBACK_FAILED',
+    'DELETE_COMPLETE',
+    'DELETE_FAILED',
+]
+
+CHANGESET_COMPLETE_STATUS = [
+    'CREATE_COMPLETE',
+    'UPDATE_ROLLBACK_FAILED',
+    'FAILED',
+]
+
+SHOW_TABLE_FIELDS = [
+    'EnvStackVersion',
+    'EnvRole',
+    'StackName',
+    'StackType',
+    'UpdateMode',
+    'EnvApp1Version',
+    'LastUpdatedTime',
+]
+
+SSM_BASE_PATH = '/ibox'
