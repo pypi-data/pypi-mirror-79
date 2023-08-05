b95e8df84b6e3bf59d9b527266ab7deaad96c498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package tap-mailchimp/1.1.1/tap-mailchimp-1.1.1.tar.gz

---
+++ packages/tap-mailchimp/tap-mailchimp-1.1.1.tar.gz/tap-mailchimp-1.1.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-mailchimp',
+      version='1.1.1',
+      description='Singer.io tap for extracting data from the Mailchimp API',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_mailchimp'],
+      install_requires=[
+          'backoff==1.8.0',
+          'requests==2.20.1',
+          'singer-python==5.9.0'
+      ],
+      extras_require= {
+          'dev': [
+              'pylint==2.5.3',
+              'nose==1.3.7',
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-mailchimp=tap_mailchimp:main
+      ''',
+      packages=['tap_mailchimp'],
+      package_data = {
+          'tap_mailchimp': ['schemas/*.json'],
+      }
+)
