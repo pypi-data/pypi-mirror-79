da07f67a7d55a77d7d84be885286e7287863c7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:19 +0000

    Add package wapt-tools/1.2.0/wapt_tools-1.2.0-py2-none-any.whl

---
+++ packages/wapt-tools/wapt_tools-1.2.0-py2-none-any.whl/wapttools/config.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+def loadVersionCheck():
+    """ Load version-check.json configuration file
+
+    Returns
+    -------
+    config: dict
+        configuration dictionnary
+    """
+    with open('version-check.json') as file:
+        config = json.load(file)
+
+    return config
+
+def loadControl():
+    """ Load WAPT/control configuration file
+
+    This only extract name and version.
+
+    Returns
+    -------
+    config: dict
+        configuration dictionnary
+    """
+    config = dict()
+    with open('WAPT' + os.sep + 'control', 'r') as control:
+        for line in control:
+            if line.startswith('name'):
+                config['name'] = line.split(':')[1].strip()
+            if line.startswith('version'):
+                config['version'] = line.split(':')[1].split('-')[0].strip()
+            if line.startswith('homepage'):
+                config['homepage'] = line.split(':')[1].strip()
+
+    return config
