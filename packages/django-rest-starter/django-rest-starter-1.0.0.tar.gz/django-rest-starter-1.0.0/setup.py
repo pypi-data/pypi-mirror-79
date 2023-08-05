5445ab7a36fc4cf329c710ae9fe2b19110e28275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:42 +0000

    Add package django-rest-starter/1.0.0/django-rest-starter-1.0.0.tar.gz

---
+++ packages/django-rest-starter/django-rest-starter-1.0.0.tar.gz/django-rest-starter-1.0.0/setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+
+with open('README.md', "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-rest-starter',
+    version='1.0.0',
+    url='https://github.com/LeonardoCruzx/django-rest-starter',
+    author='Leonardo Cruz',
+    author_email='leonardo.m.cruz0@gmail.com',
+    description='Package to start a django with rest',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        
+    ],
+    python_requires='>=3.7',
+)
