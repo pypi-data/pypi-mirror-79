3c69ec7fa13bd1408f46aa93bb4a2bbdcbba3e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:53 +0000

    Add package sensiml-dev/2020.2.5/sensiml-dev-2020.2.5.tar.gz

---
+++ packages/sensiml-dev/sensiml-dev-2020.2.5.tar.gz/sensiml-dev-2020.2.5/sensiml/datamanager/__init__.py
@@ -0,0 +1,18 @@
+from sensiml.datamanager.functions import Functions
+from sensiml.datamanager.platformdescriptions import PlatformDescriptions
+from sensiml.datamanager.pipelineseeds import PipelineSeeds
+from sensiml.datamanager.featurefiles import FeatureFiles
+from sensiml.datamanager.projects import Projects
+from sensiml.datamanager.segmenter import SegmenterSet
+from sensiml.datamanager.capture_configurations import CaptureConfigurations
+
+
+__all__ = [
+    "Functions",
+    "FeatureFiles",
+    "Projects",
+    "SegmenterSet",
+    "PipelineSeeds",
+    "PlatformDescriptions",
+    "CaptureConfigurations",
+]
