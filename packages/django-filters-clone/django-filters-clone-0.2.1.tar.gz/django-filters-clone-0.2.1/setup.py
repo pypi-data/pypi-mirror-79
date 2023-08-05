6f57cabadbba6e7a420059359b5ffd79d97491fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:04 +0000

    Add package django-filters-clone/0.2.1/django-filters-clone-0.2.1.tar.gz

---
+++ packages/django-filters-clone/django-filters-clone-0.2.1.tar.gz/django-filters-clone-0.2.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-filters-clone',
+    version=__import__('filters').__version__,
+    description='A filterset and filters',
+    long_description="it is the same as django-filters",
+    author='Bas Koopmans',
+    author_email='django@baskoopmans.nl',
+    url='http://github.com/baskoopmans/django-filters/',
+    download_url='http://github.com/baskoopmans/django-filters/downloads',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False, # because we're including media that Django needs
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
