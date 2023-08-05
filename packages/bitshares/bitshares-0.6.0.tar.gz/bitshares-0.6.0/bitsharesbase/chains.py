7c75c535ac87be31e2651fc0145915e429f674a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package bitshares/0.6.0/bitshares-0.6.0.tar.gz

---
+++ packages/bitshares/bitshares-0.6.0.tar.gz/bitshares-0.6.0/bitsharesbase/chains.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+known_chains = {
+    "BTS": {
+        "chain_id": "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8",
+        "core_symbol": "BTS",
+        "prefix": "BTS",
+    },
+    "CTS": {
+        "chain_id": "53952fc10d52a3af6d572a298e8f99a0a2e7afd0f7dbad354d8d65e8c36c962d",
+        "core_symbol": "CTS",
+        "prefix": "CTS",
+    },
+    "TEST": {
+        "chain_id": "39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447",
+        "core_symbol": "TEST",
+        "prefix": "TEST",
+    },
+}
