ab80a95086634cec262cd67bda09e665e41929c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:46 +0000

    Add package minionize/0.1.6/minionize-0.1.6.tar.gz

---
+++ packages/minionize/minionize-0.1.6.tar.gz/minionize-0.1.6/minionize/cli.py
@@ -0,0 +1,17 @@
+from minionize import minionizer, ProcessCallback
+import sys
+
+
+def run():
+    class _Callback(ProcessCallback):
+        """Simplest callback
+
+        Takes param as a string and append them to the original command.
+        """
+
+        def to_cmd(self, param):
+            return " ".join(sys.argv[1:]) + " " + str(param)
+
+    callback = _Callback()
+    m = minionizer(callback)
+    m.run()
