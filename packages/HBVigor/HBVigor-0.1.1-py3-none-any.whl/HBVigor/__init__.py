b60d45ef719e3a753bb12a3a59b1a6f6eeb5a510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:56 +0000

    Add package HBVigor/0.1.1/HBVigor-0.1.1-py3-none-any.whl

---
+++ packages/HBVigor/HBVigor-0.1.1-py3-none-any.whl/HBVigor/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This script allow user to import directly the most useful functions."""
+
+__version__ = "0.1.1"
+
+from HBVigor.HBVigor import (TimeDomain, FreqDomain, Poincare, Nonlinear)
+
