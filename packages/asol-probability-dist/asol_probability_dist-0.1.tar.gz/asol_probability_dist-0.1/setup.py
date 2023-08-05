f73092ad98ee9004927de232496edd19f9df8faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:29 +0000

    Add package asol-probability-dist/0.1/asol_probability_dist-0.1.tar.gz

---
+++ packages/asol-probability-dist/asol_probability_dist-0.1.tar.gz/asol_probability_dist-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='asol_probability_dist',
+      version='0.1',
+      author="Andrew Leung",
+      author_email="Andrew.sLeung@gmail.com",
+      description='A package to visualize Gaussian and Binomial probability distributions',
+      packages=['asol_probability_dist'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
+
+
