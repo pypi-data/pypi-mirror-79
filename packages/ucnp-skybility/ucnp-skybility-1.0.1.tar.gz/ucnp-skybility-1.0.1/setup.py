0b6627978ef96c52f2c2dc249bfc5bf7e0bb0485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:06 +0000

    Add package ucnp-skybility/1.0.1/ucnp-skybility-1.0.1.tar.gz

---
+++ packages/ucnp-skybility/ucnp-skybility-1.0.1.tar.gz/ucnp-skybility-1.0.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+long_description = ""
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="ucnp-skybility",
+    version="1.0.1",
+    author="skybility ha team",
+    url="https://gitlab.skybilityha.com/experiments/ucnp",
+    author_email="support@skybilityha.com",
+    description="this is a client to ucnp server for linux platform only",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pyperclip>=1.8.0",
+        "python-engineio>=3.13.2",
+        "python-socketio>=4.6.0",
+        "requests>=2.24.0",
+        "wxPython>=4.1.0",
+        "pypubsub>=4.0.3",
+        "pinyin>=0.4.0",
+        ],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        ],
+    python_requires=">=3.8",
+    scripts=['client/ucnp_start']
+    )
