809f76e6507d5dbdfc91a122f82399b205c72395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:33 +0000

    Add package valleytext/0.1/valleytext-0.1.tar.gz

---
+++ packages/valleytext/valleytext-0.1.tar.gz/valleytext-0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+setuptools.setup(
+    name="valleytext",
+    version="0.1",
+    url='https://github.com/yzho0907/valleytext.git',
+    author='young, xiuzhizheng',
+    author_email='598299410@qq.com, xiuzhi_zxz@163.com',
+    description='Extract keywords from sentences.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
