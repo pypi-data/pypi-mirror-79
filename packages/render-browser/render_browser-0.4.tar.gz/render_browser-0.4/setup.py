54ab98d9657b64464a3d2419808eb3e97da26b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:36 +0000

    Add package render-browser/0.4/render_browser-0.4.tar.gz

---
+++ packages/render-browser/render_browser-0.4.tar.gz/render_browser-0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='render_browser',  
+     version='0.4',
+     author="Dhruv Ramani",
+     author_email="dhruvramani98@gmail.com",
+     description="Render gym environments to a web browser.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/dhruvramani/gym-render-browser",
+     packages=setuptools.find_packages(),
+     include_package_data = True,
+     install_requires=[
+        'Flask',
+        'opencv-python',
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
