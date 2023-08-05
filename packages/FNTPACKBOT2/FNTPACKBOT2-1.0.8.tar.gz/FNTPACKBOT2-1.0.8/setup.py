95380e909e05020c4d4a0e7caa9a61bb85d42095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package FNTPACKBOT2/1.0.8/FNTPACKBOT2-1.0.8.tar.gz

---
+++ packages/FNTPACKBOT2/FNTPACKBOT2-1.0.8.tar.gz/FNTPACKBOT2-1.0.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+    
+setuptools.setup(
+    name="FNTPACKBOT2",
+    version="1.0.8",
+    author="Bryna",
+    description="Lobby bot.",
+    url="https://www.youtube.com/channel/UCiDx7oYO4f0qbDEzGdgaq_w?view_as=subscriber&pbjreload=101",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'crayons',
+        'fortnitepy',
+        'BenBotAsync',
+        'FortniteAPIAsync',
+        'uvloop',
+        'sanic',
+        'aiohttp'
+    ],
+)
