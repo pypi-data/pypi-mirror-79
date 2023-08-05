69e21ee2d80a8fe8a64bed67275caf8370c4fbb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:58 +0000

    Add package borisml/0.1.10/borisml-0.1.10.tar.gz

---
+++ packages/borisml/borisml-0.1.10.tar.gz/borisml-0.1.10/boris/transforms/__init__.py
@@ -0,0 +1,8 @@
+""" boris.transforms
+
+    The boris.transforms module provides custom transforms
+    for data augmentation.
+
+"""
+
+from ._gaussian_blur import GaussianBlur
---
+++ packages/borisml/borisml-0.1.10.tar.gz/borisml-0.1.10/boris/utils/__init__.py
@@ -0,0 +1,8 @@
+""" boris.utils
+
+    The boris.utils module provides commonly used utility methods
+"""
+
+from ._format_conversion import embeddings_to_pandas
+from ._format_conversion import pandas_to_dict
+from ._2d_embeddings import fit_pca
