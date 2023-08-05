0626f5a824d8b725a6504c9061f9a92a79060136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:47 +0000

    Add package ipympl/0.5.8/ipympl-0.5.8.tar.gz

---
+++ packages/ipympl/ipympl-0.5.8.tar.gz/ipympl-0.5.8/README.md
@@ -0,0 +1,113 @@
+# ipympl
+
+[![TravisCI build status](https://img.shields.io/travis/com/matplotlib/ipympl/master?logo=travis)](https://travis-ci.com/matplotlib/ipympl)
+[![Latest PyPI version](https://img.shields.io/pypi/v/ipympl?logo=pypi)](https://pypi.python.org/pypi/ipympl)
+[![Latest conda-forge version](https://img.shields.io/conda/vn/conda-forge/ipympl?logo=conda-forge)](https://anaconda.org/conda-forge/ipympl)
+[![Latest npm version](https://img.shields.io/npm/v/jupyter-matplotlib?logo=npm)](https://www.npmjs.com/package/jupyter-matplotlib)
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/matplotlib/ipympl/stable?urlpath=%2Flab%2Ftree%2Fexamples%2Fipympl.ipynb)
+[![Gitter](https://img.shields.io/badge/gitter-Join_chat-blue?logo=gitter)](https://gitter.im/jupyter-widgets/Lobby)
+
+Leveraging the Jupyter interactive widgets framework, `ipympl` enables the interactive features of matplotlib in the Jupyter notebook and in JupyterLab.
+
+Besides, the figure `canvas` element is a proper Jupyter interactive widget which can be positioned in interactive widget layouts.
+
+## Usage
+
+To enable the `ipympl` backend, simply use the `matplotlib` Jupyter
+magic:
+
+```
+%matplotlib widget
+```
+
+## Example
+
+![matplotlib screencast](matplotlib.gif)
+
+## Installation
+
+### With conda:
+
+```bash
+conda install -c conda-forge ipympl
+```
+
+### With pip:
+
+```bash
+pip install ipympl
+```
+
+### Install the JupyterLab extension
+
+In order to install the JupyterLab extension `jupyter-matplotlib`, you will first need to install `nodejs`, you can install it with `conda` doing
+
+```bash
+conda install -c conda-forge nodejs
+```
+
+Starting from ipympl `0.5.6`, **you do not need to manually install the JupyterLab extension**, but you still need to install the JupyterLab widget manager:
+```bash
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+# If you already installed the @jupyter-widgets/jupyterlab-manager extension, you will still need to rebuild JupyterLab after you installed ipympl
+jupyter lab build
+```
+
+#### Install an old JupyterLab extension
+
+You will need to install the right `jupyter-matplotlib` version, according to the `ipympl` and `jupyterlab` versions you installed.
+For example, if you installed ipympl `0.5.1`, you need to install jupyter-matplotlib `0.7.0`, and this version is only compatible with JupyterLab `1`.
+
+```bash
+conda install -c conda-forge ipympl==0.5.1
+jupyter labextension install @jupyter-widgets/jupyterlab-manager jupyter-matplotlib@0.7.0
+```
+
+Versions lookup table:
+
+
+| `ipympl` | `jupyter-matplotlib` | `JupyterLab` | `Matplotlib` |
+|----------|----------------------|--------------|--------------|
+| 0.5.8    | 0.7.4                | 1 or 2       | 3.3.1        |
+| 0.5.7    | 0.7.3                | 1 or 2       | 3.2.*        |
+| ...      | ...                  | ...          |              |
+| 0.5.3    | 0.7.2                | 1 or 2       |              |
+| 0.5.2    | 0.7.1                | 1            |              |
+| 0.5.1    | 0.7.0                | 1            |              |
+| 0.5.0    | 0.6.0                | 1            |              |
+| 0.4.0    | 0.5.0                | 1            |              |
+| 0.3.3    | 0.4.2                | 1            |              |
+| 0.3.2    | 0.4.1                | 1            |              |
+| 0.3.1    | 0.4.0                | 0 or 1       |              |
+
+### For a development installation (requires nodejs):
+
+```bash
+git clone https://github.com/matplotlib/ipympl.git
+cd ipympl
+pip install -e .
+
+# If using classic Jupyter Notebook
+jupyter nbextension install --py --symlink --sys-prefix ipympl
+jupyter nbextension enable --py --sys-prefix ipympl
+
+# If using JupyterLab
+jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
+jupyter labextension install ./js
+```
+
+#### How to see your changes
+**Javascript**:
+
+To continuously monitor the project for changes and automatically trigger a rebuild, start Jupyter in watch mode:
+
+```bash
+jupyter lab --watch
+```
+
+After a change wait for the build to finish and then refresh your browser and the changes should take effect.
+
+**Python:**
+
+If you make a change to the python code then you will need to restart the notebook kernel to have it take effect.
---
+++ packages/ipympl/ipympl-0.5.8.tar.gz/ipympl-0.5.8/setup.py
@@ -0,0 +1,189 @@
+from __future__ import print_function
+from distutils import log
+from setuptools import setup, find_packages, Command
+from setuptools.command.sdist import sdist
+from setuptools.command.build_py import build_py
+from setuptools.command.egg_info import egg_info
+from subprocess import check_call
+import os
+import json
+import sys
+from os.path import join as pjoin
+
+# Name of the project
+name = 'ipympl'
+
+here = os.path.dirname(os.path.abspath(__file__))
+node_root = pjoin(here, 'js')
+is_repo = os.path.exists(pjoin(here, '.git'))
+
+npm_path = os.pathsep.join([
+    pjoin(node_root, 'node_modules', '.bin'),
+    os.environ.get('PATH', os.defpath),
+])
+
+log.info('setup.py entered')
+log.info('$PATH=%s' % os.environ['PATH'])
+
+LONG_DESCRIPTION = 'Matplotlib Jupyter Extension'
+
+
+def js_prerelease(command, strict=False):
+    """decorator for building minified js/css prior to another command"""
+    class DecoratedCommand(command):
+        def run(self):
+            jsdeps = self.distribution.get_command_obj('jsdeps')
+            if not is_repo and all(os.path.exists(t) for t in jsdeps.targets):
+                # sdist, nothing to do
+                command.run(self)
+                return
+
+            try:
+                self.distribution.run_command('jsdeps')
+            except Exception as e:
+                missing = [t for t in jsdeps.targets if not os.path.exists(t)]
+                if strict or missing:
+                    log.warn('rebuilding js and css failed')
+                    if missing:
+                        log.error('missing files: %s' % missing)
+                    raise e
+                else:
+                    log.warn('rebuilding js and css failed (not a problem)')
+                    log.warn(str(e))
+            command.run(self)
+            update_package_data(self.distribution)
+    return DecoratedCommand
+
+
+def update_package_data(distribution):
+    """update package_data to catch changes during setup"""
+    build_py = distribution.get_command_obj('build_py')
+    distribution.data_files = get_data_files()
+    # re-init build_py options which load package_data
+    build_py.finalize_options()
+
+
+def get_data_files():
+    """Get the data files for the package.
+    """
+    with open(pjoin(node_root, 'package.json')) as f:
+        package_json = json.load(f)
+    tgz = '%s-%s.tgz' % (package_json['name'], package_json['version'])
+
+    return [
+        ('share/jupyter/nbextensions/jupyter-matplotlib', [
+            'ipympl/static/extension.js',
+            'ipympl/static/index.js',
+            'ipympl/static/index.js.map',
+            'ipympl/static/package.json'
+        ]),
+        ('etc/jupyter/nbconfig/notebook.d', ['jupyter-matplotlib.json']),
+        ('share/jupyter/lab/extensions', ['js/' + tgz]),
+    ]
+
+
+class NPM(Command):
+    description = 'install package.json dependencies using npm'
+
+    user_options = []
+
+    node_modules = pjoin(node_root, 'node_modules')
+
+    targets = [
+        pjoin(here, 'ipympl', 'static', 'extension.js'),
+        pjoin(here, 'ipympl', 'static', 'index.js'),
+        pjoin(here, 'ipympl', 'static', 'package.json')
+    ]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def has_npm(self):
+        try:
+            check_call(['npm', '--version'])
+            return True
+        except Exception:
+            return False
+
+    def run(self):
+        has_npm = self.has_npm()
+        if not has_npm:
+            log.error("`npm` unavailable.  If you're running this command "
+                      "using sudo, make sure `npm` is available to sudo")
+
+        env = os.environ.copy()
+        env['PATH'] = npm_path
+
+        if self.has_npm():
+            log.info("Installing build dependencies with npm.  "
+                     "This may take a while...")
+            check_call(['npm', 'install'], cwd=node_root, stdout=sys.stdout,
+                       stderr=sys.stderr)
+            check_call(['npm', 'pack'], cwd=node_root, stdout=sys.stdout,
+                       stderr=sys.stderr)
+
+        for t in self.targets:
+            if not os.path.exists(t):
+                msg = 'Missing file: %s' % t
+                if not has_npm:
+                    msg += ('\nnpm is required to build a development version '
+                            'of widgetsnbextension')
+                raise ValueError(msg)
+
+        # update package data in case this created new files
+        update_package_data(self.distribution)
+
+
+version_ns = {}
+with open(pjoin(here, 'ipympl', '_version.py')) as f:
+    exec(f.read(), {}, version_ns)
+
+setup_args = dict(
+    name=name,
+    version=version_ns['__version__'],
+    description='Matplotlib Jupyter Extension',
+    long_description=LONG_DESCRIPTION,
+    license='BSD License',
+    include_package_data=True,
+    data_files=get_data_files(),
+    install_requires=[
+        'ipykernel>=4.7',
+        'ipywidgets>=7.5.0',
+        'matplotlib>=2.0.0'
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    cmdclass={
+        'build_py': js_prerelease(build_py),
+        'egg_info': js_prerelease(egg_info),
+        'sdist': js_prerelease(sdist, strict=True),
+        'jsdeps': NPM,
+    },
+    author='Matplotlib Development Team',
+    author_email='matplotlib-users@python.org',
+    url='http://matplotlib.org',
+    keywords=[
+        'ipython',
+        'jupyter',
+        'widgets',
+        'graphics',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Multimedia :: Graphics',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
+
+setup(**setup_args)
