b968bcba734bf6eee29aa3967c331f474d88a4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:10 +0000

    Add package gitly/1.0.1/gitly-1.0.1.tar.gz

---
+++ packages/gitly/gitly-1.0.1.tar.gz/gitly-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='gitly',
+    packages=setuptools.find_packages(),
+    description='This is a home made lib to help you plot your fency graphs from plotly in github while using Google Colab notebook.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='1.0.1',
+    url='https://github.com/Tiagoeem/gitly',
+    author='Tiago Sanches da Silva',
+    author_email='tiago.eem@gmail.com',
+    license='mit',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+    )
