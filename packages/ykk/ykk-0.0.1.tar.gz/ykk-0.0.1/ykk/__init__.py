94eba3676278dcf53ff713c1e8df4847a82dfb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:10 +0000

    Add package ykk/0.0.1/ykk-0.0.1.tar.gz

---
+++ packages/ykk/ykk-0.0.1.tar.gz/ykk-0.0.1/ykk/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Package __init__"""
+
+from ykk._version import VERSION_INFO
+from ykk._version import VERSION_MINOR
+from ykk._version import VERSION_MAJOR
+from ykk._version import VERSION_PATCH
+from ykk._version import __version__
+
+
+__all__ = [
+    "VERSION_MAJOR",
+    "VERSION_MINOR",
+    "VERSION_PATCH",
+    "VERSION_INFO",
+    "__version__",
+]
