6ed9d81c1c0530bc1a7dc2a7dfdaca0a9024ae69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package giotto-tda-nightly/20200909.15/giotto_tda_nightly-20200909.15-cp38-cp38-macosx_10_14_x86_64.whl

---
+++ packages/giotto-tda-nightly/giotto_tda_nightly-20200909.15-cp38-cp38-macosx_10_14_x86_64.whl/gtda/__init__.py
@@ -0,0 +1,4 @@
+from ._version import __version__
+
+__all__ = ['mapper', 'homology', 'time_series', 'graphs', 'diagrams', 'images',
+           'utils', 'point_clouds', 'externals', 'plotting', '__version__']
