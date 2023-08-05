95224079b73ac1e4ce792379e468c8b8179c871b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:57 +0000

    Add package boml/0.1.1a2/boml-0.1.1a2.tar.gz

---
+++ packages/boml/boml-0.1.1a2.tar.gz/boml-0.1.1a2/boml/setup_model/__init__.py
@@ -0,0 +1,20 @@
+from boml.setup_model import network
+from boml.setup_model.feedforward import BOMLNetFeedForward
+from boml.setup_model.meta_init_v1 import (
+    BOMLNetMetaInitV1,
+    BOMLNetMiniMetaInitV1,
+    BOMLNetOmniglotMetaInitV1,
+)
+from boml.setup_model.meta_init_v2 import (
+    BOMLNetMiniMetaInitV2,
+    BOMLNetOmniglotMetaInitV2,
+)
+from boml.setup_model.meta_repr_v2 import (
+    BOMLNetMiniMetaReprV2,
+    BOMLNetOmniglotMetaReprV2,
+)
+from boml.setup_model.meta_repr_v1 import (
+    BOMLNetMetaReprV1,
+    BOMLNetMiniMetaReprV1,
+    BOMLNetOmniglotMetaReprV1,
+)
---
+++ packages/boml/boml-0.1.1a2.tar.gz/boml-0.1.1a2/boml/upper_iter/__init__.py
@@ -0,0 +1,5 @@
+from boml.upper_iter.outer_grad import BOMLOuterGrad
+from boml.upper_iter.reverse import BOMLOuterGradReverse
+from boml.upper_iter.simple import BOMLOuterGradSimple
+from boml.upper_iter.implicit import BOMLOuterGradImplicit
+from boml.upper_iter.darts import BOMLOuterGradDarts
