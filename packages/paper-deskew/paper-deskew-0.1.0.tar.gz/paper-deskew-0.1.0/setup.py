2d5de36e81cd7b63ffc7fcd3fa65db86944440e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package paper-deskew/0.1.0/paper-deskew-0.1.0.tar.gz

---
+++ packages/paper-deskew/paper-deskew-0.1.0.tar.gz/paper-deskew-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="paper-deskew",
+    version="0.1.0",
+    author="pbcquoc",
+    author_email="pbcquoc@gmail.com",
+    description="deskew",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pbcquoc/deskew",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'gdown==3.11.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
