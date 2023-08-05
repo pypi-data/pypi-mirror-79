2e1958c305efdce44a9b814e78b84a79ed94034e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:19 +0000

    Add package passwordlookup/0.0.2/passwordlookup-0.0.2.tar.gz

---
+++ packages/passwordlookup/passwordlookup-0.0.2.tar.gz/passwordlookup-0.0.2/passwordlookup/provider/keepass_store.py
@@ -0,0 +1,69 @@
+from .simple_entry import SimpleEntry
+from .store_provider import StoreProvider
+import libkeepass
+from .session import Session
+import logging
+import sys
+import getpass
+from os import path
+
+
+class KeepassStore(StoreProvider):
+    def __init__(self, kee_pass_file, clear_session=False):
+        self.session = Session('keepass_key', 'keepass_session')
+        if clear_session:
+            self.session.clear()
+        self.session.load()
+        self.entries = None
+        self.kee_pass_file = kee_pass_file
+        if not path.exists(kee_pass_file):
+            logging.error('failed to open keepass file' + str(kee_pass_file))
+            raise IOError('keepass file does not exist')
+
+    def _session_is_established(self):
+        if self.session.session is None or self.session.session == '':
+            return False
+
+        return True
+
+    def load_cache(self):
+        with libkeepass.open(self.kee_pass_file, password=self.session.session) as kpdb:
+            self.session.write()
+            self.entries = []
+            for kpEntry in kpdb.obj_root.findall('.//Entry'):
+                simple_entry = SimpleEntry(None, [], None)
+                for s in kpEntry.findall('./String'):
+                    key = s.find('./Key').text
+                    val = s.find('./Value').text
+                    if val is not None and val != '':
+                        if key == 'URL':
+                            simple_entry.addresses.append(val)
+                        elif key == 'Password':
+                            simple_entry.password = val
+                        elif key == 'UserName':
+                            simple_entry.username = val
+                        elif key == 'Title':
+                            simple_entry.name = val
+                if simple_entry.name is not None and simple_entry.password is not None:
+                    self.entries.append(simple_entry)
+
+    def ensure_session_and_load_cache(self):
+        while not self._session_is_established():
+            try:
+                self.session.session = getpass.getpass(
+                    '\nPlease enter your password for file: ' + str(self.kee_pass_file) + ": ")
+
+            # except CredentialsError as e:
+            #     print('wrong password')
+            #     self.session.session = ''
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                raise
+
+    def get_and_cache_simple_entries(self):
+        self.ensure_session_and_load_cache()
+        self.load_cache()
+        return self.entries
+
+    def get_items(self):
+        pass
---
+++ packages/passwordlookup/passwordlookup-0.0.2.tar.gz/passwordlookup-0.0.2/passwordlookup/provider/session.py
@@ -0,0 +1,93 @@
+import time
+
+time.clock = time.process_time
+import os
+import sys
+import logging
+from .crypt_util import CryptUtil
+
+
+def get_script_path():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+
+def get_key_filename(key_suffix):
+    return (get_script_path() + "/{}.key").format(key_suffix)
+
+
+def remove_key_file(key_suffix):
+    if os.path.exists(get_key_filename(key_suffix)):
+        os.remove(get_key_filename(key_suffix))
+
+
+def get_key(key_suffix):
+    if not os.path.exists(get_key_filename(key_suffix)):
+        return generate_key(key_suffix)
+    try:
+        with open(get_key_filename(key_suffix), 'rb') as key_file:
+            return key_file.readline()
+    except IOError:
+        return generate_key(key_suffix)
+
+
+def generate_key(key_suffix):
+    logging.debug('generating key')
+    cu = CryptUtil()
+    key = cu.generate_key()
+    with open(get_key_filename(key_suffix), "wb") as key_file:
+        key_file.write(key)
+    return key
+
+
+class Session:
+    session_file = '/tmp/{}.session'
+
+    def __init__(self, key_suffix, session_suffix):
+        self.key_suffix = key_suffix
+        self.session_suffix = session_suffix
+        self.session = None
+
+    def _remove_session_file(self):
+        if os.path.exists(self.get_session_file()):
+            os.remove(self.get_session_file())
+
+    def clear(self):
+        self.session = None
+        remove_key_file(self.key_suffix)
+        self._remove_session_file()
+
+    def load(self):
+        try:
+            self.session = self.read()
+        except:
+            logging.debug("error occurred reading existing session - clearing and retrying")
+            self.clear()
+            self.session = self.read()
+
+    def get_session_file(self):
+        return Session.session_file.format(self.session_suffix)
+
+    def set(self, session):
+        self.session = session
+
+    def write(self):
+        with open(self.get_session_file(), "wb") as f:
+            cu = CryptUtil()
+            key = get_key(self.key_suffix)
+            f.write(cu.encrypt_str(self.session, key))
+
+    def read(self):
+        if not os.path.exists(self.get_session_file()):
+            self.session = ''
+        else:
+            try:
+                with open(self.get_session_file(), 'rb') as f:
+                    key = get_key(self.key_suffix)
+                    logging.debug('decrypting with key ' + str(key))
+                    cu = CryptUtil()
+                    self.session = cu.decrypt_bytes(f.read(), key)
+                    # Do something with the file
+            except IOError:
+                logging.debug("read File not accessible")
+                self.session = ''
+        return self.session
