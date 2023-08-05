9da869269eec07d3079232da735343fa7a028952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package jsm-transactional-ruler/0.1.3/jsm-transactional-ruler-0.1.3.tar.gz

---
+++ packages/jsm-transactional-ruler/jsm-transactional-ruler-0.1.3.tar.gz/jsm-transactional-ruler-0.1.3/jsm_transactional_ruler/events.py
@@ -0,0 +1,36 @@
+from typing import Dict
+from typing import List
+from typing import Optional
+
+from .enums import EventType
+
+
+class Event:
+
+    __slots__ = ["user_id", "_event_type", "data"]
+
+    def __init__(self, user_id: str, event_type: EventType, data: Optional[List[Dict[str, str]]] = None) -> None:
+        self.user_id = user_id
+        self.event_type = event_type
+        self.data = data
+
+    @property
+    def event_type(self):
+        return self._event_type
+
+    @event_type.setter
+    def event_type(self, event_type):
+        if not isinstance(event_type, EventType):
+            raise AttributeError("event_type is not instance from EventType")
+        self._event_type = event_type
+
+    def to_dict(self) -> Dict:
+        data = {
+            "user_id": self.user_id,
+            "event_type": self.event_type.value,
+        }
+
+        if self.data:
+            data["data"] = self.data
+
+        return data
