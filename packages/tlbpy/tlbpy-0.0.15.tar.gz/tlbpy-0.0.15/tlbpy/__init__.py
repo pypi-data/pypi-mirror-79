977cf8a083045116a09b5e3415701b2706f95a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:40 +0000

    Add package tlbpy/0.0.15/tlbpy-0.0.15.tar.gz

---
+++ packages/tlbpy/tlbpy-0.0.15.tar.gz/tlbpy-0.0.15/tlbpy/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    My personnal toolbox for Python
+"""
+__version__="0.0.15"
+from tlbpy.cnf_parser import cnf_parser
+from tlbpy.path import get_file_ext, get_files
+from tlbpy import rl_algos
+from tlbpy.rl_algos import rl_algo, spg
