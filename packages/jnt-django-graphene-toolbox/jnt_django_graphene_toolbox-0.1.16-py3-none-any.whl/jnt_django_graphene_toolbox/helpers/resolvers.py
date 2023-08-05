9c562250464003a79ce3d655f23409c1921c90e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:01 +0000

    Add package jnt-django-graphene-toolbox/0.1.16/jnt_django_graphene_toolbox-0.1.16-py3-none-any.whl

---
+++ packages/jnt-django-graphene-toolbox/jnt_django_graphene_toolbox-0.1.16-py3-none-any.whl/jnt_django_graphene_toolbox/helpers/resolvers.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import functools
+
+from jnt_django_graphene_toolbox.errors import GraphQLPermissionDenied
+
+
+def authenticated_only(func):
+    """Forbids unauthenticated requests."""
+
+    @functools.wraps(func)
+    def wrapper_decorator(parent, info, **kwargs):  # noqa: WPS110, WPS430
+        if not info.context.user.is_authenticated:
+            raise GraphQLPermissionDenied()
+        return func(parent, info, **kwargs)
+
+    return wrapper_decorator
