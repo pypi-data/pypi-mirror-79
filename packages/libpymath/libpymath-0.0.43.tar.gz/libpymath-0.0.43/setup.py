29fd92eeaae5a1b4c5ef3d6f036b11b2dc18297e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package libpymath/0.0.43/libpymath-0.0.43.tar.gz

---
+++ packages/libpymath/libpymath-0.0.43.tar.gz/libpymath-0.0.43/setup.py
@@ -0,0 +1,83 @@
+"""
+Copyright 2020 Toby Davis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from pathlib import Path
+import platform
+
+try:
+    from setuptools import setup, Extension, find_packages
+except ImportError:
+    from distutils.core import setup, Extension
+
+    def find_packages(where='.'):
+        return [folder.replace("/", ".").lstrip(".")
+                for (folder, _, fils) in os.walk(where)
+                if "__init__.py" in fils]
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
+compiler_flags = ["-std=c99", "-O3", "-mavx"] + (["-m64"] if platform.system() != "Linux" else []) + (["-fopenmp", "-lgomp"] if platform.system() in ("Linux", "Windows") else [])
+link_args = [] + (["-openmp", "-lgomp"] if platform.system() in ("Linux", "Windows") else [])
+
+print(compiler_flags)
+
+ext_modules = [
+    Extension(
+        "libpymath.core.testModule",
+        ["libpymath/LibPyMathModules/testModule.c"],
+        extra_compile_args=compiler_flags
+    ),
+    Extension(
+        "libpymath.core.matrix",
+        ["libpymath/LibPyMathModules/matrix/matrixModule.c"],
+        extra_compile_args=compiler_flags,
+        # extra_link_args=link_args,
+        include_dirs=["./"]
+    )
+]
+
+setup(
+    name="libpymath",
+    version="0.0.43",
+    description="A general purpose Python math module",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Toby Davis",
+    author_email="pencilcaseman@gmail.com",
+    url="https://www.github.com/pencilcaseman/gpc",
+    ext_modules=ext_modules,
+    packages=["libpymath"] + ["libpymath." + mod for mod in find_packages("libpymath")],
+    license="MIT Licences",
+    keywords=["math", "matrix", "vector", "network", "neural network",
+    		  "libpymath", "pymath", "libmath", "progress bar", "progress"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
