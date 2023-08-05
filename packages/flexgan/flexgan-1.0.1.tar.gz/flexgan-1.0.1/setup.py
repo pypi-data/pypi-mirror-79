9ec84df9ea8d9fb1f85da641f76c0a102bc3142d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package flexgan/1.0.1/flexgan-1.0.1.tar.gz

---
+++ packages/flexgan/flexgan-1.0.1.tar.gz/flexgan-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'flexgan',
+  packages = ['flexgan'],
+  version = '1.0.1',
+  license='MIT',
+  description = 'The FlexGAN library provides autonomous synthetic data generation for structured data sets.',
+  author = 'Eric Muccino',
+  author_email = 'emuccino@mindboard.com',
+  url = 'https://github.com/emuccino/flexgan',
+  download_url = 'https://github.com/emuccino/flexgan/archive/v1.0.1.tar.gz',
+  keywords = ['SYNTHETIC', 'DATA', 'GENERATION','GENERATE','GENERATIVE'],
+  install_requires=[
+          'numpy',
+          'pandas',
+          'tensorflow',
+          'scikit-learn',
+          'scipy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
