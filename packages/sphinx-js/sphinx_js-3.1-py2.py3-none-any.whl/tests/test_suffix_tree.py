c095bebe37991e87f0b1c7c4cca58f87d1f4bc31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:12 +0000

    Add package sphinx-js/3.1/sphinx_js-3.1-py2.py3-none-any.whl

---
+++ packages/sphinx-js/sphinx_js-3.1-py2.py3-none-any.whl/tests/test_suffix_tree.py
@@ -0,0 +1,24 @@
+import pytest
+
+from sphinx_js.suffix_tree import SuffixAmbiguous, SuffixNotFound, SuffixTree
+
+
+def test_things():
+    s = SuffixTree()
+    s.add(['./', 'dir/', 'utils.', 'max'], 1)
+    s.add(['./', 'dir/', 'footils.', 'max'], 2)
+    s.add(['./', 'dir/', 'footils.', 'hacks'], 3)
+
+    assert s.get(['hacks']) == 3
+    assert s.get(['footils.', 'max']) == 2
+    with pytest.raises(SuffixNotFound):
+        s.get(['quacks.', 'max'])
+    with pytest.raises(SuffixAmbiguous):
+        s.get(['max'])
+
+
+def test_full_path():
+    """Looking up a full path should not crash."""
+    s = SuffixTree()
+    s.add(['./', 'dir/', 'footils.', 'jacks'], 4)
+    assert s.get(['./', 'dir/', 'footils.', 'jacks']) == 4
