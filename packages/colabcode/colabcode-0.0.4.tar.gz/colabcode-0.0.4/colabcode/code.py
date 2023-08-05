3ab4b87ae144ea76b7110387029a4356ea6030e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package colabcode/0.0.4/colabcode-0.0.4.tar.gz

---
+++ packages/colabcode/colabcode-0.0.4.tar.gz/colabcode-0.0.4/colabcode/code.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+from pyngrok import ngrok
+try:
+    from google.colab import drive
+    colab_env = True
+except ImportError:
+    colab_env = False
+
+
+class ColabCode:
+    def __init__(self, port=10000, password=None, gcloud=False):
+        self.port = port
+        self.password = password
+        self._mount = gcloud
+        self._install_code()
+        self._start_server()
+        self._run_code()
+
+    def _install_code(self):
+        subprocess.run(
+            ["wget", "https://code-server.dev/install.sh"], stdout=subprocess.PIPE
+        )
+        subprocess.run(["sh", "install.sh"], stdout=subprocess.PIPE)
+
+    def _start_server(self):
+        active_tunnels = ngrok.get_tunnels()
+        for tunnel in active_tunnels:
+            public_url = tunnel.public_url
+            ngrok.disconnect(public_url)
+        url = ngrok.connect(port=self.port)
+        print(f"Code Server can be accessed on: {url}")
+
+    def _run_code(self):
+        os.system(f"fuser -n tcp -k {self.port}")
+        if self._mount and colab_env:
+            drive.mount("/content/drive")
+        if self.password:
+            code_cmd = f"PASSWORD={self.password} code-server --port {self.port} --disable-telemetry"
+        else:
+            code_cmd = f"code-server --port {self.port} --auth none --disable-telemetry"
+        with subprocess.Popen(
+            [code_cmd],
+            shell=True,
+            stdout=subprocess.PIPE,
+            bufsize=1,
+            universal_newlines=True,
+        ) as proc:
+            for line in proc.stdout:
+                print(line, end="")
