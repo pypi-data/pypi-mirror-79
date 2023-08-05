7d5e219afa48cb540f6c78214d476a26683f99bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package Flask-Filter/0.1.0.dev5/Flask-Filter-0.1.0.dev5.tar.gz

---
+++ packages/Flask-Filter/Flask-Filter-0.1.0.dev5.tar.gz/Flask-Filter-0.1.0.dev5/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name='Flask-Filter',
+    version='0.1.0dev5',
+    author="Exley McCormick",
+    author_email="exleym@gmail.com",
+    description="A Flask extension for creating standard resource searches",
+    packages=find_packages(exclude=["tests", "docs", "contrib"]),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/exleym/Flask-Filter ",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "marshmallow", "sqlalchemy", "Flask-SQLAlchemy"],
+)
