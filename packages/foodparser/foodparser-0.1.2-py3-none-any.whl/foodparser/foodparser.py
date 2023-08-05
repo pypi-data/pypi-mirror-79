27820e5cce18be5c5f5adb62d4152f6e0cab99d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package foodparser/0.1.2/foodparser-0.1.2-py3-none-any.whl

---
+++ packages/foodparser/foodparser-0.1.2-py3-none-any.whl/foodparser/foodparser.py
@@ -0,0 +1,11 @@
+def say_hi():
+    '''
+        This is a testing function just to test if the package is working well.
+    '''
+    return 'Hi, you are using foodparser version:' + foodparser.__version__
+
+def parse_single_entry(entry):
+    '''
+        This is a naive parsing function for testing purpose
+    '''
+    return 'Parsed: ' + entry
