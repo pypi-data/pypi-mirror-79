aba12db65e353a1824ad3a785ce47c38f9bd4f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:48 +0000

    Add package kinto/14.0.1/kinto-14.0.1.tar.gz

---
+++ packages/kinto/kinto-14.0.1.tar.gz/kinto-14.0.1/kinto/core/storage/exceptions.py
@@ -0,0 +1,46 @@
+"""Exceptions raised by storage backend.
+"""
+
+
+class BackendError(Exception):
+    """A generic exception raised by storage on error.
+
+    :param Exception original: the wrapped exception raised by underlying
+        library.
+    """
+
+    def __init__(self, original=None, message=None, *args, **kwargs):
+        self.original = original
+        if message is None:
+            message = f"{original.__class__.__name__}: {original}"
+        super().__init__(message, *args, **kwargs)
+
+
+class RecordNotFoundError(Exception):
+    """Deprecated exception name."""
+
+    pass
+
+
+class ObjectNotFoundError(RecordNotFoundError):
+    """An exception raised when a specific object could not be found."""
+
+    pass
+
+
+class IntegrityError(BackendError):
+    pass
+
+
+class UnicityError(IntegrityError):
+    """An exception raised on unicity constraint violation.
+
+    Raised by storage backend when the creation or the modification of a
+    object violates the unicity constraints defined by the resource.
+
+    """
+
+    def __init__(self, field, *args, **kwargs):
+        self.field = field
+        self.msg = f"{field} is not unique"
+        super().__init__(*args, **kwargs)
---
+++ packages/kinto/kinto-14.0.1.tar.gz/kinto-14.0.1/kinto/plugins/admin/build/precache-manifest.fac4f567e4547e195d6b55ed6c1e6d9d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e392cf4ae8ece3dbe6fb3d80299d093",
+    "url": "/v1/admin/index.html"
+  },
+  {
+    "revision": "9a55f61d356d56e13c7f",
+    "url": "/v1/admin/static/css/2.3c8e866b.chunk.css"
+  },
+  {
+    "revision": "9a55f61d356d56e13c7f",
+    "url": "/v1/admin/static/js/2.7a1d3940.chunk.js"
+  },
+  {
+    "revision": "755161fc24a4bd19085bf6fbb7805e9d",
+    "url": "/v1/admin/static/js/2.7a1d3940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53443cad972980a50e38",
+    "url": "/v1/admin/static/js/main.72a7f666.chunk.js"
+  },
+  {
+    "revision": "9ef0d6507b831d1cab94",
+    "url": "/v1/admin/static/js/runtime-main.6c5c2596.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/v1/admin/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
