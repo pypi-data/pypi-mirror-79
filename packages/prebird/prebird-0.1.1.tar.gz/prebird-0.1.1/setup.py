9cc7c6232170904305658e17b2709f4028e1ba07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:42 +0000

    Add package prebird/0.1.1/prebird-0.1.1.tar.gz

---
+++ packages/prebird/prebird-0.1.1.tar.gz/prebird-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+setup(
+  name = 'prebird',         # How you named your package folder (MyLib)
+  packages = ['prebird'],   # Chose the same as "name"
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
+  license='afl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Intended to help preprocessing birds songs',   # Give a short description about your library
+  author = 'Liber Adrián Hernández Abad',                   # Type in your name
+  author_email = 'liber.a.hernandez@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/RoyFocker',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/RoyFocker/preBird/archive/v0.1.1.tar.gz',    # I explain this later on
+  keywords = ['bird', 'birds', 'audio','preprocessing','processing'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'matplotlib',
+          'scypi',
+          'sklearn',
+          'glob',
+          'pickle',
+          'statistics',
+          'skimage',
+          'playsound',
+          'copy',
+          'argparse'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
