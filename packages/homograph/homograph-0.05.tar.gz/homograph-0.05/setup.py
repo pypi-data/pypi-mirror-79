8c8af53637b4e7ee61f21798dbe4c4a5e8c96d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:33 +0000

    Add package homograph/0.05/homograph-0.05.tar.gz

---
+++ packages/homograph/homograph-0.05.tar.gz/homograph-0.05/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'homograph',
+  packages = ['homograph'],
+  version = '0.05',
+  license='MIT',
+  description = 'IDN Homograph detection tools',
+  author = 'Alex Alvarado',
+  author_email = 'alex@flowcrypt.com',
+  url = 'https://github.com/seisvelas/idn-homograph-database',
+  # Note: The below release doesn't exist yet!
+  download_url = 'https://github.com/seisvelas/idn-homographs-database/archive/0.02.tar.gz',
+  keywords = ['IDN', 'homograph', 'attack', 'security'],
+  install_requires = [],
+  include_package_data = True,
+  classifiers = [
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Topic :: Security',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
