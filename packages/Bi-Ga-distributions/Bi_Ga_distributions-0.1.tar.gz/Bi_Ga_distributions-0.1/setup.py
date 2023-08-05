9ece19ba84f8a7749ee457ff6be12bef01aadfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:36 +0000

    Add package Bi-Ga-distributions/0.1/Bi_Ga_distributions-0.1.tar.gz

---
+++ packages/Bi-Ga-distributions/Bi_Ga_distributions-0.1.tar.gz/Bi_Ga_distributions-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Bi_Ga_distributions',
+      version='0.1',
+      description='Gaussianand Binomial distributions',
+      packages=['Bi_Ga_distributions'],
+      author='Lamia Hasan Mohammed',
+      author_email='lamiahasan4@gmail.com',
+      zip_safe=False
+     )
