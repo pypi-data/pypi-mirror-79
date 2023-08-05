04495eae9194c754a7ca2e814242613f633b28ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package rai-core-flask/0.0.2/rai_core_flask-0.0.2-py3-none-any.whl

---
+++ packages/rai-core-flask/rai_core_flask-0.0.2-py3-none-any.whl/rai_core_flask/environment_detector.py
@@ -0,0 +1,147 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+import os
+from .environments import AzureNBEnvironment
+from .environments import DatabricksEnvironment
+from .environments import LocalIPythonEnvironment
+
+"""Environment detection related utilities.
+A good portion of this code has largely been sourced from open-source licensed
+code available between StackOverflow and plotly.
+
+Plotly derived code comes from below:
+https://github.com/plotly/plotly.py/blob/944af4a0b28bef2b139307a7808c02c51d804c4d/packages/python/plotly/plotly/io/_renderers.py#L455
+"""
+
+
+def _detect_ipython():
+    """Detect if called in an IPython environment.
+
+    Mostly derived from StackOverflow below:
+    https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
+
+    Returns
+    -------
+        True if in IPython environment.
+
+    """
+    try:
+        from IPython import get_ipython
+
+        return get_ipython() is not None
+    except NameError:  # pragma: no cover
+        return False
+
+
+def _detect_ipython_zmq():
+    """Detect if running in an IPython environment using ZMQ.
+
+    i.e. notebook/qtconsole/lab.
+
+    Mostly derived from StackOverflow below:
+    https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook/24937408
+
+    Returns
+    -------
+        True if called in IPython notebook or qtconsole.
+
+    """
+    try:
+        from IPython import get_ipython
+
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":
+            return True  # Jupyter notebook or qtconsole
+        elif shell == "TerminalInteractiveShell":
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:  # pragma: no cover
+        return False  # Probably standard Python interpreter
+
+
+def _detect_colab():
+    try:
+        import google.colab  # noqa: F401
+
+        return True
+    except ImportError:
+        return False
+
+
+def _detect_binder():
+    return "BINDER_SERVICE_HOST" in os.environ
+
+
+def _detect_sagemaker():
+    return "SM_NUM_CPUS" in os.environ
+
+
+def _detect_kaggle():
+    return os.path.exists("/kaggle/input")
+
+
+def _detect_azure_notebook():
+    return "AZURE_NOTEBOOKS_HOST" in os.environ
+
+
+def _detect_azureml_notebook_vm():
+    return "AZUREML_NB_PATH" in os.environ
+
+
+def _detect_vscode():
+    return "VSCODE_PID" in os.environ
+
+
+def _detect_databricks():
+    return "DATABRICKS_RUNTIME_VERSION" in os.environ
+
+
+def is_cloud_env(detected):
+    """Detect whether it is running in a cloud environment."""
+    cloud_env = [
+        "databricks",
+        "azure",
+        "azureml_vm",
+        "kaggle",
+        "sagemaker",
+        "binder",
+        "colab",
+    ]
+    if len(set(cloud_env).intersection(detected)) != 0:
+        return True
+    else:
+        return False
+
+
+def build_environment(ip, port):
+    """Create a runtime environment object."""
+    # legacy
+    checks = {  # noqa: F841
+        "azure_nb": AzureNBEnvironment,
+        "databricks": _detect_databricks,
+        "vscode": _detect_vscode,
+        "azure": _detect_azure_notebook,
+        "azureml_vm": _detect_azureml_notebook_vm,
+        "kaggle": _detect_kaggle,
+        "sagemaker": _detect_sagemaker,
+        "binder": _detect_binder,
+        "colab": _detect_colab,
+        "ipython-zmq": _detect_ipython_zmq,
+        "ipython": _detect_ipython,
+    }
+
+    azure_nb_environment = AzureNBEnvironment(ip, port)
+    databricks_environment = DatabricksEnvironment(ip, port)
+    local_ipython_environment = LocalIPythonEnvironment(ip, port)
+
+    # Todo: Add case for detecting and using a Jupyterlab environment
+    if databricks_environment.successfully_detected:
+        return databricks_environment
+    if azure_nb_environment.successfully_detected:
+        return azure_nb_environment
+    if local_ipython_environment.successfully_detected:
+        return local_ipython_environment
+    else:
+        raise Exception("Failed to detect Ipython environment")
---
+++ packages/rai-core-flask/rai_core_flask-0.0.2-py3-none-any.whl/rai_core_flask/environments/local_ipython_environment.py
@@ -0,0 +1,33 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+
+class LocalIPythonEnvironment:
+    """Environment class for local IPython environments.
+
+    LocalIPythonEnvironment represents functionality to detect whether it is
+    executed in a local python environment based on IPython's availability.
+    Additionally, it can display corresponding visualizations.
+    """
+
+    def __init__(self, ip, port):
+        self.successfully_detected = False
+        self.base_url = None
+        self.externally_available = None
+        self.port = port
+
+        try:
+            from IPython.display import HTML, display  # noqa: F401
+        except NameError:
+            self.successfully_detected = False
+        else:
+            self.successfully_detected = True
+            self.base_url = "http://{0}:{1}".format(
+                ip,
+                port)
+            self.externally_available = True
+
+    def display(self, html):
+        """Display the passed HTML using IPython."""
+        from IPython.display import HTML, display
+        display(HTML(html))
