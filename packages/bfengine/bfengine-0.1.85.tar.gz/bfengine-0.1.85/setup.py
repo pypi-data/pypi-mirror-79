83d9dd0fc280d57392e896cb75fb78b98b211f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:22 +0000

    Add package bfengine/0.1.85/bfengine-0.1.85.tar.gz

---
+++ packages/bfengine/bfengine-0.1.85.tar.gz/bfengine-0.1.85/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bfengine",
+    version="0.1.85",
+    author="JeremyXin",
+    author_email="chengjiexin@emotibot.com",
+    description="bf engine sdk for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    package_data={
+        '': ['data/*']
+    },
+    install_requires=[
+        "requests",
+        "tqdm"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
