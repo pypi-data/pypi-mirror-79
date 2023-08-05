7781defeecf9990cae4c3751aafcc04c70a64e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:49 +0000

    Add package openpivtlib/1.2.0/openpivtlib-1.2.0.tar.gz

---
+++ packages/openpivtlib/openpivtlib-1.2.0.tar.gz/openpivtlib-1.2.0/openpivtlib/plugin.py
@@ -0,0 +1,45 @@
+import logging
+from typing import Optional
+from . import util
+
+
+class Plugin:
+    def __init__(self, pipeline: str, job_id: int, args: Optional[dict] = None):
+        self.pipeline = pipeline
+        self.job_id = job_id
+        self.args = args if args is not None else {}
+
+        self.logger = PluginLoggerAdapter(util.get_class_logger(self), self.pipeline, self.job_id)
+
+    def execute(self, data: Optional[dict] = None):
+        raise NotImplementedError
+
+
+class Trigger:
+    def __init__(self, pipeline: str, args: Optional[dict] = None):
+        self.pipeline = pipeline
+        self.args = args if args is not None else {}
+
+        self.logger = TriggerLoggerAdapter(util.get_class_logger(self), self.pipeline)
+
+    def run(self):
+        raise NotImplementedError
+
+
+class PluginLoggerAdapter(logging.LoggerAdapter):
+    def __init__(self, logger: logging.Logger, pipeline: str, job_id: int):
+        super().__init__(logger, {})
+        self.pipeline = pipeline
+        self.job_id = job_id
+
+    def process(self, msg, kwargs):
+        return f'[{self.pipeline}:{self.job_id}] {msg}', kwargs
+
+
+class TriggerLoggerAdapter(logging.LoggerAdapter):
+    def __init__(self, logger: logging.Logger, pipeline: str):
+        super().__init__(logger, {})
+        self.pipeline = pipeline
+
+    def process(self, msg, kwargs):
+        return f'[{self.pipeline}] {msg}', kwargs
