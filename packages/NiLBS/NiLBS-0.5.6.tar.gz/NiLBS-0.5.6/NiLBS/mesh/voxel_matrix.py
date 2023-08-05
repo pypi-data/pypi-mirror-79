e65c742f57b04e9ef6c36ce2f9f15a584e5960a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:47 +0000

    Add package NiLBS/0.5.6/NiLBS-0.5.6.tar.gz

---
+++ packages/NiLBS/NiLBS-0.5.6.tar.gz/NiLBS-0.5.6/NiLBS/mesh/voxel_matrix.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+
+
+class VoxelMatrix:
+    """
+
+    Utility class for representing a voxel matrix which can be sub-divided ad scaled
+
+
+    """
+    def __init__(self, resolution, voxel_size):
+        """
+        :param resolution: [Int], (n, m, p) size of the voxel grid
+        """
+
+        self.resolution = resolution
+        self.matrix = np.full((resolution[0], resolution[1], resolution[2]), False)
+        self.backing_scale = [1, 1, 1]
+
+    def set(self, point):
+        """
+        :param point: the point to set
+        :return:
+        """
---
+++ packages/NiLBS/NiLBS-0.5.6.tar.gz/NiLBS-0.5.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+   name='NiLBS',
+   version='0.5.6',
+   description='NiLBS 3D implementation.',
+   author='Joe March',
+   author_email='jgm45@cam.ac.uk',
+   packages=['NiLBS',
+             'NiLBS.body',
+             'NiLBS.demo',
+             'NiLBS.mesh',
+             'NiLBS.occupancy',
+             'NiLBS.occupancy.voxel',
+             'NiLBS.pose',
+             'NiLBS.sampling',
+             'NiLBS.weighting'],
+   package_data={'NiLBS': ['data/*.npz']},
+   install_requires=['trimesh', 'pyrender', 'tensorflow'], #external packages as dependencies
+)
