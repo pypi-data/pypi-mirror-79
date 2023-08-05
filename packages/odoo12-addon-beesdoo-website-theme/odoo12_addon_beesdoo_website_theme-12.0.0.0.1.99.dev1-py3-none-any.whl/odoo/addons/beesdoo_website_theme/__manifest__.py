2a4890674b0d514a933f7a4575ba1c5c1f301af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:39 +0000

    Add package odoo12-addon-beesdoo-website-theme/12.0.0.0.1.99.dev1/odoo12_addon_beesdoo_website_theme-12.0.0.0.1.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-beesdoo-website-theme/odoo12_addon_beesdoo_website_theme-12.0.0.0.1.99.dev1-py3-none-any.whl/odoo/addons/beesdoo_website_theme/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "BEES coop Website Theme",
+    "summary": """
+        Apply BEES coop design rules.
+    """,
+    "author": "Rémy Taymans, Coop IT Easy SCRLfs",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "license": "AGPL-3",
+    "category": "Themes",
+    "version": "12.0.0.0.1",
+    "application": True,
+    "depends": ["website"],
+    "data": ["views/assets.xml"],
+}
