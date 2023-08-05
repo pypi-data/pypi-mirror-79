f752bb8db99c08e77231fed617b94197e87f463a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:38 +0000

    Add package pyariable/0.3.1/pyariable-0.3.1.tar.gz

---
+++ packages/pyariable/pyariable-0.3.1.tar.gz/pyariable-0.3.1/pyariable/__init__.py
@@ -0,0 +1,53 @@
+class Variable:
+    def __eq__(self, b: object) -> bool:
+        try:
+            return self.value == b
+        except AttributeError:
+            self.value: object = b
+            return True
+
+    def __lt__(self, b) -> bool:
+        try:
+            return self.value < b
+        except AttributeError:
+            raise Exception("Unknown value")
+
+    def __le__(self, b) -> bool:
+        try:
+            return self.value <= b
+        except AttributeError:
+            raise Exception("Unknown value")
+
+    def __gt__(self, b) -> bool:
+        try:
+            return self.value > b
+        except AttributeError:
+            raise Exception("Unknown value")
+
+    def __ge__(self, b) -> bool:
+        try:
+            return self.value >= b
+        except AttributeError:
+            raise Exception("Unknown value")
+
+    def __repr__(self) -> str:
+        try:
+            return repr(self.value)
+        except AttributeError:
+            return "?"
+
+    def __round__(self, x):
+        return round(self.value, x)
+
+    def __int__(self):
+        return int(self.value)
+
+
+class UniversalVariable:
+    def __eq__(self, b: object) -> bool:
+        return True
+
+
+def variables(n):
+    for _ in range(n):
+        yield Variable()
