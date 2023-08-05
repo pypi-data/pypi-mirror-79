be3b2a538b2da41fc3de847b57c4ad1126015ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:01 +0000

    Add package vbelts/0.1.0/vbelts-0.1.0.tar.gz

---
+++ packages/vbelts/vbelts-0.1.0.tar.gz/vbelts-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    # Mandatory and initial setup
+    name='vbelts',
+    version='0.1.0',
+    description='Utilities for v-belt dimensioning',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    package_data={'':["data/*.csv"]},
+    include_package_data=True,
+    author='Glademir Karpinski Junior, Hector Balke Nodari',
+    author_email='gkarpinskijr@gmail.com, hectornodari@gmail.com',
+
+    # Secondary setup
+    url='https://github.com/traumatic-monotype/vbelts',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        ],
+    extras_require = {"dev": ["pytest>=6.0.1","check-manifest>=0.42",]},
+    
+)
