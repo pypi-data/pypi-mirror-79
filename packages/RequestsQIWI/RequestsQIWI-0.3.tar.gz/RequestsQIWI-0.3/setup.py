66b84184feeb560b509a669dc240bb86e4de3592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:36 +0000

    Add package RequestsQIWI/0.3/RequestsQIWI-0.3.tar.gz

---
+++ packages/RequestsQIWI/RequestsQIWI-0.3.tar.gz/RequestsQIWI-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='RequestsQIWI',
+    version='0.3',
+    author='YolSolo',
+    author_email='aenshtein49@gmail.com',
+    description='QIWI API for Humans',
+    long_description='See https://github.com/YolSolo/RequestsQIWI',
+    url='https://github.com/YolSolo/RequestsQIWI',
+    install_requires=['requests', 'uuid'],
+    packages=['RequestsQIWI']
+)
