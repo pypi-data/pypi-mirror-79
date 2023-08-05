7095a878dfbfeff972012993be674403787db36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:47 +0000

    Add package wchowdhu-distributions/0.1/wchowdhu_distributions-0.1.tar.gz

---
+++ packages/wchowdhu-distributions/wchowdhu_distributions-0.1.tar.gz/wchowdhu_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='wchowdhu_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['wchowdhu_distributions'],
+      zip_safe=False)
