b04e1fb2c5eb2b989bec632aa0c0e4b3be781a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:23 +0000

    Add package django-content-editor/3.0.1/django_content_editor-3.0.1-py2.py3-none-any.whl

---
+++ packages/django-content-editor/django_content_editor-3.0.1-py2.py3-none-any.whl/content_editor/static/content_editor/tabbed_fieldsets.js
@@ -0,0 +1,60 @@
+/* global django */
+django.jQuery(function ($) {
+  var tabbed = $(".tabbed");
+  if (tabbed.length > 1) {
+    tabbed
+      .eq(0)
+      .before(
+        '<div id="tabbed" class="clearfix">' +
+          '<div class="tabs clearfix"></div>' +
+          '<div class="modules"></div>' +
+          "</div>"
+      );
+
+    var $tabs = $("#tabbed > .tabs"),
+      $modules = $("#tabbed > .modules"),
+      errorIndex = -1,
+      uncollapseIndex = -1;
+
+    tabbed.each(function createTabs(index) {
+      var $old = $(this),
+        $title = $old.children("h2");
+
+      if ($old.find(".errorlist").length) {
+        $title.addClass("has-error");
+        errorIndex = errorIndex < 0 ? index : errorIndex;
+      }
+      if ($old.is(".uncollapse")) {
+        uncollapseIndex = uncollapseIndex < 0 ? index : uncollapseIndex;
+      }
+
+      $title.attr("data-index", index);
+      $tabs.append($title);
+
+      $old.addClass("content-editor-hidden");
+
+      $modules.append($old);
+    });
+
+    $tabs.on("click", "[data-index]", function () {
+      var $tab = $(this);
+      if ($tab.hasClass("active")) {
+        $tab.removeClass("active");
+        $modules.children().addClass("content-editor-hidden");
+      } else {
+        $tabs.find(".active").removeClass("active");
+        $tab.addClass("active");
+        $modules
+          .children()
+          .addClass("content-editor-hidden")
+          .eq($tab.data("index"))
+          .removeClass("content-editor-hidden");
+      }
+    });
+
+    if (errorIndex >= 0 || uncollapseIndex >= 0) {
+      var index = errorIndex >= 0 ? errorIndex : uncollapseIndex;
+      $tabs.find("[data-index=" + index + "]").click();
+    }
+  }
+});
