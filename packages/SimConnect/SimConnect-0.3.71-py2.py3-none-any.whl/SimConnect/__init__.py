14dc4d90257eef10c6ee08219d28034bcb9fe04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package SimConnect/0.3.71/SimConnect-0.3.71-py2.py3-none-any.whl

---
+++ packages/SimConnect/SimConnect-0.3.71-py2.py3-none-any.whl/SimConnect/__init__.py
@@ -0,0 +1,16 @@
+from .SimConnect import SimConnect, Request, Event, millis, DWORD
+from .RequestList import AircraftRequests
+from .EventList import AircraftEvents
+
+
+def int_or_str(value):
+	try:
+		return int(value)
+	except TypeError:
+		return value
+
+
+__version__ = "0.3.71"
+VERSION = tuple(map(int_or_str, __version__.split(".")))
+
+__all__ = ["SimConnect", "Request", "Event", "millis", "DWORD", "AircraftRequests", "AircraftEvents"]
