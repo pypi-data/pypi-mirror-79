f45dad59972c79e5bc90c0c6aedb5c0c35b7d0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:02 +0000

    Add package mutalyzer-mutator/0.0.2/mutalyzer-mutator-0.0.2.tar.gz

---
+++ packages/mutalyzer-mutator/mutalyzer-mutator-0.0.2.tar.gz/mutalyzer-mutator-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="mutalyzer-mutator",
+    version="0.0.2",
+    packages=["mutalyzer_mutator"],
+    python_requires=">=3.0",
+    description="Mutalyzer sequence mutator.",
+    url="http://github.com/mutalyzer/mutator",
+    author="Mihai Lefter",
+    author_email="m.lefter@lumc.nl",
+    keywords="mutalyzer mutate description sequence dna",
+    license="MIT",
+)
