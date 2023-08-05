b4c494b63279f4ced53d2a4a8eb3adfc260919b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:43 +0000

    Add package ucnp-skybility/2.0.2/ucnp-skybility-2.0.2.tar.gz

---
+++ packages/ucnp-skybility/ucnp-skybility-2.0.2.tar.gz/ucnp-skybility-2.0.2/client/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File: __init__.py.py
+Author: Scott Yang(Scott)
+Email: yangyingfa@skybility.com
+Copyright: Copyright (c) 2020, Skybility Software Co.,Ltd. All rights reserved.
+Description:
+"""
+import os
+
+exe_path = os.path.dirname(os.path.abspath(__file__))
+user_home = os.path.expanduser("~")
