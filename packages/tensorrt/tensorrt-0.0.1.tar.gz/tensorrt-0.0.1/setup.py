74fd9d142908cda11a4396161c27e2d956272d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:00 +0000

    Add package tensorrt/0.0.1/tensorrt-0.0.1.tar.gz

---
+++ packages/tensorrt/tensorrt-0.0.1.tar.gz/tensorrt-0.0.1/setup.py
@@ -0,0 +1,20 @@
+
+from distutils.core import setup
+
+setup(name='tensorrt',
+      version='0.0.1',
+      description='Just a demo package',
+      license='WTF',
+      author='others',
+      author_email='others@WTF.com',
+      classifiers=[
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Operating System :: OS Independent',
+      ]
+     )
