c6c1a592170d38301fa6ba12b8a299f0b021bbf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:43 +0000

    Add package triku/1.2.1/triku-1.2.1-py3.7.egg

---
+++ packages/triku/triku-1.2.1-py3.7.egg/triku/__init__.py
@@ -0,0 +1,10 @@
+__author__ = ", ".join(["Alex M. Ascensión"])
+__email__ = ", ".join(
+    [
+        "alexmascension@gmail.com",
+        # We don’t need all, the main authors are sufficient.
+    ]
+)
+
+
+__version__ = "1.2.1"
