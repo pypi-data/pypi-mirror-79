f325fc0be7094f587ce04234bb46138f093349d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:37 +0000

    Add package reciprocalspaceship/0.8.9/reciprocalspaceship-0.8.9.tar.gz

---
+++ packages/reciprocalspaceship/reciprocalspaceship-0.8.9.tar.gz/reciprocalspaceship-0.8.9/tests/test_dataset_hkl_to_asu.py
@@ -0,0 +1,37 @@
+import pytest
+import numpy as np
+import reciprocalspaceship as rs
+
+@pytest.mark.parametrize("inplace", [True, False])
+@pytest.mark.parametrize("reset_index", [True, False])
+def test_hkl_to_asu(mtz_by_spacegroup, inplace, reset_index):
+    """Test DataSet.hkl_to_asu() for common spacegroups"""
+    x = rs.read_mtz(mtz_by_spacegroup)
+    y = rs.read_mtz(mtz_by_spacegroup[:-4] + '_p1.mtz')
+    y.spacegroup = x.spacegroup
+
+    if reset_index:
+        y.reset_index(inplace=True)
+
+    yasu = y.hkl_to_asu(inplace=inplace)
+
+    if reset_index:
+        yasu.set_index(['H', 'K', 'L'], inplace=True)
+
+    assert len(x.index.difference(yasu.index)) == 0
+    assert len(yasu.index.difference(x.index)) == 0
+
+    Fx    = x.loc[yasu.index, 'FMODEL'].values.astype(float) 
+    Fyasu = yasu['FMODEL'].values.astype(float) 
+    assert np.allclose(Fx, Fyasu)
+
+    Phx    = x.loc[yasu.index, 'PHIFMODEL'].values.astype(float) 
+    Phyasu = yasu['PHIFMODEL'].values.astype(float) 
+    Sx    = Fx*np.exp(1j*np.deg2rad(Phx))
+    Syasu = Fyasu*np.exp(1j*np.deg2rad(Phyasu))
+    assert np.allclose(Sx, Syasu, rtol=1e-3)
+
+    if inplace:
+        assert id(yasu) == id(y)
+    else:
+        assert id(yasu) != id(y)
