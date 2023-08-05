c3196693af80fd666d6ac1b95302d949c67ed14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package robots_controller/1.0.7/robots_controller-1.0.7-py2.py3-none-any.whl

---
+++ packages/robots_controller/robots_controller-1.0.7-py2.py3-none-any.whl/robots_controller/__test.py
@@ -0,0 +1,16 @@
+import json
+
+from connectors.ServiceProxyConnector import ServiceProxyConnector
+
+
+#_json = json.loads('{"id":6,"name":"sgsafono","email":"sgsafono@mts.ru","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwibmFtZSI6InNnc2Fmb25vIiwiZW1haWwiOiJzZ3NhZm9ub0BtdHMucnUiLCJleHAiOjE2MDQ4MjY5MjAsImlhdCI6MTU5OTY0MjkyMH0.mPPWNTYxDDXfx9lQrWQ6x2eeGyT_RYgsFwdtUeaiaB8"}')
+#print(_json)
+#tok = _json["token"]
+
+
+
+
+obj = ServiceProxyConnector()
+res = obj.getInqueryByNumber("1-688357270685")
+
+print(res)
