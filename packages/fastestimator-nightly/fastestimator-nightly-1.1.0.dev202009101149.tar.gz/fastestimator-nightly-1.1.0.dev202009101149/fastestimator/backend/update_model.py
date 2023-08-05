f13d6825ca8b3ba64a7528100dbbe3011baa828f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:46 +0000

    Add package fastestimator-nightly/1.1.0.dev202009101149/fastestimator-nightly-1.1.0.dev202009101149.tar.gz

---
+++ packages/fastestimator-nightly/fastestimator-nightly-1.1.0.dev202009101149.tar.gz/fastestimator-nightly-1.1.0.dev202009101149/fastestimator/backend/update_model.py
@@ -0,0 +1,83 @@
+# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from typing import Optional, Union
+
+import tensorflow as tf
+import torch
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
+
+from fastestimator.backend.get_gradient import get_gradient
+from fastestimator.backend.reduce_mean import reduce_mean
+
+
+def update_model(model: Union[tf.keras.Model, torch.nn.Module],
+                 loss: Union[tf.Tensor, torch.Tensor],
+                 tape: Optional[tf.GradientTape] = None,
+                 retain_graph: bool = True):
+    """Update `model` weights based on a given `loss`.
+
+    This method can be used with TensorFlow models:
+    ```python
+    m = fe.build(fe.architecture.tensorflow.LeNet, optimizer_fn="adam")
+    x = tf.ones((3,28,28,1))  # (batch, height, width, channels)
+    y = tf.constant((1, 0, 1))
+    with tf.GradientTape(persistent=True) as tape:
+        pred = fe.backend.feed_forward(m, x)  # [[~0.5, ~0.5], [~0.5, ~0.5], [~0.5, ~0.5]]
+        loss = fe.backend.sparse_categorical_crossentropy(y_pred=pred, y_true=y)  # ~2.3
+        fe.backend.update_model(m, loss=loss, tape=tape)
+    ```
+
+    This method can be used with PyTorch models:
+    ```python
+    m = fe.build(fe.architecture.pytorch.LeNet, optimizer_fn="adam")
+    x = torch.ones((3,1,28,28))  # (batch, channels, height, width)
+    y = torch.tensor((1, 0, 1))
+    pred = fe.backend.feed_forward(m, x)  # [[~0.5, ~0.5], [~0.5, ~0.5], [~0.5, ~0.5]]
+    loss = fe.backend.sparse_categorical_crossentropy(y_pred=pred, y_true=y)  # ~2.3
+    fe.backend.update_model(m, loss=loss)
+    ```
+
+    Args:
+        model: A neural network instance to update.
+        loss: A loss value to compute gradients from.
+        tape: A TensorFlow GradientTape which was recording when the `loss` was computed (iff using TensorFlow).
+        retain_graph: Whether to keep the model graph in memory (applicable only for PyTorch).
+
+    Raises:
+        ValueError: If `model` is an unacceptable data type.
+    """
+    loss = reduce_mean(loss)
+    if isinstance(model, tf.keras.Model):
+        # scale up loss for mixed precision training to avoid underflow
+        if isinstance(model.current_optimizer, mixed_precision.LossScaleOptimizer):
+            loss = model.current_optimizer.get_scaled_loss(loss)
+        # for multi-gpu training, the gradient will be combined by sum, normalize the loss
+        strategy = tf.distribute.get_strategy()
+        if isinstance(strategy, tf.distribute.MirroredStrategy):
+            loss = loss / strategy.num_replicas_in_sync
+        gradients = get_gradient(loss, model.trainable_variables, tape=tape)
+        with tape.stop_recording():
+            # scale down gradient to balance scale-up loss
+            if isinstance(model.current_optimizer, mixed_precision.LossScaleOptimizer):
+                gradients = model.current_optimizer.get_unscaled_gradients(gradients)
+            model.current_optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    elif isinstance(model, torch.nn.Module):
+        trainable_params = [p for p in model.parameters() if p.requires_grad]
+        gradients = get_gradient(loss, trainable_params, retain_graph=retain_graph)
+        for gradient, parameter in zip(gradients, trainable_params):
+            parameter.grad = gradient
+        model.current_optimizer.step()
+    else:
+        raise ValueError("Unrecognized model instance {}".format(type(model)))
---
+++ packages/fastestimator-nightly/fastestimator-nightly-1.1.0.dev202009101149.tar.gz/fastestimator-nightly-1.1.0.dev202009101149/fastestimator/op/tensorop/model/update.py
@@ -0,0 +1,55 @@
+# Copyright 2019 The FastEstimator Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from typing import Any, Dict, Iterable, List, TypeVar, Union
+
+import tensorflow as tf
+import torch
+
+from fastestimator.backend.update_model import update_model
+from fastestimator.op.tensorop.tensorop import TensorOp
+from fastestimator.util.traceability_util import traceable
+
+Tensor = TypeVar('Tensor', tf.Tensor, torch.Tensor)
+
+
+@traceable()
+class UpdateOp(TensorOp):
+    """This class performs updates to a model's weights based on the loss.
+
+    Args:
+        model: Model instance compiled by fe.build.
+        loss_name: The name of loss.
+        mode: What mode(s) to execute this Op in. For example, "train", "eval", "test", or "infer". To execute
+            regardless of mode, pass None. To execute in all modes except for a particular one, you can pass an argument
+            like "!infer" or "!train".
+    """
+    def __init__(self,
+                 model: Union[tf.keras.Model, torch.nn.Module],
+                 loss_name: str,
+                 mode: Union[None, str, Iterable[str]] = "train"):
+        super().__init__(inputs=loss_name, outputs=None, mode=mode)
+        self.model = model
+        self.retain_graph = False
+        self.weight_decay = isinstance(self.model, tf.keras.Model) and self.model.losses
+        if not hasattr(self.model, "loss_name"):
+            self.model.loss_name = {loss_name}
+        else:
+            self.model.loss_name.add(loss_name)
+
+    def forward(self, data: Union[Tensor, List[Tensor]], state: Dict[str, Any]):
+        if not state["warmup"]:
+            if self.weight_decay:
+                data = data + tf.reduce_sum(self.model.losses)
+            update_model(self.model, data, tape=state['tape'], retain_graph=self.retain_graph)
