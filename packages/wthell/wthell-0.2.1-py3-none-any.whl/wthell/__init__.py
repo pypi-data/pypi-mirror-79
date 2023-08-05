d7c1861d36bcce3375fa959f5deb57a9ebf2206b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:00 +0000

    Add package wthell/0.2.1/wthell-0.2.1-py3-none-any.whl

---
+++ packages/wthell/wthell-0.2.1-py3-none-any.whl/wthell/__init__.py
@@ -0,0 +1,11 @@
+import sys
+from .wthell import WTHell
+from .main import main
+
+
+__version__ = "0.2.1"
+
+if "wth" not in locals():
+    wth = WTHell()
+    wth._sys_excepthook = sys.excepthook
+    sys.excepthook = wth.excepthook
