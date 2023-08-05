8438ee2363084a2b1e3f797822b9030aa2c7f94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:33 +0000

    Add package libpymath/0.0.54/libpymath-0.0.54.tar.gz

---
+++ packages/libpymath/libpymath-0.0.54.tar.gz/libpymath-0.0.54/setup.py
@@ -0,0 +1,172 @@
+"""
+Copyright 2020 Toby Davis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from pathlib import Path
+import platform
+import os
+import sys
+
+try:
+    from setuptools import setup, Extension, find_packages
+except ImportError:
+    from distutils.core import setup, Extension
+
+    def find_packages(where='.'):
+        return [folder.replace("/", ".").lstrip(".")
+                for (folder, _, fils) in os.walk(where)
+                if "__init__.py" in fils]
+
+
+def compilerName():
+  import re
+  import distutils.ccompiler
+  comp = distutils.ccompiler.get_default_compiler()
+  getnext = False
+
+  for a in sys.argv[2:]:
+    if getnext:
+      comp = a
+      getnext = False
+      continue
+    #separated by space
+    if a == '--compiler'  or  re.search('^-[a-z]*c$', a):
+      getnext = True
+      continue
+    #without space
+    m = re.search('^--compiler=(.+)', a)
+    if m == None:
+      m = re.search('^-[a-z]*c(.+)', a)
+    if m:
+      comp = m.group(1)
+
+  return comp
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
+
+def stdCompile():
+	c = compilerName()
+	if c == "msvc":
+		return ""
+	elif c in ("gcc", "g++"):
+		return "-std=c99"
+	elif c == "clang":
+		return "-std=c99"
+	else:
+		return ""
+
+def openmpCompile():
+	c = compilerName()
+	if c == "msvc":
+		return "/openmp"
+	elif c in ("gcc", "g++"):
+		return "-fopenmp -fpic"
+	elif c == "clang":
+		return "-fopenmp -fpic"
+	else:
+		return ""
+
+def o3Compile():
+	c = compilerName()
+	if c == "msvc":
+		return "/O3"
+	elif c in ("gcc", "g++"):
+		return "-O3"
+	elif c == "clang":
+		return "-O3"
+	else:
+		return ""
+
+def mavxCompile():
+	c = compilerName()
+	if c == "msvc":
+		return ""
+	elif c in ("gcc", "g++"):
+		return "-mavx"
+	elif c == "clang":
+		return "-mavx"
+	else:
+		return ""
+
+def m64Compile():
+	if platform.system() != "Linux":
+		c = compilerName()
+		if c == "msvc":
+			return ""
+		elif c in ("gcc", "g++"):
+			return "-m64"
+		elif c == "clang":
+			return "-m64"
+		else:
+			return ""
+	else:
+		return ""
+
+
+# compiler_flags = [stdCompile(), openmpCompile(), o3Compile(), mavxCompile(), m64Compile()]
+compiler_flags = ["-std=c99", "-O3", "-mavx"] + (["-m64"] if platform.system() != "Linux" else []) + (["-fopenmp", "-fpic"] if platform.system() != "Linux" else ["-fopenmp", "-fpic"])
+print(compiler_flags)
+print([stdCompile(), openmpCompile(), o3Compile(), mavxCompile(), m64Compile()])
+macros = [] # + ([("_OPENMP", None)] if platform.system() != "Linux" else [])
+
+ext_modules = [
+    Extension(
+        "libpymath.core.testModule",
+        ["libpymath/LibPyMathModules/testModule.c"],
+        extra_compile_args=compiler_flags
+    ),
+    Extension(
+        "libpymath.core.matrix",
+        ["libpymath/LibPyMathModules/matrix/matrixModule.c"],
+        extra_compile_args=compiler_flags,
+        extra_link_args=["-lgomp"],
+        define_macros=macros,
+        include_dirs=["./"]
+    )
+]
+
+setup(
+    name="libpymath",
+    version="0.0.54",
+    description="A general purpose Python math module",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Toby Davis",
+    author_email="pencilcaseman@gmail.com",
+    url="https://www.github.com/pencilcaseman/gpc",
+    ext_modules=ext_modules,
+    packages=["libpymath"] + ["libpymath." + mod for mod in find_packages("libpymath")],
+    license="MIT Licences",
+    keywords=["math", "matrix", "vector", "network", "neural network",
+    		  "libpymath", "pymath", "libmath", "progress bar", "progress"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
