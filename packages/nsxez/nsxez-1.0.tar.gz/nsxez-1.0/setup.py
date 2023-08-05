beffa5c793d1a2549c2c3bafa2fc71054b0d9d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:27 +0000

    Add package nsxez/1.0/nsxez-1.0.tar.gz

---
+++ packages/nsxez/nsxez-1.0.tar.gz/nsxez-1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='nsxez',  
+     version='1.0',
+     author="Francois Prowse",
+     author_email="nsxez@prowsehouse.com",
+     description="A API abstraction package for NSX-T 3.0 and greater",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/farsonic/nsxez",
+     packages=["nsxez"],
+     install_requires=[            # I get to this in a second
+          'requests',
+          'uuid',
+      ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
