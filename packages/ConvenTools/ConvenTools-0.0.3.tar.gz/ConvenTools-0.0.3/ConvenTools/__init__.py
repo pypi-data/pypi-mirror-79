5f1cf6a489cc97a7afb5fee11a2b4c424f66d953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:07 +0000

    Add package ConvenTools/0.0.3/ConvenTools-0.0.3.tar.gz

---
+++ packages/ConvenTools/ConvenTools-0.0.3.tar.gz/ConvenTools-0.0.3/ConvenTools/__init__.py
@@ -0,0 +1,12 @@
+# from openpyxl.compat.numbers import NUMPY, PANDAS
+# from openpyxl.xml import DEFUSEDXML, LXML
+# from openpyxl.workbook import Workbook
+# from openpyxl.reader.excel import load_workbook as open
+# from openpyxl.reader.excel import load_workbook
+# import openpyxl._constants as constants
+
+from ConvenTools import utils
+from ConvenTools import template
+from ConvenTools import generator
+from ConvenTools import common
+
