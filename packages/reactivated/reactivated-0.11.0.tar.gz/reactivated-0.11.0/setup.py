b8eaa826b5c6f66ce7e3416c0c499f0d3f529482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:52 +0000

    Add package reactivated/0.11.0/reactivated-0.11.0.tar.gz

---
+++ packages/reactivated/reactivated-0.11.0.tar.gz/reactivated-0.11.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="reactivated",
+    version="0.11.0",
+    author="Silvio Gutierrez",
+    author_email="silviogutierrez@gmail.com",
+    description="A statically typed framework to create Django sites with a React frontend.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/silviogutierrez/django-react",
+    packages=setuptools.find_packages(),
+    package_data={"reactivated": ["py.typed", "templates/**/*"]},
+    scripts=[],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
