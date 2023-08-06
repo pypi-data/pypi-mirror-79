1d94b98fe0396a774decf4de13dc53f8dbec41d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:51 +0000

    Add package servergrimoire/1.0.7/servergrimoire-1.0.7.tar.gz

---
+++ packages/servergrimoire/servergrimoire-1.0.7.tar.gz/servergrimoire-1.0.7/servergrimoire/configmanager.py
@@ -0,0 +1,80 @@
+import json
+import os
+from pathlib import Path
+
+
+class ConfigManager:
+    def __init__(self, path):
+        from servergrimoire.print_stuff import PrintColor
+        self.config = {}
+        if path is None:
+            path = Path.home() / ".servergrimoire_config"
+        self.path = path
+
+        if not os.path.exists(path):
+            self.__create_default__()
+        else:
+            with open(path) as data_file:
+                self.config = json.load(data_file)
+        self.__preset__()
+
+        self.__write_config__()
+        self.l = PrintColor(self)
+
+    def __preset__(self):
+        from servergrimoire.print_stuff import PrintColor
+        l = PrintColor(self)
+        l.debug(self.data_path)
+        self.config["data_path"] = f'{self.config["data_path"]}'
+        l.debug(self.colors)
+
+    def __write_config__(self):
+        with open(Path(self.path), "w") as outfile:
+            self.__preset__()
+            self.config["data_path"] = f'{self.config["data_path"]}'
+            json.dump(self.config, outfile)
+
+    def __create_default__(self) -> dict:
+        self.__write_config__()
+        return self.config
+
+    @property
+    def data_path(self):
+        return self.config["data_path"]
+
+    @data_path.setter
+    def data_path(self, var):
+        self.config["data_path"] = var
+
+    @data_path.getter
+    def data_path(self):
+        if self.config.get("data_path", None) is None:
+            self.config["data_path"] = Path.home() / ".servergrimoire_data"
+        return self.config["data_path"]
+
+    @property
+    def colors(self):
+        return self.config['colors']
+
+    @colors.setter
+    def colors(self, var: dict):
+        if self.config.get("colors", None) is None:
+            self.config["colors"] = {}
+        self.config["colors"]["info_colo"] = var.get("info_colo", "\033[94m")
+        self.config["colors"]["debug_color"] = var.get("debug_color", "\033[92m")
+        self.config["colors"]["warning_color"] = var.get("warning_color", "\033[93m")
+        self.config["colors"]["warning_color"] = var.get("fail_color", "\033[91m")
+        self.config["colors"]["end_color"] = var.get("end_color", "\033[0m")
+        self.config["colors"] = var
+
+    @colors.getter
+    def colors(self):
+        var = self.config.get("colors", dict())
+        if self.config.get("colors", None) is None:
+            self.config["colors"] = {}
+        self.config["colors"]["info_colo"] = var.get("info_colo", "\033[94m")
+        self.config["colors"]["debug_color"] = var.get("debug_color", "\033[92m")
+        self.config["colors"]["warning_color"] = var.get("warning_color", "\033[93m")
+        self.config["colors"]["warning_color"] = var.get("fail_color", "\033[91m")
+        self.config["colors"]["end_color"] = var.get("end_color", "\033[0m")
+        return self.config["colors"]
