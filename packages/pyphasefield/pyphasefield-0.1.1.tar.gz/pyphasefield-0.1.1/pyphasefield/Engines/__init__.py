d79f9ab4cea506695dadb9bc91b1c7173d4d86ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:33 +0000

    Add package pyphasefield/0.1.1/pyphasefield-0.1.1.tar.gz

---
+++ packages/pyphasefield/pyphasefield-0.1.1.tar.gz/pyphasefield-0.1.1/pyphasefield/Engines/__init__.py
@@ -0,0 +1,7 @@
+from .Diffusion import init_Diffusion
+from .DiffusionGPU import init_DiffusionGPU
+from .CahnAllen import init_CahnAllen
+from .CahnHilliard import init_CahnHilliard
+from .Warren1995 import Warren1995, init_Warren1995
+from .NComponent import init_NComponent
+from .NCGPU import init_NCGPU
---
+++ packages/pyphasefield/pyphasefield-0.1.1.tar.gz/pyphasefield-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyphasefield",
+    version="0.1.1",
+    author="Scott Peters",
+    author_email="scott@dpeters.net",
+    description="Python phase field simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AdditiveModeling/pyphasefield",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	install_requires=[
+        "numpy",
+		"scipy",
+		"matplotlib",
+		"meshio",
+    ],
+    python_requires='>=3.6',
+)
