6d0c97ea510cf6f808bf9342f296a70751929a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:30 +0000

    Add package aws-cdk.aws-sdb/1.63.0/aws-cdk.aws-sdb-1.63.0.tar.gz

---
+++ packages/aws-cdk.aws-sdb/aws-cdk.aws-sdb-1.63.0.tar.gz/aws-cdk.aws-sdb-1.63.0/src/aws_cdk/aws_sdb/__init__.py
@@ -0,0 +1,152 @@
+"""
+## Amazon SimpleDB Construct Library
+
+<!--BEGIN STABILITY BANNER-->---
+
+
+![cfn-resources: Stable](https://img.shields.io/badge/cfn--resources-stable-success.svg?style=for-the-badge)
+
+> All classes with the `Cfn` prefix in this module ([CFN Resources](https://docs.aws.amazon.com/cdk/latest/guide/constructs.html#constructs_lib)) are always stable and safe to use.
+
+---
+<!--END STABILITY BANNER-->
+
+This module is part of the [AWS Cloud Development Kit](https://github.com/aws/aws-cdk) project.
+"""
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+from ._jsii import *
+
+import aws_cdk.core
+
+
+@jsii.implements(aws_cdk.core.IInspectable)
+class CfnDomain(
+    aws_cdk.core.CfnResource,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@aws-cdk/aws-sdb.CfnDomain",
+):
+    """A CloudFormation ``AWS::SDB::Domain``.
+
+    see
+    :see: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-simpledb.html
+    cloudformationResource:
+    :cloudformationResource:: AWS::SDB::Domain
+    """
+
+    def __init__(
+        self,
+        scope: aws_cdk.core.Construct,
+        id: str,
+        *,
+        description: typing.Optional[str] = None,
+    ) -> None:
+        """Create a new ``AWS::SDB::Domain``.
+
+        :param scope: - scope in which this resource is defined.
+        :param id: - scoped id of the resource.
+        :param description: ``AWS::SDB::Domain.Description``.
+        """
+        props = CfnDomainProps(description=description)
+
+        jsii.create(CfnDomain, self, [scope, id, props])
+
+    @jsii.member(jsii_name="inspect")
+    def inspect(self, inspector: aws_cdk.core.TreeInspector) -> None:
+        """Examines the CloudFormation resource and discloses attributes.
+
+        :param inspector: - tree inspector to collect and process attributes.
+
+        stability
+        :stability: experimental
+        """
+        return jsii.invoke(self, "inspect", [inspector])
+
+    @jsii.member(jsii_name="renderProperties")
+    def _render_properties(
+        self, props: typing.Mapping[str, typing.Any]
+    ) -> typing.Mapping[str, typing.Any]:
+        """
+        :param props: -
+        """
+        return jsii.invoke(self, "renderProperties", [props])
+
+    @jsii.python.classproperty
+    @jsii.member(jsii_name="CFN_RESOURCE_TYPE_NAME")
+    def CFN_RESOURCE_TYPE_NAME(cls) -> str:
+        """The CloudFormation resource type name for this resource class."""
+        return jsii.sget(cls, "CFN_RESOURCE_TYPE_NAME")
+
+    @builtins.property
+    @jsii.member(jsii_name="cfnProperties")
+    def _cfn_properties(self) -> typing.Mapping[str, typing.Any]:
+        return jsii.get(self, "cfnProperties")
+
+    @builtins.property
+    @jsii.member(jsii_name="description")
+    def description(self) -> typing.Optional[str]:
+        """``AWS::SDB::Domain.Description``.
+
+        see
+        :see: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-simpledb.html#cfn-sdb-domain-description
+        """
+        return jsii.get(self, "description")
+
+    @description.setter
+    def description(self, value: typing.Optional[str]) -> None:
+        jsii.set(self, "description", value)
+
+
+@jsii.data_type(
+    jsii_type="@aws-cdk/aws-sdb.CfnDomainProps",
+    jsii_struct_bases=[],
+    name_mapping={"description": "description"},
+)
+class CfnDomainProps:
+    def __init__(self, *, description: typing.Optional[str] = None) -> None:
+        """Properties for defining a ``AWS::SDB::Domain``.
+
+        :param description: ``AWS::SDB::Domain.Description``.
+
+        see
+        :see: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-simpledb.html
+        """
+        self._values = {}
+        if description is not None:
+            self._values["description"] = description
+
+    @builtins.property
+    def description(self) -> typing.Optional[str]:
+        """``AWS::SDB::Domain.Description``.
+
+        see
+        :see: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-simpledb.html#cfn-sdb-domain-description
+        """
+        return self._values.get("description")
+
+    def __eq__(self, rhs) -> bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs) -> bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "CfnDomainProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "CfnDomain",
+    "CfnDomainProps",
+]
+
+publication.publish()
