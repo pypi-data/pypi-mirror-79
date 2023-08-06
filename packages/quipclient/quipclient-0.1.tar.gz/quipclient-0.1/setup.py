26e811b807fa9f3dc598fad2dd6ef53f26516b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:40 +0000

    Add package quipclient/0.1/quipclient-0.1.tar.gz

---
+++ packages/quipclient/quipclient-0.1.tar.gz/quipclient-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'quipclient',
+  packages = ['quipclient'],
+  version = '0.1',
+  license='apache-2.0',
+  description = 'Quip API Python Client',
+  author = 'David Wright',
+  author_email = 'david_v_wright@yahoo.com',
+  url = 'https://github.com/dvwright/quipclient',
+  download_url = 'https://github.com/dvwright/quipclient/archive/v0.1.tar.gz',
+  keywords = ['Quip', 'API', 'Python'],
+  #install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
