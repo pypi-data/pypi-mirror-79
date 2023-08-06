f4043d1cc089ec5ea8bc92e7159e59190bfca1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:27 +0000

    Add package clarite/1.1.1/clarite-1.1.1-py3-none-any.whl

---
+++ packages/clarite/clarite-1.1.1-py3-none-any.whl/clarite/internal/regression/__init__.py
@@ -0,0 +1,5 @@
+from .glm_regression import GLMRegression
+from .r_survey_regression import RSurveyRegression
+from .weighted_glm_regression import WeightedGLMRegression
+
+__all__ = [GLMRegression, RSurveyRegression, WeightedGLMRegression]
