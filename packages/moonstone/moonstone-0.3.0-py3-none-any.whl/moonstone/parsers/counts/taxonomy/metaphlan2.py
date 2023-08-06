260635312cd2e8db833aff75c2932ec4953e071c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:36 +0000

    Add package moonstone/0.3.0/moonstone-0.3.0-py3-none-any.whl

---
+++ packages/moonstone/moonstone-0.3.0-py3-none-any.whl/moonstone/parsers/counts/taxonomy/metaphlan2.py
@@ -0,0 +1,17 @@
+from pandas import DataFrame
+
+from moonstone.parsers.counts.taxonomy.base import TaxonomyCountsBaseParser
+
+
+class Metaphlan2Parser(TaxonomyCountsBaseParser):
+    """
+    Parse output from `Metaphlan2 <https://github.com/biobakery/MetaPhlAn/>`_ merged table.
+    """
+
+    taxa_column = 'ID'
+
+    def to_dataframe(self) -> DataFrame:
+        df = super().to_dataframe()
+        df = self.split_taxa_fill_none(df, sep="|")
+        df = df.set_index(self.taxonomical_names[:self._rank_level])
+        return df
