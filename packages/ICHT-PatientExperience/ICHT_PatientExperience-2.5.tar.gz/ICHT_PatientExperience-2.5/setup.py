5824fbf6c27e29dfd67ec9879cd633bff70a8070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:07 +0000

    Add package ICHT-PatientExperience/2.5/ICHT_PatientExperience-2.5.tar.gz

---
+++ packages/ICHT-PatientExperience/ICHT_PatientExperience-2.5.tar.gz/ICHT_PatientExperience-2.5/setup.py
@@ -0,0 +1,26 @@
+import codecs
+import os
+import sys
+try:
+	from setuptools import setup, find_packages
+except ImportError:
+	from distutils.core import setup, find_packages
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name = 'ICHT_PatientExperience',
+      description = 'A pre-trained text classification model',
+      long_description = read("README.txt"),
+      version = '2.05',
+      author = 'Jingxian You',
+      author_email = 'jingxian.you@nhs.net',
+      packages = find_packages(),
+      url = "https://github.com/ykevingrox/ICHT_PatientExperience",
+      
+      package_data={
+              'ICHT_PatientExperience.models': ['*'],
+              },
+      include_package_data=True,
+      
+      )
