930a22cb881b287720a7d4bdb9d75fad8d696086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:19 +0000

    Add package cmaps/1.0.3/cmaps-1.0.3.tar.gz

---
+++ packages/cmaps/cmaps-1.0.3.tar.gz/cmaps-1.0.3/README.rst
@@ -0,0 +1,88 @@
+cmaps
+=====
+
+Make it easier to use user defined colormaps in matplotlib. Default colormaps are from NCL_ website.
+
+.. _NCL: http://www.ncl.ucar.edu/Document/Graphics/color_table_gallery.shtml
+
+
+Users can define a environmental variable CMAP_DIR pointing to the folder containing the self-defined rgb files.
+
+
+Special thanks to Dr. Shen_: for suggestions and the help of uploading this package to Pypi and anaconda cloud.
+
+.. _Shen: https://github.com/wqshen
+
+Installation::
+
+    pip install cmaps
+
+or::
+
+    conda install -c conda-forge cmaps
+
+or::
+    
+    git clone https://github.com/hhuangwx/cmaps.git
+    cd cmaps
+    python setup.py install
+
+
+.. image:: examples/colormaps.png
+
+Usage::
+
+    import matplotlib.pyplot as plt
+    import cmaps
+    import numpy as np
+
+    x = y = np.arange(-3.0, 3.01, 0.05)
+    X, Y = np.meshgrid(x, y)
+
+    sigmax = sigmay = 1.0
+    mux = muy = sigmaxy=0.0
+
+    Xmu = X-mux
+    Ymu = Y-muy
+
+    rho = sigmaxy/(sigmax*sigmay)
+    z = Xmu**2/sigmax**2 + Ymu**2/sigmay**2 - 2*rho*Xmu*Ymu/(sigmax*sigmay)
+    denom = 2*np.pi*sigmax*sigmay*np.sqrt(1-rho**2)
+    Z = np.exp(-z/(2*(1-rho**2))) / denom
+
+    plt.pcolormesh(X,Y,Z,cmap=cmaps.WhiteBlueGreenYellowRed)
+    plt.colorbar()
+
+List the colormaps using the code in the examples::
+
+    import cmaps
+    import numpy as np
+    import inspect
+
+    import matplotlib.pyplot as plt
+    import matplotlib
+    matplotlib.rc('text', usetex=False)
+
+
+    def list_cmaps():
+        attributes = inspect.getmembers(cmaps, lambda _: not (inspect.isroutine(_)))
+        colors = [_[0] for _ in attributes if
+                  not (_[0].startswith('__') and _[0].endswith('__'))]
+        return colors
+
+
+    if __name__ == '__main__':
+        color = list_cmaps()
+
+        a = np.outer(np.arange(0, 1, 0.001), np.ones(10))
+        plt.figure(figsize=(20, 20))
+        plt.subplots_adjust(top=0.95, bottom=0.05, left=0.01, right=0.99)
+        ncmaps = len(color)
+        nrows = 8
+        for i, k in enumerate(color):
+            plt.subplot(nrows, ncmaps // nrows + 1, i + 1)
+            plt.axis('off')
+            plt.imshow(a, aspect='auto', cmap=getattr(cmaps, k), origin='lower')
+            plt.title(k, rotation=90, fontsize=10)
+            plt.title(k, fontsize=10)
+        plt.savefig('colormaps.png', dpi=300)
---
+++ packages/cmaps/cmaps-1.0.3.tar.gz/cmaps-1.0.3/setup.py
@@ -0,0 +1,86 @@
+from glob import glob
+
+from setuptools import setup
+import os
+
+VERSION = '1.0.3'
+CMAPSFILE_DIR = os.path.join('./cmaps/colormaps')
+
+
+def write_version_py(version=VERSION, filename='cmaps/_version.py'):
+    cnt = '# THIS FILE IS GENERATED FROM SETUP.PY\n' + \
+          '__version__ = "%(version)s"\n'
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': version})
+    finally:
+        a.close()
+
+
+def _listfname():
+    l = {}
+
+    l.update({'ncl': {
+        'p': 'os.path.join(CMAPSFILE_DIR, "ncar_ncl", ',
+        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'ncar_ncl/*.rgb')))}})
+    l.update({'self_defined': {
+        'p': 'os.path.join(CMAPSFILE_DIR, "self_defined", ',
+        'l': sorted(glob(os.path.join(CMAPSFILE_DIR, 'self_defined/*.rgb')))}})
+
+    return l
+
+
+def write_cmaps(template_file='./cmaps.template'):
+    with open(template_file, 'rt') as f:
+        c = f.read()
+    l = _listfname()
+    for t in l.keys():
+        for cmap_file in l[t]['l']:
+            cname = os.path.basename(cmap_file).split('.rgb')[0]
+            # start with the number will result illegal attribute
+            if cname[0].isdigit() or cname.startswith('_'):
+                cname = 'N' + cname
+            if '-' in cname:
+                cname = cname.replace('-', '_')
+            if '+' in cname:
+                cname = cname.replace('+', '_')
+            c += '    @property\n'
+            c += '    def {}(self):\n'.format(cname)
+            c += '        cname = "{}"\n'.format(cname)
+            c += '        cmap_file = {} "{}")\n'.format(
+                l[t]['p'], os.path.basename(cmap_file))
+            c += '        cmap = Colormap(self._coltbl(cmap_file), name=cname)\n'
+            c += '        matplotlib.cm.register_cmap(name=cname, cmap=cmap)\n'
+            c += '        return cmap\n\n'
+
+            c += '    @property\n'
+            c += '    def {}(self):\n'.format(cname + '_r')
+            c += '        cname = "{}"\n'.format(cname + '_r')
+            c += '        cmap_file = {} "{}")\n'.format(
+                l[t]['p'], os.path.basename(cmap_file))
+            c += '        cmap = Colormap(self._coltbl(cmap_file)[::-1], name=cname)\n'
+            c += '        matplotlib.cm.register_cmap(name=cname, cmap=cmap)\n'
+            c += '        return cmap\n\n'
+
+    cmapspy = './cmaps/cmaps.py'
+    with open(cmapspy, 'wt') as fw:
+        fw.write(c)
+
+
+write_version_py()
+write_cmaps()
+setup(
+    name='cmaps',
+    author='Hao Huang',
+    version=VERSION,
+    author_email='hhuangwx@gmail.com',
+    packages=['cmaps', ],
+    package_data={'cmaps': ['colormaps/ncar_ncl/*',
+                            'colormaps/self_defined/*'], },
+    data_files=[('', ['cmaps.template', 'LICENSE']),],
+    url='',
+    license='LICENSE',
+    description='',
+    long_description='',
+    install_requires=['matplotlib', 'numpy'],
+)
