35825c487e5c8974af937bc1ae697c2733079c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:35 +0000

    Add package vermanager/0.1.1/vermanager-0.1.1.tar.gz

---
+++ packages/vermanager/vermanager-0.1.1.tar.gz/vermanager-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='vermanager',
+    version='0.1.1',
+    author='junruoyu-zheng',
+    author_email='zhengjry@outlook.com',
+    url='https://gitee.com/junruoyu-zheng/ver-manager',
+    description=u'A manager for versions. Modules with version requirements can be written as `ModuleName(==|>=|<=)1.2.3`. The VerManager is aimed at version control. ',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['vermanager'],
+    install_requires=[],
+    entry_points={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
