8f4a0164c8e949fac5b55527d845bc9b8a6b7f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:22 +0000

    Add package wagtail-resume/1.0.6/wagtail-resume-1.0.6.tar.gz

---
+++ packages/wagtail-resume/wagtail-resume-1.0.6.tar.gz/wagtail-resume-1.0.6/wagtail_resume/views.py
@@ -0,0 +1,46 @@
+import datetime
+import logging
+
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.utils.text import slugify
+from wagtail.core.models import Page
+from weasyprint import HTML
+
+from wagtail_resume.templatetags.wagtail_resume_extras import space_to_plus
+
+logger = logging.getLogger("weasyprint")
+logger.addHandler(logging.NullHandler())
+logger.setLevel(40)  # Only show errors, use 50
+
+
+def resume_pdf(request):
+
+    page_id = request.GET.get("page_id")
+    if not page_id:
+        return HttpResponseBadRequest("Missing page id for resume generation")
+    response = HttpResponse(content_type="application/pdf")
+    resume = Page.objects.filter(id=page_id).first().specific
+
+    resume_url = resume.full_url
+    name = slugify(resume.full_name)
+    font = resume.font
+
+    response[
+        "Content-Disposition"
+    ] = "inline; filename={name}-resume-{date}.pdf".format(
+        name=name,
+        date=datetime.datetime.now().strftime("%Y-%m-%d"),
+    )
+
+    if font:
+        font = space_to_plus(font).title()
+        HTML(url=resume_url).write_pdf(
+            response,
+            stylesheets=[
+                # pylint: disable=line-too-long
+                f"https://fonts.googleapis.com/css?family={font}&display=swap"
+            ],
+        )
+    else:
+        HTML(url=resume_url).write_pdf(response)
+    return response
