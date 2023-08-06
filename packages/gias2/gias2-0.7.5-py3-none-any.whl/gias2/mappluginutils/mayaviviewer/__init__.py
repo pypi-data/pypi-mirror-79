1fd15f2f7b6e82197df9cecf54b7ef5568a8d740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:31 +0000

    Add package gias2/0.7.5/gias2-0.7.5-py3-none-any.whl

---
+++ packages/gias2/gias2-0.7.5-py3-none-any.whl/gias2/mappluginutils/mayaviviewer/__init__.py
@@ -0,0 +1,19 @@
+"""
+===============================================================================
+This file is part of GIAS2. (https://bitbucket.org/jangle/gias2)
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+===============================================================================
+"""
+
+# import os, sys
+
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# if current_dir not in sys.path:
+#     # Using __file__ will not work if py2exe is used,
+#     # Possible problem of OSX10.6 also.
+#     sys.path.insert(0, current_dir)
+
+# import class that derives itself from the step mountpoint.
