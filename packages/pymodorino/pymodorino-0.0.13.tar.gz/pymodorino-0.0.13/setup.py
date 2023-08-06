6cec21ecfbddd2a04f8331e40e84aed52acc52e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:08 +0000

    Add package pymodorino/0.0.13/pymodorino-0.0.13.tar.gz

---
+++ packages/pymodorino/pymodorino-0.0.13.tar.gz/pymodorino-0.0.13/setup.py
@@ -0,0 +1,35 @@
+import io
+import os
+import sys
+
+from setuptools import Command, find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pymodorino",
+    version="0.0.13",
+    author="Dominick Vale",
+    author_email="dominickveil@gmail.com",
+    description="Very simple and cross-platform (hopefully) pomodoro timer script.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DominickVale/Pymodorino",
+    python_requires=">=3.6.0",
+    keywords = ['pomodoro timer', 'pomodoro', 'timer'],
+    package_data={"": ["*.png", "*.wav"]},
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    setup_requires=["wheel"],
+    entry_points={"console_scripts": ["pymodorino = pymodorino.pymodoro:main"]},
+    install_requires=[
+        "blessed",
+        "notify-py"
+    ],
+)
