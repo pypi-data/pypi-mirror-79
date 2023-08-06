d96da707c455379f300203322a46da839be39ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:24 +0000

    Add package semopy/2.0.0a5/semopy-2.0.0a5.tar.gz

---
+++ packages/semopy/semopy-2.0.0a5.tar.gz/semopy-2.0.0a5/semopy/__init__.py
@@ -0,0 +1,11 @@
+"""semopy: Structural Equation Modeling Optimization in Python"""
+from .regularization import create_regularization
+from .model_effects import ModelEffects
+from .model_means import ModelMeans
+from .stats import gather_statistics
+from .means import estimate_means
+from .model import Model
+from . import examples
+
+name = "semopy"
+__version__ = "2.0.0-alpha-5"
