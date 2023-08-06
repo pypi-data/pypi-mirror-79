5591e30b4acd4d5591855aa3c0e3e1e4b1b9d028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:58 +0000

    Add package VampireAPI/0.2.0/VampireAPI-0.2.0.tar.gz

---
+++ packages/VampireAPI/VampireAPI-0.2.0.tar.gz/VampireAPI-0.2.0/java/nio/file/Files.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2020 - Sebastian Ritter <bastie@users.noreply.github.com>
+# SPDX-License-Identifier: Apache-2.0
+
+'''
+Created on 01.09.2020
+
+@author: Sͬeͥbͭaͭsͤtͬian
+'''
+from builtins import staticmethod, str
+from java.nio.file.Path import Path
+from java.lang import Object
+import codecs
+
+class Files(Object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+    
+    @staticmethod
+    def readString (path : Path) -> str:
+        '''
+        Java 11 like implementation
+        see https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/nio/file/Files.html#readString(java.nio.nio.nio.file.Path) 
+        '''
+        textFile = codecs.open(path.toString(), "r", "utf-8")
+        text = textFile.read()
+        textFile.close()
+        return text
+
+    
---
+++ packages/VampireAPI/VampireAPI-0.2.0.tar.gz/VampireAPI-0.2.0/setup.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2020 - Sebastian Ritter <bastie@users.noreply.github.com>
+# SPDX-License-Identifier: Apache-2.0
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="VampireAPI", 
+    version="0.2.0", # Major = not compatible, version = compatible extends, patch = compatible bugfix
+    author="Sͬeͥbͭaͭsͤtͬian",
+    author_email="bastie@users.noreply.github.com",
+    description="Just Another Vampire Api 4 Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bastie/PythonVampire",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Java Libraries"
+    ],
+    python_requires='>=3.6',
+)
