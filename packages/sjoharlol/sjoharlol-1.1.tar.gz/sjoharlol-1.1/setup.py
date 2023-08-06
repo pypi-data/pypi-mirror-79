1a2b48474ae770e138f7177ac7dff6ecca32c1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:11 +0000

    Add package sjoharlol/1.1/sjoharlol-1.1.tar.gz

---
+++ packages/sjoharlol/sjoharlol-1.1.tar.gz/sjoharlol-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='sjoharlol',
+      packages=['./'],
+      version='1.1',
+      description='sjohar test',
+      url='https://github.com/SouravJohar/chatbots-101',
+      author='Sourav Johar',
+      author_email='johar.sourav97@gmail.com',
+      license='MIT',
+      install_requires=[
+      ],
+      zip_safe=False,
+      long_description="sjohar",
+      long_description_content_type='text/markdown'
+      )
