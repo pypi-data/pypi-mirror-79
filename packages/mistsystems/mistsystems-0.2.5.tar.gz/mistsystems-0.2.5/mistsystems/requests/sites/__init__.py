b18dd4b248eadcda56afb113161cbd30bee5ffe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:44 +0000

    Add package mistsystems/0.2.5/mistsystems-0.2.5.tar.gz

---
+++ packages/mistsystems/mistsystems-0.2.5.tar.gz/mistsystems-0.2.5/mistsystems/requests/sites/__init__.py
@@ -0,0 +1,36 @@
+"""
+A site represents a project, a deployment. For MSP, it can be 
+as small as a coffee shop or a five-star 600-room hotel. A 
+site contains a set of Maps, Wlans, Policies, Zones.
+"""
+
+from mistsystems.requests.sites import alarms
+from mistsystems.requests.sites import assetfilters
+from mistsystems.requests.sites import assets
+from mistsystems.requests.sites import beacons
+from mistsystems.requests.sites import clients
+from mistsystems.requests.sites import devices
+from mistsystems.requests.sites import guests
+from mistsystems.requests.sites import insights
+from mistsystems.requests.sites import location
+from mistsystems.requests.sites import machine_learning
+from mistsystems.requests.sites import maps
+from mistsystems.requests.sites import mxedges
+from mistsystems.requests.sites import packet_captures
+from mistsystems.requests.sites import psks
+from mistsystems.requests.sites import rogues
+from mistsystems.requests.sites import rrm
+from mistsystems.requests.sites import rssizones
+from mistsystems.requests.sites import settings
+from mistsystems.requests.sites import site
+from mistsystems.requests.sites import skyatp
+from mistsystems.requests.sites import stats
+from mistsystems.requests.sites import subscriptions
+from mistsystems.requests.sites import system_events
+from mistsystems.requests.sites import vbeacons
+from mistsystems.requests.sites import webhooks
+from mistsystems.requests.sites import wlans
+from mistsystems.requests.sites import wxrules
+from mistsystems.requests.sites import wxtags
+from mistsystems.requests.sites import wxtunnels
+from mistsystems.requests.sites import zones
