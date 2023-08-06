e11d8587c9280cc276829d85f998209a53e19091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:03 +0000

    Add package pytchat/0.2.3/pytchat-0.2.3-py3-none-any.whl

---
+++ packages/pytchat/pytchat-0.2.3-py3-none-any.whl/pytchat/util/__init__.py
@@ -0,0 +1,18 @@
+import httpx
+import json
+import datetime
+from .. import config
+
+
+def extract(url):
+    _session = httpx.Client(http2=True)
+    html = _session.get(url, headers=config.headers)
+    with open(str(datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+                  ) + 'test.json', mode='w', encoding='utf-8') as f:
+        json.dump(html.json(), f, ensure_ascii=False)
+
+
+def save(data, filename, extention):
+    with open(filename + "_" + (datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')) + extention,
+              mode='w', encoding='utf-8') as f:
+        f.writelines(data)
