f5435d99aae323702d78a011fa8522addd085798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:00 +0000

    Add package yaqd-fakes/2020.9.1/yaqd-fakes-2020.9.1.tar.gz

---
+++ packages/yaqd-fakes/yaqd-fakes-2020.9.1.tar.gz/yaqd-fakes-2020.9.1/yaqd_fakes/_fake_continuous_hardware.py
@@ -0,0 +1,38 @@
+__all__ = ["FakeContinuousHardware"]
+
+
+import asyncio
+import math
+
+from yaqd_core import ContinuousHardware
+
+
+class FakeContinuousHardware(ContinuousHardware):
+    _kind = "fake-continuous-hardware"
+
+    def __init__(self, name, config, config_filepath):
+        super().__init__(name, config, config_filepath)
+        self._velocity = config["velocity"]
+        self._units = config["units"]
+
+    def _set_position(self, position: float) -> None:
+        pass
+
+    async def update_state(self):
+        while True:
+            if math.isnan(self._state["position"]):
+                if math.isnan(self._state["destination"]):
+                    self._busy = False
+                    await self._busy_sig.wait()
+                    continue
+                self._state["position"] = self._state["destination"]
+            diff = self._state["position"] - self._state["destination"]
+            step = math.copysign(self._velocity, diff) * 0.001
+            if abs(diff) <= abs(step):  # within one step
+                self._state["position"] = self._state["destination"]
+                self._busy = False
+                await self._busy_sig.wait()
+            else:
+                self._state["position"] -= step
+                await asyncio.sleep(0.001)
+            self.logger.debug(f"position: {self._state['position']}")
