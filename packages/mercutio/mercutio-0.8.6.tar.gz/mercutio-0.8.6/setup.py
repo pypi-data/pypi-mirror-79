154579b465bcdfcfb8aba388d83c1319b604e481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:25 +0000

    Add package mercutio/0.8.6/mercutio-0.8.6.tar.gz

---
+++ packages/mercutio/mercutio-0.8.6.tar.gz/mercutio-0.8.6/setup.py
@@ -0,0 +1,34 @@
+from glob import glob
+import setuptools, re, os
+from mercutio import __author__ as AUTHOR
+from mercutio import __license__ as LICENSE
+from mercutio import __email__ as EMAIL
+from mercutio._version import __version__ as VERSION
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = re.sub(r'\!\[cropped dragon gif\]\(docs\/cropped.gif\)', '', f.read()) # substitute out the gif image
+
+with open('requirements.txt') as f:
+    REQUIRED = f.read().splitlines()
+
+setuptools.setup(
+    name="mercutio",
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    license=LICENSE,
+    description="yet another character creation engine",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/signebedi/mercutio",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIRED,
+    python_requires='>=3.6',
+)
