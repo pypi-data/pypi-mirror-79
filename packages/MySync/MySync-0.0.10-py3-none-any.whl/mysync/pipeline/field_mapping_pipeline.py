e4820f083d721a4398804dd7b725a482517becf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:00 +0000

    Add package MySync/0.0.10/MySync-0.0.10-py3-none-any.whl

---
+++ packages/MySync/MySync-0.0.10-py3-none-any.whl/mysync/pipeline/field_mapping_pipeline.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+
+def pipeline(config, rows):
+    """字段映射处理器"""
+    field_mapping = config['pipeline']['field_mapping']
+
+    if not field_mapping:
+        return
+
+    for row in rows:
+        for key, new_key in field_mapping.items():
+            value = row.pop(key)
+            row[new_key] = value
+
+
+def main():
+    config = {
+        "pipeline": {
+            "field_mapping": {
+                "key": "new_key"
+            }
+        }
+    }
+
+    data = [
+        {
+            "key": "value",
+            "age": 23
+        }
+    ]
+
+    pipeline(config, data)
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
