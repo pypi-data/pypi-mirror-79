515d23b84cd8af0a069e9e0e435307ad032cb711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:09 +0000

    Add package pynecone/0.0.7/pynecone-0.0.7.tar.gz

---
+++ packages/pynecone/pynecone-0.0.7.tar.gz/pynecone-0.0.7/pynecone/config.py
@@ -0,0 +1,27 @@
+class Config:
+
+    def __init__(self,  api_base_url, auth_url, callback_url, client_id, client_key, client_secret):
+        self.api_base_url = api_base_url
+        self.auth_url = auth_url
+        self.callback_url = callback_url
+        self.client_id = client_id
+        self.client_key = client_key
+        self.client_secret = client_secret
+
+    def get_client_id(self):
+        return self.client_id
+
+    def get_client_key(self):
+        return self.client_key
+
+    def get_client_secret(self):
+        return self.client_secret
+
+    def get_callback_url(self):
+        return self.callback_url
+
+    def get_auth_url(self):
+        return self.auth_url
+
+    def get_api_base_url(self):
+        return self.api_base_url
