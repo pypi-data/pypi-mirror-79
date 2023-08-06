37c1caa3d8ef35c0616c230c186779c6045f644d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:07 +0000

    Add package b4tf/0.0.1/b4tf-0.0.1.tar.gz

---
+++ packages/b4tf/b4tf-0.0.1.tar.gz/b4tf-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name="b4tf",
+      author="Hiroyuki Yamada",
+      version="0.0.1",
+      description="Bayes for TensorFlow",
+      install_requires=["tensorflow>=2","tensorflow_probability"],
+      packages=find_packages(),
+      classifiers = ["Development Status :: 3 - Alpha",
+                     "Intended Audience :: Developers",
+                     "Intended Audience :: Science/Research",
+                     "License :: OSI Approved :: MIT License",
+                     "Programming Language :: Python :: 3 :: Only",
+                     "Topic :: Scientific/Engineering :: Artificial Intelligence"])
