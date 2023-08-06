d509fb63f236144b24e761e23fdec91c2fe66fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:04 +0000

    Add package pbi-dataflow-reader/0.1.2/pbi-dataflow-reader-0.1.2.tar.gz

---
+++ packages/pbi-dataflow-reader/pbi-dataflow-reader-0.1.2.tar.gz/pbi-dataflow-reader-0.1.2/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="pbi-dataflow-reader",
+    version="0.1.2",
+    description="Python client library to access data from PowerBI dataflow",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/allenwux/powerbi-datasource",
+    author="Xiaochen Wu",
+    author_email="xiwu@microsoft.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["dataflow"],
+    include_package_data=True,
+    install_requires=["azure-storage-blob", "pandas"],
+    entry_points={},
+)
