d1f639617fdcf9d352a4e205fe0c67e96e61a9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:42 +0000

    Add package pyytdata/0.1.6/pyytdata-0.1.6.tar.gz

---
+++ packages/pyytdata/pyytdata-0.1.6.tar.gz/pyytdata-0.1.6/pyytdata/pyytdata.py
@@ -0,0 +1,32 @@
+"""
+Contains a class PyYtData having implementation of youtube data v3 client.
+"""
+
+import os
+import webbrowser
+import json
+
+from apiclient.discovery import build
+
+from util.vidinfo import VidInfo
+
+
+class PyYtData:
+    """
+    Class which acts as a client to the youtube data v3 API,
+    having attributes as the query parameter for API.
+    """
+
+    def __init__(self, keyword, maxlen, order="relevance", type="video"):
+        self.keyword = keyword
+        self.order = order
+        self.maxlen = maxlen
+        self.type = type
+
+    def get_videoinfo(self):
+        rslt = []
+
+        for i in range(self.maxlen):
+            vid = VidInfo(self.keyword, self.maxlen, i, self.order, self.type)
+            rslt.append(vid)
+        return rslt
