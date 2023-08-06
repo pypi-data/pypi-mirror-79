4d974f80152c6fdb172a74bfd92e3d72094005c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:51 +0000

    Add package prob-dist-ap/1.0/prob_dist_ap-1.0.tar.gz

---
+++ packages/prob-dist-ap/prob_dist_ap-1.0.tar.gz/prob_dist_ap-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_dist_ap',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_dist_ap'],
+      zip_safe=False)
