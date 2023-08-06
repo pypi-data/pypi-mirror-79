d1601a290b2e92995cd72f9bc4500d63563dd1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:50 +0000

    Add package ccrawl/1.4/ccrawl-1.4-py3-none-any.whl

---
+++ packages/ccrawl/ccrawl-1.4-py3-none-any.whl/ccrawl/ext/amoco.py
@@ -0,0 +1,51 @@
+from click import secho
+from ccrawl.formatters.amoco import id_amoco,c_type,fieldformat
+try:
+    from amoco.system.structs import (Consts,
+                                     TypeDefine,
+                                     StructDefine,UnionDefine,
+                                     StructFormatter)
+except ImportError:
+    secho("amoco package not found",fg="red")
+    def build(t,db):
+        raise NotImplementedError
+else:
+    def build(obj,db):
+        for subtype in (obj.subtypes.values() or []):
+            if subtype is None: continue
+            build(subtype,db)
+        if obj._is_typedef:
+            t = c_type(obj)
+            rn,n = fieldformat(t)
+            TypeDefine(obj.identifier, rn or n)
+            return StructDefine.All[obj.identifier]
+        if obj._is_macro:
+            v = obj.strip()
+            try:
+                v = int(v,base=0)
+            except ValueError:
+                pass
+            globals()[obj.identifier] = v
+            return v
+        x = id_amoco(obj.identifier)
+        if obj._is_enum:
+            TypeDefine(x,'i')
+            Consts.All[x] = {}.update(obj)
+        elif obj._is_struct or obj._is_union:
+            define = StructDefine
+            if obj._is_union: define = UnionDefine
+            cls = type(x,(StructFormatter,),{})
+            fmt = []
+            for t,n,c in iter(obj):
+                r = c_type(t)
+                rt,t = fieldformat(r)
+                fmt.append('{} : {} ;{}'.format(rt or t,n,c or ''))
+            fmt = '\n'.join(fmt)
+            define(fmt)(cls)
+        elif obj._is_class:
+            x = obj.as_cStruct(db)
+            x.unfold(db)
+            return build(x,db)
+        else:
+            raise NotImplementedError
+        return StructDefine.All[x]
---
+++ packages/ccrawl/ccrawl-1.4-py3-none-any.whl/ccrawl/formatters/__init__.py
@@ -0,0 +1,8 @@
+formats = ['raw', 'C', 'ctypes', 'amoco']
+
+from .raw     import  *
+from .C       import  *
+from .ctypes_ import  *
+from .amoco   import  *
+
+default = ccore_raw
