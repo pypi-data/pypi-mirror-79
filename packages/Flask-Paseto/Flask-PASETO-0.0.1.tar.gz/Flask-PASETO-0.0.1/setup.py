c85db2e0128d45e800458d5e3ed720c29f242ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:53 +0000

    Add package Flask-Paseto/0.0.1/Flask-PASETO-0.0.1.tar.gz

---
+++ packages/Flask-Paseto/Flask-PASETO-0.0.1.tar.gz/Flask-PASETO-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+import os 
+
+import flask_paseto
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(basedir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+setup(
+    name="Flask-PASETO",
+    packages=find_packages(),
+    version="0.0.1",
+    include_package_data=True,
+    description="Paseto integration for flask",
+    author="Akerele Abraham",
+    license="MIT",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    package_data={"": ["*"]},
+    test_suite="tests",
+    url="https://github.com/haibeey/FLASK-PASETO",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',    
+        'Intended Audience :: Developers',    
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3', 
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+  ],
+)
