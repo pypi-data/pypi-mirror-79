d4d9b986eed91a1ce45d779018c0266b1e55df1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:40 +0000

    Add package bbd/0.0.4/bbd-0.0.4-py3-none-any.whl

---
+++ packages/bbd/bbd-0.0.4-py3-none-any.whl/bbd/census/get_shapefile.py
@@ -0,0 +1,122 @@
+from typing import Dict, Union
+from urllib.parse import urljoin
+from pathlib import Path
+from zipfile import ZipFile
+from io import BytesIO
+import logging
+
+import us
+import requests
+
+from .geography import Geography
+from ..cache import cache_
+
+"""Maps year to congressional district number"""
+CD = {
+    2019: 116,
+    2018: 116,
+    2017: 115,
+    2016: 115,
+    2015: 114,
+    2014: 114,
+    2013: 113,
+    2012: 113,
+    2011: 112,
+}
+
+
+def shapefile_urls(fips: str, year=2019) -> Dict[str, str]:
+    """Generates urls to shapefiles associated with a given fips code
+    on the census ftp site.
+
+    Note that these urls may not work for every year.
+    Note that these urls point to a zip file.
+    """
+    try:
+        cd = CD[year]
+    except KeyError:
+        logging.warning(f"Unsupported year: {year}. Urls may be incorrect")
+        cd = "xxx"
+
+    base = f"https://www2.census.gov/geo/tiger/TIGER{year}/"
+    urls = {
+        "tract": urljoin(base, f"TRACT/tl_{year}_{fips}_tract.zip"),  # 2019
+        "cd": urljoin(base, f"CD/tl_{year}_us_cd{cd}.zip"),  # 2019
+        "county": urljoin(base, f"COUNTY/tl_{year}_us_county.zip"),
+        "state": urljoin(base, f"STATE/tl_{year}_us_state.zip"),
+        "zcta": urljoin(base, f"ZCTA5/tl_{year}_{fips}_zcta5.zip"),
+        "block": urljoin(base, f"TABBLOCK/tl_{year}_{fips}_tabblock10.zip"),  # 14-19
+        "blockgroup": urljoin(base, f"BG/tl_{year}_{fips}_bg.zip"),
+    }
+
+    return urls
+
+
+def get_shapefile(
+    geography: Geography,
+    state: Union[int, str],
+    year: int,
+    cache: bool = False,
+) -> Path:
+    """Download and extract a census shapefile for a specified geography.
+    Returns the name of the extracted directory.
+
+    Shapefiles are also available directly from the US Census Bureau:
+        https://www.census.gov/cgi-bin/geo/shapefiles/index.php
+    """
+
+    # Convert state to fips
+    if isinstance(state, int):
+        state_lookup = f"{state:02d}"  # Pad with leading zero, e.g. 8 -> "08"
+    else:
+        state_lookup = state
+
+    us_state = us.states.lookup(state_lookup)
+
+    if us_state is None:
+        raise RuntimeError(
+            f"Could not find the requested state: {state}."
+            f"Looked up: {state_lookup}."
+        )
+
+    if us_state.fips is None:
+        raise RuntimeError(f"The state of {us_state} does not contain a fips code :(")
+
+    fips = us_state.fips
+
+    # Get the shapefile URL
+    url = shapefile_urls(fips, year)[geography]
+
+    # Determine name of zip file
+    zip_name = url.split("/")[-1]  # e.g. "tl_2019_us_cd.zip"
+    dir_name = zip_name.split(".")[0]  # e.g. "tl_2019_us_cd"
+    save_to = cache_.make_path(dir_name)
+
+    # If it's okay to use the cached directory, check if it exists
+    # and return it if possible
+    if cache and cache_.has_dir(dir_name):
+        logging.debug(f"Using cached directory: {dir_name}")
+        return save_to
+
+    # Not using the cached file, download and extract
+    logging.info(
+        "Not using chached directory. "
+        "Downloading shapefile from: {url}; to: {save_to}"
+    )
+
+    r = requests.get(url, stream=True)
+    if not r.ok:
+        raise RuntimeError(f"Bad request. Status code: {r.status_code} Url: {url}")
+
+    with ZipFile(BytesIO(r.content)) as z:
+        z.extractall(save_to)
+
+    # Return path to extracted directory
+    return save_to
+
+
+if __name__ == "__main__":
+    cache_.set_working_directory(Path(__file__))
+    state = "CO"
+    year = 2019
+    name = get_shapefile(Geography.COUNTY, state, year)
---
+++ packages/bbd/bbd-0.0.4-py3-none-any.whl/bbd/gis/make_map.py
@@ -0,0 +1,201 @@
+from pathlib import Path
+from typing import Optional, Union
+
+import shapefile
+import folium
+import branca
+
+from .magic import Magic
+from .utils import get_geojson_bounds
+
+
+def make_map(
+    shapefile_path: Union[Path, str],
+    data: dict,
+    join_on: str,
+    color_by: Optional[str] = None,
+    include: Optional[Union[list, dict]] = None,
+    map_: Optional[folium.Map] = None,
+    save_to: Optional[Union[str, Path]] = None,
+):
+    """Creates a folium.features.GeoJson map object.
+    Joins map properties with the properties in `data` and shows `data` in the map popup tooltips.
+
+    :param shapefile_path: File path to shapefile. No need to include file extension.
+    :param data: dict in the form of {join_on: [values], "other property": [values]}.
+        If you want the tooltip properties to be displayed in any particular order,
+        you can use an `OrderedDict`.
+    :param join_on: The data key (header) used to join the shapefile property table with `data`.
+        This parameter must be a key in the `data` dict.
+    :param color_by: Optional. If None (default) all shapes will be the same color. Otherwise, a linear
+        colormap will be generated based on the min and max values of the data[color_by] list.
+    :param include: Optional. If None (default) all keys in the `data` table will be shown in the
+        tooltip. If `include` is a `list` of `str`, only those values will be shown in the tooltip.
+        If `include` is a `dict` of `{str:str}`, the tooltip fields are set to the 'keys' and the
+        aliases are set to the 'values'.
+    :param map_: Optional. If included, the folium.GeoJson and branca.ColorMap objects will be added to
+        the map as they are created. If not included, you can add the folium.GeoJson object manually.
+    :param save_to: Optional path to save file. If included, the map will be automatically saved to
+        the location specified by `save_to`. If you use this parameter, you don't need to pass in a `map_`
+        as one will automatically generated with default settings.
+    """
+
+    data = data.copy()
+
+    try:
+        joiner = data.pop(join_on)  # List of values to join shapefile and data on
+    except KeyError:
+        raise KeyError(
+            f"The join_on parameter '{join_on}' was not found in the data's keys: {data.keys()}"
+        )
+
+    if not all([isinstance(v, list) for v in data.values()]):
+        raise ValueError("All values in the data dict must be lists")
+
+    if not all([len(joiner) == len(v) for v in data.values()]):
+        raise ValueError("All values in the data dict must be the same length!")
+
+    # If the caller wants us to save but does not provide a map, create one
+    if save_to is not None and map_ is None:
+        map_ = folium.Map(tiles="cartodbpositron")
+
+    # If the shapefile path is a directory with a .shp file of the same name,
+    # that's okay. It is also okay to just pass in the path to the file directly.
+    #
+    # TODO should be consistent about using the working directory or absolute
+    # paths. Or, perhaps both are okay? If you pass in an absolute path, use it.
+    # If you pass in a relative path, use it relative to the working directory...?
+    # Alternatively you could just expect the user to manage the paths all on their own.
+    p = Path(shapefile_path)
+    if p.is_dir() and (p / (p.name + ".shp")).exists():
+        shapefile_path_use = p / p.name
+    else:
+        shapefile_path_use = p
+
+    # Read shapefile
+    with shapefile.Reader(str(shapefile_path_use)) as shpf:
+        # NOTE: This is a work-around until the shapefile.Reader.__geo_interface__
+        # bug is fixed... TODO add bug report number
+        geojson = {
+            "type": "FeatureCollection",
+            "bbox": shpf.bbox,
+            "features": [sfr.__geo_interface__ for sfr in shpf.iterShapeRecords()],
+        }
+
+    # Presently, can only operate on feature collections
+    if not geojson["type"] == "FeatureCollection":
+        raise AssertionError(
+            f"Shapefile {shapefile_path_use} must be a FeatureCollection, not '{geojson['type']}''"
+        )
+
+    # Add new data properties to geojson features
+    for feature in geojson["features"]:
+
+        properties = feature["properties"]
+
+        # Initialize empty property fields. All features must have
+        # the same properties.
+        [properties.update({k: None}) for k in data.keys()]
+
+        # Check if this feature has the property to join on
+        try:
+            key_property = properties[join_on]
+        except KeyError:
+            continue
+
+        # Get the index of this feature's property in the data
+        # that we are about to insert.
+        try:
+            join_index = joiner.index(key_property)
+        except ValueError:
+            continue
+
+        # Add new property data to the feature
+        [properties.update({k: data[k][join_index]}) for k in data.keys()]
+
+    # The bbox is stored as a shapefile._Array, which is not serializable
+    geojson["bbox"] = list(geojson["bbox"])
+
+    # Create style function
+    if color_by is not None:
+
+        # Remove magic numbers that represent missing data
+        color_by_values = [x for x in data[color_by] if x not in Magic.MISSING_VALUES]
+
+        colormap = branca.colormap.LinearColormap(
+            colors=["#764aed", "#fc6665"],
+            index=None,  # Will default to linear range between colors
+            vmin=min(color_by_values),
+            vmax=max(color_by_values),
+            caption=str(color_by),
+        )
+
+        if map_ is not None:
+            map_.add_child(colormap)
+
+        def style_function(feature: dict):
+            value = feature["properties"][color_by]
+
+            # Don't color missing values and don't color if
+            # the shape doesn't have the property to color by.
+            if value in Magic.MISSING_VALUES or value is None:
+                fill = "grey"
+            else:
+                fill = colormap(value)
+
+            return {
+                "fillColor": fill,
+                "color": "black",
+                "weight": 2,
+                "fillOpacity": 0.5,
+            }
+
+    else:
+        style_function = None
+
+    if isinstance(include, list):  # Display these fields as is
+        fields = include
+        aliases = include
+
+    elif isinstance(include, dict):  # Display fields as key=field, value=alias
+        fields = []
+        aliases = []
+        [(fields.append(k), aliases.append(v)) for k, v in include.items()]
+
+    elif include is None:  # Display all fields as given in the joined data
+        fields = list(data.keys())
+        aliases = fields
+
+    else:
+        raise ValueError(
+            f"The `include` parameter must be a list, dict, or None. Not: {type(include)}"
+        )
+
+    # Create GeoJson map object
+    geojson_map = folium.GeoJson(
+        geojson,
+        name=shapefile_path_use.name,
+        style_function=style_function,
+        tooltip=folium.GeoJsonTooltip(fields=fields, aliases=aliases, localize=True),
+    )
+
+    # Add to folium.Map if that parameter was passed in
+    if map_ is not None:
+        geojson_map.add_to(map_)
+
+    # Save map_ if a save path was provided
+    if save_to is not None:
+        assert map_ is not None
+
+        # Fit bounds to the GeoJson shapes
+        map_.fit_bounds(get_geojson_bounds(geojson_map.data))
+
+        # Ensure path exists
+        # TODO save_to path handling should be consistent with how shapefile_path
+        # is handled with respect to the working directory.
+        Path(save_to).parent.mkdir(exist_ok=True, parents=True)
+
+        # Save
+        map_.save(str(save_to))
+
+    return geojson_map
