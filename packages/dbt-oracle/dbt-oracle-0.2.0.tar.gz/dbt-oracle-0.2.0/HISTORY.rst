95180dd923d083bfc352f317224b13b3cbbc6a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:11 +0000

    Add package dbt-oracle/0.2.0/dbt-oracle-0.2.0.tar.gz

---
+++ packages/dbt-oracle/dbt-oracle-0.2.0.tar.gz/dbt-oracle-0.2.0/HISTORY.rst
@@ -0,0 +1,13 @@
+=======
+History
+=======
+
+0.1.0 (2020-06-02)
+------------------
+
+* First release on PyPI.
+
+0.2.0 (2020-06-02)
+------------------
+
+* Added full macro implementation. Thanks Fabrice Etanchaud
---
+++ packages/dbt-oracle/dbt-oracle-0.2.0.tar.gz/dbt-oracle-0.2.0/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+        'dbt-core==0.17.2',
+        'cx_Oracle==7.3.0'
+]
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author="Indicium Tech",
+    author_email='vitor.avancini@indicium.tech',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="An Oracle DBT Adapater",
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='dbt-oracle',
+    name='dbt-oracle',
+    packages=find_packages(),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/techindicium/dbt-oracle',
+    version='0.2.0',
+    zip_safe=False,
+    package_data={
+        'dbt': [
+            'include/oracle/dbt_project.yml',
+            'include/oracle/macros/*.sql',
+            'include/oracle/macros/**/**/*.sql'
+        ]
+    }
+)
