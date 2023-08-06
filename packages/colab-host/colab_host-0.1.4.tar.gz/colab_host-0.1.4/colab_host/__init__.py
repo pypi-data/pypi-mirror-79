c4efa55b4311c1f721af738d195a472f398d2837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:23 +0000

    Add package colab-host/0.1.4/colab_host-0.1.4.tar.gz

---
+++ packages/colab-host/colab_host-0.1.4.tar.gz/colab_host-0.1.4/colab_host/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for ColabHost."""
+
+__author__ = """Puneetha Pai"""
+__email__ = "puneethapai29@gmail.com"
+__version__ = "0.1.4"
+
+from .colab_host import Host, SimpleHttpServer, JupyterNotebook
