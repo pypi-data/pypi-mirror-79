234fa9e2bf4d3d4906c3eaf62798884168d0cdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:58 +0000

    Add package cm-text-sdk-python/1.0.1/CM_text_sdk_python-1.0.1.tar.gz

---
+++ packages/cm-text-sdk-python/CM_text_sdk_python-1.0.1.tar.gz/CM_text_sdk_python-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CM_text_sdk_python", # Replace with your own username
+    version="1.0.1",
+    author="Joris Pennings",
+    author_email="joris.pennings@cm.com",
+    description="Python SDK for text with CM.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cmdotcom/text-sdk-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
