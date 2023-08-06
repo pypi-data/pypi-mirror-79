5777cdf308990d9cb937f4f4661999cd2905b787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:20 +0000

    Add package jupyter-firefly-extensions/2.0.1/jupyter_firefly_extensions-2.0.1-py3-none-any.whl

---
+++ packages/jupyter-firefly-extensions/jupyter_firefly_extensions-2.0.1-py3-none-any.whl/jupyter_firefly_extensions/_version.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Trey Roby.
+# Distributed under the terms of the Modified BSD License.
+
+
+version_info = (2, 0, 1)
+__version__ = ".".join(map(str, version_info))
+version_short = ".".join(map(str,version_info[0:3]))
