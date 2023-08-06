30cf3171f241f9cfe6e13fdd11e59927d97bc1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:04 +0000

    Add package prob-udacitybatu/0.1/prob_udacitybatu-0.1.tar.gz

---
+++ packages/prob-udacitybatu/prob_udacitybatu-0.1.tar.gz/prob_udacitybatu-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_udacitybatu',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_udacitybatu'],
+      author = 'Batuhan Panavur',
+      author_email = 'bpanavuraj@gmail.com',
+      zip_safe=False)
