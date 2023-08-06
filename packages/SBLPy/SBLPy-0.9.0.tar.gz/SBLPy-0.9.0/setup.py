8d70bfd0afd0b79bc80ffe750e7a43d0023d4ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:16 +0000

    Add package SBLPy/0.9.0/SBLPy-0.9.0.tar.gz

---
+++ packages/SBLPy/SBLPy-0.9.0.tar.gz/SBLPy-0.9.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SBLPy',
+    version='0.9.0',
+    packages=['sblpy'],
+    url='',
+    license='MIT',
+    author='EEKIM10',
+    author_email='eek@clicksminuteper.net',
+    description='SBLP\'s HTTP system, wrapped into python'
+)
