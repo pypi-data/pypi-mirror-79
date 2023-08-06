bd0548ac61fb96e40d58684c6d2220fa7aee42ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:16 +0000

    Add package workos/0.6.0/workos-0.6.0.tar.gz

---
+++ packages/workos/workos-0.6.0.tar.gz/workos-0.6.0/workos/resources/sso.py
@@ -0,0 +1,19 @@
+from workos.resources.base import WorkOSBaseResource
+
+
+class WorkOSProfile(WorkOSBaseResource):
+    """Representation of a User Profile as returned by WorkOS through the SSO feature.
+
+    Attributes:
+        OBJECT_FIELDS (list): List of fields a WorkOSProfile is comprised of.
+    """
+
+    OBJECT_FIELDS = [
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+        "connection_type",
+        "idp_id",
+        "raw_attributes",
+    ]
