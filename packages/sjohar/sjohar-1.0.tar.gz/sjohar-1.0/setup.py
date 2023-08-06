491cd8208009d772a690dad6f7f21398f25756ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:10 +0000

    Add package sjohar/1.0/sjohar-1.0.tar.gz

---
+++ packages/sjohar/sjohar-1.0.tar.gz/sjohar-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='sjohar',
+      packages=['./'],
+      version='1.0',
+      description='sjohar test',
+      url='https://github.com/SouravJohar/gangsta',
+      author='Sourav Johar',
+      author_email='johar.sourav97@gmail.com',
+      license='MIT',
+      install_requires=[
+      ],
+      zip_safe=False,
+      long_description="sjohar",
+      long_description_content_type='text/markdown'
+      )
