df0ce3448f00a809c412596853c4c6c7fdb91e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:49 +0000

    Add package coleo/0.1.8/coleo-0.1.8-py3-none-any.whl

---
+++ packages/coleo/coleo-0.1.8-py3-none-any.whl/coleo/__init__.py
@@ -0,0 +1,4 @@
+from ptera import ABSENT, ConflictError, default, tag, tooled
+
+from .cli import ArgsExpander, Argument, auto_cli, catalogue, setvars
+from .config import ConfigFile, config, register_extension
