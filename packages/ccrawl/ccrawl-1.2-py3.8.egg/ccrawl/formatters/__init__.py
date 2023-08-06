304e2f01fc68ca3ec5f15c125269634d0f703882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:51 +0000

    Add package ccrawl/1.2/ccrawl-1.2-py3.8.egg

---
+++ packages/ccrawl/ccrawl-1.2-py3.8.egg/ccrawl/formatters/__init__.py
@@ -0,0 +1,6 @@
+formats = ['raw', 'C', 'ctypes', 'amoco']
+
+from .raw     import  *
+from .C       import  *
+from .ctypes_ import  *
+from .amoco   import  *
---
+++ packages/ccrawl/ccrawl-1.2-py3.8.egg/ccrawl/formatters/raw.py
@@ -0,0 +1,12 @@
+import pprint
+
+__all__ = ['ccore_raw']
+
+# default raw formatter:
+#------------------------------------------------------------------------------
+
+def ccore_raw(obj,db,recursive):
+    return '{}:\n{}'.format(obj.identifier,pprint.pformat(obj))
+
+default = ccore_raw
+
