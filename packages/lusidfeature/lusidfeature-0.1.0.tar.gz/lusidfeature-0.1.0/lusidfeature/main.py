cc307fb6f67f1ec99f1f04d160d5dc7d7201741d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:49 +0000

    Add package lusidfeature/0.1.0/lusidfeature-0.1.0.tar.gz

---
+++ packages/lusidfeature/lusidfeature-0.1.0.tar.gz/lusidfeature-0.1.0/lusidfeature/main.py
@@ -0,0 +1,11 @@
+import sys
+
+from lusidfeature.entrypoint import extract_features_to_file
+
+
+def main(argv):
+    extract_features_to_file(argv)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
