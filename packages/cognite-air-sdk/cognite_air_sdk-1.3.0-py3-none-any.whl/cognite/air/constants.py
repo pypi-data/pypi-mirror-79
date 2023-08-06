3361114c3feda75d0361e06473b42a45d2d82909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:36 +0000

    Add package cognite-air-sdk/1.3.0/cognite_air_sdk-1.3.0-py3-none-any.whl

---
+++ packages/cognite-air-sdk/cognite_air_sdk-1.3.0-py3-none-any.whl/cognite/air/constants.py
@@ -0,0 +1,27 @@
+# Model- and schedule asset constants:
+SA_EXT_ID = "schedule_asset_ext_id"
+SA_FIELD_META_DATA = "data"
+MA_FIELD_META_FIELDS = "fields"
+MA_FIELD_META_MODELVERSION = "modelVersion"
+
+# Constants related to CDF Event and Time Series resources:
+EVENT_EXT_ID = "external_id"
+EVENT_TYPE = "type"
+EVENT_SUBTYPE = "subtype"
+EVENT_ASSET_IDS = "asset_ids"
+EVENT_DATA_SET_ID = "data_set_id"
+EVENT_DATA_SET_IDS = "data_set_ids"
+EVENT_METADATA = "metadata"
+AIR_EVENTS_FIELD_TYPE = "AIR"
+AIR_EVENTS_FIELD_SUBTYPE = "model_output"
+AIR_EVENTS_META_KEY_MODEL = "model"
+AIR_EVENTS_META_KEY_MODEL_VERSION = "model_version"
+AIR_EVENTS_META_KEY_SA_EXT_ID = SA_EXT_ID
+
+AIR_TS_FIELD_DATASET = "data_set_id"
+AIR_TS_FIELD_ASSET_ID = "asset_id"
+AIR_TS_FIELD_METADATA = "metadata"
+AIR_TS_META_KEY_MODEL_VERSION = "model_version"
+
+AIR_META_BACKFILL_COMPLETE = "backfill_complete"
+AIR_META_BACKFILLED_UNTIL = "backfilled_until"
