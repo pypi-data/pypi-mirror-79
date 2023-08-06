57a23cb82a0190232d7d276d992bec9498aa7f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:15 +0000

    Add package ezapi-tmdb/0.6.0/ezapi_tmdb-0.6.0.tar.gz

---
+++ packages/ezapi-tmdb/ezapi_tmdb-0.6.0.tar.gz/ezapi_tmdb-0.6.0/setup.py
@@ -0,0 +1,36 @@
+from os import path
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="ezapi_tmdb",
+    keywords="TMDb",
+    version="0.6.0",
+    packages=find_packages(),
+    description="An Python wrapper for TMDb API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zehengl/ezapi-tmdb",
+    author="Zeheng Li",
+    author_email="imzehengl@gmail.com",
+    maintainer="Zeheng Li",
+    maintainer_email="imzehengl@gmail.com",
+    license="MIT",
+    install_requires=["requests", "wrapt"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    test_suite="tests",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
