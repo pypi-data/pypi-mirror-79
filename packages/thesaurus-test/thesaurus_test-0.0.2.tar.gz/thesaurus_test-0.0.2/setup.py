c0ceda25a44f6faa05390ab7debe0163b562886f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:44 +0000

    Add package thesaurus-test/0.0.2/thesaurus_test-0.0.2.tar.gz

---
+++ packages/thesaurus-test/thesaurus_test-0.0.2.tar.gz/thesaurus_test-0.0.2/setup.py
@@ -0,0 +1,44 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="thesaurus_test",
+    version="0.0.2",
+    url="https://github.com/gniu25/package",
+    license='MIT',
+
+    author="RCDC",
+    author_email="guiran.niu@nih.gov",
+
+    description="A package to help analysts extract key terms",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
---
+++ packages/thesaurus-test/thesaurus_test-0.0.2.tar.gz/thesaurus_test-0.0.2/thesaurus_test/core.py
@@ -0,0 +1,10 @@
+import logging
+
+class MyClass(object):
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("package initialized")
+    def get_5(self):
+        return 5
+    def hello():
+        return "Hello"
