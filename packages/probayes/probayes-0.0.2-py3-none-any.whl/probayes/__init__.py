f667599d53023ef76b191261ce8b2ff1eca944d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:17 +0000

    Add package probayes/0.0.2/probayes-0.0.2-py3-none-any.whl

---
+++ packages/probayes/probayes-0.0.2-py3-none-any.whl/probayes/__init__.py
@@ -0,0 +1,9 @@
+from probayes.domain import Domain
+from probayes.prob import Prob
+from probayes.rv import RV
+from probayes.rj import RJ
+from probayes.rf import RF
+from probayes.sp import SP
+from probayes.manifold import Manifold
+from probayes.dist import Dist
+from probayes.dist_utils import product, summate, iterdict
