4f42dddddf750bd9b235f0314602d01bc36d04ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:15 +0000

    Add package gnssrefl0/0.0.10/gnssrefl0-0.0.10.tar.gz

---
+++ packages/gnssrefl0/gnssrefl0-0.0.10.tar.gz/gnssrefl0-0.0.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["numpy","wget","scipy","matplotlib","requests"]
+
+setup(
+    name="gnssrefl0",
+    version="0.0.10",
+    author="Kristine Larson",
+    author_email="kristinem.larson@gmail.com",
+    description="A GNSS reflectometry software package ",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kristinemlarson/gnssrefl0/",
+    packages=find_packages(),
+    entry_points ={ 
+        'console_scripts': [ 
+            'gnssir = gnssrefl0.gnssir_cl:main',
+            'rinex2snr = gnssrefl0.rinex2snr_cl:main',
+            'quickLook= gnssrefl0.quickLook_cl:main',
+            'download_rinex = gnssrefl0.download_rinex:main',
+            'make_json_input = gnssrefl0.make_json_input:main',
+            ], 
+        },
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
