cc856c1b1a19f914b3dd6596c998a8c04008dfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:04 +0000

    Add package p2py/0.22/p2py-0.22.tar.gz

---
+++ packages/p2py/p2py-0.22.tar.gz/p2py-0.22/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+  name = 'p2py',
+  packages = ['p2py'],
+  version = '0.22',
+  author = 'Adam Szokalski',
+  author_email = 'aszokalski@ipds.team',
+  url = 'https://github.com/ipds/p2py',
+  download_url = 'https://github.com/ipds/p2py/archive/0.1.tar.gz',
+)
