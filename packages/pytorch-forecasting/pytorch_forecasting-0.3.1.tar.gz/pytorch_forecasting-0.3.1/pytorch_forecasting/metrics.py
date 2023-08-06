7373ce98d52184eb6821681346dbd61c89616f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:27 +0000

    Add package pytorch-forecasting/0.3.1/pytorch_forecasting-0.3.1.tar.gz

---
+++ packages/pytorch-forecasting/pytorch_forecasting-0.3.1.tar.gz/pytorch_forecasting-0.3.1/pytorch_forecasting/metrics.py
@@ -0,0 +1,278 @@
+"""
+Implementation of metrics for (mulit-horizon) timeseries forecasting.
+"""
+import abc
+from typing import Dict, List, Union
+
+from pytorch_lightning.metrics import TensorMetric
+import scipy.stats
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.nn.utils import rnn
+
+
+class Metric(TensorMetric):
+    """
+    Base metric class that has basic functions that can handle predicting quantiles and operate in log space
+
+    Other metrics should inherit from this base class
+    """
+
+    def __init__(self, name: str = None, quantiles: List[float] = [0.5], reduction="mean"):
+        """
+        Initialize metric
+
+        Args:
+            name (str): metric name. Defaults to class name.
+            quantiles (List[float], optional): quantiles for probability range. Defaults to [0.5].
+            reduction (str, optional): Reduction, "none", "mean" or "sqrt-mean". Defaults to "mean".
+        """
+        self.quantiles = quantiles
+        self.reduction = reduction
+        if name is None:
+            name = self.__class__.__name__
+        super().__init__(name)
+
+    def forward(self, y_pred: torch.Tensor, y_actual: torch.Tensor) -> torch.Tensor:
+        """
+        Abstract method that calcualtes metric
+
+        Should be overriden in derived classes
+
+        Args:
+            y_pred: network output
+            y_actual: actual values
+
+        Returns:
+            torch.Tensor: metric value on which backpropagation can be applied
+        """
+        raise NotImplementedError()
+
+    def to_prediction(self, y_pred: torch.Tensor) -> torch.Tensor:
+        """
+        Convert network prediction into a point prediction.
+
+        Args:
+            y_pred: prediction output of network
+
+        Returns:
+            torch.Tensor: point prediction
+        """
+        if y_pred.ndim == 3:
+            idx = self.quantiles.index(0.5)
+            y_pred = y_pred[..., idx]
+        return y_pred
+
+    def to_quantiles(self, y_pred: torch.Tensor) -> torch.Tensor:
+        """
+        Convert network prediction into a quantile prediction.
+
+        Args:
+            y_pred: prediction output of network
+
+        Returns:
+            torch.Tensor: prediction quantiles
+        """
+        if y_pred.ndim == 2:
+            y_pred = y_pred.unsqueeze(-1)
+        return y_pred
+
+
+class MultiHorizonMetric(Metric):
+    """
+    Abstract class for defining metric for a multihorizon forecast
+    """
+
+    @abc.abstractmethod
+    def loss(self, y_pred: Dict[str, torch.Tensor], target: torch.Tensor) -> torch.Tensor:
+        """
+        Calculate loss without reduction. Override in derived classes
+
+        Args:
+            y_pred: network output
+            y_actual: actual values
+
+        Returns:
+            torch.Tensor: loss/metric as a single number for backpropagation
+        """
+        raise NotImplementedError()
+
+    def forward(self, y_pred: Dict[str, torch.Tensor], target: Union[torch.Tensor, rnn.PackedSequence]) -> torch.Tensor:
+        """
+        Forward method of metric that handles masking of values.
+
+        Do not override this method but :py:meth:`~loss` instead
+
+        Args:
+            y_pred (Dict[str, torch.Tensor]): network output
+            target (Union[torch.Tensor, rnn.PackedSequence]): actual values
+
+        Returns:
+            torch.Tensor: loss as a single number for backpropagation
+        """
+        # unpack
+        if isinstance(target, rnn.PackedSequence):
+            target, lengths = rnn.pad_packed_sequence(target, batch_first=True)
+            # batch sizes reside on the CPU by default -> we need to bring them to GPU
+            lengths = lengths.to(target.device)
+        else:
+            lengths = torch.ones(target.size(0), device=target.device, dtype=torch.long) * target.size(1)
+        assert not target.requires_grad
+
+        # calculate loss with "none" reduction
+        if target.ndim == 3:
+            weight = target[..., 1]
+            target = target[..., 0]
+        else:
+            weight = None
+
+        losses = self.loss(y_pred, target)
+        # weight samples
+        if weight is not None:
+            losses = losses * weight.unsqueeze(-1)
+
+        # mask loss
+        mask = torch.arange(target.size(1), device=target.device).unsqueeze(0) >= lengths.unsqueeze(-1)
+        if losses.ndim > 2:
+            mask = mask.unsqueeze(-1)
+            dim_normalizer = losses.size(-1)
+        else:
+            dim_normalizer = 1.0
+        # reduce to one number
+        if self.reduction == "none":
+            loss = losses.masked_fill(mask, float("nan"))
+        else:
+            if self.reduction == "mean":
+                losses = losses.masked_fill(mask, 0.0)
+                loss = losses.sum() / lengths.sum() / dim_normalizer
+            elif self.reduction == "sqrt-mean":
+                losses = losses.masked_fill(mask, 0.0)
+                loss = losses.sum() / lengths.sum() / dim_normalizer
+                loss = loss.sqrt()
+            assert not torch.isnan(loss), (
+                "Loss should not be nan - i.e. something went wrong "
+                "in calculating the loss (e.g. log of a negative number)"
+            )
+            assert torch.isfinite(
+                loss
+            ), "Loss should not be infinite - i.e. something went wrong (e.g. input is not in log space)"
+        return loss
+
+
+class PoissonLoss(MultiHorizonMetric):
+    """
+    Poisson loss for count data
+    """
+
+    def __init__(self, name: str = "poisson_loss", *args, **kwargs):
+        return super().__init__(name, *args, **kwargs)
+
+    def loss(self, y_pred: Dict[str, torch.Tensor], target: torch.Tensor) -> torch.Tensor:
+        return F.poisson_nll_loss(
+            super().to_prediction(y_pred), target, log_input=True, full=False, eps=1e-6, reduction="none"
+        )
+
+    def to_prediction(self, out: Dict[str, torch.Tensor]):
+        rate = torch.exp(super().to_prediction(out))
+        return rate
+
+    def to_quantiles(self, out: Dict[str, torch.Tensor], quantiles=None):
+        if quantiles is None:
+            quantiles = self.quantiles
+        predictions = super().to_prediction(out)
+        return torch.stack([torch.tensor(scipy.stats.poisson(predictions).ppf(q)) for q in quantiles], dim=-1)
+
+
+class QuantileLoss(MultiHorizonMetric):
+    """
+    Quantile loss, i.e. a quantile of ``q=0.5`` will give half of the mean absolute error as it is calcualted as
+
+    Defined as ``max(q * (y-y_pred), (1-q) * (y_pred-y))``
+    """
+
+    def __init__(
+        self,
+        name: str = "quantile_loss",
+        quantiles: List[float] = [0.02, 0.1, 0.25, 0.5, 0.75, 0.9, 0.98],
+        *args,
+        **kwargs
+    ):
+        """
+        Quantile loss
+
+        Args:
+            name: name of metric
+        """
+        super().__init__(name, quantiles=quantiles, *args, **kwargs)
+
+    def loss(self, y_pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        # calculate quantile loss
+        losses = []
+        for i, q in enumerate(self.quantiles):
+            errors = target - y_pred[..., i]
+            losses.append(torch.max((q - 1) * errors, q * errors).unsqueeze(-1))
+        losses = torch.cat(losses, dim=2)
+
+        return losses
+
+
+class SMAPE(MultiHorizonMetric):
+    """
+    Symmetric mean average percentage. Assumes ``y >= 0``.
+
+    Defined as ``2*(y - y_pred).abs() / (y.abs() + y_pred.abs())``
+    """
+
+    def __init__(self, name: str = "sMAPE", *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
+
+    def loss(self, y_pred, target):
+        y_pred = self.to_prediction(y_pred)
+        loss = 2 * (y_pred - target).abs() / (y_pred.abs() + target.abs() + 1e-8)
+        return loss
+
+
+class MAPE(MultiHorizonMetric):
+    """
+    Mean average percentage. Assumes ``y >= 0``.
+
+    Defined as ``(y - y_pred).abs() / y.abs()``
+    """
+
+    def __init__(self, name: str = "MAPE", *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
+
+    def loss(self, y_pred, target):
+        loss = (self.to_prediction(y_pred) - target).abs() / (target.abs() + 1e-8)
+        return loss
+
+
+class MAE(MultiHorizonMetric):
+    """
+    Mean average absolute error.
+
+    Defined as ``(y_pred - target).abs()``
+    """
+
+    def __init__(self, name: str = "MAE", *args, **kwargs):
+        super().__init__(name, *args, **kwargs)
+
+    def loss(self, y_pred, target):
+        loss = (self.to_prediction(y_pred) - target).abs()
+        return loss
+
+
+class RMSE(MultiHorizonMetric):
+    """
+    Root mean square error
+
+    Defined as ``(y_pred - target)**2``
+    """
+
+    def __init__(self, name: str = "RMSE", reduction="sqrt-mean", *args, **kwargs):
+        super().__init__(name, *args, reduction=reduction, **kwargs)
+
+    def loss(self, y_pred: Dict[str, torch.Tensor], target):
+        loss = torch.pow(self.to_prediction(y_pred) - target, 2)
+        return loss
---
+++ packages/pytorch-forecasting/pytorch_forecasting-0.3.1.tar.gz/pytorch_forecasting-0.3.1/pytorch_forecasting/models/nbeats/__init__.py
@@ -0,0 +1,289 @@
+"""
+N-Beats model for timeseries forecasting without covariates.
+"""
+from typing import Dict, List
+
+import matplotlib.pyplot as plt
+import torch
+from torch import nn
+
+from pytorch_forecasting.data import TimeSeriesDataSet
+from pytorch_forecasting.metrics import SMAPE
+from pytorch_forecasting.models.base_model import BaseModel
+from pytorch_forecasting.models.nbeats.sub_modules import NBEATSGenericBlock, NBEATSSeasonalBlock, NBEATSTrendBlock
+
+
+class NBeats(BaseModel):
+    def __init__(
+        self,
+        stack_types: List[str] = ["trend", "seasonality"],
+        num_blocks=[3, 3],
+        num_block_layers=[3, 3],
+        widths=[16, 128, 16],
+        sharing: List[int] = [True, True],
+        expansion_coefficient_lengths: List[int] = [5, 7],
+        prediction_length: int = 1,
+        context_length: int = 1,
+        dropout: float = 0.1,
+        learning_rate: float = 1e-2,
+        log_interval: int = -1,
+        log_gradient_flow: bool = False,
+        log_val_interval: int = None,
+        weight_decay: float = 1e-3,
+        loss=SMAPE(),
+        reduce_on_plateau_patience: int = 1000,
+        **kwargs,
+    ):
+        """
+        Initialize NBeats Model - use its :py:meth:`~from_dataset` method if possible.
+
+        Based on the article
+        `N-BEATS: Neural basis expansion analysis for interpretable time series
+        forecasting <http://arxiv.org/abs/1905.10437>`_.
+
+        Args:
+            stack_types: One of the following values: “generic”, “seasonality" or “trend". A list of strings
+                of length 1 or ‘num_stacks’. Default and recommended value
+                for generic mode: [“generic”] Recommended value for interpretable mode: [“trend”,”seasonality”]
+            num_blocks: The number of blocks per stack. A list of ints of length 1 or ‘num_stacks’.
+                Default and recommended value for generic mode: [1] Recommended value for interpretable mode: [3]
+            num_block_layers: Number of fully connected layers with ReLu activation per block. A list of ints of length
+                1 or ‘num_stacks’.
+                Default and recommended value for generic mode: [4] Recommended value for interpretable mode: [4]
+            width: Widths of the fully connected layers with ReLu activation in the blocks.
+                A list of ints of length 1 or ‘num_stacks’. Default and recommended value for generic mode: [512]
+                Recommended value for interpretable mode: [256, 2048]
+            sharing: Whether the weights are shared with the other blocks per stack.
+                A list of ints of length 1 or ‘num_stacks’. Default and recommended value for generic mode: [False]
+                Recommended value for interpretable mode: [True]
+            expansion_coefficient_length: If the type is “G” (generic), then the length of the expansion
+                coefficient.
+                If type is “T” (trend), then it corresponds to the degree of the polynomial. If the type is “S”
+                (seasonal) then this is the minimum period allowed, e.g. 2 for changes every timestep.
+                A list of ints of length 1 or ‘num_stacks’. Default value for generic mode: [32] Recommended value for
+                interpretable mode: [3]
+            prediction_length: Length of the prediction. Also known as 'horizon'.
+            context_length: Number of time units that condition the predictions. Also known as 'lookback period'.
+                Should be between 1-10 times the prediction length.
+            has_backcast: Only the last block of the network doesn't.
+            log_gradient_flow: if to log gradient flow, this takes time and should be only done to diagnose training
+                failures
+            reduce_on_plateau_patience (int): patience after which learning rate is reduced by a factor of 10
+        """
+        self.save_hyperparameters()
+        super().__init__(**kwargs)
+        self.loss = loss
+
+        # setup stacks
+        self.net_blocks = nn.ModuleList()
+        for stack_id, stack_type in enumerate(stack_types):
+            for _ in range(num_blocks[stack_id]):
+                if stack_type == "generic":
+                    net_block = NBEATSGenericBlock(
+                        units=self.hparams.widths[stack_id],
+                        thetas_dim=self.hparams.expansion_coefficient_lengths[stack_id],
+                        num_block_layers=self.hparams.num_block_layers[stack_id],
+                        backcast_length=context_length,
+                        forecast_length=prediction_length,
+                        dropout=self.hparams.dropout,
+                    )
+                elif stack_type == "seasonality":
+                    net_block = NBEATSSeasonalBlock(
+                        units=self.hparams.widths[stack_id],
+                        num_block_layers=self.hparams.num_block_layers[stack_id],
+                        backcast_length=context_length,
+                        forecast_length=prediction_length,
+                        min_period=self.hparams.expansion_coefficient_lengths[stack_id],
+                        dropout=self.hparams.dropout,
+                    )
+                elif stack_type == "trend":
+                    net_block = NBEATSTrendBlock(
+                        units=self.hparams.widths[stack_id],
+                        thetas_dim=self.hparams.expansion_coefficient_lengths[stack_id],
+                        num_block_layers=self.hparams.num_block_layers[stack_id],
+                        backcast_length=context_length,
+                        forecast_length=prediction_length,
+                        dropout=self.hparams.dropout,
+                    )
+                else:
+                    raise ValueError(f"Unknown stack type {stack_type}")
+
+                self.net_blocks.append(net_block)
+
+    def forward(self, x: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        """
+        Pass forward of network.
+
+        Args:
+            x (Dict[str, torch.Tensor]): input from dataloader generated from
+                :py:class:`~pytorch_forecasting.data.timeseries.TimeSeriesDataSet`.
+
+        Returns:
+            Dict[str, torch.Tensor]: output of model
+        """
+        target = x["encoder_cont"][..., 0]
+
+        timesteps = self.hparams.context_length + self.hparams.prediction_length
+        generic_forecast = [torch.zeros((target.size(0), timesteps), dtype=torch.float32, device=self.device)]
+        trend_forecast = [torch.zeros((target.size(0), timesteps), dtype=torch.float32, device=self.device)]
+        seasonal_forecast = [torch.zeros((target.size(0), timesteps), dtype=torch.float32, device=self.device)]
+        forecast = torch.zeros(
+            (target.size(0), self.hparams.prediction_length), dtype=torch.float32, device=self.device
+        )
+
+        backcast = target  # initialize backcast
+        for i, block in enumerate(self.net_blocks):
+            # evaluate block
+            backcast_block, forecast_block = block(backcast)
+
+            # add for interpretation
+            full = torch.cat([backcast_block.detach(), forecast_block.detach()], dim=1)
+            if isinstance(block, NBEATSTrendBlock):
+                trend_forecast.append(full)
+            elif isinstance(block, NBEATSSeasonalBlock):
+                seasonal_forecast.append(full)
+            else:
+                generic_forecast.append(full)
+
+            # update backcast and forecast
+            backcast = (
+                backcast - backcast_block
+            )  # do not use backcast -= backcast_block as this signifies an inline operation
+            forecast = forecast + forecast_block
+
+        return dict(
+            prediction=forecast,
+            target_scale=x["target_scale"],
+            backcast=backcast,
+            trend=torch.stack(trend_forecast, dim=0).sum(0),
+            seasonality=torch.stack(seasonal_forecast, dim=0).sum(0),
+            generic=torch.stack(generic_forecast, dim=0).sum(0),
+        )
+
+    @classmethod
+    def from_dataset(cls, dataset: TimeSeriesDataSet, **kwargs):
+        """
+        Convenience function to create network from :py:class`~pytorch_forecasting.data.timeseries.TimeSeriesDataSet`.
+
+        Args:
+            dataset (TimeSeriesDataSet): dataset where sole predictor is the target.
+            **kwargs: additional arguments to be passed to ``__init__`` method.
+
+        Returns:
+            NBeats
+        """
+        new_kwargs = {"prediction_length": dataset.max_prediction_length, "context_length": dataset.max_encoder_length}
+        new_kwargs.update(kwargs)
+
+        # validate arguments
+        assert (
+            dataset.min_encoder_length == dataset.max_encoder_length
+        ), "only fixed encoder length is allowed, but min_encoder_length != max_encoder_length"
+
+        assert (
+            dataset.max_prediction_length == dataset.min_prediction_length
+        ), "only fixed prediction length is allowed, but max_prediction_length != min_prediction_length"
+
+        assert dataset.randomize_length is None, "length has to be fixed, but randomize_length is not None"
+        assert not dataset.add_relative_time_idx, "add_relative_time_idx has to be False"
+
+        assert (
+            len(dataset.flat_categoricals) == 0
+            and len(dataset.reals) == 1
+            and len(dataset.time_varying_unknown_reals) == 1
+            and dataset.time_varying_unknown_reals[0] == dataset.target
+        ), "The only variable as input should be the target which is part of time_varying_unknown_reals"
+
+        # initialize class
+        return super().from_dataset(dataset, **new_kwargs)
+
+    def step(self, x, y, batch_idx, label) -> Dict[str, torch.Tensor]:
+        """
+        Take training / validation step.
+        """
+        log, out = super().step(x, y, batch_idx=batch_idx, label=label)
+        self._log_interpretation(x, out, batch_idx=batch_idx, label=label)
+        return log, out
+
+    def _log_interpretation(self, x, out, batch_idx, label="train"):
+        """
+        Log interpretation of network predictions in tensorboard.
+        """
+        if self.log_interval(label == "train") > 0 and batch_idx % self.log_interval(label == "train") == 0:
+            fig = self.plot_interpretation(x, out, idx=0)
+            name = f"{label.capitalize()} interpretation of item 0 in "
+            if label == "train":
+                name += f"step {self.global_step}"
+            else:
+                name += f"batch {batch_idx}"
+            self.logger.experiment.add_figure(name, fig, global_step=self.global_step)
+
+    def plot_interpretation(
+        self,
+        x: Dict[str, torch.Tensor],
+        output: Dict[str, torch.Tensor],
+        idx: int,
+        ax=None,
+        plot_seasonality_and_generic_on_secondary_axis: bool = False,
+    ) -> plt.Figure:
+        """
+        Plot interpretation.
+
+        Plot two pannels: prediction and backcast vs actuals and
+        decomposition of prediction into trend, seasonality and generic forecast.
+
+        Args:
+            x (Dict[str, torch.Tensor]): network input
+            output (Dict[str, torch.Tensor]): network output
+            idx (int): index of sample for which to plot the interpretation.
+            ax (List[matplotlib axes], optional): list of two matplotlib axes onto which to plot the interpretation.
+                Defaults to None.
+            plot_seasonality_and_generic_on_secondary_axis (bool, optional): if to plot seasonality and
+                generic forecast on secondary axis in second panel. Defaults to False.
+
+        Returns:
+            plt.Figure: matplotlib figure
+        """
+        if ax is None:
+            fig, ax = plt.subplots(2, 1, figsize=(6, 8))
+        else:
+            fig = ax.get_figure()
+
+        time = torch.arange(-self.hparams.context_length, self.hparams.prediction_length)
+
+        def to_prediction(y):
+            return self.transform_output(dict(prediction=y[[idx]], target_scale=x["target_scale"][[idx]]))[0]
+
+        # plot target vs prediction
+        ax[0].plot(time, torch.cat([x["encoder_target"][idx], x["decoder_target"][idx]]), label="target")
+        ax[0].plot(
+            time,
+            torch.cat(
+                [
+                    to_prediction(x["encoder_cont"][idx, :, 0] - output["backcast"].detach()),
+                    output["prediction"][idx].detach(),
+                ],
+                dim=0,
+            ),
+            label="prediction",
+        )
+        ax[0].set_xlabel("Time")
+
+        # plot blocks
+        if plot_seasonality_and_generic_on_secondary_axis:
+            ax2 = ax[1].twinx()
+            ax2.set_ylabel("Seasonality / Generic")
+        else:
+            ax2 = ax[1]
+        for title in ["trend", "seasonality", "generic"]:
+            if title not in self.hparams.stack_types:
+                continue
+            if title == "trend":
+                ax[1].plot(time, to_prediction(output[title]), label=title.capitalize())
+            else:
+                ax2.plot(time, to_prediction(output[title]), label=title.capitalize())
+        ax[1].set_xlabel("Time")
+        ax[1].set_ylabel("Trend")
+
+        fig.legend()
+        return fig
