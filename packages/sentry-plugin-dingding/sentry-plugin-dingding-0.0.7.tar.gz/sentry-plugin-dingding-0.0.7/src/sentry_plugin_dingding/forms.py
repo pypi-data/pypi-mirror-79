6cce54c189bd7e5b810652d274ee77c7ceee51ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:04 +0000

    Add package sentry-plugin-dingding/0.0.7/sentry-plugin-dingding-0.0.7.tar.gz

---
+++ packages/sentry-plugin-dingding/sentry-plugin-dingding-0.0.7.tar.gz/sentry-plugin-dingding-0.0.7/src/sentry_plugin_dingding/forms.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+from django import forms
+CHOICES = (
+    (1, "title1"),
+    (2, "title2"),
+    (3, "title3"),
+    (4, "title4"),
+)
+print('_dingding _dingding plugin')
+class DingDingOptionsForm(forms.Form):
+    access_token = forms.CharField(
+        max_length=255,
+        help_text='DingTalk robot access_token',
+    )
+    # select = forms.TypedChoiceField(
+    #     label='携带字段',
+    #     coerce=lambda x: x == '1',
+    #     choices=CHOICES,
+    #     widget=forms.CheckboxSelectMultiple()
+    # )
