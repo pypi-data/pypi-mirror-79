8ca2e95d5d323d1960a59fb95f8ca8bbfa5deaf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:40 +0000

    Add package cirrocumulus/1.1.7.post3/cirrocumulus-1.1.7.post3.tar.gz

---
+++ packages/cirrocumulus/cirrocumulus-1.1.7.post3.tar.gz/cirrocumulus-1.1.7.post3/src/EmbeddingChart.js
@@ -0,0 +1,155 @@
+import Paper from '@material-ui/core/Paper';
+import React from 'react';
+
+
+import {connect} from 'react-redux';
+import {
+    handleBrushFilterUpdated,
+    handleCategoricalNameChange,
+    handleColorChange,
+    handleDimensionFilterUpdated,
+    handleMeasureFilterUpdated,
+    setChartOptions
+} from './actions';
+import CategoricalLegend from './CategoricalLegend';
+import ColorSchemeLegendWrapper from './ColorSchemeLegendWrapper';
+import ImageChart from './ImageChart';
+import ScatterChartThree from './ScatterChartThree';
+import {getChartSize} from './util';
+
+
+class EmbeddingChart extends React.PureComponent {
+
+    setChartRef = (element) => {
+        this.props.chartOptions.ref = element;
+    };
+
+
+    render() {
+        const {
+            traceInfo, categoricalNames, shape, nObsSelected, globalFeatureSummary, featureSummary,
+            datasetFilter, handleColorChange, handleNameChange, handleDimensionFilterUpdated, handleMeasureFilterUpdated
+        } = this.props;
+        const chartSize = getChartSize();
+        return (
+
+            <div style={{position: 'relative'}}>
+                <Paper elevation={0} style={{position: 'absolute', right: 10, zIndex: 1000}}>
+                    {traceInfo.continuous ?
+                        <ColorSchemeLegendWrapper
+                            width={140}
+                            label={true}
+                            showColorScheme={true}
+                            height={30}
+                            handleUpdate={handleMeasureFilterUpdated}
+                            datasetFilter={datasetFilter}
+                            scale={traceInfo.colorScale}
+                            featureSummary={featureSummary}
+                            globalFeatureSummary={globalFeatureSummary}
+                            nObs={shape[0]}
+                            nObsSelected={nObsSelected}
+                            name={traceInfo.name}
+                        /> :
+                        <CategoricalLegend datasetFilter={datasetFilter}
+                                           handleClick={handleDimensionFilterUpdated}
+                                           handleColorChange={handleColorChange}
+                                           handleNameChange={handleNameChange}
+                                           name={traceInfo.name}
+                                           categoricalNames={categoricalNames}
+                                           scale={traceInfo.colorScale}
+                                           maxHeight={chartSize.height - 24}
+                                           clickEnabled={true}
+                                           nObs={shape[0]}
+                                           nObsSelected={nObsSelected}
+                                           globalFeatureSummary={globalFeatureSummary}
+                                           featureSummary={featureSummary}/>}
+                </Paper>
+
+
+                {!traceInfo.isImage &&
+                <ScatterChartThree traceInfo={traceInfo}
+                                   setChartOptions={this.props.handleChartOptions}
+                                   chartOptions={this.props.chartOptions}
+                                   categoricalNames={categoricalNames}
+                                   selection={this.props.selection}
+                                   onDeselect={this.props.onDeselect}
+                                   onSelected={this.props.onSelect}
+                                   pointSize={this.props.pointSize}
+                                   markerOpacity={this.props.markerOpacity}
+                                   unselectedMarkerOpacity={this.props.unselectedMarkerOpacity}
+                                   color={traceInfo.colors}
+                                   onGallery={this.props.onGallery}
+                                   onMoreOptions={this.props.onMoreOptions}
+                                   ref={this.setChartRef}
+
+                />}
+
+
+                {traceInfo.isImage && <ImageChart
+                    setChartOptions={this.props.handleChartOptions}
+                    chartOptions={this.props.chartOptions}
+                    style={{display: 'inline-block'}}
+                    traceInfo={traceInfo}
+                    categoricalNames={categoricalNames}
+                    selection={this.props.selection}
+                    onInitialized={this.onInitialized}
+                    markerOpacity={this.props.markerOpacity}
+                    unselectedMarkerOpacity={this.props.unselectedMarkerOpacity}
+                    onDeselect={this.props.onDeselect}
+                    onSelected={this.props.onSelect}
+                    onGallery={this.props.onGallery}
+                    onMoreOptions={this.props.onMoreOptions}
+                    ref={this.setChartRef}
+                />}
+
+
+            </div>);
+
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        chartOptions: state.chartOptions,
+        categoricalNames: state.categoricalNames,
+        numberOfBins: state.numberOfBins,
+        binValues: state.binValues,
+        embeddingChartSize: state.embeddingChartSize,
+        datasetFilter: state.datasetFilter,
+        featureSummary: state.featureSummary,
+        shape: state.dataset.shape,
+        nObsSelected: state.selection.count,
+        pointSize: state.pointSize,
+        globalFeatureSummary: state.globalFeatureSummary
+    };
+};
+const mapDispatchToProps = dispatch => {
+    return {
+        handleChartOptions: (options) => {
+            dispatch(setChartOptions(options));
+        },
+        handleDimensionFilterUpdated: (e) => {
+            dispatch(handleDimensionFilterUpdated(e));
+        },
+        handleColorChange: (e) => {
+            dispatch(handleColorChange(e));
+        },
+        handleNameChange: (e) => {
+            dispatch(handleCategoricalNameChange(e));
+        },
+        handleMeasureFilterUpdated: (e) => {
+            dispatch(handleMeasureFilterUpdated(e));
+        },
+        onSelect: (e) => {
+            dispatch(handleBrushFilterUpdated(e));
+        },
+        onDeselect: (e) => {
+            dispatch(handleBrushFilterUpdated(e));
+        }
+    };
+};
+
+export default (connect(
+    mapStateToProps, mapDispatchToProps,
+)(EmbeddingChart));
+
---
+++ packages/cirrocumulus/cirrocumulus-1.1.7.post3.tar.gz/cirrocumulus-1.1.7.post3/src/EmbeddingCharts.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import {connect} from 'react-redux';
+import {getEmbeddingKey, getTraceKey, MORE_OPTIONS_DIALOG, setDialog} from './actions';
+import EmbeddingChart from './EmbeddingChart';
+import {getChartSize} from './util';
+
+const emptySet = new Set();
+
+class EmbeddingCharts extends React.PureComponent {
+
+    render() {
+        const {primaryTraceKey, embeddingData, markerOpacity, unselectedMarkerOpacity, selection} = this.props;
+        let primaryTraces = embeddingData.filter(traceInfo => getTraceKey(traceInfo) === primaryTraceKey);
+        const primaryTrace = primaryTraces.length === 1 ? primaryTraces[0] : null;
+        let userPoints = emptySet;
+        if (primaryTrace) {
+            const embedding = primaryTrace.embedding;
+            const fullName = getEmbeddingKey(embedding);
+            const chartSelection = selection != null && selection.chart != null ? selection.chart[fullName] : null;
+            userPoints = chartSelection ? chartSelection.userPoints : emptySet;
+        }
+
+        if (primaryTrace == null) {
+            const chartSize = getChartSize();
+            return <div style={{height: chartSize.height}}></div>;
+        }
+        return (<EmbeddingChart
+                markerOpacity={markerOpacity}
+                unselectedMarkerOpacity={unselectedMarkerOpacity}
+                traceInfo={primaryTrace}
+                selection={userPoints}
+                color={primaryTrace.colors}
+                onMoreOptions={this.props.handleMoreOptions}
+                onGallery={this.props.onGallery}
+            />
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        embeddingData: state.embeddingData,
+        markerOpacity: state.markerOpacity,
+        unselectedMarkerOpacity: state.unselectedMarkerOpacity,
+        selection: state.selection,
+        primaryTraceKey: state.primaryTraceKey
+    };
+};
+const mapDispatchToProps = dispatch => {
+    return {
+        handleMoreOptions: () => {
+            dispatch(setDialog(MORE_OPTIONS_DIALOG));
+        }
+    };
+};
+
+export default (connect(
+    mapStateToProps, mapDispatchToProps,
+)(EmbeddingCharts));
+
