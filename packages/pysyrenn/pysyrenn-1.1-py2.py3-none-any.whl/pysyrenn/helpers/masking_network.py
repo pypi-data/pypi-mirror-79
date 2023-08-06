69a69ab155fefa81c48eaed7f88c081f1e6c7602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:33 +0000

    Add package pysyrenn/1.1/pysyrenn-1.1-py2.py3-none-any.whl

---
+++ packages/pysyrenn/pysyrenn-1.1-py2.py3-none-any.whl/pysyrenn/helpers/masking_network.py
@@ -0,0 +1,10 @@
+"""Stub for MaskingNetwork."""
+
+class MaskingNetwork:
+    """Stub for MaskingNetwork."""
+    def __init__(self, activation_layers, value_layers):
+        """Warns that MaskingNetworks are no longer in this package."""
+        print("Patching and MaskingNetworks are no longer part of pysyrenn.")
+        print("We are working to provide a new patching-specific package.")
+        print("\nIn the meantime, please use pysyrenn==1.0")
+        raise NotImplementedError
