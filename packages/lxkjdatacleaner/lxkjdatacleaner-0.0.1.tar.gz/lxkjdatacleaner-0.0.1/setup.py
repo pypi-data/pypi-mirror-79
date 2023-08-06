019e78d9ff05ee00f144e35dcef49b98fe0ec4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:20 +0000

    Add package lxkjdatacleaner/0.0.1/lxkjdatacleaner-0.0.1.tar.gz

---
+++ packages/lxkjdatacleaner/lxkjdatacleaner-0.0.1.tar.gz/lxkjdatacleaner-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'lxkjdatacleaner',
+    version = '0.0.1',
+    keywords='dataclean',
+    description = 'a library for data clean',
+    license = 'MIT License',
+    url = 'https://github.com/qihao123/lxdatacleaner',
+    author = 'Hao Qi',
+    author_email = '1047355811@qq.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
