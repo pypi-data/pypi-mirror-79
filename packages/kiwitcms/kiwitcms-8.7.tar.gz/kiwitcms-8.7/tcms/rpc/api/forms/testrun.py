24c024e063c1cb0983a4ee56b05875ee8f45069f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:18 +0000

    Add package kiwitcms/8.7/kiwitcms-8.7.tar.gz

---
+++ packages/kiwitcms/kiwitcms-8.7.tar.gz/kiwitcms-8.7/tcms/rpc/api/forms/testrun.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from tcms.core.forms.fields import UserField
+from tcms.management.models import Build
+from tcms.rpc.api.forms import UpdateModelFormMixin, DateTimeField
+from tcms.testplans.models import TestPlan
+from tcms.testruns.forms import BaseRunForm
+from tcms.testruns.models import TestRun, TestExecution
+
+User = get_user_model()  # pylint: disable=invalid-name
+
+
+class NewForm(BaseRunForm):
+    plan = forms.ModelChoiceField(
+        queryset=TestPlan.objects.none(),
+    )
+
+    def assign_plan(self, plan_id):
+        self.fields['plan'].queryset = TestPlan.objects.filter(pk=plan_id)
+        self.populate(self.fields['plan'].queryset.first().product_id)
+
+
+class UpdateForm(UpdateModelFormMixin, forms.ModelForm):
+    class Meta:
+        model = TestRun
+        exclude = ('tag', 'cc')  # pylint: disable=modelform-uses-exclude
+
+    manager = UserField()
+    default_tester = UserField()
+    start_date = DateTimeField()
+    stop_date = DateTimeField()
+
+    def populate(self, product_id):
+        self.fields['build'].queryset = Build.objects.filter(product_id=product_id, is_active=True)
+
+
+class UpdateExecutionForm(UpdateModelFormMixin, forms.ModelForm):
+    class Meta:
+        model = TestExecution
+        fields = '__all__'
+
+    assignee = UserField()
+    tested_by = UserField()
