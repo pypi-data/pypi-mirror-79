f9271190b7d9e2cc3c7f3ef8a25b03d10335ed01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:26 +0000

    Add package djangocms-link-all/0.3.1.1/djangocms-link-all-0.3.1.1.tar.gz

---
+++ packages/djangocms-link-all/djangocms-link-all-0.3.1.1.tar.gz/djangocms-link-all-0.3.1.1/link_all/settings.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from django.conf import settings
+
+from link_all.dataclasses import LinkAllModel
+
+
+link_all_models_default = [
+    LinkAllModel(app_label='cms', model_name='Page'),
+    LinkAllModel(app_label='filer', model_name='File', url_method='url', is_show_url_in_select=False),
+]
+
+
+LINK_ALL_MODELS: List[LinkAllModel] = getattr(settings, 'LINK_ALL_MODELS_ADDITIONAL', []) + link_all_models_default
