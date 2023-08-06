3716e631644025b42f8fadc87ffdc9950abd0483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:11 +0000

    Add package abdoTheBest/0.8.5/abdoTheBest-0.8.5.tar.gz

---
+++ packages/abdoTheBest/abdoTheBest-0.8.5.tar.gz/abdoTheBest-0.8.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="abdoTheBest",
+    version="0.8.5",
+    author="Abdelrahman Mahmoud, Fahed Sabellioglu",
+    author_email="magedmahmoud@std.sehir.edu.tr, fahedshaabani@std.sehir.edu.tr",
+    description="High level interface for text applications using PyTroch RNN's.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/FahedSabellioglu/genn',
+    packages=setuptools.find_packages(),
+    install_requires  = ['torch==1.4.0','numpy', 'fasttext'],
+    license = 'MIT'
+)
