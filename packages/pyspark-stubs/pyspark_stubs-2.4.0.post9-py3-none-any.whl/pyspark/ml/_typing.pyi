0df54576c70251ef4d11b0b50be141d43377a936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:29 +0000

    Add package pyspark-stubs/2.4.0.post9/pyspark_stubs-2.4.0.post9-py3-none-any.whl

---
+++ packages/pyspark-stubs/pyspark_stubs-2.4.0.post9-py3-none-any.whl/pyspark/ml/_typing.pyi
@@ -0,0 +1,37 @@
+from typing import Any, Dict, TypeVar, Union
+from typing_extensions import Literal
+
+import pyspark.ml.param
+import pyspark.ml.base
+import pyspark.ml.util
+import pyspark.ml.wrapper
+
+ParamMap = Dict[pyspark.ml.param.Param, Any]
+PipelineStage = Union[pyspark.ml.base.Estimator, pyspark.ml.base.Transformer]
+
+T = TypeVar("T")
+P = TypeVar("P", bound=pyspark.ml.param.Params)
+M = TypeVar("M", bound=pyspark.ml.base.Transformer)
+JM = TypeVar("JM", bound=pyspark.ml.wrapper.JavaTransformer)
+
+BinaryClassificationEvaluatorMetricType = Union[
+    Literal["areaUnderROC"], Literal["areaUnderPR"]
+]
+RegressionEvaluatorMetricType = Union[
+    Literal["rmse"], Literal["mse"], Literal["r2"], Literal["mae"], Literal["var"]
+]
+MulticlassClassificationEvaluatorMetricType = Union[
+    Literal["f1"],
+    Literal["accuracy"],
+    Literal["weightedPrecision"],
+    Literal["weightedRecall"],
+    Literal["weightedTruePositiveRate"],
+    Literal["weightedFalsePositiveRate"],
+    Literal["weightedFMeasure"],
+    Literal["truePositiveRateByLabel"],
+    Literal["falsePositiveRateByLabel"],
+    Literal["precisionByLabel"],
+    Literal["recallByLabel"],
+    Literal["fMeasureByLabel"],
+]
+ClusteringEvaluatorMetricType = Union[Literal["silhouette"]]
