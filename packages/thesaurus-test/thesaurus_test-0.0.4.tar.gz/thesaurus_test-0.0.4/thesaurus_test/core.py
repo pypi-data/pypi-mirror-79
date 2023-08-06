b9805d3c92c99fd578a28bad3eb9e3244bd118d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:27 +0000

    Add package thesaurus-test/0.0.4/thesaurus_test-0.0.4.tar.gz

---
+++ packages/thesaurus-test/thesaurus_test-0.0.4.tar.gz/thesaurus_test-0.0.4/thesaurus_test/core.py
@@ -0,0 +1,11 @@
+import logging
+
+def hello():
+        return "Hello"
+class MyClass(object):
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("package initialized")
+    def get_5(self):
+        return 5
+  
