e09f0b6a8d9884d0c9d69efb2c892b9a5d82727a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:21 +0000

    Add package pyega3/3.4.0/pyega3-3.4.0.tar.gz

---
+++ packages/pyega3/pyega3-3.4.0.tar.gz/pyega3-3.4.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="pyega3",
+    description="EGA python client",
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    packages=find_packages(),
+    version = "3.4.0",
+    author="EGA team",
+    author_email="ega-helpdesk@ebi.ac.uk",
+    install_requires=["requests", "tqdm", "htsget", "psutil"],
+    keywords=["EGA", "archive"],
+    license="Apache License, Version 2.0",
+    url="https://github.com/EGA-archive/ega-download-client",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.5"
+    ],    
+    entry_points={
+            "console_scripts": [
+                "pyega3 = pyega3.pyega3:main",
+            ]
+        }
+)
