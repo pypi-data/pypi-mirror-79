8c31da5b0b3200d80574ab3233a69e75f2f23525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:09 +0000

    Add package mautrix-hangouts/0.1.2/mautrix-hangouts-0.1.2.tar.gz

---
+++ packages/mautrix-hangouts/mautrix-hangouts-0.1.2.tar.gz/mautrix-hangouts-0.1.2/mautrix_hangouts/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = 'v0.1.2'
+git_revision = '52b52d57'
+version = '0.1.2'
+linkified_version = '[0.1.2](https://github.com/tulir/mautrix-hangouts/releases/tag/v0.1.2)'
