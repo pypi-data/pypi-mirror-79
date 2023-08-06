86aecdbdb2d361251d0fb87623bd6ef39039afd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:33 +0000

    Add package car-rl/0.0.2/car-rl-0.0.2.tar.gz

---
+++ packages/car-rl/car-rl-0.0.2.tar.gz/car-rl-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'Readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='car-rl',
+      version='0.0.2',
+      description='Car game for reinforcement learning',
+      url='https://github.com/MatthiasSchinzel/CaRL',
+      author='Matthias Schinzel',
+      author_email='unused@unused.com',
+      license='MIT',
+      packages=['carl'],
+      install_requires=[
+        'opencv-python>=4',
+        'numpy>=1',
+        'pygame>=1',
+      ],
+      package_data={
+        'Tracks': ['*.png']
+      },
+      include_package_data=True,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      zip_safe=False)
