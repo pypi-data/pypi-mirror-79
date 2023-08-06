0171117c6982e107441731fb13ded6f4eb7fa763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:54 +0000

    Add package dcor-control/0.1.2/dcor_control-0.1.2.tar.gz

---
+++ packages/dcor-control/dcor_control-0.1.2.tar.gz/dcor_control-0.1.2/dcor_control/update.py
@@ -0,0 +1,40 @@
+import subprocess as sp
+import sys
+
+import click
+
+
+def package_is_editable(name):
+    """Is the package an editable install?
+
+    This only works if the package name is in sys.path somehow.
+    It is not a universal solution, but works for DCOR!
+    """
+    for path_item in sys.path:
+        if name in path_item:
+            return True
+    return False
+
+
+@click.command()
+@click.confirmation_option(
+    prompt="Are you sure you want to update your DCOR installation?")
+def update():
+    """Update all DCOR CKAN extensions"""
+    for name in [
+        "ckanext-dc_log_view",
+        "ckanext-dc_serve",
+        "ckanext-dc_view",
+        "ckanext-dcor_depot",
+        "ckanext-dcor_schemas",
+        "ckanext-dcor_theme",
+        "dcor_shared",
+        "dcor_control",
+    ]:
+        if not package_is_editable:
+            click.secho("Updating '{}'...".format(name), bold=True)
+            sp.check_output("pip install --upgrade {}".format(name),
+                            shell=True)
+        else:
+            click.secho("Not updating '{}', because it looks".format(name)
+                        + "like an editable install.", bold=True)
---
+++ packages/dcor-control/dcor_control-0.1.2.tar.gz/dcor_control-0.1.2/dcor_control/util.py
@@ -0,0 +1,33 @@
+import pathlib
+
+CKANINI = "/etc/ckan/default/ckan.ini"
+
+
+class ConfigOptionNotFoundError(BaseException):
+    pass
+
+
+def parse_config_options(ini):
+    opt_dict = {}
+    with open(ini) as fd:
+        for line in fd.readlines():
+            line = line.strip()
+            if line.startswith("#") or line.startswith("["):
+                continue
+            elif line.count("="):
+                key, value = line.split("=", 1)
+                opt_dict[key.strip()] = value.strip()
+    return opt_dict
+
+
+def get_storage_path():
+    return pathlib.Path(get_config_option("ckan.storage_path"))
+
+
+def get_config_option(option, ini=CKANINI):
+    opt_dict = parse_config_options(ini)
+    if option in opt_dict:
+        value = opt_dict[option]
+    else:
+        raise ConfigOptionNotFoundError("Could not find '{}'!".format(option))
+    return value
