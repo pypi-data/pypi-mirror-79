6d46622ce0827e3cddbe323e73a9801807aaafc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:22 +0000

    Add package PyWaves/0.8.35/PyWaves-0.8.35.tar.gz

---
+++ packages/PyWaves/PyWaves-0.8.35.tar.gz/PyWaves-0.8.35/pywaves/oracle.py
@@ -0,0 +1,39 @@
+import requests
+import pywaves as pw
+
+class Oracle(object):
+
+    def __init__(self, oracleAddress = None, seed = None, pywaves=pw):
+        self.pw = pywaves
+        if seed != None:
+            self.oracleAddress = self.pw.Address(seed=seed)
+        else:
+            self.oracleAddress = oracleAddress
+
+    def getData(self, key = None, regex = None):
+        if key == None and regex == None:
+            result = self._getDataWithoutKey()
+        elif key != None:
+            result = self._getDataWithKey(key)
+        elif regex != None:
+            result = self._getDataWithRegex(regex)
+
+        return result
+
+    def _getDataWithRegex(self, regex):
+        return requests.get(self.pw.NODE + '/addresses/data/' + self.oracleAddress + '?matches=' + regex).json()
+
+    def _getDataWithoutKey(self):
+        return requests.get(self.pw.NODE + '/addresses/data/' + self.oracleAddress).json()
+
+    def _getDataWithKey(self, key):
+        return requests.get(self.pw.NODE + '/addresses/data/' + self.oracleAddress + '/' + key).json()['value']
+
+    def storeData(self, key, type, dataEntry, minimalFee=500000):
+        dataToStore = [{
+            'type': type,
+            'key': key,
+            'value': dataEntry
+        }]
+
+        return self.oracleAddress.dataTransaction(dataToStore,minimalFee=minimalFee)
