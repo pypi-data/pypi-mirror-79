637ba134b7b1268a9f26efc5386bf03eb09763bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package HBVigor/0.1.4/HBVigor-0.1.4-py3-none-any.whl

---
+++ packages/HBVigor/HBVigor-0.1.4-py3-none-any.whl/HBVigor/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) [2020] [fengzhizi32]
+[HBVigor is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+"""
+#This script allow user to import directly the most useful functions.
+
+__version__ = "0.1.4"
+
+from HBVigor.HBVigor import (TimeDomain, FreqDomain, Poincare, Nonlinear, GetHRV)
+
