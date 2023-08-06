066a110889038fbf6611eef269d6a5256164d4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:12 +0000

    Add package mitxpro-openedx-extensions/0.2/mitxpro-openedx-extensions-0.2.tar.gz

---
+++ packages/mitxpro-openedx-extensions/mitxpro-openedx-extensions-0.2.tar.gz/mitxpro-openedx-extensions-0.2/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mitxpro_core', 'mitxpro_core.settings']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django>=1.11.20,<1.12.0']
+
+extras_require = \
+{':python_version >= "2.7" and python_version < "2.8"': ['futures==3.2.0']}
+
+entry_points = \
+{'lms.djangoapp': ['mitxpro_core = mitxpro_core.apps:MITxProCoreConfig']}
+
+setup_kwargs = {
+    'name': 'mitxpro-openedx-extensions',
+    'version': '0.2',
+    'description': 'MIT xPro plugins for Open edX',
+    'long_description': None,
+    'author': 'MIT Office of Open Learning',
+    'author_email': 'mitx-devops@mit.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5, !=2.7.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.6.*, !=3.7.*',
+}
+
+
+setup(**setup_kwargs)
