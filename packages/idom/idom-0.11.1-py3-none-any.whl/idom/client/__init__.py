1d9b00db668b3370a5c4e85e5f21c9d5d2ae8144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:00 +0000

    Add package idom/0.11.1/idom-0.11.1-py3-none-any.whl

---
+++ packages/idom/idom-0.11.1-py3-none-any.whl/idom/client/__init__.py
@@ -0,0 +1,23 @@
+from .manage import (
+    web_module_url,
+    find_path,
+    delete_web_modules,
+    register_web_module,
+    web_module_exists,
+    web_module_path,
+    install,
+    installed,
+    restore,
+)
+
+__all__ = [
+    "web_module_url",
+    "find_path",
+    "delete_web_modules",
+    "register_web_module",
+    "web_module_exists",
+    "web_module_path",
+    "install",
+    "installed",
+    "restore",
+]
