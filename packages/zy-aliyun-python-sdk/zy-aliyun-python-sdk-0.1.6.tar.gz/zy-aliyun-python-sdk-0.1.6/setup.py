280940c58db03b95378c2fc3cdb81a8bd2701b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:15 +0000

    Add package zy-aliyun-python-sdk/0.1.6/zy-aliyun-python-sdk-0.1.6.tar.gz

---
+++ packages/zy-aliyun-python-sdk/zy-aliyun-python-sdk-0.1.6.tar.gz/zy-aliyun-python-sdk-0.1.6/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zy-aliyun-python-sdk",
+    version="0.1.6",
+    author="JohnToms",
+    license='MIT',
+    author_email="johntoms@163.com",
+    description="sdk for aliyun",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johntoms/aliyun-sdk",
+    install_requires=[
+        'certifi>=2019.6.16',
+        'chardet>=3.0.4',
+        'decorator>=4.4.0',
+        'idna>=2.8',
+        'requests>=2.22.0',
+        'retry>=0.9.2',
+        'urllib3>=1.25.3',
+        'xmltodict>=0.12.0'
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
