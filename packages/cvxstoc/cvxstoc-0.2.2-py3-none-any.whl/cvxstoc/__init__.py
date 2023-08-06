f7d33accabc56f9bdc74bf25b305c91e4ae42ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:31 +0000

    Add package cvxstoc/0.2.2/cvxstoc-0.2.2-py3-none-any.whl

---
+++ packages/cvxstoc/cvxstoc-0.2.2-py3-none-any.whl/cvxstoc/__init__.py
@@ -0,0 +1,9 @@
+from cvxstoc.expectation import expectation
+from cvxstoc.random_variable import (
+    RandomVariable,
+    RandomVariableFactory,
+    NormalRandomVariable,
+    CategoricalRandomVariable,
+    UniformRandomVariable,
+)
+from cvxstoc.prob import prob, Phi
