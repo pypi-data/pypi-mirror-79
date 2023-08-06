3c129accf5dbe98a377b902e4ac0b5e82a110798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:26 +0000

    Add package openemr/0.1.2/openemr-0.1.2.tar.gz

---
+++ packages/openemr/openemr-0.1.2.tar.gz/openemr-0.1.2/openemr/__init__.py
@@ -0,0 +1,3 @@
+name = "openemr"
+__version__ = "0.1.1"
+from openemr.client import Client
---
+++ packages/openemr/openemr-0.1.2.tar.gz/openemr-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openemr",
+    version="0.1.2",
+    author="Stein van Broekhoven",
+    author_email="stein@aapjeisbaas.nl",
+    description="An OpenEMR api connector",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/aapjeisbaas/openemr-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests'
+    ]
+)
