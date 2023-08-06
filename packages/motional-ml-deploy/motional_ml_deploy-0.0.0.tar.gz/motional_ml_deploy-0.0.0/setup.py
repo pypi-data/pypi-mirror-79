743ea2e1d71388fa7a7454ac3cb79fe74946bcd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:41 +0000

    Add package motional-ml-deploy/0.0.0/motional_ml_deploy-0.0.0.tar.gz

---
+++ packages/motional-ml-deploy/motional_ml_deploy-0.0.0.tar.gz/motional_ml_deploy-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="motional_ml_deploy",
+    version="0.0.0",
+    author="Motional",
+    author_email="devops@motional.com",
+    description="motional_ml_deploy",
+    long_description="motional_ml_deploy",
+    long_description_content_type="text/markdown",
+    url="https://motional.com/",
+    packages=[],
+    python_requires='>=5',
+)
