b147cefdbb7fe1d02e2c8f1a8a4244f11ec9d2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:17 +0000

    Add package html-generators/1.2.1/html_generators-1.2.1.tar.gz

---
+++ packages/html-generators/html_generators-1.2.1.tar.gz/html_generators-1.2.1/src/html_generators/__init__.py
@@ -0,0 +1,9 @@
+'''
+Note - we recommend importing as:
+    import html_generators as h
+'''
+from .document import Document
+# Note - imports Element and VoidElement, as well as all html elements
+from .elements import *
+from .fragment import Fragment
+from .mark_safe import MarkSafe
