6259e98694735d94b8c1e74f8b9deb4299039e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:07 +0000

    Add package PyQuestion/0.1.0/PyQuestion-0.1.0.tar.gz

---
+++ packages/PyQuestion/PyQuestion-0.1.0.tar.gz/PyQuestion-0.1.0/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup,find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'PyQuestion'
+AUTHOR = 'Onkar Hanchate'
+AUTHOR_EMAIL = 'onkarhanchate14@gmail.com'
+URL = 'https://github.com/onkarhanchate14/PyQuestion'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Return a variety of Python problem/question to solve'
+LONG_DESCRIPTION = long_description
+LONG_DESC_TYPE = "text/markdown"
+
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      packages=find_packages()
+      )
