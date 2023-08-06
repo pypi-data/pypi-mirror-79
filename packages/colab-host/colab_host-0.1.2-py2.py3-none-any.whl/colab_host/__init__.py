d3efcdbfd5929adcf9d150d73a3563ebae6ce668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:12 +0000

    Add package colab-host/0.1.2/colab_host-0.1.2-py2.py3-none-any.whl

---
+++ packages/colab-host/colab_host-0.1.2-py2.py3-none-any.whl/colab_host/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for ColabHost."""
+
+__author__ = """Puneetha Pai"""
+__email__ = "puneethapai29@gmail.com"
+__version__ = "__version__ = "0.1.2"'"
+
+from .colab_host import Host, SimpleHttpServer
