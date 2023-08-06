e7db8ba40771f0425f4eae977e5e01c6ba84507b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:14 +0000

    Add package yamllint-junit/0.6/yamllint-junit-0.6.tar.gz

---
+++ packages/yamllint-junit/yamllint-junit-0.6.tar.gz/yamllint-junit-0.6/setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+from yamllint_junit_bootstrap import bootstrap
+
+version = bootstrap.version()
+
+setup(
+    name='yamllint-junit',
+    packages=['yamllint_junit_bootstrap'],
+    version=version,
+    description='yamllint to JUnit converter.',
+    author='wasil',
+    author_email='piotr.m.wasilewski@gmail.com',
+    url='https://github.com/wasilak/yamllint-junit',
+    download_url='https://github.com/wasilak/yamllint-junit/archive/%s.tar.gz' % version,
+    keywords=['yaml', 'junit'],
+    classifiers=[],
+    entry_points={
+        "console_scripts": ['yamllint-junit = yamllint_junit_bootstrap.bootstrap:main']
+    },
+    install_requires=[
+        'yamllint',
+    ],
+    tests_require=[
+        'pytest',
+        'flake8',
+        'coverage',
+        'mock',
+    ],
+)
