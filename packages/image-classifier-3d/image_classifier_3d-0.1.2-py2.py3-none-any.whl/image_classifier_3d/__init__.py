740e75f44a26323bc3998b32626fe2cace25778d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package image-classifier-3d/0.1.2/image_classifier_3d-0.1.2-py2.py3-none-any.whl

---
+++ packages/image-classifier-3d/image_classifier_3d-0.1.2-py2.py3-none-any.whl/image_classifier_3d/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Image Classifier 3D."""
+
+__author__ = "Jianxu Chen"
+__email__ = "jianxuc@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.1.2"
+
+
+def get_module_version():
+    return __version__
+
+
+# from .proj_tester import ProjectTester
+# from .proj_trainer import ProjectTrainer
