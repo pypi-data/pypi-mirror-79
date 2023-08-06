b0c3e84a7b81c4e5f3ed92623efeed46962bb5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:47 +0000

    Add package spanishconjugator/2.3.1/spanishconjugator-2.3.1.tar.gz

---
+++ packages/spanishconjugator/spanishconjugator-2.3.1.tar.gz/spanishconjugator-2.3.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='spanishconjugator',
+    version='2.3.1',
+    description='A python library to conjugate spanish words with parameters tense, mood and pronoun',
+    packages=['spanishconjugator',
+              'spanishconjugator.tenses',
+              'spanishconjugator.tenses.conditional',
+              'spanishconjugator.tenses.indicitive',
+              'spanishconjugator.tenses.imperative',
+              'spanishconjugator.tenses.subjunctive',
+              'spanishconjugator.irregulars'],
+    package_dir={'': 'src'},
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    extras_require={
+        "dev": [
+            "pytest>=6.0.1",
+            "check-manifest>=0.42",
+            "twine>=3.2.0"
+        ],
+    },
+    url="https://github.com/Benedict-Carling/spanish-conjugator",
+    author="Benedict Carling",
+    author_email="bencarling1@gmail.com"
+)
---
+++ packages/spanishconjugator/spanishconjugator-2.3.1.tar.gz/spanishconjugator-2.3.1/src/spanishconjugator/tenses/indicitive/future.py
@@ -0,0 +1,31 @@
+# -*- coding: iso-8859-15 -*-
+def indicitive_future(root_verb, pronoun):
+    if pronoun == "yo":
+        if root_verb[-2:] == "ar" or "er" or "ir":
+            conjugation = root_verb + "é"
+            return conjugation
+
+    if pronoun == "tu":
+        if root_verb[-2:] == "ar" or "er" or "ir":
+            conjugation = root_verb + "ás"
+            return conjugation
+            
+    if pronoun == "usted":
+        if root_verb[-2:] == "ar" or "er" or "ir":
+            conjugation = root_verb + "á"
+            return conjugation
+
+    if pronoun == "nosotros":
+        if root_verb[-2:] == "ar" or "er" or "ir":
+            conjugation = root_verb + "emos"
+            return conjugation
+
+    if pronoun == "vosotros":
+        if root_verb[-2:] == "ar" or "er" or "ir":
+            conjugation = root_verb + "éis"
+            return conjugation
+
+    if pronoun == "ustedes":
+        if root_verb[-2:] == "ar" or "er" or "ir":
+            conjugation = root_verb + "án"
+            return conjugation
