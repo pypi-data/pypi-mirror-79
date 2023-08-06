02ee5dbb61ba9fe67e6b5a50bfb60e0641e441dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:18 +0000

    Add package tess-waldo/1.0.5/tess-waldo-1.0.5.tar.gz

---
+++ packages/tess-waldo/tess-waldo-1.0.5.tar.gz/tess-waldo-1.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tess-waldo',
+version = '1.0.5',
+description = "A friendly package for finding targets on TESS CCDs.",
+author = 'Simon J. Murphy',
+author_email = 'simon.murphy@sydney.edu.au',
+license = 'MIT',
+install_requires = ['tess-point', 'lightkurve', 'astroquery'],
+packages = ['tess_waldo'])
