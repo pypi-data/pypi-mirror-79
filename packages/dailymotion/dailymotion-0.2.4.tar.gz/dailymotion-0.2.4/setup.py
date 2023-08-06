2a843a012a67c7cc1d821e8d9d215c0ce7e80453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:18 +0000

    Add package dailymotion/0.2.4/dailymotion-0.2.4.tar.gz

---
+++ packages/dailymotion/dailymotion-0.2.4.tar.gz/dailymotion-0.2.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os, sys
+
+setup(name='dailymotion',
+      version='0.2.4',
+      description='Dailymotion API SDK',
+      long_description='Dailymotion API SDK',
+      classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3"
+        ],
+      keywords=['dailymotion', 'api', 'sdk', 'graph'],
+      author='Samir AMZANI',
+      author_email='samir.amzani@gmail.com',
+      url='http://github.com/dailymotion/dailymotion-sdk-python',
+      license='Apache License, Version 2.0',
+      include_package_data=True,
+      zip_safe=False,
+      py_modules = ['dailymotion',],
+      install_requires=[
+          'requests',
+          'requests_toolbelt'
+      ],
+)
