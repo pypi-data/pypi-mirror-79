0d6d3046c1f5c58782574977819dce00b95e2805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:29 +0000

    Add package rlyehian/0.2/rlyehian-0.2.tar.gz

---
+++ packages/rlyehian/rlyehian-0.2.tar.gz/rlyehian-0.2/rlyehian/__init__.py
@@ -0,0 +1,3 @@
+from .ainghft import *
+
+name="rlyehian"
---
+++ packages/rlyehian/rlyehian-0.2.tar.gz/rlyehian-0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rlyehian",
+    version="0.2",
+    author="Jimmy Le (JLDevOps)",
+    author_email="jldevops@gmail.com",
+    description="Translate to the language of the \"old ones\" with the serpent's tong in the digital world.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JLDevOps/Rlyehian",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
