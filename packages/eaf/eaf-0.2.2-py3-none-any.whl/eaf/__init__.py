a83ee1773a8cbfa29d78b0eba5b3815c695bdac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:08 +0000

    Add package eaf/0.2.2/eaf-0.2.2-py3-none-any.whl

---
+++ packages/eaf/eaf-0.2.2-py3-none-any.whl/eaf/__init__.py
@@ -0,0 +1,24 @@
+"""Enterprise Application Framework.
+
+This framework contains all the pieces you need to create feature-rich
+enterprise-grade distributed and not applications.
+
+Also means Extensible As Fuck.
+"""
+
+from eaf.app import Application
+from eaf.core import Vec3
+from eaf.render import Image, Renderer, Renderable
+from eaf.state import State
+from eaf.timer import Timer
+
+
+__all__ = [
+    Application.__name__,
+    Image.__name__,
+    Renderable.__name__,
+    Renderer.__name__,
+    State.__name__,
+    Timer.__name__,
+    Vec3.__name__,
+]
