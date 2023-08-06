a60720a1a89c6ec8b0c408dc9f001873b6ba122e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:30 +0000

    Add package dialogflow-fulfillment/0.4.0/dialogflow-fulfillment-0.4.0.tar.gz

---
+++ packages/dialogflow-fulfillment/dialogflow-fulfillment-0.4.0.tar.gz/dialogflow-fulfillment-0.4.0/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+description = 'Create fulfillment webhook services for Dialogflow using Python'
+
+setup(
+    name='dialogflow-fulfillment',
+    version='0.4.0',
+    author='Gabriel Farias Caccáos',
+    author_email='gabriel.caccaos@gmail.com',
+    packages=['dialogflow_fulfillment'],
+    url='https://github.com/gcaccaos/dialogflow-fulfillment',
+    license='Apache License 2.0',
+    description=description,
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    python_requires='>=3',
+    extras_require={
+        'dev': [
+            'tox>=3.14'
+        ],
+        'docs': [
+            'sphinx>=3.1',
+            'sphinx-autobuild>=0.7',
+            'sphinx-rtd-theme>=0.5'
+        ],
+        'tests': [
+            'pytest>=5.4',
+            'coverage>=5.1',
+            'flake8>=3.8',
+            'isort>=4.3'
+        ]
+    },
+    keywords=[
+        'dialogflow',
+        'fulfillment',
+        'webhook',
+        'api',
+        'python',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
