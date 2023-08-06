3a22020b451a53c308b771eb9191617de9fc9170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:13 +0000

    Add package pyytdata/0.1.8/pyytdata-0.1.8.tar.gz

---
+++ packages/pyytdata/pyytdata-0.1.8.tar.gz/pyytdata-0.1.8/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pyytdata",
+    version="0.1.8",
+    author="Prince Roshan",
+    author_email="princekrroshan01@gmail.com",
+    url="https://github.com/princekrroshan01/youtube-api-wrapper",
+    description=("this is a simple client for youtube data api v3"),
+    long_description=read("README.rst"),
+    license="MIT",
+    py_modules=["pyytdata/pyytdata"],
+    keywords=[
+        "youtube-api" "recommend-youtube-videos",
+        "youtube-data",
+        "python",
+        "wrapper",
+        "youtube-data-api-v3",
+        "youtube-api-wrapper",
+        "youtube-api-v3",
+    ],
+    python_requires=">=3.6",
+    install_requires=["google-api-python-client"],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
