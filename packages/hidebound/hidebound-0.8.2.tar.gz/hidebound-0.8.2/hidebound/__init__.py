48849521195928cc827ce9cf757ea8a72aacbb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:38 +0000

    Add package hidebound/0.8.2/hidebound-0.8.2.tar.gz

---
+++ packages/hidebound/hidebound-0.8.2.tar.gz/hidebound-0.8.2/hidebound/__init__.py
@@ -0,0 +1,12 @@
+'''
+A local database framework for defining and validating assets.
+
+Author: Alex Braun
+Email: Alexander.G.Braun@gmail.com
+Github: https://github.com/theNewFlesh
+'''
+
+
+import hidebound.core  # noqa F401
+import hidebound.exporters  # noqa F401
+import hidebound.server  # noqa F401
