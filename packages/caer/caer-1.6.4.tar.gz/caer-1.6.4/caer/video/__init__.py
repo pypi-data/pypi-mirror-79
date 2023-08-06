6dd0bad9d12e92e85c828d8d4d73098af013142b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:06 +0000

    Add package caer/1.6.4/caer-1.6.4.tar.gz

---
+++ packages/caer/caer-1.6.4.tar.gz/caer-1.6.4/caer/video/__init__.py
@@ -0,0 +1,11 @@
+# Author: Jason Dsouza
+# Github: http://www.github.com/jasmcaus
+
+
+from .extract_frames import extract_frames
+
+from .default_videostream import DefaultVideoStream
+
+from .videostream import VideoStream
+
+from .count_frames import count_frames
