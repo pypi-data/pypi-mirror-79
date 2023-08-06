928419d28b83ea026f2f682488f00fd6963abf90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:07 +0000

    Add package MAPLEAF/0.8.6/MAPLEAF-0.8.6.tar.gz

---
+++ packages/MAPLEAF/MAPLEAF-0.8.6.tar.gz/MAPLEAF-0.8.6/MAPLEAF/ENV/__init__.py
@@ -0,0 +1,6 @@
+'''
+Environmental modelling: main class is `MAPLEAF.ENV.Environment.Environment`.
+`MAPLEAF.ENV.Environment.Environment` wraps the atmospheric models, gravity models, mean wind models, and turbulence models.
+
+.. image:: https://www.goodfreephotos.com/albums/astrophotography/sunrise-over-the-earth.jpg
+'''
