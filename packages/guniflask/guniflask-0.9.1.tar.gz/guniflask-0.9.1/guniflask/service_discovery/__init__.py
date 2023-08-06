c9160cc7fb477c717f431cf61ba3b836e72f1169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:11 +0000

    Add package guniflask/0.9.1/guniflask-0.9.1.tar.gz

---
+++ packages/guniflask/guniflask-0.9.1.tar.gz/guniflask-0.9.1/guniflask/service_discovery/__init__.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+from .config import ServiceDiscoveryConfigurer
+from .discovery_client import DiscoveryClient
+from .load_balancer_client import LoadBalancerClient
+from .load_balancer_client import LoadBalancedRequest
+from .service_instance import ServiceInstance
