8882d795269183313a67beb4f2bc3b51fb648ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:37 +0000

    Add package jobio/0.0.0a8/jobio-0.0.0a8-py3-none-any.whl

---
+++ packages/jobio/jobio-0.0.0a8-py3-none-any.whl/jobio/defaults.py
@@ -0,0 +1,14 @@
+# Package name
+PACKAGE_NAME = "jobio"
+
+JOBIO = "JOBIO"
+
+# Argument groups defaults
+EXECUTE = "EXECUTE"
+JOB = "JOB"
+STORAGE = "STORAGE"
+S3 = "S3"
+BUCKET = "BUCKET"
+
+# Job defaults
+JOB_DEFAULT_NAME = "job"
