f57104aad1288c810ae5c7e22b0ffc491b680c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:03 +0000

    Add package aurflux/1.2.90/aurflux-1.2.90.tar.gz

---
+++ packages/aurflux/aurflux-1.2.90.tar.gz/aurflux-1.2.90/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aurflux', 'aurflux.command']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aurcore', 'discord.py', 'loguru>=0.5.2,<0.6.0', 'pyyaml']
+
+setup_kwargs = {
+    'name': 'aurflux',
+    'version': '1.2.90',
+    'description': 'Aurflux!',
+    'long_description': None,
+    'author': 'Zenith',
+    'author_email': 'inbox@zenith.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
