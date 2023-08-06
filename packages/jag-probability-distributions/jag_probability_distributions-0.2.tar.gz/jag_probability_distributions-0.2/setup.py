aab7d1e7739b193e8a6226a8e8f11e0eb6b223f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:33 +0000

    Add package jag-probability-distributions/0.2/jag_probability_distributions-0.2.tar.gz

---
+++ packages/jag-probability-distributions/jag_probability_distributions-0.2.tar.gz/jag_probability_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jag_probability_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['jag_probability_distributions'],
+      author='Jeroen Goddijn',
+      author_email = 'jeroen.goddijn@gmail.com',
+      zip_safe=False)
