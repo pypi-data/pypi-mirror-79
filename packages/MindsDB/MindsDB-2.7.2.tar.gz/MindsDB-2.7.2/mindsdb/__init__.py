bd9892ce11563f77f44a81dfd8d89aa34c5d0104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:16 +0000

    Add package MindsDB/2.7.2/MindsDB-2.7.2.tar.gz

---
+++ packages/MindsDB/MindsDB-2.7.2.tar.gz/MindsDB-2.7.2/mindsdb/__init__.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from mindsdb.utilities.fs import get_or_create_dir_struct, create_directory
+from mindsdb.utilities.wizards import cli_config
+from mindsdb.utilities.config import Config
+from mindsdb.utilities.functions import args_parse
+
+config_dir, storage_dir = get_or_create_dir_struct()
+
+config_path = os.path.join(config_dir, 'config.json')
+if not os.path.exists(config_path):
+    _ = cli_config(None, None, storage_dir, config_dir, use_default=True)
+
+args = args_parse()
+if args.config is not None:
+    config_path = args.config
+
+try:
+    config = Config(config_path)
+except Exception as e:
+    print(str(e))
+    sys.exit(1)
+
+paths = config.paths
+create_directory(paths['datasources'])
+create_directory(paths['predictors'])
+create_directory(paths['static'])
+create_directory(paths['tmp'])
+
+os.environ['MINDSDB_STORAGE_PATH'] = paths['predictors']
+
+from mindsdb_native import *
+# Figure out how to add this as a module
+import lightwood
+#import dataskillet
+import mindsdb.utilities.wizards as wizards
