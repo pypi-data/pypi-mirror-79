2e738a2cf39eadb472080ab0e73172ab96e77a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:18 +0000

    Add package dummy-package-dalmo/0.0.4/dummy_package_dalmo-0.0.4.tar.gz

---
+++ packages/dummy-package-dalmo/dummy_package_dalmo-0.0.4.tar.gz/dummy_package_dalmo-0.0.4/README.md
@@ -0,0 +1,16 @@
+# README
+
+## Purpose
+
+This repo contains dummy code to test integrations between Github and
+other stuff.
+
+## CI with PyPI: organization
+
+master -> development repository
+
+release -> CI with PyPI. Only action to do is merge from master
+
+## Colab test
+
+Notebook in examples folder are linked to Colab by Google
---
+++ packages/dummy-package-dalmo/dummy_package_dalmo-0.0.4.tar.gz/dummy_package_dalmo-0.0.4/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='dummy_package_dalmo',
+    version='0.0.4',
+    author='Marco Dal Molin',
+    author_email='marco.dalmolin.1991@gmail.com',
+    description='Test package to test CI with PyPI',
+    license='Apache Software License',
+    classifiers=[
+        'Development Status :: 3 - Alpha'  # https://martin-thoma.com/software-development-stages/
+    ]
+)
