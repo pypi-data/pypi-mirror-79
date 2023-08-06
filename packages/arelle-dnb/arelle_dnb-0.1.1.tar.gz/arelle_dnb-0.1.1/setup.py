34a71bc5cf7a4301f726d6c4e51cbd7d5f3c32ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:23 +0000

    Add package arelle-dnb/0.1.1/arelle_dnb-0.1.1.tar.gz

---
+++ packages/arelle-dnb/arelle_dnb-0.1.1.tar.gz/arelle_dnb-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'arelle_dnb',
+    packages = ['arelle_dnb'],
+    description = 'our version of arelle to work from',
+    url='https://github.com/DeNederlandscheBank/arelle_dnb',
+    version='0.1.1',
+    author="De Nederlandsche Bank",
+    author_email='ECDB_berichten@dnb.nl',
+    download_url = 'https://github.com/DeNederlandscheBank/arelle_dnb/archive/master.zip',
+)
