81b2086d11cac96c212befbf44b040363d51a2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:30 +0000

    Add package botw-save-editor/1.0.3/botw-save-editor-1.0.3.tar.gz

---
+++ packages/botw-save-editor/botw-save-editor-1.0.3.tar.gz/botw-save-editor-1.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import pathlib
+
+with open("README.md", "r") as desc:
+    long_description = desc.read()
+
+setuptools.setup(
+    name="botw-save-editor",
+    version='1.0.3',
+    author="SDarkMagic",
+    author_email="TheSDarkMagic@gmail.com",
+    description="A program for converting data between JSON and .sav file formats.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SDarkMagic/botew-save-editor",
+    include_package_data=True,
+    packages=['saveEditor'],
+#    package_dir={'bmpm': 'scripts'},
+    entry_points={
+        'console_scripts': ['sav_to_json=saveEditor.sav_to_json:main', 'json_to_sav=saveEditor.json_to_sav:main']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "pathlib"
+
+    ],
+)
