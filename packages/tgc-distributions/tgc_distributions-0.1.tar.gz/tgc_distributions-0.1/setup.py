dbb5a10ab29ba3086390e5be3a03cdd5b94504ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:43 +0000

    Add package tgc-distributions/0.1/tgc_distributions-0.1.tar.gz

---
+++ packages/tgc-distributions/tgc_distributions-0.1.tar.gz/tgc_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='tgc_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['tgc_distributions'],
+      zip_safe=False)
