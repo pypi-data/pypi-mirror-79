0de1790319624b62f0075e6dda7316c427799013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:52 +0000

    Add package easy-py/0.1.1/easy-py-0.1.1.tar.gz

---
+++ packages/easy-py/easy-py-0.1.1.tar.gz/easy-py-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easy-py",
+    version="0.1.1",
+    author="Kaspar Papli",
+    author_email="kaspar.papli@gmail.com",
+    description="Python SDK for Easy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kspar/easy-py",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask~=1.1.2',
+        'requests~=2.24.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
