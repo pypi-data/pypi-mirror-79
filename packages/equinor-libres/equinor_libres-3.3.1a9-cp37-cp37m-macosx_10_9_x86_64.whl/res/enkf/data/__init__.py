e8c90b94109753fe120f6611720387847c961f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:16 +0000

    Add package equinor-libres/3.3.1a9/equinor_libres-3.3.1a9-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/equinor-libres/equinor_libres-3.3.1a9-cp37-cp37m-macosx_10_9_x86_64.whl/res/enkf/data/__init__.py
@@ -0,0 +1,15 @@
+from .custom_kw import CustomKW
+from .field import Field
+from .gen_data import GenData
+from .gen_kw import GenKw
+from .ext_param import ExtParam
+from .enkf_node import EnkfNode
+from .summary import Summary
+
+__all__ = ["Field",
+           "CustomKW",
+           "Summary",
+           "GenKw",
+           "GenData",
+           "ExtParam",
+           "EnkfNode"]
