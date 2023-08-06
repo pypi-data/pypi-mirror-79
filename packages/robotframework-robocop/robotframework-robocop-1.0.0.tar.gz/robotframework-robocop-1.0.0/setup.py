c518fb018311c7d56f621e8989c077be0b9a1e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:38 +0000

    Add package robotframework-robocop/1.0.0/robotframework-robocop-1.0.0.tar.gz

---
+++ packages/robotframework-robocop/robotframework-robocop-1.0.0.tar.gz/robotframework-robocop-1.0.0/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup
+from robocop.version import __version__
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.rst").read_text()
+CLASSIFIERS = """
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+""".strip().splitlines()
+
+setup(
+    name='robotframework-robocop',
+    version=__version__,
+    description='Linter for Robot Framework',
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/bhirsz/robotframework-robocop",
+    author="Bartlomiej Hirsz, Mateusz Nojek",
+    author_email="bartek.hirsz@gmail.com, matnojek@gmail.com",
+    license="Apache License 2.0",
+    platforms="any",
+    classifiers=CLASSIFIERS,
+    packages=['robocop'],
+    include_package_data=True,
+    install_requires=['robotframework>=3.2.1'],
+    entry_points={'console_scripts': ['robocop=robocop:run_robocop']},
+)
