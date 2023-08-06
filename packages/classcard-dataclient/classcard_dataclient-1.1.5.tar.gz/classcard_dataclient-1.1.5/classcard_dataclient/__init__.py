50343d0405b4fa4d05c9b18d66450f475d8c9371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:41 +0000

    Add package classcard-dataclient/1.1.5/classcard_dataclient-1.1.5.tar.gz

---
+++ packages/classcard-dataclient/classcard_dataclient-1.1.5.tar.gz/classcard_dataclient-1.1.5/classcard_dataclient/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created By Murray(m18527) on 2019/9/9 11:34
+"""
+
+from .utils.core import config
+from .client.action import DataClient
+
+__all__ = [
+    'DataClient',
+]
