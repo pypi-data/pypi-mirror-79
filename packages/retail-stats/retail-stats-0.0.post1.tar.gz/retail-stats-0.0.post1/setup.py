77158085ba02421cee7e882405eec12601f1a0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:52 +0000

    Add package retail-stats/0.0.post1/retail-stats-0.0.post1.tar.gz

---
+++ packages/retail-stats/retail-stats-0.0.post1.tar.gz/retail-stats-0.0.post1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="retail-stats",
+  version="0.0.post1",
+  author="Sumanas Sarma",
+  author_email="insectatorious+pypi@gmail.com",
+  keywords="price-elasticity cross-elasticity sales-analysis",
+  description="A simple way to calculate retail stats",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  install_requires=[
+    "numpy~=1.19"
+  ],
+  url="https://github.com/insectatorious/retail-stats",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+    "Development Status :: 3 - Alpha",
+    "Topic :: Office/Business"
+  ],
+  python_requires='>=3.7',
+)
