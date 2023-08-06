61f592626eef5544c1a8a79d3f47b894b5911a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:39 +0000

    Add package sbe/0.2.4/sbe-0.2.4.tar.gz

---
+++ packages/sbe/sbe-0.2.4.tar.gz/sbe-0.2.4/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="sbe",
+    version="0.2.4",
+    author="Chris Yuen",
+    author_email="chris@kizzx2.com",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/kizzx2/sbe-python",
+    install_requires=["lxml", "bitstring"],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
