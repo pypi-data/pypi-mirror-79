ce5ff1c613671d5dfacefc480e23ecbeb6fab0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:05 +0000

    Add package srupy/0.0.3/srupy-0.0.3-py3-none-any.whl

---
+++ packages/srupy/srupy-0.0.3-py3-none-any.whl/srupy/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.0.3'
+
+from srupy.response import SRUResponse
+from .app import SRUpy
---
+++ packages/srupy/srupy-0.0.3-py3-none-any.whl/srupy/sruexceptions.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+"""
+    srupy.sruexceptions
+    ~~~~~~~~~~~~~~~~~~~~
+    SRU errors.
+    :copyright: Copyright 2020 Andreas Lüschow
+"""
+
+
+class SRUError(Exception):
+    """
+    Context specific SRU errors not covered by the classes above.
+    """
+    pass
