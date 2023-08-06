03845ce0b043a23e5dc655b8c240636c82e58309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:33 +0000

    Add package unimi-common-tools/0.0.1/unimi-common-tools-0.0.1.tar.gz

---
+++ packages/unimi-common-tools/unimi-common-tools-0.0.1.tar.gz/unimi-common-tools-0.0.1/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    try:
+        requirements = [str(ir.req) for ir in reqs]
+    except:
+        requirements = [str(ir.requirement) for ir in reqs]
+    return requirements
+
+
+def long_description(fname):
+    with open(fname) as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='unimi-common-tools',
+    version='0.0.1',
+    scripts=[],
+    author="Binay Kumar Ray",
+    author_email="binayray2009@gmail.com",
+    description="UniMI Common Tools",
+    long_description=long_description('README.md'),
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=load_requirements('requirements.txt'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
