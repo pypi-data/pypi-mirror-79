424f21f7bf36fe824c62732bfd6ae84be3ff98e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:29 +0000

    Add package dist-tool/0.1/dist-tool-0.1.tar.gz

---
+++ packages/dist-tool/dist-tool-0.1.tar.gz/dist-tool-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist-tool',
+      version='0.1',
+      description='Lightweight tool for manipulating statistical distributions',
+      packages=['dist-tool'],
+      author='Vlad-Marius Griguta',
+      author_email='vlad.griguta@gmail.com',
+      zip_safe=False)
