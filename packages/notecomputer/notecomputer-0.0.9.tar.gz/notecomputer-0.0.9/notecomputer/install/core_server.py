8353b894df6e45952e4c9db9b2865ea94d348169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:41 +0000

    Add package notecomputer/0.0.9/notecomputer-0.0.9.tar.gz

---
+++ packages/notecomputer/notecomputer-0.0.9.tar.gz/notecomputer-0.0.9/notecomputer/install/core_server.py
@@ -0,0 +1,25 @@
+from .base import run_cmd
+
+config_dir = '/root/configs/'
+
+
+def install_drive():
+    from google.colab import drive
+    drive.mount('/content/gdrive/')
+
+
+def install_code_server():
+    run_cmd("curl -fsSL https://code-server.dev/install.sh | sh")
+    run_cmd("mkdir -vp /root/logs/code-server/")
+
+    run_cmd(
+        "nohup code-server --auth none --config {}code/code-server.yaml >>/root/logs/code-server/code-server.log 2>&1 &".format(
+            config_dir))
+
+
+def install_natapp():
+    run_cmd("wget http://download.natapp.cn/assets/downloads/clients/2_3_9/natapp_linux_amd64/natapp -O natapp")
+    run_cmd("chmod a+x natapp")
+
+    run_cmd("mkdir -vp /root/logs/natapp/")
+    run_cmd("nohup ./natapp -authtoken=65a40f94924dc275  >>/root/logs/natapp/natapp.log 2>&1 &")
