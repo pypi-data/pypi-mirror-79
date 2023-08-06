501b4aa921b83f303641a133ad9d7bc69c643b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:07 +0000

    Add package pytal/0.0.6/pytal-0.0.6.zip

---
+++ packages/pytal/pytal-0.0.6.zip/pytal-0.0.6/src/monitor/build/precache-manifest.432005f7bb4bafbac198ec257e978271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdf477eceb7e88f7137bfb64df8f4d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b92c9e72fa0172abb5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "62ac110c0efcef279694",
+    "url": "/static/js/2.ec486e57.chunk.js"
+  },
+  {
+    "revision": "be76c1072259f9ba78917d9856358ff1",
+    "url": "/static/js/2.ec486e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b92c9e72fa0172abb5",
+    "url": "/static/js/main.23af674e.chunk.js"
+  },
+  {
+    "revision": "afe65997df98ff032b37",
+    "url": "/static/js/runtime-main.b772fc2b.js"
+  }
+]);
