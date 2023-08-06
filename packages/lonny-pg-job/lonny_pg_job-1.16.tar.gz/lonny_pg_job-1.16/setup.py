4cc3ed92efb5c4c968e908cfdc9e0650046d5489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:56 +0000

    Add package lonny-pg-job/1.16/lonny_pg_job-1.16.tar.gz

---
+++ packages/lonny-pg-job/lonny_pg_job-1.16.tar.gz/lonny_pg_job-1.16/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+with open("requirements.in") as f:
+    setup(
+        name = "lonny_pg_job",
+        version = "1.16",
+        packages = find_packages(),
+        install_requires = f.read().splitlines(),
+        scripts = ["bin/pg_job"]
+    )
