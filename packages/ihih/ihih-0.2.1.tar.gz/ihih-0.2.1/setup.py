65d82283621c22fb0aed8d7565449c5352bfc492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:03 +0000

    Add package ihih/0.2.1/ihih-0.2.1.tar.gz

---
+++ packages/ihih/ihih-0.2.1.tar.gz/ihih-0.2.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    import warnings
+    warnings.warn('setuptools not found, falling back to distutils')
+    from distutils.core import setup
+
+sys.path.insert(0, os.path.dirname(__file__))
+import ihih
+
+
+
+
+
+home = 'https://gitlab.com/romain-dartigues/python-ihih'
+
+setup(
+    name='ihih',
+    version=ihih.__version__,
+    description='Configuration parsers with lazy parameters evaluation',
+    long_description=open('README.rst').read(),
+    author='Romain Dartigues',
+    license='BSD 3-Clause License',
+    keywords='conf config configuration parser',
+    url=home,
+    download_url=home,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    py_modules=('ihih',),
+    python_requires='>=2.7',
+)
