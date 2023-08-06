8237bacabc3c12362453a9bda04f4b96864e654e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:33 +0000

    Add package listless/0.0.1/listless-0.0.1.tar.gz

---
+++ packages/listless/listless-0.0.1.tar.gz/listless-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['listless']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'listless',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'jesse',
+    'author_email': 'jessekrubin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
