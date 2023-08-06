5b6c3d216a9bb13da8e6db3602837b326bd0394a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:09 +0000

    Add package sentry-plugin-dingding/0.0.12/sentry-plugin-dingding-0.0.12.tar.gz

---
+++ packages/sentry-plugin-dingding/sentry-plugin-dingding-0.0.12.tar.gz/sentry-plugin-dingding-0.0.12/src/sentry_plugin_dingding/valueDict.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import re
+
+rule = r'\{\{.+\}\}'
+markdownMap = {
+    u"PROJECT_NAME": u'',
+    u"TITLE": u"title",
+    u"MESSAGE": u"message",
+    u"ENV": u"env",
+    u"PLATFORM": u"platform",
+    u"DATETIME": u"datetime",
+		u"LEVEL": u'level',
+		u"RELEASE": u'release'
+}
+
+defaultMarkdown = u'''
+### {PROJECT_NAME}
+#### {TITLE}
+{MESSAGE}
+[查看详情]({URL})
+'''
+
+
+def formatTemplate(detail, template=defaultMarkdown):
+	template = template.format(
+		PROJECT_NAME=detail.get('PROJECT_NAME'),
+		TITLE=detail.get('title'),
+		MESSAGE=detail.get('message').get('formatted'),
+		URL=detail.get('URL'),
+		ENV=detail.get('environment'),
+		PLATFORM=detail.get('platform'),
+		DATETIME=detail.get('datetime'),
+		RELEASE=detail.get('release'),
+	)
+	return template
