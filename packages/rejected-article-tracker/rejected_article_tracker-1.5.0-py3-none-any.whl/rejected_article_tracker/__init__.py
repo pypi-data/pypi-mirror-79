578a52ff1e764a5f2eeaf30c0c5f21135e46b5b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:33 +0000

    Add package rejected-article-tracker/1.5.0/rejected_article_tracker-1.5.0-py3-none-any.whl

---
+++ packages/rejected-article-tracker/rejected_article_tracker-1.5.0-py3-none-any.whl/rejected_article_tracker/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import sys
+import os
+
+from .src.RejectedArticlesMatch import RejectedArticlesMatch
+from .src.ScholarOneRejectedArticlesMatch import ScholarOneRejectedArticlesMatch
+
+sys.path.append(os.path.abspath("."))
+
