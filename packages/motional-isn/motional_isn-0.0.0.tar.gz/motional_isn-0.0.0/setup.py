2a6bbd80027f0a76af1d76d9f8ae6ad667de9b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:40 +0000

    Add package motional-isn/0.0.0/motional_isn-0.0.0.tar.gz

---
+++ packages/motional-isn/motional_isn-0.0.0.tar.gz/motional_isn-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="motional_isn",
+    version="0.0.0",
+    author="Motional",
+    author_email="devops@motional.com",
+    description="motional_isn",
+    long_description="motional_isn",
+    long_description_content_type="text/markdown",
+    url="https://motional.com/",
+    packages=[],
+    python_requires='>=5',
+)
