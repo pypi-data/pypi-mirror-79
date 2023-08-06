e18abf9916bc375c2be8efd8f52bdc62c8ede926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:42 +0000

    Add package dirtyR/0.0.1/dirtyR-0.0.1.tar.gz

---
+++ packages/dirtyR/dirtyR-0.0.1.tar.gz/dirtyR-0.0.1/dirtyR/r_smooth_loop.py
@@ -0,0 +1,52 @@
+import glob
+
+
+def r_smooth_loop(input_dir, output_dir, trial_type):
+
+    if output_dir.endswith('/') is False:
+
+        raise ValueError(
+            'Output directory does not end with a trailing slash "/"!')
+
+    if trial_type == 'cat_alone':
+
+        variables = [
+            'x', 'y', 'cat_distance', 'velocity', 'acceleration'
+        ]
+
+    elif trial_type == 'with_owner':
+
+        variables = [
+            'x_cat', 'y_cat', 'x_owner', 'y_owner', 'distance', 'cat_distance',
+            'velocity', 'acceleration'
+        ]
+
+    with open(output_dir + 'smooth.r', 'w') as f:
+
+        # Load packages
+
+        print('library(readr)\nlibrary(signal)\n', file=f)
+
+        # Load files and create dataframes
+
+        csv_files_dir = input_dir + '/*.csv'
+
+        files = glob.glob(csv_files_dir)
+
+        for file in files:
+
+            print('\n\ndf <- read_csv("' + file + '")\n', file=f)
+            print('df <- df[-c(1, 2  ), ]\n', file=f)
+
+            # Loess
+
+            for i in variables:
+                print(i + '_loessMod' + '<- loess(' + i,
+                      '~ time, data = df, span = 0.05', ')', file=f)
+                print(i + '_loess05'
+                       + '<- predict(' + i + '_loessMod' + ')', file=f)
+                print('df$' + i + '_loess05'
+                       + ' <- ' + i + '_loess05', file=f)
+
+            print('write.csv(df, "' + file.strip('.csv') +
+                  '_smoothed.csv" , row.names = FALSE)', file=f)
