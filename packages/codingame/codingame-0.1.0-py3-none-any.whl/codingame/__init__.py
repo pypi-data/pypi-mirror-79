62928972f1deffdd1943ddebc4653c6ce9df27a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:10 +0000

    Add package codingame/0.1.0/codingame-0.1.0-py3-none-any.whl

---
+++ packages/codingame/codingame-0.1.0-py3-none-any.whl/codingame/__init__.py
@@ -0,0 +1,24 @@
+"""
+CodinGame API Wrapper
+~~~~~~~~~~~~~~~~~~~~~
+
+Basic wrapper for the undocumented CodinGame API.
+"""
+
+__title__ = 'codingame'
+__author__ = 'takos22'
+__version__ = '0.1.0'
+
+__all__ = [
+    "Client",
+    "CodinGamer",
+]
+
+from collections import namedtuple
+
+from .client import Client
+from .codingamer import CodinGamer
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel='', serial=0)
