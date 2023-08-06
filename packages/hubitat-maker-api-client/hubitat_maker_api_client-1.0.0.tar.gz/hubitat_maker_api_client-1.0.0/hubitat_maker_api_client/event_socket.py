ac2873f05cb8de389d4d91aeadc068712c95512d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:25 +0000

    Add package hubitat-maker-api-client/1.0.0/hubitat_maker_api_client-1.0.0.tar.gz

---
+++ packages/hubitat-maker-api-client/hubitat_maker_api_client-1.0.0.tar.gz/hubitat_maker_api_client-1.0.0/hubitat_maker_api_client/event_socket.py
@@ -0,0 +1,11 @@
+import time
+
+
+class HubitatEvent:
+    def __init__(self, json_dict):
+        self.device_id = json_dict['deviceId']
+        self.device_label = json_dict['displayName']
+        self.attr_key = json_dict['name']
+        self.attr_value = json_dict['value']
+        self.timestamp = int(time.time())
+        self.raw_event = json_dict
