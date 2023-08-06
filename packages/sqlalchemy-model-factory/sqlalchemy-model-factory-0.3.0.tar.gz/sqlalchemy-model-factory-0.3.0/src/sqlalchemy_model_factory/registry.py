431bb0680318ab4eb82abc2d62e8fdf787200278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:15 +0000

    Add package sqlalchemy-model-factory/0.3.0/sqlalchemy-model-factory-0.3.0.tar.gz

---
+++ packages/sqlalchemy-model-factory/sqlalchemy-model-factory-0.3.0.tar.gz/sqlalchemy-model-factory-0.3.0/src/sqlalchemy_model_factory/registry.py
@@ -0,0 +1,37 @@
+class Registry:
+    def __init__(self):
+        self._registered_methods = {}
+
+    def namespaces(self):
+        return list(self._registered_methods)
+
+    def methods(self, *namespace_path):
+        return self._registered_methods[namespace_path]
+
+    def clear(self):
+        self._registered_methods = {}
+
+    def register_at(self, *namespace_path, name="new", **call_options):
+        def wrapper(fn):
+            registry_namespace = self._registered_methods.setdefault(namespace_path, {})
+            if name in registry_namespace:
+                raise ValueError(
+                    "Name '{}' is already registered in namespace {}".format(
+                        name, namespace_path
+                    )
+                )
+
+            registry_namespace[name] = Method(fn, call_options)
+            return fn
+
+        return wrapper
+
+
+class Method:
+    def __init__(self, fn, call_options):
+        self.fn = fn
+        self.call_options = call_options
+
+
+registry = Registry()
+register_at = registry.register_at
