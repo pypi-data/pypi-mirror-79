a896f4af00254899d7d710bc50ea54fec8626a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:11 +0000

    Add package pynecone/0.0.20/pynecone-0.0.20-py3-none-any.whl

---
+++ packages/pynecone/pynecone-0.0.20-py3-none-any.whl/pynecone/output.py
@@ -0,0 +1,49 @@
+from tabulate import tabulate
+import json
+
+
+class OutputFormat:
+    def __init__(self, type='table', header=['NAME'], rows=['name']):
+        self.type = type
+        self.header = header
+        self.rows = rows
+
+
+class Output:
+
+    @classmethod
+    def format(cls, output, output_format=OutputFormat()):
+        if type(output) is list:
+            return cls.format_items(output, output_format)
+        else:
+            return cls.format_item(output, output_format)
+
+    @classmethod
+    def extract_rows(cls, items, output_format=OutputFormat()):
+        return [[i[r] for r in output_format.rows] for i in items]
+
+    @classmethod
+    def format_items(cls, items, output_format=OutputFormat()):
+        if output_format.type == 'json':
+            return json.dumps(items)
+        else:
+            return tabulate(cls.extract_rows(items, output_format), output_format.header)
+
+    @classmethod
+    def format_item(cls, item, output_format=OutputFormat()):
+        if output_format.type == 'json':
+            return json.dumps(item)
+        else:
+            return tabulate(cls.extract_rows(item, output_format), output_format.header)
+
+    @classmethod
+    def output(cls, data, path=None):
+        if path:
+            if isinstance(data, str):
+                with open(path, 'w') as f:
+                    print(data, file=f)
+            else:
+                with open(path, 'wb') as f:
+                    f.write(data)
+        else:
+            print(data)
