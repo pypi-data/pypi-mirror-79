ad76e87f64486a03029adb65effde4a2b750a20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:04 +0000

    Add package infotools/0.6.1/infotools-0.6.1.tar.gz

---
+++ packages/infotools/infotools-0.6.1.tar.gz/infotools-0.6.1/infotools/numbertools/__init__.py
@@ -0,0 +1,2 @@
+from ._numbertools import human_readable, is_number, to_number
+from ._scale import scale, Magnitude
---
+++ packages/infotools/infotools-0.6.1.tar.gz/infotools-0.6.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+"""
+python setup.py sdist bdist_wheel
+twine upload -repository pypi dist/*
+"""
+
+import infotools
+if infotools.DEBUG:
+	message = f"The module is still in debug mode!"
+	raise ValueError(message)
+
+setup(
+	name = 'infotools',
+	version = '0.6.1',
+	packages = ['infotools', 'infotools.timetools', 'infotools.numbertools'],
+	url = 'https://github.com/Kokitis/infotools',
+	license = 'MIT',
+	author = 'proginoskes',
+	author_email = 'chrisdeitrick1@gmail.com',
+	description = 'A collection of tools to make common tasks simpler.',
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	], install_requires = ['pendulum', 'fuzzywuzzy', 'loguru', 'psutil', 'pandas', 'tqdm'],
+	tests_requires = ['pytest']
+)
