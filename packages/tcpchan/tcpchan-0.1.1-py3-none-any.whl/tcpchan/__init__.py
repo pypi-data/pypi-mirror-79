9041589ffa99e2712230334b2305805be790572c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:11 +0000

    Add package tcpchan/0.1.1/tcpchan-0.1.1-py3-none-any.whl

---
+++ packages/tcpchan/tcpchan-0.1.1-py3-none-any.whl/tcpchan/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+""" TCPChan
+
+    A simple TCP multiplexing library 
+"""
+
+__version__ = "0.1.1"
+__author__ = "Frank Chang"
+__author_email__ = "frank@csie.io"
+__license__ = "BSD"
