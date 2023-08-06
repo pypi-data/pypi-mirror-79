d3b6b761b3c3a9288e1c850b56b6d816c453a217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:04 +0000

    Add package requests-exoscale-auth/1.1.2/requests-exoscale-auth-1.1.2.tar.gz

---
+++ packages/requests-exoscale-auth/requests-exoscale-auth-1.1.2.tar.gz/requests-exoscale-auth-1.1.2/setup.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+TESTS_REQUIRE = []
+with open("requirements.test.txt", "r", encoding="utf-8") as f:
+    TESTS_REQUIRE = list(i.rstrip() for i in f.readlines())
+
+setup(
+    name='requests-exoscale-auth',
+    version='1.1.2',
+    url='https://github.com/exoscale/requests-exoscale-auth',
+    license='BSD',
+    author=u'Exoscale',
+    author_email=u'contact@exoscale.com',
+    description=('Exoscale APIs support for Python-Requests.'),
+    long_description=long_description,
+    py_modules=('exoscale_auth',),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=(
+        'requests',
+    ),
+    tests_require=[TESTS_REQUIRE],
+)
