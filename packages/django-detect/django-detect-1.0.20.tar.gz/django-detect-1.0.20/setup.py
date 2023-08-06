493aea8053cea20153dfa155843574a98e17927d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:31 +0000

    Add package django-detect/1.0.20/django-detect-1.0.20.tar.gz

---
+++ packages/django-detect/django-detect-1.0.20.tar.gz/django-detect-1.0.20/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+version = '1.0.20'
+
+
+setup(
+    name='django-detect',
+    version=version,
+    keywords='django-detect',
+    description='Django UserAgent Detect',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/Brightcells/django-detect',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    packages=['detect'],
+    py_modules=[],
+    install_requires=['django-six'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
+)
