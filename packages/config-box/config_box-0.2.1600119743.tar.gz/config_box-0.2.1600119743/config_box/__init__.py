192081c83d70cef4c11f17868d8599f6c722bfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:51 +0000

    Add package config-box/0.2.1600119743/config_box-0.2.1600119743.tar.gz

---
+++ packages/config-box/config_box-0.2.1600119743.tar.gz/config_box-0.2.1600119743/config_box/__init__.py
@@ -0,0 +1,8 @@
+from .main import ConfigBox
+from .exception import (
+    ConfigBoxException,
+    ConfigBoxCredentialsRootNotFound,
+    ConfigBoxCredentialNotFound,
+    ConfigBoxConfigurationRootNotFound,
+    ConfigBoxConfigurationItemNotFound
+)
