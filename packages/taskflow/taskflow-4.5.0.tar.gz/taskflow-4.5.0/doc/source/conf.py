c80037500eff08d6626a630eefa927d2ae3f3f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:06 +0000

    Add package taskflow/4.5.0/taskflow-4.5.0.tar.gz

---
+++ packages/taskflow/taskflow-4.5.0.tar.gz/taskflow-4.5.0/doc/source/conf.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+# -- General configuration ----------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.extlinks',
+    'sphinx.ext.inheritance_diagram',
+    'sphinx.ext.viewcode',
+    'openstackdocstheme'
+]
+
+# openstackdocstheme options
+openstackdocs_repo_name = 'openstack/taskflow'
+openstackdocs_auto_name = False
+openstackdocs_bug_project = 'taskflow'
+openstackdocs_bug_tag = ''
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['templates']
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build']
+
+# General information about the project.
+project = u'TaskFlow'
+copyright = u'%s, OpenStack Foundation' % datetime.date.today().year
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = True
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = True
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'native'
+
+# Prefixes that are ignored for sorting the Python module index
+modindex_common_prefix = ['taskflow.']
+
+# Shortened external links.
+source_tree = 'https://opendev.org/openstack/taskflow/src/branch/master/'
+extlinks = {
+    'example': (source_tree + '/taskflow/examples/%s.py', ''),
+    'pybug': ('http://bugs.python.org/issue%s', ''),
+}
+
+
+# -- Options for HTML output --------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  Major themes that come with
+# Sphinx are currently 'default' and 'sphinxdoc'.
+# html_theme_path = ["."]
+html_theme = 'openstackdocs'
+
+
+# -- Options for autoddoc ----------------------------------------------------
+
+# Keep source order
+autodoc_member_order = 'bysource'
+
+# Always include members
+autodoc_default_options = {'members': None, 'show-inheritance': None}
---
+++ packages/taskflow/taskflow-4.5.0.tar.gz/taskflow-4.5.0/releasenotes/source/index.rst
@@ -0,0 +1,15 @@
+===========================
+ taskflow Release Notes
+===========================
+
+ .. toctree::
+    :maxdepth: 1
+
+    unreleased
+    ussuri
+    train
+    stein
+    rocky
+    queens
+    pike
+    ocata
