e4e3e717c3eeff5bd723b8990407326590c2ea42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:42 +0000

    Add package pywaclient/0.2.1/pywaclient-0.2.1-py3-none-any.whl

---
+++ packages/pywaclient/pywaclient-0.2.1-py3-none-any.whl/pywaclient/models/__init__.py
@@ -0,0 +1,5 @@
+from pywaclient.models.user import User
+from pywaclient.models.manuscript import Manuscript, ManuscriptVersion, ManuscriptExport
+from pywaclient.models.world import World
+from pywaclient.models.article import Article
+from pywaclient.models.block import Block
