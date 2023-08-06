e076fa8412650aada877e1db0d96e5ed512a7592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:08 +0000

    Add package paypayopa/0.5.3/paypayopa-0.5.3.tar.gz

---
+++ packages/paypayopa/paypayopa-0.5.3.tar.gz/paypayopa-0.5.3/paypayopa/resources/__init__.py
@@ -0,0 +1,11 @@
+from .code import Code
+from .payment import Payment
+from .account import Account
+from .preauth import Preauth
+
+__all__ = [
+        'Code',
+        'Payment',
+        'Account',
+        'Preauth',
+]
