858ecf60bb0cec1d74ccbcd50b01931f1a5b4e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:15 +0000

    Add package ocrd-utils/2.16.3/ocrd_utils-2.16.3.tar.gz

---
+++ packages/ocrd-utils/ocrd_utils-2.16.3.tar.gz/ocrd_utils-2.16.3/ocrd_utils/constants.py
@@ -0,0 +1,86 @@
+"""
+Constants for ocrd_utils.
+"""
+from pkg_resources import get_distribution
+
+__all__ = [
+    'EXT_TO_MIME',
+    'LOG_FORMAT',
+    'LOG_TIMEFMT',
+    'MIMETYPE_PAGE',
+    'MIME_TO_EXT',
+    'MIME_TO_PIL',
+    'PIL_TO_MIME',
+    'REGEX_PREFIX',
+    'VERSION',
+]
+
+VERSION = get_distribution('ocrd_utils').version
+
+MIMETYPE_PAGE = 'application/vnd.prima.page+xml'
+
+EXT_TO_MIME = {
+    '.tif': 'image/tiff',
+    '.tiff': 'image/tiff',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.xml': MIMETYPE_PAGE,
+    '.jp2': 'image/jp2',
+    '.pdf': 'application/pdf',
+    '.ps': 'application/postscript',
+    '.eps': 'application/postscript',
+    '.xps': 'application/oxps',
+    '.ppm': 'image/x-portable-pixmap',
+    '.pnm': 'image/x-portable-anymap',
+    '.pbm': 'image/x-portable-bitmap',
+}
+
+MIME_TO_EXT = {
+    'image/tiff': '.tif',
+    'image/tif': '.tif',
+    'image/png': '.png',
+    'image/jpg': '.jpg',
+    'image/jpeg': '.jpg',
+    MIMETYPE_PAGE: '.xml',
+    'application/alto+xml': '.xml',
+    'image/jp2': '.jp2',
+    'application/pdf': '.pdf',
+    'application/postscript': '.ps',
+    'application/oxps': '.xps',
+    'image/x-portable-pixmap': '.ppm',
+    'image/x-portable-anymap': '.pnm',
+    'image/x-portable-bitmap': '.pbm',
+}
+
+#
+# Translate between what PIL expects as ``format`` and IANA media types.
+#
+PIL_TO_MIME = {
+    'BMP':  'image/bmp',
+    'EPS':  'application/postscript',
+    'GIF':  'image/gif',
+    'JPEG': 'image/jpeg',
+    'JP2':  'image/jp2',
+    'PNG':  'image/png',
+    'PPM':  'image/x-portable-pixmap',
+    'TIFF': 'image/tiff',
+}
+
+MIME_TO_PIL = {
+    'image/bmp': 'BMP',
+    'application/postscript': 'EPS',
+    'image/gif': 'GIF',
+    'image/jpeg': 'JPEG',
+    'image/jp2': 'JP2',
+    'image/png': 'PNG',
+    'image/x-portable-pixmap': 'PPM',
+    'image/tiff': 'TIFF',
+}
+
+# Prefix to denote query is regular expression not fixed string
+REGEX_PREFIX = '//'
+
+# Log level format implementing https://ocr-d.de/en/spec/cli#logging
+LOG_FORMAT = r'%(asctime)s.%(msecs)03d %(levelname)s %(name)s - %(message)s'
+LOG_TIMEFMT = r'%H:%M:%S'
