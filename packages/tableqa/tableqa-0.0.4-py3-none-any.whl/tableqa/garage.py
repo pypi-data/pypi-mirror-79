0722c238cec557deaa6d5f8fb9d206a7fc7a37a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:16 +0000

    Add package tableqa/0.0.4/tableqa-0.0.4-py3-none-any.whl

---
+++ packages/tableqa/tableqa-0.0.4-py3-none-any.whl/tableqa/garage.py
@@ -0,0 +1,3 @@
+from data_utils import data_utils
+d=data_utils('/Users/abhijithneilabraham/Documents/GitHub/tableQA/tableqa/cleaned_data', '/Users/abhijithneilabraham/Documents/GitHub/tableQA/tableqa/schema')
+d.get_schema_for_csv('/Users/abhijithneilabraham/Documents/GitHub/tableQA/tableqa/cleaned_data/Activities - Data.csv')
