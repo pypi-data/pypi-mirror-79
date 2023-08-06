685fedde6288a6e654341e56cab4847e00db7118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:08 +0000

    Add package summa-testing-framework-summasolutions/0.1.21/summa-testing-framework-summasolutions-0.1.21.tar.gz

---
+++ packages/summa-testing-framework-summasolutions/summa-testing-framework-summasolutions-0.1.21.tar.gz/summa-testing-framework-summasolutions-0.1.21/stf/command/config.py
@@ -0,0 +1,25 @@
+import click
+from click_help_colors import HelpColorsCommand
+
+import stf.common.config as common_config
+import stf.common.filesystem as common_filesystem
+import stf.common.docker as common_docker
+import os
+import sys
+import shutil
+
+sys.path.insert(1, os.getcwd())
+
+
+@click.command(
+    cls=HelpColorsCommand,
+    help_headers_color='yellow',
+    help_options_color='green',
+)
+def init():
+    click.secho("Initializing project config", fg='blue')
+
+    base_project_config_file = "%s/config.base.yml" % (common_config.get_assets_dir())
+    target = "%s/config.yml" % os.getcwd()
+
+    shutil.copyfile(base_project_config_file, target)
