47902252c5bf1265c8acb99e1c6f4fd50b730e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:27 +0000

    Add package flask-sieve/1.1.3/flask_sieve-1.1.3-py3-none-any.whl

---
+++ packages/flask-sieve/flask_sieve-1.1.3-py3-none-any.whl/tests/test_exceptions.py
@@ -0,0 +1,20 @@
+import unittest
+
+from flask import Flask
+
+from flask_sieve.exceptions import ValidationException, register_error_handler
+
+
+class TestErrorHandler(unittest.TestCase):
+    def test_error_handler(self):
+        app = Flask(__name__)
+        register_error_handler(app)
+        self.assertIn(ValidationException, app.error_handler_spec[None][None])
+        errors = {'field': 'Test error'}
+
+        with app.app_context():
+            response, status = app.error_handler_spec[None][None][ValidationException](
+                ValidationException(errors)
+            )
+        self.assertEqual(400, status)
+        self.assertIn('Test error', str(response.get_json()))
---
+++ packages/flask-sieve/flask_sieve-1.1.3-py3-none-any.whl/tests/test_sieve.py
@@ -0,0 +1,21 @@
+import unittest
+
+from flask import Flask
+
+from flask_sieve import Sieve
+from flask_sieve.exceptions import ValidationException
+
+
+class TestSieve(unittest.TestCase):
+    def test_registers_error_handler(self):
+        app = Flask(__name__)
+        Sieve(app)
+        self.assertIn(ValidationException, app.error_handler_spec[None][None])
+        errors = {'field': 'Test error'}
+
+        with app.app_context():
+            response, status = app.error_handler_spec[None][None][ValidationException](
+                ValidationException(errors)
+            )
+        self.assertEqual(400, status)
+        self.assertIn('Test error', str(response.get_json()))
