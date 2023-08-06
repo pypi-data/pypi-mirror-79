dcfb657b3928d2f4e285ccc44fe953b51871bbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:08 +0000

    Add package wasu-test/1.6/wasu_test-1.6.tar.gz

---
+++ packages/wasu-test/wasu_test-1.6.tar.gz/wasu_test-1.6/drivers/appium.py
@@ -0,0 +1,13 @@
+"""
+
+@FileName: appium.py
+@Author: chenxiaodong
+@CreatTime: 2020/9/15 11:01
+@Descriptions: 
+
+"""
+
+
+class appium_driver:
+    def __init__(self, version):
+        self._version = version
