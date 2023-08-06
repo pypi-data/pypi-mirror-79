ef55fda89df19f666564c49b3ea7c513d08bfa65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:05 +0000

    Add package django-model2puml/0.1.5/django-model2puml-0.1.5.tar.gz

---
+++ packages/django-model2puml/django-model2puml-0.1.5.tar.gz/django-model2puml-0.1.5/puml_generator/management/commands/generate_puml.py
@@ -0,0 +1,56 @@
+from django.apps import apps
+from django.core.management import BaseCommand
+
+from puml_generator.management.commands.utils.utils import generate_puml_class_diagram
+
+
+def add_bool_arg(parser, name, help_yes, help_no, default=False):
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('--' + name, dest=name, help=help_yes, action='store_true')
+    group.add_argument('--no-' + name, dest=name, help=help_no, action='store_false')
+    parser.set_defaults(**{name: default})
+
+
+class Command(BaseCommand):
+    help = 'Generate PlantUML diagram of project'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--file',
+            type=str,
+            help='output file',
+            default='models_diagram.puml',
+        )
+        parser.add_argument(
+            '--omit',
+            type=str,
+            nargs='+',
+            help='omit applications',
+        )
+        add_bool_arg(
+            parser, 'add-help',
+            'docstrings should be included to diagram',
+            'docstrings should not be included to diagram'
+        )
+        add_bool_arg(
+            parser, 'add-choices',
+            'models Choices fields should be described',
+            'models Choices fields should not be described'
+        )
+
+    def handle(self, *args, **options):
+        output = options['file']
+        generate_with_help = options['add-help']
+        generate_with_choices = options['add-choices']
+        omit = options['omit']
+
+        models = apps.get_models()
+        uml = generate_puml_class_diagram(
+            models,
+            with_help=generate_with_help,
+            with_choices=generate_with_choices,
+            omit=omit,
+        )
+
+        with open(output, 'w') as file:
+            file.write(uml)
