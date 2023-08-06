01e5a3018c0515ccaceccbefde91c0552bc042e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package stograde/4.1.0/stograde-4.1.0.tar.gz

---
+++ packages/stograde/stograde-4.1.0.tar.gz/stograde-4.1.0/stograde/formatters/__init__.py
@@ -0,0 +1,4 @@
+from .base import format_collected_data
+from .html import format_assignment_html as html
+from .markdown import format_assignment_markdown as markdown
+from .truncate import truncate
