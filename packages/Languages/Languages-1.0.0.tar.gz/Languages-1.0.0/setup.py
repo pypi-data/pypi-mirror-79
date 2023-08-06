1c79402c3c58ae9470e6d253d8988f6d7d54d6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:01 +0000

    Add package Languages/1.0.0/Languages-1.0.0.tar.gz

---
+++ packages/Languages/Languages-1.0.0.tar.gz/Languages-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Languages", # Replace with your own username
+    version="1.0.0",
+    author="Aswanth Vc",
+    author_email="no-mail@mail.com",
+    description="Programming language decoder. Now You can Decode HyperTextMarkupLanguage (html) using this package.",
+    keywords=["html","hypertextmarkup decoder","programming language decoder","languages decoder","programming language decoder"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aswanthabam/Language",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
