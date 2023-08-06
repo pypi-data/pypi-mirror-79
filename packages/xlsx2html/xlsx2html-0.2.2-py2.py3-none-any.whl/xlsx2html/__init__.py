0316b8051d10a686bd2129e51f3343fff3eb6c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:24 +0000

    Add package xlsx2html/0.2.2/xlsx2html-0.2.2-py2.py3-none-any.whl

---
+++ packages/xlsx2html/xlsx2html-0.2.2-py2.py3-none-any.whl/xlsx2html/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import warnings
+from .core import xlsx2html
+
+__all__ = ['xls2html', 'xlsx2html']
+
+__version__ = '0.2.2'
+
+
+def xls2html(*args, **kwargs):
+    warnings.warn("This func was renamed to xlsx2html.", DeprecationWarning)
+    return xlsx2html(*args, **kwargs)
