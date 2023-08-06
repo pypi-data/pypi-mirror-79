90b64f13de1fdcf930fc502ccd61d6323b505a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:40 +0000

    Add package sym-cli/0.0.31b1/sym_cli-0.0.31b1-py3-none-any.whl

---
+++ packages/sym-cli/sym_cli-0.0.31b1-py3-none-any.whl/sym/cli/helpers/sym_group.py
@@ -0,0 +1,76 @@
+from contextlib import ExitStack
+from typing import Any, Callable, ClassVar, List
+
+from click import Command, Context, Group
+from sentry_sdk.api import push_scope
+
+from ..errors import get_active_env_vars
+from ..saml_clients.chooser import choose_saml_client
+from . import segment
+from .tee import Tee, TeeStdErr, TeeStdOut
+
+
+class AutoTagCommand(Command):
+    """
+    A command where each invocation sets the Sentry tag with the
+    command's name automatically. Additionally, any CliErrors
+    raised from the command are logged.
+    """
+
+    def invoke(self, ctx: Context) -> Any:
+        segment.track(
+            "Command Executed", command=ctx.info_name, options=ctx.obj.to_dict()
+        )
+        with push_scope() as scope:
+            scope.set_tag("command", ctx.info_name)
+            scope.set_extra("options", ctx.obj.to_dict())
+            return super().invoke(ctx)
+
+    def parse_args(self, ctx, args):
+        if (
+            self.params
+            and (resource := ctx.parent.params.get("resource"))
+            and self.params[0].name == "resource"
+            and (
+                not args
+                or not choose_saml_client(
+                    ctx.parent.params["saml_client_name"]
+                ).validate_resource(args[0])
+            )
+        ):
+            args = [resource] + args
+        return super().parse_args(ctx, args)
+
+    def format_epilog(self, ctx, formatter):
+        if not self.epilog:
+            formatter.write(get_active_env_vars())
+        super().format_epilog(ctx, formatter)
+
+
+class SymGroup(Group):
+    """
+    A group where any defined commands automatically use
+    AutoTagCommand.
+    """
+
+    tees: ClassVar[List[Tee]] = []
+
+    def __init__(self, *args: Any, **attrs: Any) -> None:
+        super().__init__(*args, **attrs)
+
+    def __del__(self):
+        for tee in self.__class__.tees:
+            tee.close()
+
+    def invoke(self, ctx: Context) -> Any:
+        if (log_dir := ctx.params.get("log_dir")) :
+            # Don't register exit handler so exceptions are teed.
+            # Instead, __del__ will be called when the program exits.
+            self.__class__.tees.extend((TeeStdOut(log_dir), TeeStdErr(log_dir)))
+
+        return super().invoke(ctx)
+
+    def command(
+        self, *args: Any, **kwargs: Any
+    ) -> Callable[[Callable[..., Any]], AutoTagCommand]:
+        return super().command(*args, **kwargs, cls=AutoTagCommand)  # type: ignore
