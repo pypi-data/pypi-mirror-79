8dc12e2fea0843a82bd07a13b4be72fe5fb2eb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:45 +0000

    Add package spym/0.4.2/spym-0.4.2-py3-none-any.whl

---
+++ packages/spym/spym-0.4.2-py3-none-any.whl/spym/__init__.py
@@ -0,0 +1,13 @@
+from ._version import __version__
+
+from ._accessors import _SpymDataArray, _SpymDataset
+from .io import load
+
+__all__ = [
+    'A_CONSTANT',
+    'YET_ANOTHER',
+    'a_function',
+    'MyClass',
+    'AnotherClass',
+    'MyException'
+]
