38b5381a54ca95295ff6bbd9e5b15a2d65020b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:44 +0000

    Add package bapy/0.1.38/bapy-0.1.38.tar.gz

---
+++ packages/bapy/bapy-0.1.38.tar.gz/bapy-0.1.38/setup.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The setup script."""
+import atexit
+import os
+import subprocess
+
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+from bapy import path, ic, ask, console, Url, User, asdict
+
+## Alt 1
+
+# post = True
+#
+#
+# def post_command(obj):
+#     """
+#     Post Install Command.
+#
+#     Args:
+#         obj: post command self obj.
+#     """
+#     ic(asdict(obj))
+#     if post and not obj.root.startswith('build/bdist.macosx'):
+#         if ask('Do you want to?'):
+#             console.print("Start installation!")
+#     return obj
+#
+#
+# class PostDevelopCommand(develop):
+#     """Post-installation for development mode."""
+#     function = post_command
+#     # Pre Install.
+#
+#     def run(self):
+#         # Post Install.
+#         if self.function:
+#             # noinspection PyArgumentList
+#             self.function()
+#         develop.run(self)
+#
+#
+# class PostInstallCommand(install):
+#     """Post-installation for installation mode."""
+#     function = post_command
+#     # Pre Install.
+#
+#     def run(self):
+#         # Post Install.
+#         if self.function:
+#             # noinspection PyArgumentList
+#             self.function()
+#         install.run(self)
+#
+#
+# PostDevelopCommand.function = PostInstallCommand.function = post_command
+#
+#
+# setup(
+#     author=User.gecos,
+#     author_email=Url.email(User.name),
+#     cmdclass={
+#         'develop': PostDevelopCommand,
+#         'install': PostInstallCommand,
+#     },
+#     description=path.project.description(),
+#     entry_points={
+#         'console_scripts': [
+#             f'{path.repo} = {path.repo}:app',
+#         ],
+#     },
+#     include_package_data=True,
+#     install_requires=path.project.requirements['requirements'],
+#     name=path.repo,
+#     package_data={
+#         path.repo: [f'{path.repo}/scripts/*', f'{path.repo}/templates/*'],
+#     },
+#     packages=find_packages(include=[path.repo], exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+#     python_requires='>=3.8,<4',
+#     scripts=[item.relative_to(path.project).text for item in path.scripts],
+#     setup_requires=path.project.requirements['requirements_setup'],
+#     tests_require=path.project.requirements['requirements_test'],
+#     url=Url.lumenbiomics(http=True, repo=path.repo).url,
+#     use_scm_version=False,
+#     version='0.1.38',
+#     zip_safe=False,
+# )
+
+
+## Alt 2
+
+# def post_wrapper(obj):
+#     """
+#     Post Install Command.
+#
+#     Args:
+#         obj: post command self obj.
+#     """
+#     def post_actions():
+#         ic(obj)
+#         if ask('Do you want to?'):
+#             console.print("Start installation!")
+#     post_actions()
+#     return obj
+#
+#
+# setup = post_wrapper(
+#     setup(
+#         author=User.gecos,
+#         author_email=Url.email(User.name),
+#         description=path.project.description(),
+#         entry_points={
+#             'console_scripts': [
+#                 f'{path.repo} = {path.repo}:app',
+#             ],
+#         },
+#         include_package_data=True,
+#         install_requires=path.project.requirements['requirements'],
+#         name=path.repo,
+#         package_data={
+#             path.repo: [f'{path.repo}/scripts/*', f'{path.repo}/templates/*'],
+#         },
+#         packages=find_packages(include=[path.repo], exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+#         python_requires='>=3.8,<4',
+#         scripts=[item.relative_to(path.project).text for item in path.scripts],
+#         setup_requires=path.project.requirements['requirements_setup'],
+#         tests_require=path.project.requirements['requirements_test'],
+#         url=Url.lumenbiomics(http=True, repo=path.repo).url,
+#         use_scm_version=False,
+#         version='0.1.38',
+#         zip_safe=False,
+#     )
+# )
+
+
+## Alt 3
+
+# cmd_class = {}
+#
+# if 'darwin' in sys.platform:
+#     class PostInstall(install):
+#         """ Post installation - run install_name_tool on Darwin """
+#         def run(self):
+#             install.run(self)
+#             if ask('Do you want to?'):
+#                 console.print("Start installation!")
+#
+#
+#     cmd_class = dict(install=PostInstall)
+#
+# setup(
+#     author=User.gecos,
+#     author_email=Url.email(User.name),
+#     cmdclass=cmd_class,
+#     description=path.project.description(),
+#     entry_points={
+#         'console_scripts': [
+#             f'{path.repo} = {path.repo}:app',
+#         ],
+#     },
+#     include_package_data=True,
+#     install_requires=path.project.requirements['requirements'],
+#     name=path.repo,
+#     package_data={
+#         path.repo: [f'{path.repo}/scripts/*', f'{path.repo}/templates/*'],
+#     },
+#     packages=find_packages(include=[path.repo], exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+#     python_requires='>=3.8,<4',
+#     scripts=[item.relative_to(path.project).text for item in path.scripts],
+#     setup_requires=path.project.requirements['requirements_setup'],
+#     tests_require=path.project.requirements['requirements_test'],
+#     url=Url.lumenbiomics(http=True, repo=path.repo).url,
+#     use_scm_version=False,
+#     version='0.1.38',
+#     zip_safe=False,
+# )
+
+
+## Alt 4
+
+
+# class PostDevelopCommand(develop):
+#     """Post-installation for development mode."""
+#     def run(self):
+#         develop.run(self)
+#         subprocess.check_call("ls")
+#         if ask('Do you want to?'):
+#             console.print("Start installation!")
+#
+#
+# class PostInstallCommand(install):
+#     """Post-installation for installation mode."""
+#     def run(self):
+#         install.run(self)
+#         subprocess.check_call("ls")
+#         if ask('Do you want to?'):
+#             console.print("Start installation!")
+#
+#
+# setup(
+#     author=User.gecos,
+#     author_email=Url.email(User.name),
+#     cmdclass=dict(develop=PostDevelopCommand, install=PostInstallCommand),
+#     description=path.project.description(),
+#     entry_points={
+#         'console_scripts': [
+#             f'{path.repo} = {path.repo}:app',
+#         ],
+#     },
+#     include_package_data=True,
+#     install_requires=path.project.requirements['requirements'],
+#     name=path.repo,
+#     package_data={
+#         path.repo: [f'{path.repo}/scripts/*', f'{path.repo}/templates/*'],
+#     },
+#     packages=find_packages(include=[path.repo], exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+#     python_requires='>=3.8,<4',
+#     scripts=[item.relative_to(path.project).text for item in path.scripts],
+#     setup_requires=path.project.requirements['requirements_setup'],
+#     tests_require=path.project.requirements['requirements_test'],
+#     url=Url.lumenbiomics(http=True, repo=path.repo).url,
+#     use_scm_version=False,
+#     version='0.1.38',
+#     zip_safe=False,
+# )
+
+
+## Alt 5
+# def _post_install():
+#     ic('POST INSTALL')
+#     if ask('Do you want to?'):
+#         console.print("Start installation!")
+#
+#
+# class new_install(install):
+#     def __init__(self, *args, **kwargs):
+#         super(new_install, self).__init__(*args, **kwargs)
+#         atexit.register(_post_install)
+#
+#
+# setup(
+#     author=User.gecos,
+#     author_email=Url.email(User.name),
+#     cmdclass={'install': new_install},
+#     description=path.project.description(),
+#     entry_points={
+#         'console_scripts': [
+#             f'{path.repo} = {path.repo}:app',
+#         ],
+#     },
+#     include_package_data=True,
+#     install_requires=path.project.requirements['requirements'],
+#     name=path.repo,
+#     package_data={
+#         path.repo: [f'{path.repo}/scripts/*', f'{path.repo}/templates/*'],
+#     },
+#     packages=find_packages(include=[path.repo], exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+#     python_requires='>=3.8,<4',
+#     scripts=[item.relative_to(path.project).text for item in path.scripts],
+#     setup_requires=path.project.requirements['requirements_setup'],
+#     tests_require=path.project.requirements['requirements_test'],
+#     url=Url.lumenbiomics(http=True, repo=path.repo).url,
+#     use_scm_version=False,
+#     version='0.1.38',
+#     zip_safe=False,
+# )
+
+
+## Alt 6
+def _post_install():
+    ic('POST INSTALL')
+    if ask('Do you want to?'):
+        console.print("Start installation!")
+
+
+setup(
+    author=User.gecos,
+    author_email=Url.email(User.name),
+    description=path.project.description(),
+    entry_points={
+        'console_scripts': [
+            f'{path.repo} = {path.repo}:app',
+        ],
+    },
+    include_package_data=True,
+    install_requires=path.project.requirements['requirements'],
+    name=path.repo,
+    package_data={
+        path.repo: [f'{path.repo}/scripts/*', f'{path.repo}/templates/*'],
+    },
+    packages=find_packages(include=[path.repo], exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    python_requires='>=3.8,<4',
+    scripts=[item.relative_to(path.project).text for item in path.scripts],
+    setup_requires=path.project.requirements['requirements_setup'],
+    tests_require=path.project.requirements['requirements_test'],
+    url=Url.lumenbiomics(http=True, repo=path.repo).url,
+    use_scm_version=False,
+    version='0.1.38',
+    zip_safe=False,
+)
+
+_post_install()
