07fbb73deda91a8ee0ad5b54c04a096d29a8d47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:17 +0000

    Add package wbia-vtool/3.3.0/wbia_vtool-3.3.0-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/wbia-vtool/wbia_vtool-3.3.0-cp37-cp37m-manylinux2010_x86_64.whl/vtool/__main__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+def main():  # nocover
+    import vtool
+
+    print('Looks like the imports worked')
+    print('vtool = {!r}'.format(vtool))
+    print('vtool.__file__ = {!r}'.format(vtool.__file__))
+    print('vtool.__version__ = {!r}'.format(vtool.__version__))
+
+    from vtool._pyflann_backend import pyflann
+
+    print('pyflann = {!r}'.format(pyflann))
+
+    from vtool import sver_c_wrapper
+
+    print('sver_c_wrapper.lib_fname = {!r}'.format(sver_c_wrapper.lib_fname))
+    print('sver_c_wrapper.lib_fname_cand = {!r}'.format(sver_c_wrapper.lib_fname_cand))
+    print('sver_c_wrapper.c_sver = {!r}'.format(sver_c_wrapper.c_sver))
+
+    try:
+        import cv2
+
+        print('cv2 = {!r}'.format(cv2))
+        print('cv2.__file__ = {!r}'.format(cv2.__file__))
+        print('cv2.__version__ = {!r}'.format(cv2.__version__))
+    except Exception:
+        print('OpenCV (cv2) failed to import')
+
+
+if __name__ == '__main__':
+    """
+    CommandLine:
+       python -m vtool
+    """
+    main()
