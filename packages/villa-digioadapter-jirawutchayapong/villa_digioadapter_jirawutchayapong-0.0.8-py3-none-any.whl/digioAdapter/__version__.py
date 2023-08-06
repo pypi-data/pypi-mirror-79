7766eb62ea9eb29565e2fda05c3188e76a032987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:57 +0000

    Add package villa-digioadapter-jirawutchayapong/0.0.8/villa_digioadapter_jirawutchayapong-0.0.8-py3-none-any.whl

---
+++ packages/villa-digioadapter-jirawutchayapong/villa_digioadapter_jirawutchayapong-0.0.8-py3-none-any.whl/digioAdapter/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'villa-digioadapter-jirawutchayapong'
+__description__ = 'calling digio endpoint'
+__version__ = '0.0.8'
+__author__ = 'JC'
+__author_email__ = 'chayapongjirawut@gmail.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://github.com/thanakijwanavit/villaWallet'
