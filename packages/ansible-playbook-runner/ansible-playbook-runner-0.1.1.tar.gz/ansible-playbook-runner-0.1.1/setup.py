ff4e5f8ba076cd39b4bd8fc02ee402ebf121db5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:59 +0000

    Add package ansible-playbook-runner/0.1.1/ansible-playbook-runner-0.1.1.tar.gz

---
+++ packages/ansible-playbook-runner/ansible-playbook-runner-0.1.1.tar.gz/ansible-playbook-runner-0.1.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open('readme.MD') as f:
+    long_description = f.read()
+
+
+setup(
+    name='ansible-playbook-runner',
+    description='simple ansible playbook runner',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Avi Naftalis',
+    author_email='anaftalis@gmail.com',
+    packages=['ansible_playbook_runner'],
+    install_requires='ansible>=2',
+    version='0.1.1',
+    setup_requires=['flake8'],
+)
