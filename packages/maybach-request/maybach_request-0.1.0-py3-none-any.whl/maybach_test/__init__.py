9756e72bea072bad95457ff0e550bd855ec50aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:39 +0000

    Add package maybach-request/0.1.0/maybach_request-0.1.0-py3-none-any.whl

---
+++ packages/maybach-request/maybach_request-0.1.0-py3-none-any.whl/maybach_test/__init__.py
@@ -0,0 +1,42 @@
+import requests
+import sys
+import json
+from time import sleep
+port = None
+def send(data):
+  port = getPort()
+  requests.post('http://127.0.0.1:' + port + '/test', data = data)
+  
+def get_print():
+  sleep(200)
+  port = getPort()
+  r = requests.get('http://127.0.0.1:' + port + '/test')
+  if r.status_code == 200:
+    try:
+      info = json.loads(r.text)
+      if info.get('status') == 1:
+        return []
+      return info.get('msg')
+    except Exception as e:
+      print(e)
+      return []
+      
+  
+def getPort():
+  global port
+  if (port != None):
+    return port
+  match = False
+  for i in sys.argv:
+    if match:
+      port = i
+      break;
+    if i == '--port':
+      match = True
+  return port
+
+def pass_test(msg):
+  send({ 'status': 0, 'msg': msg })
+  
+def no_pass_test(msg):
+  send({ 'status': 1, 'msg': msg })
