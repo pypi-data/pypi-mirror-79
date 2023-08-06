2f531adc3df035d99c92df0a259de53f80ead32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:28 +0000

    Add package mistsystems/0.1.1/mistsystems-0.1.1-py3-none-any.whl

---
+++ packages/mistsystems/mistsystems-0.1.1-py3-none-any.whl/mistsystems/requests/sites/const.py
@@ -0,0 +1,39 @@
+class Const():
+
+    def __init__(self, session):
+        self.session = session
+
+    def get_applications(self):
+        uri = "/api/v1/const/applications"
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def get_ap_led_status(self):
+        uri = "/api/v1/const/ap_led_status"
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def get_ap_models(self):
+        uri = "/api/v1/const/device_models"
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def get_client_events(self):
+        uri = "/api/v1/const/client_events"
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def get_country_codes(self):
+        uri = "/api/v1/const/countries"
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def get_system_events(self):
+        uri = "/api/v1/const/system_events"
+        resp = self.session.mist_get(uri)
+        return resp
+
+    def get_insights(self):
+        uri = "/api/v1/const/insight_metrics"
+        resp = self.session.mist_get(uri)
+        return resp
