b14403b17b2b528ac4a0d42c7653ce8058a89970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:07 +0000

    Add package staroid/0.0.9/staroid-0.0.9.tar.gz

---
+++ packages/staroid/staroid-0.0.9.tar.gz/staroid-0.0.9/staroid/namespace.py
@@ -0,0 +1,297 @@
+import requests
+import logging
+import json
+import subprocess
+import atexit
+import time
+from .commit import Commit
+
+class Namespace:
+    def __init__(self, json):
+        self.__json = json
+
+    def id(self):
+        return int(self.__json["id"])
+
+    def namespace(self):
+        return self.__json["name"]
+
+    def alias(self):
+        return self.__json["instanceName"]
+
+    def type(self):
+        return self.__json["type"]
+    
+    def phase(self):
+        return self.__json["phase"]
+
+    def status(self):
+        return self.__json["status"]
+
+    def access(self):
+        return self.__json["access"]
+
+    def url(self):
+        return self.__json["url"]
+
+    def created_date(self):
+        return self.__json["createdDate"]
+
+    def terminated_date(self):
+        return self.__json["terminatedDate"]
+
+class NamespaceApi:
+    """Namespace api"""
+
+    def __init__(self, staroid, cluster):
+        self.__staroid = staroid
+        self.__cluster = cluster
+        self.__tunnel_processes = {}
+
+    def get_all(self):
+        r = self.__staroid._api_get(
+            "orgs/{}/vc/{}/instance".format(
+                self.__staroid.get_account(),
+                self.__cluster.id()
+            )
+        )
+        if r.status_code == 200:
+            json_object_list = json.loads(r.text)
+            namespace_list = []
+            for js in json_object_list:
+                namespace_list.append(Namespace(js))
+            return namespace_list
+        else:
+            logging.error("Can not get clusters {}", r.status_code)
+            return None
+
+    def get(self, instance_name):
+        namespaces = self.get_all()
+        if namespaces == None:
+            return None
+
+        removed = None
+        for ns in namespaces:
+            if ns.phase() == "REMOVED":
+                removed = ns
+                continue
+            if ns.alias() == instance_name:
+                return ns
+
+        return removed
+
+    def get_by_id(self, instance_id):
+        r = self.__staroid._api_get(
+            "orgs/{}/vc/{}/instance/{}".format(
+                self.__staroid.get_account(),
+                self.__cluster.id(),
+                instance_id
+            )
+        )
+        if r.status_code == 200:
+            js = json.loads(r.text)
+            return Namespace(js)
+        else:
+            logging.error("Can not get namespace {}", r.status_code)
+            return None
+
+    def create(self, instance_name, commit_url):
+        c = Commit(commit_url)
+
+        r = self.__staroid._api_post(
+            "orgs/{}/vc/{}/instance".format(
+                self.__staroid.get_account(),
+                self.__cluster.id()
+            ),
+            {
+                "provider": c.provider(),
+                "owner": c.owner(),
+                "repo": c.repo(),
+                "branch": c.branch(),
+                "commit": c.commit(),
+                "instanceName": instance_name
+            }
+        )
+        if r.status_code == 200:
+            js = json.loads(r.text)
+            return Namespace(js)
+        elif r.status_code == 409: # already exists
+            return self.get(instance_name)
+        else:
+            logging.error("Can not create namespace {}", r.status_code)
+            return None
+
+    def delete(self, instance_name):
+        ns = self.get(instance_name)
+        if ns == None:
+            return None
+
+        r = self.__staroid._api_delete(
+            "orgs/{}/vc/{}/instance/{}".format(
+                self.__staroid.get_account(),
+                self.__cluster.id(),
+                ns.id()
+            )
+        )
+        if r.status_code == 200:
+            js = json.loads(r.text)
+            return Namespace(js)
+        else:
+            logging.error("Can not delete namespace {}", r.status_code)
+            return None
+
+
+    def start(self, instance_name):
+        ns = self.get(instance_name)
+        if ns == None:
+            return None
+
+        r = self.__staroid._api_put(
+            "orgs/{}/vc/{}/instance/{}/resume".format(
+                self.__staroid.get_account(),
+                self.__cluster.id(),
+                ns.id()
+            )
+        )
+        if r.status_code == 200:
+            js = json.loads(r.text)
+            return Namespace(js)
+        else:
+            logging.error("Can not start namespace {}", r.status_code)
+            return None
+
+
+    def stop(self, instance_name):
+        ns = self.get(instance_name)
+        if ns == None:
+            return None
+
+        r = self.__staroid._api_put(
+            "orgs/{}/vc/{}/instance/{}/pause".format(
+                self.__staroid.get_account(),
+                self.__cluster.id(),
+                ns.id()
+            )
+        )
+        if r.status_code == 200:
+            js = json.loads(r.text)
+            return Namespace(js)
+        else:
+            logging.error("Can not stop namespace {}", r.status_code)
+            return None
+
+
+    def shell_start(self, instance_name):
+        ns = self.get(instance_name)
+        if ns == None:
+            return None
+
+        r = self.__staroid._api_post(
+            "orgs/{}/vc/{}/instance/{}/shell".format(
+                self.__staroid.get_account(),
+                self.__cluster.id(),
+                ns.id()
+            )
+        )
+        if r.status_code == 200:
+            js = json.loads(r.text)
+            return js
+        else:
+            logging.error("Can not start shell {}", r.status_code)
+            return None
+
+    def shell_stop(self, instance_name):
+        ns = self.get(instance_name)
+        if ns == None:
+            return None
+
+        r = self.__staroid._api_delete(
+            "orgs/{}/vc/{}/instance/{}/shell".format(
+                self.__staroid.get_account(),
+                self.__cluster.id(),
+                ns.id()
+            )
+        )
+        if r.status_code == 200:
+            return None
+        else:
+            logging.error("Can not stop shell {}", r.status_code)
+            return None
+
+    def get_all_resources(self, instance_name):
+        ns = self.get(instance_name)
+        if ns == None:
+            return None
+
+        r = self.__staroid._api_get(
+            "namespace/{}".format(
+                ns.namespace()
+            )
+        )
+        if r.status_code == 200:
+            js = json.loads(r.text)
+            return js
+        else:
+            logging.error("Can not get namespace resources {}", r.status_code)
+            return None
+
+    def is_tunnel_running(self, instance_name):
+        if instance_name in self.__tunnel_processes:
+            p = self.__tunnel_processes[instance_name]
+            p.poll()
+            return p.returncode == None
+        else:
+            return False
+
+    def start_tunnel(self, instance_name, tunnels):
+        if self.is_tunnel_running(instance_name):
+            return
+
+        chisel_path = self.__staroid.get_chisel_path()
+
+        ns = self.get(instance_name)
+        if ns == None:
+            return None
+        resources = self.get_all_resources(instance_name)
+
+        shell_service = None
+        for s in resources["services"]["items"]:
+            if "labels" in s["metadata"]:
+                if "resource.staroid.com/system" in s["metadata"]["labels"]:
+                    if s["metadata"]["labels"]["resource.staroid.com/system"] == "shell":
+                        shell_service = s
+                        break                        
+
+        if shell_service == None:
+            raise Exception("Shell service not found")
+
+        tunnel_server = "https://p{}-{}--{}".format("57682", shell_service["metadata"]["name"], ns.url()[len("https://"):])
+        cmd = [
+            chisel_path,
+            "client",
+            "--header",
+            "Authorization: token {}".format(self.__staroid.get_access_token()),
+            "--keepalive",
+            "10s",
+            tunnel_server
+        ]
+        cmd.extend(tunnels)
+        self.__tunnel_processes[instance_name]=subprocess.Popen(cmd)
+        atexit.register(self.__cleanup)
+
+    def __cleanup(self):
+        timeout_sec = 5
+        for p in self.__tunnel_processes.values(): # list of your processes
+            p_sec = 0
+            for second in range(timeout_sec):
+                if p.poll() == None:
+                    time.sleep(1)
+                    p_sec += 1
+            if p_sec >= timeout_sec:
+                p.kill() # supported from python 2.6
+
+    def stop_tunnel(self, instance_name):
+        if self.is_tunnel_running(instance_name):
+            self.__tunnel_processes[instance_name].kill()
+            del self.__tunnel_processes[instance_name]
+
---
+++ packages/staroid/staroid-0.0.9.tar.gz/staroid-0.0.9/tests/test_namespace.py
@@ -0,0 +1,67 @@
+import unittest
+import tempfile
+import os, time
+
+from staroid import Staroid
+
+def integration_test_ready():
+    return "STAROID_ACCESS_TOKEN" in os.environ and "STAROID_ACCOUNT" in os.environ
+
+def wait_for_phase(ns_api, ns, phase):
+    max_wait = 300
+    while ns.phase() != phase:
+        time.sleep(1)
+        ns = ns_api.get_by_id(ns.id())
+        max_wait -= 1
+        if max_wait == 0:
+            break
+
+class TestNamespace(unittest.TestCase):
+    @unittest.skipUnless(integration_test_ready(), "Integration test environment is not configured")
+    def test_crud_namespace(self):
+        # given
+        s = Staroid(access_token=os.environ["STAROID_ACCESS_TOKEN"], account=os.environ["STAROID_ACCOUNT"])
+        c = s.cluster().create("staroid-python it-test-namespace")
+
+        # when create a namespace
+        ns_api = s.namespace(c)
+        ns = ns_api.create("instance1", "GITHUB/staroids/namespace:master")
+
+        # then namespace becomes RUNNING
+        wait_for_phase(ns_api, ns, "RUNNING")
+        self.assertEqual("RUNNING", ns_api.get_by_id(ns.id()).phase())
+
+        # start shell
+        ns_api.shell_start("instance1")
+
+        resources = ns_api.get_all_resources("instance1")
+        self.assertTrue(len(resources["services"]) > 0)
+
+        # start tunnel
+        ns_api.start_tunnel("instance1", ["57683:localhost:57683"])
+
+        # stop tunnel
+        ns_api.stop_tunnel("instance1")
+
+        # stop shell
+        ns_api.shell_stop("instance1")
+
+        # pause
+        ns = ns_api.stop("instance1")
+        wait_for_phase(ns_api, ns, "PAUSED")
+        self.assertEqual("PAUSED", ns_api.get_by_id(ns.id()).phase())
+
+        # resume
+        ns = ns_api.start("instance1")
+        wait_for_phase(ns_api, ns, "RUNNING")
+        self.assertEqual("RUNNING", ns_api.get_by_id(ns.id()).phase())
+
+        # when delete a namespace
+        ns = ns_api.delete("instance1")
+
+        # then namespace becomes REMOVED
+        wait_for_phase(ns_api, ns, "REMOVED")
+        self.assertEqual("REMOVED", ns_api.get_by_id(ns.id()).phase())
+
+        # when delete
+        s.cluster().delete("staroid-python it-test-namespace")
