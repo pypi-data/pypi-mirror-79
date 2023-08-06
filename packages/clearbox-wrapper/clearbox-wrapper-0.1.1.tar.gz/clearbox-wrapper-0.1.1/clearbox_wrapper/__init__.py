105ce44ecafac60619d00a582126472c4d9f1f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:30 +0000

    Add package clearbox-wrapper/0.1.1/clearbox-wrapper-0.1.1.tar.gz

---
+++ packages/clearbox-wrapper/clearbox-wrapper-0.1.1.tar.gz/clearbox-wrapper-0.1.1/clearbox_wrapper/__init__.py
@@ -0,0 +1,11 @@
+from .SklearnWrapper import SklearnWrapper
+from .XgboostWrapper import XgboostWrapper
+from .PytorchWrapper import PytorchWrapper
+from .KerasWrapper import KerasWrapper
+
+__all__ = [
+    "SklearnWrapper",
+    "XgboostWrapper",
+    "PytorchWrapper",
+    "KerasWrapper"
+]
