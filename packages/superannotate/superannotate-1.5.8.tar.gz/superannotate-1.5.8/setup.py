0c1eb33cd1c6f4933a450802294dfc705b53f926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:23 +0000

    Add package superannotate/1.5.8/superannotate-1.5.8.tar.gz

---
+++ packages/superannotate/superannotate-1.5.8.tar.gz/superannotate-1.5.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read()
+
+requirements = requirements.splitlines()
+
+with open('README.md') as f:
+    readme = f.read()
+
+readme = readme[2:]
+
+packages = find_packages()
+
+setup(
+    name='superannotate',
+    version='1.5.8',
+    description='Python SDK to SuperAnnotate platform',
+    license='MIT',
+    author='Hovnatan Karapetyan',
+    author_email='hovnatan@superannotate.com',
+    url='https://github.com/superannotateai/superannotate-python-sdk',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    setup_requires=['wheel'],
+    packages=find_packages(exclude=('tests', )),
+    # entry_points={
+    #     'console_scripts': ['superannotate = superannotate.__main__:main']
+    # },
+    python_requires='>=3.6'
+)
