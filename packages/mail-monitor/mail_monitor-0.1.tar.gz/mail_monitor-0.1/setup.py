83f994152efe5451aa9e96b875fdc352756d9a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:22 +0000

    Add package mail-monitor/0.1/mail_monitor-0.1.tar.gz

---
+++ packages/mail-monitor/mail_monitor-0.1.tar.gz/mail_monitor-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mail_monitor',
+    version='0.1',
+    packages=['mail_monitor'],
+    url='https://github.com/OpenJarbas/mail_monitor',
+    license='Apache',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='simple util to monitor new emails'
+)
