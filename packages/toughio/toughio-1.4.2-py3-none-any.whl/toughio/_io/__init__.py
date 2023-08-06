8ff89e2a75295d4ca8b944fd53dc38779ab90924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:40 +0000

    Add package toughio/1.4.2/toughio-1.4.2-py3-none-any.whl

---
+++ packages/toughio/toughio-1.4.2-py3-none-any.whl/toughio/_io/__init__.py
@@ -0,0 +1,15 @@
+from .input import read as read_input
+from .input import write as write_input
+from .output import Output
+from .output import read as read_output
+from .output import read_history
+from .output import write as write_output
+
+__all__ = [
+    "Output",
+    "read_input",
+    "write_input",
+    "read_output",
+    "write_output",
+    "read_history",
+]
