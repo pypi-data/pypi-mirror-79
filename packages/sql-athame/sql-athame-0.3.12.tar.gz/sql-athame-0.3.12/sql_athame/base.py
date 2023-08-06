02f1ceb3974834b02efc6c75878000e6c2b55a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:02 +0000

    Add package sql-athame/0.3.12/sql-athame-0.3.12.tar.gz

---
+++ packages/sql-athame/sql-athame-0.3.12.tar.gz/sql-athame-0.3.12/sql_athame/base.py
@@ -0,0 +1,345 @@
+import dataclasses
+import json
+import re
+import string
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    Iterator,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    Union,
+    cast,
+    overload,
+)
+
+from .escape import escape
+
+
+@dataclasses.dataclass(eq=False)
+class Placeholder:
+    __slots__ = ["name"]
+    name: str
+
+    def __repr__(self):
+        return f"Placeholder(id={id(self)}, name={repr(self.name)})"
+
+
+@dataclasses.dataclass(frozen=True)
+class Slot:
+    __slots__ = ["name"]
+    name: str
+
+
+Part = Union[str, Placeholder, Slot, "Fragment"]
+FlatPart = Union[str, Placeholder, Slot]
+
+
+newline_whitespace_re = re.compile(r"\s*\n\s*")
+auto_numbered_re = re.compile(r"[A-Za-z0-9_]")
+
+
+def auto_numbered(field_name):
+    return not auto_numbered_re.match(field_name)
+
+
+def process_slot_value(
+    name: str,
+    value: Any,
+    values: Dict[Placeholder, Any],
+    placeholders: Dict[str, Placeholder],
+) -> Union["Fragment", Placeholder]:
+    if isinstance(value, Fragment):
+        return value
+    else:
+        if name not in placeholders:
+            placeholders[name] = Placeholder(name)
+            values[placeholders[name]] = value
+        return placeholders[name]
+
+
+@dataclasses.dataclass
+class Fragment:
+    __slots__ = ["parts", "values"]
+    parts: List[Part]
+    values: Dict[Placeholder, Any]
+
+    def flatten_into(self, parts: List[FlatPart], values: Dict[Placeholder, Any]):
+        for part in self.parts:
+            if isinstance(part, Fragment):
+                part.flatten_into(parts, values)
+            elif isinstance(part, Placeholder):
+                parts.append(part)
+                values[part] = self.values[part]
+            else:
+                parts.append(part)
+
+    def compile(self) -> Callable[..., "Fragment"]:
+        flattened = self.flatten()
+        env = dict(
+            in_values=flattened.values,
+            process_slot_value=process_slot_value,
+            Fragment=Fragment,
+        )
+        func = [
+            "def compiled(**slots):",
+            " values = in_values.copy()",
+            " placeholders = {}",
+            " return Fragment([",
+        ]
+        for i, part in enumerate(flattened.parts):
+            if isinstance(part, Slot):
+                func.append(
+                    f"  process_slot_value({repr(part.name)}, slots[{repr(part.name)}], values, placeholders),"
+                )
+            elif isinstance(part, str):
+                func.append(f"  {repr(part)},")
+            else:
+                env[f"part_{i}"] = part
+                func.append(f"  part_{i},")
+        func += [" ], values)"]
+        exec("\n".join(func), env)
+        return env["compiled"]  # type: ignore
+
+    def flatten(self) -> "Fragment":
+        parts: List[FlatPart] = []
+        values: Dict[Placeholder, Any] = {}
+        self.flatten_into(parts, values)
+        out_parts: List[Part] = []
+        for part in parts:
+            if isinstance(part, str) and out_parts and isinstance(out_parts[-1], str):
+                out_parts[-1] += part
+            else:
+                out_parts.append(part)
+        return Fragment(out_parts, values)
+
+    def fill(self, **kwargs) -> "Fragment":
+        parts: List[Part] = []
+        values: Dict[Placeholder, Any] = {}
+        self.flatten_into(cast(List[FlatPart], parts), values)
+        placeholders: Dict[str, Placeholder] = {}
+        for i, part in enumerate(parts):
+            if isinstance(part, Slot):
+                parts[i] = process_slot_value(
+                    part.name, kwargs[part.name], values, placeholders
+                )
+        return Fragment(parts, values)
+
+    def prep_query(self, allow_slots=False):
+        parts: List[FlatPart] = []
+        values: Dict[Placeholder, Any] = {}
+        self.flatten_into(parts, values)
+        args: List[Union[Placeholder, Slot]] = []
+        placeholder_ids: Dict[Placeholder, int] = {}
+        slot_ids: Dict[Slot, int] = {}
+        out_parts: List[str] = []
+        for part in parts:
+            if isinstance(part, Slot):
+                if not allow_slots:
+                    raise ValueError(f"Unfilled slot: {repr(part.name)}")
+                if part not in slot_ids:
+                    args.append(part)
+                    slot_ids[part] = len(args)
+                out_parts.append(f"${slot_ids[part]}")
+            elif isinstance(part, Placeholder):
+                if part not in placeholder_ids:
+                    args.append(part)
+                    placeholder_ids[part] = len(args)
+                out_parts.append(f"${placeholder_ids[part]}")
+            else:
+                assert isinstance(part, str)
+                out_parts.append(part)
+        return "".join(out_parts).strip(), values, args
+
+    def query(self) -> Tuple[str, List[Any]]:
+        query, values, args = self.prep_query()
+        placeholder_values = [values[arg] for arg in args]
+        return query, placeholder_values
+
+    def prepare(self) -> Tuple[str, Callable[..., List[Any]]]:
+        query, values, args = self.prep_query(allow_slots=True)
+        env = dict()
+        func = [
+            "def generate_args(**kwargs):",
+            " return [",
+        ]
+        for i, arg in enumerate(args):
+            if isinstance(arg, Slot):
+                func.append(f"  kwargs[{repr(arg.name)}],")
+            else:
+                env[f"value_{i}"] = values[arg]
+                func.append(f"  value_{i},")
+        func += [" ]"]
+        exec("\n".join(func), env)
+        return query, env["generate_args"]  # type: ignore
+
+    def __iter__(self) -> Iterator[Any]:
+        sql, args = self.query()
+        return iter((sql, *args))
+
+    def join(self, parts: Iterable["Fragment"]) -> "Fragment":
+        return Fragment(list(join_parts(parts, infix=self)), {})
+
+
+class SQLFormatter:
+    def __call__(
+        self, fmt: str, *args, preserve_formatting=False, **kwargs
+    ) -> Fragment:
+        if not preserve_formatting:
+            fmt = newline_whitespace_re.sub(" ", fmt)
+        fmtr = string.Formatter()
+        parts: List[Part] = []
+        values: Dict[Placeholder, Any] = {}
+        placeholders: Dict[str, Placeholder] = {}
+        next_auto_field = 0
+        for literal_text, field_name, format_spec, conversion in fmtr.parse(fmt):
+            parts.append(literal_text)
+            if field_name is not None:
+                if auto_numbered(field_name):
+                    field_name = f"{next_auto_field}{field_name}"
+                    next_auto_field += 1
+                try:
+                    value = fmtr.get_field(field_name, args, kwargs)[0]
+                except KeyError:
+                    value = Slot(field_name)
+                if isinstance(value, Fragment) or isinstance(value, Slot):
+                    parts.append(value)
+                else:
+                    if field_name not in placeholders:
+                        placeholders[field_name] = Placeholder(field_name)
+                    part = placeholders[field_name]
+                    parts.append(part)
+                    values[part] = value
+        return Fragment(parts, values)
+
+    @staticmethod
+    def value(value: Any):
+        placeholder = Placeholder("value")
+        return Fragment([placeholder], {placeholder: value})
+
+    @staticmethod
+    def escape(value: Any) -> Fragment:
+        return lit(escape(value))
+
+    @staticmethod
+    def slot(name: str) -> Fragment:
+        return Fragment([Slot(name)], {})
+
+    @staticmethod
+    def literal(text: str):
+        return Fragment([text], {})
+
+    @staticmethod
+    def identifier(name: str, prefix: Optional[str] = None):
+        if prefix:
+            return lit(f"{quote_identifier(prefix)}.{quote_identifier(name)}")
+        else:
+            return lit(f"{quote_identifier(name)}")
+
+    @overload
+    def all(self, parts: Iterable[Fragment]) -> Fragment:
+        ...
+
+    @overload
+    def all(self, *parts: Fragment) -> Fragment:
+        ...
+
+    def all(self, *parts) -> Fragment:  # type: ignore
+        if parts and not isinstance(parts[0], Fragment):
+            parts = parts[0]
+        return any_all(list(parts), "AND", "TRUE")
+
+    @overload
+    def any(self, parts: Iterable[Fragment]) -> Fragment:
+        ...
+
+    @overload
+    def any(self, *parts: Fragment) -> Fragment:
+        ...
+
+    def any(self, *parts) -> Fragment:  # type: ignore
+        if parts and not isinstance(parts[0], Fragment):
+            parts = parts[0]
+        return any_all(list(parts), "OR", "FALSE")
+
+    @overload
+    def list(self, parts: Iterable[Fragment]) -> Fragment:
+        ...
+
+    @overload
+    def list(self, *parts: Fragment) -> Fragment:
+        ...
+
+    def list(self, *parts) -> Fragment:  # type: ignore
+        if parts and not isinstance(parts[0], Fragment):
+            parts = parts[0]
+        return Fragment(list(join_parts(parts, infix=", ")), {})
+
+    @staticmethod
+    def unnest(data: Iterable[Sequence[Any]], types: Iterable[str]) -> Fragment:
+        nested = list(nest_for_type(x, t) for x, t in zip(zip(*data), types))
+        if not nested:
+            nested = list(nest_for_type([], t) for t in types)
+        return Fragment(["UNNEST(", sql.list(nested), ")"], {})
+
+
+sql = SQLFormatter()
+
+
+json_types = ("JSON", "JSONB")
+
+
+def is_json_type(typename: str) -> bool:
+    return typename.upper() in json_types
+
+
+def nest_for_type(data: Sequence[Any], typename: str) -> Fragment:
+    ph = Placeholder("data")
+
+    if is_json_type(typename):
+        # https://github.com/MagicStack/asyncpg/issues/345
+
+        # KLUDGE - this doesn't work for trying to store literal
+        # strings when autoconverting; None is treated as SQL NULL
+        processed_data = [
+            x if x is None or isinstance(x, str) else json.dumps(x) for x in data
+        ]
+        return Fragment([ph, f"::TEXT[]::{typename}[]"], {ph: processed_data})
+    else:
+        return Fragment([ph, f"::{typename}[]"], {ph: data})
+
+
+def lit(text: str):
+    return Fragment([text], {})
+
+
+def any_all(frags: List[Fragment], op: str, base_case: str):
+    if not frags:
+        return lit(base_case)
+    parts = join_parts(frags, prefix="(", infix=f") {op} (", suffix=")")
+    return Fragment(list(parts), {})
+
+
+def join_parts(
+    parts: Iterable[Part],
+    infix: Part,
+    prefix: Optional[Part] = None,
+    suffix: Optional[Part] = None,
+) -> Iterator[Part]:
+    if prefix:
+        yield prefix
+    for i, part in enumerate(parts):
+        if i:
+            yield infix
+        yield part
+    if suffix:
+        yield suffix
+
+
+def quote_identifier(name: str):
+    quoted = name.replace('"', '""')
+    return f'"{quoted}"'
---
+++ packages/sql-athame/sql-athame-0.3.12.tar.gz/sql-athame-0.3.12/sql_athame/dataclasses.py
@@ -0,0 +1,433 @@
+import datetime
+import uuid
+from dataclasses import dataclass, field, fields
+from typing import (
+    Any,
+    AsyncGenerator,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Mapping,
+    Optional,
+    Set,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+)
+
+from .base import Fragment, sql
+
+Where = Union[Fragment, Iterable[Fragment]]
+# KLUDGE to avoid a string argument being valid
+SequenceOfStrings = Union[List[str], Tuple[str, ...]]
+FieldNames = SequenceOfStrings
+FieldNamesSet = Union[SequenceOfStrings, Set[str]]
+
+
+@dataclass
+class ColumnInfo:
+    type: str
+    create_type: str
+    constraints: Tuple[str, ...]
+
+    def create_table_string(self):
+        return " ".join((self.create_type, *self.constraints))
+
+
+def model_field_metadata(
+    type: str, constraints: Union[str, Iterable[str]] = ()
+) -> Dict[str, Any]:
+    if isinstance(constraints, str):
+        constraints = (constraints,)
+    info = ColumnInfo(
+        sql_create_type_map.get(type.upper(), type), type, tuple(constraints)
+    )
+    return {"sql_athame": info}
+
+
+def model_field(*, type: str, constraints: Union[str, Iterable[str]] = (), **kwargs):
+    return field(**kwargs, metadata=model_field_metadata(type, constraints))  # type: ignore
+
+
+sql_create_type_map = {
+    "BIGSERIAL": "BIGINT",
+    "SERIAL": "INTEGER",
+    "SMALLSERIAL": "SMALLINT",
+}
+
+
+sql_type_map = {
+    Optional[bool]: ("BOOLEAN",),
+    Optional[bytes]: ("BYTEA",),
+    Optional[datetime.date]: ("DATE",),
+    Optional[datetime.datetime]: ("TIMESTAMP",),
+    Optional[float]: ("DOUBLE PRECISION",),
+    Optional[int]: ("INTEGER",),
+    Optional[str]: ("TEXT",),
+    Optional[uuid.UUID]: ("UUID",),
+    bool: ("BOOLEAN", "NOT NULL"),
+    bytes: ("BYTEA", "NOT NULL"),
+    datetime.date: ("DATE", "NOT NULL"),
+    datetime.datetime: ("TIMESTAMP", "NOT NULL"),
+    float: ("DOUBLE PRECISION", "NOT NULL"),
+    int: ("INTEGER", "NOT NULL"),
+    str: ("TEXT", "NOT NULL"),
+    uuid.UUID: ("UUID", "NOT NULL"),
+}
+
+
+def column_info_for_field(field):
+    if "sql_athame" in field.metadata:
+        return field.metadata["sql_athame"]
+    type, *constraints = sql_type_map[field.type]
+    return ColumnInfo(type, type, tuple(constraints))
+
+
+T = TypeVar("T", bound="ModelBase")
+U = TypeVar("U")
+
+
+class ModelBase(Mapping[str, Any]):
+    _column_info: Optional[Dict[str, ColumnInfo]]
+    _cache: Dict[tuple, Any]
+    table_name: str
+    primary_key_names: Tuple[str, ...]
+
+    def __init_subclass__(
+        cls, *, table_name: str, primary_key: Union[FieldNames, str] = (), **kwargs
+    ):
+        cls._cache = {}
+        cls.table_name = table_name
+        if isinstance(primary_key, str):
+            cls.primary_key_names = (primary_key,)
+        else:
+            cls.primary_key_names = tuple(primary_key)
+
+    @classmethod
+    def _cached(cls, key: tuple, thunk: Callable[[], U]) -> U:
+        try:
+            return cls._cache[key]
+        except KeyError:
+            cls._cache[key] = thunk()
+            return cls._cache[key]
+
+    def keys(self):
+        return self.field_names()
+
+    def __getitem__(self, key: str):
+        return getattr(self, key)
+
+    def __iter__(self):
+        return iter(self.keys())
+
+    def __len__(self):
+        return len(self.keys())
+
+    def get(self, key, default=None):
+        return getattr(self, key, default)
+
+    @classmethod
+    def column_info(cls, column: str) -> ColumnInfo:
+        try:
+            return cls._column_info[column]  # type: ignore
+        except AttributeError:
+            cls._column_info = {f.name: column_info_for_field(f) for f in fields(cls)}
+            return cls._column_info[column]
+
+    @classmethod
+    def table_name_sql(cls, *, prefix=None) -> Fragment:
+        return sql.identifier(cls.table_name, prefix=prefix)
+
+    @classmethod
+    def primary_key_names_sql(cls, *, prefix=None) -> List[Fragment]:
+        return [sql.identifier(pk, prefix=prefix) for pk in cls.primary_key_names]
+
+    @classmethod
+    def field_names(cls, *, exclude: FieldNamesSet = ()) -> List[str]:
+        return [f.name for f in fields(cls) if f.name not in exclude]
+
+    @classmethod
+    def field_names_sql(
+        cls, *, prefix=None, exclude: FieldNamesSet = ()
+    ) -> List[Fragment]:
+        return [
+            sql.identifier(f, prefix=prefix) for f in cls.field_names(exclude=exclude)
+        ]
+
+    def primary_key(self) -> tuple:
+        return tuple(getattr(self, pk) for pk in self.primary_key_names)
+
+    @classmethod
+    def _get_field_values_fn(cls, exclude: FieldNamesSet = ()):
+        env: Dict[str, Any] = dict()
+        func = ["def get_field_values(self): return ["]
+        for f in fields(cls):
+            if f.name not in exclude:
+                func.append(f"self.{f.name},")
+        func += ["]"]
+        exec(" ".join(func), env)
+        return env["get_field_values"]
+
+    def field_values(self, *, exclude: FieldNamesSet = ()) -> List[Any]:
+        get_field_values = self._cached(
+            ("get_field_values", tuple(sorted(exclude))),
+            lambda: self._get_field_values_fn(exclude),
+        )
+        return get_field_values(self)
+
+    def field_values_sql(
+        self, *, exclude: FieldNamesSet = (), default_none=False
+    ) -> List[Fragment]:
+        if default_none:
+            return [
+                sql.literal("DEFAULT") if value is None else sql.value(value)
+                for value in self.field_values()
+            ]
+        else:
+            return [sql.value(value) for value in self.field_values()]
+
+    @classmethod
+    def from_tuple(
+        cls: Type[T], tup: tuple, *, offset=0, exclude: FieldNamesSet = ()
+    ) -> T:
+        names = (f.name for f in fields(cls) if f.name not in exclude)
+        kwargs = {name: tup[offset] for offset, name in enumerate(names, start=offset)}
+        return cls(**kwargs)  # type: ignore
+
+    @classmethod
+    def from_dict(
+        cls: Type[T], dct: Dict[str, Any], *, exclude: FieldNamesSet = ()
+    ) -> T:
+        names = {f.name for f in fields(cls) if f.name not in exclude}
+        kwargs = {k: v for k, v in dct.items() if k in names}
+        return cls(**kwargs)  # type: ignore
+
+    @classmethod
+    def ensure_model(cls: Type[T], row: Union[T, Mapping[str, Any]]) -> T:
+        if isinstance(row, cls):
+            return row
+        return cls(**row)  # type: ignore
+
+    @classmethod
+    def create_table_sql(cls) -> Fragment:
+        entries = [
+            sql(
+                "{} {}",
+                sql.identifier(f.name),
+                sql.literal(cls.column_info(f.name).create_table_string()),
+            )
+            for f in fields(cls)
+        ]
+        if cls.primary_key_names:
+            entries += [sql("PRIMARY KEY ({})", sql.list(cls.primary_key_names_sql()))]
+        return sql(
+            "CREATE TABLE IF NOT EXISTS {table} ({entries})",
+            table=cls.table_name_sql(),
+            entries=sql.list(entries),
+        )
+
+    @classmethod
+    def select_sql(
+        cls, where: Where = (), order_by: Union[FieldNames, str] = (), for_update=False
+    ) -> Fragment:
+        if isinstance(order_by, str):
+            order_by = (order_by,)
+        if not isinstance(where, Fragment):
+            where = sql.all(where)
+        cached = cls._cached(
+            ("select_sql", tuple(order_by)),
+            lambda: sql(
+                "SELECT {fields} FROM {name} WHERE {where}{order}",
+                fields=sql.list(cls.field_names_sql()),
+                name=cls.table_name_sql(),
+                order=(
+                    sql(" ORDER BY {}", sql.list(sql.identifier(x) for x in order_by))
+                    if order_by
+                    else sql.literal("")
+                ),
+            ).compile(),
+        )
+        query = cached(where=where)
+        if for_update:
+            query = Fragment([query, " FOR UPDATE"], {})
+        return query
+
+    @classmethod
+    async def select_cursor(
+        cls: Type[T],
+        connection,
+        order_by: Union[FieldNames, str] = (),
+        for_update=False,
+        where: Where = (),
+    ) -> AsyncGenerator[T, None]:
+        async for row in connection.cursor(
+            *cls.select_sql(order_by=order_by, for_update=for_update, where=where)
+        ):
+            yield cls(**row)  # type: ignore
+
+    @classmethod
+    async def select(
+        cls: Type[T],
+        connection_or_pool,
+        order_by: Union[FieldNames, str] = (),
+        for_update=False,
+        where: Where = (),
+    ) -> List[T]:
+        return [
+            cls(**row)  # type: ignore
+            for row in await connection_or_pool.fetch(
+                *cls.select_sql(order_by=order_by, for_update=for_update, where=where)
+            )
+        ]
+
+    @classmethod
+    def create_sql(cls: Type[T], **kwargs) -> Fragment:
+        return sql(
+            "INSERT INTO {table} ({fields}) VALUES ({values}) RETURNING {out_fields}",
+            table=cls.table_name_sql(),
+            fields=sql.list(sql.identifier(x) for x in kwargs.keys()),
+            values=sql.list(sql.value(x) for x in kwargs.values()),
+            out_fields=sql.list(cls.field_names_sql()),
+        )
+
+    @classmethod
+    async def create(cls: Type[T], connection_or_pool, **kwargs) -> T:
+        row = await connection_or_pool.fetchrow(*cls.create_sql(**kwargs))
+        return cls(**row)  # type: ignore
+
+    def insert_sql(self, exclude: FieldNamesSet = ()) -> Fragment:
+        cached = self._cached(
+            ("insert_sql", tuple(sorted(exclude))),
+            lambda: sql(
+                "INSERT INTO {table} ({fields}) VALUES ({values})",
+                table=self.table_name_sql(),
+                fields=sql.list(self.field_names_sql(exclude=exclude)),
+            ).compile(),
+        )
+        return cached(
+            values=sql.list(self.field_values_sql(exclude=exclude, default_none=True)),
+        )
+
+    async def insert(self, connection_or_pool, exclude: FieldNamesSet = ()):
+        await connection_or_pool.execute(*self.insert_sql(exclude))
+
+    @classmethod
+    def upsert_sql(cls, insert_sql: Fragment, exclude: FieldNamesSet = ()) -> Fragment:
+        cached = cls._cached(
+            ("upsert_sql", tuple(sorted(exclude))),
+            lambda: sql(
+                " ON CONFLICT ({pks}) DO UPDATE SET {assignments}",
+                insert_sql=insert_sql,
+                pks=sql.list(cls.primary_key_names_sql()),
+                assignments=sql.list(
+                    sql("{field}=EXCLUDED.{field}", field=x)
+                    for x in cls.field_names_sql(
+                        exclude=(*cls.primary_key_names, *exclude)
+                    )
+                ),
+            ).flatten(),
+        )
+        return Fragment([insert_sql, cached], {})
+
+    async def upsert(self, connection_or_pool, exclude: FieldNamesSet = ()) -> bool:
+        query = sql(
+            "{} RETURNING xmax",
+            self.upsert_sql(self.insert_sql(exclude=exclude), exclude=exclude),
+        )
+        result = await connection_or_pool.fetchrow(*query)
+        is_update = result["xmax"] != 0
+        return is_update
+
+    @classmethod
+    def delete_multiple_sql(cls: Type[T], rows: Iterable[T]) -> Fragment:
+        cached = cls._cached(
+            ("delete_multiple_sql",),
+            lambda: sql(
+                "DELETE FROM {table} WHERE ({pks}) IN (SELECT * FROM {unnest})",
+                table=cls.table_name_sql(),
+                pks=sql.list(sql.identifier(pk) for pk in cls.primary_key_names),
+            ).compile(),
+        )
+        return cached(
+            unnest=sql.unnest(
+                (row.primary_key() for row in rows),
+                (cls.column_info(pk).type for pk in cls.primary_key_names),
+            ),
+        )
+
+    @classmethod
+    async def delete_multiple(cls: Type[T], connection_or_pool, rows: Iterable[T]):
+        await connection_or_pool.execute(*cls.delete_multiple_sql(rows))
+
+    @classmethod
+    def insert_multiple_sql(cls: Type[T], rows: Iterable[T]) -> Fragment:
+        cached = cls._cached(
+            ("insert_multiple_sql",),
+            lambda: sql(
+                "INSERT INTO {table} ({fields}) SELECT * FROM {unnest}",
+                table=cls.table_name_sql(),
+                fields=sql.list(cls.field_names_sql()),
+            ).compile(),
+        )
+        return cached(
+            unnest=sql.unnest(
+                (row.field_values() for row in rows),
+                (cls.column_info(name).type for name in cls.field_names()),
+            ),
+        )
+
+    @classmethod
+    async def insert_multiple(cls: Type[T], connection_or_pool, rows: Iterable[T]):
+        await connection_or_pool.execute(*cls.insert_multiple_sql(rows))
+
+    @classmethod
+    async def upsert_multiple(cls: Type[T], connection_or_pool, rows: Iterable[T]):
+        await connection_or_pool.execute(*cls.upsert_sql(cls.insert_multiple_sql(rows)))
+
+    @classmethod
+    async def replace_multiple(
+        cls: Type[T],
+        connection,
+        rows: Union[Iterable[T], Iterable[Mapping[str, Any]]],
+        *,
+        where: Where,
+        ignore: FieldNamesSet = (),
+    ) -> Tuple[List[T], List[T], List[T]]:
+        pending = {row.primary_key(): row for row in map(cls.ensure_model, rows)}
+
+        updated = []
+        deleted = []
+
+        async for old in cls.select_cursor(
+            connection, where=where, order_by=cls.primary_key_names, for_update=True
+        ):
+            pk = old.primary_key()
+            if pk not in pending:
+                deleted.append(old)
+            else:
+                if not equal_ignoring(old, pending[pk], ignore):
+                    updated.append(pending[pk])
+                del pending[pk]
+
+        created = list(pending.values())
+
+        if created or updated:
+            await cls.upsert_multiple(connection, (*created, *updated))
+        if deleted:
+            await cls.delete_multiple(connection, deleted)
+
+        return created, updated, deleted
+
+
+def equal_ignoring(
+    old: Mapping[str, Any], new: Mapping[str, Any], ignore: FieldNamesSet
+) -> bool:
+    keys = (*old.keys(), *new.keys())
+    for key in keys:
+        if key in ignore:
+            continue
+        if old.get(key, None) != new.get(key, None):
+            return False
+    return True
