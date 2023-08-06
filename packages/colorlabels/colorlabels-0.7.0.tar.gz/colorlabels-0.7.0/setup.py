4cbde5aecdc492424880cc494770921f29de5f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:24 +0000

    Add package colorlabels/0.7.0/colorlabels-0.7.0.tar.gz

---
+++ packages/colorlabels/colorlabels-0.7.0.tar.gz/colorlabels-0.7.0/setup.py
@@ -0,0 +1,38 @@
+from io import open
+
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='colorlabels',
+    version='0.7.0',
+    description='Provides colorful and semantic labels in console. '
+                'Tailored for message display and interaction in automated scripts.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gousaiyang/colorlabels',
+    author='Saiyang Gou',
+    author_email='gousaiyang223@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: Widget Sets',
+        'Topic :: System :: Logging',
+        'Topic :: Terminals',
+        'Topic :: Utilities',
+    ],
+    keywords='console terminal color message display animation progress progressbar',
+    py_modules=['colorlabels'],
+    install_requires=['colorama;platform_system=="Windows"'],
+)
