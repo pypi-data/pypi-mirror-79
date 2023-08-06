35399736f1fe1d6288afc8f0a5b108e97645ab32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:09 +0000

    Add package webdataset/0.1.40/webdataset-0.1.40-py3-none-any.whl

---
+++ packages/webdataset/webdataset-0.1.40-py3-none-any.whl/webdataset/__init__.py
@@ -0,0 +1,27 @@
+#
+# Copyright (c) 2017-2019 NVIDIA CORPORATION. All rights reserved.
+# This file is part of the WebDataset library.
+# See the LICENSE file for licensing terms (BSD-style).
+#
+# flake8: noqa
+
+__all__ = "tenbin dataset writer".split()
+
+from . import tenbin
+from .dataset import (
+    Dataset,
+    ChoppedDataset,
+    ResizedDataset,
+    Pipeline,
+    SampleIterator,
+    reraise_exception,
+    ignore_and_continue,
+    warn_and_continue,
+    ignore_and_stop,
+    warn_and_stop,
+    worker_urls,
+    all_urls
+)
+from .writer import ShardWriter, TarWriter
+from .multi import MultiDataset
+from .autodecode import imagehandler, torch_video, torch_audio, Decoder, gzfilter
