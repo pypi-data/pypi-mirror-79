cd3d97c43aba22076c09fb061eb3e8f27f599d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:01 +0000

    Add package wordsolver/0.1.0/wordsolver-0.1.0.tar.gz

---
+++ packages/wordsolver/wordsolver-0.1.0.tar.gz/wordsolver-0.1.0/wordsolver/__init__.py
@@ -0,0 +1,4 @@
+from wordsolver.boggle import BoggleSolver
+from wordsolver.scrabble import ScrabbleSolver, VALUE_STANDARD, PREMIUM_STANDARD, EMPTY_STANDARD
+from wordsolver.hangman import HangmanSolver
+from wordsolver.wordsearch import WordSearchSolver
