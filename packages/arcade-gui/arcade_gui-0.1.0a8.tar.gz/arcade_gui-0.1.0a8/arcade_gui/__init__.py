8545338be9f0a29727e7de75c6aa65bc2863473b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:16 +0000

    Add package arcade-gui/0.1.0a8/arcade_gui-0.1.0a8.tar.gz

---
+++ packages/arcade-gui/arcade_gui-0.1.0a8.tar.gz/arcade_gui-0.1.0a8/arcade_gui/__init__.py
@@ -0,0 +1,37 @@
+__version__ = '0.1.0'
+
+from arcade_gui.button import UIButton
+from arcade_gui.core import (
+    UIEvent,
+    UIView,
+    UIElement,
+    UIException,
+    MOUSE_PRESS,
+    MOUSE_RELEASE,
+    MOUSE_SCROLL,
+    KEY_PRESS,
+    KEY_RELEASE,
+    TEXT_INPUT,
+    TEXT_MOTION,
+    TEXT_MOTION_SELECTION
+)
+from arcade_gui.inputbox import UIInputBox
+from arcade_gui.label import UILabel
+
+__all__ = [
+    UIEvent,
+    UIView,
+    UIElement,
+    UIException,
+    UILabel,
+    UIButton,
+    UIInputBox,
+    MOUSE_PRESS,
+    MOUSE_RELEASE,
+    MOUSE_SCROLL,
+    KEY_PRESS,
+    KEY_RELEASE,
+    TEXT_INPUT,
+    TEXT_MOTION,
+    TEXT_MOTION_SELECTION,
+]
