66888e2c9c63511684c605d8b8df17b3b1baf69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:29 +0000

    Add package perip/0.1.4/perip-0.1.4.tar.gz

---
+++ packages/perip/perip-0.1.4.tar.gz/perip-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['perip']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aosong>=0.0.2,<0.0.3', 'redis>=3.5.3,<4.0.0']
+
+setup_kwargs = {
+    'name': 'perip',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'David J Barnes',
+    'author_email': 'david@onepointone.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
