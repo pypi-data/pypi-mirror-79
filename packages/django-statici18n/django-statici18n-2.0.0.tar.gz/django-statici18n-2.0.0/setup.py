7e10183534ca85b7decd925d187d9fba7e000b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:26 +0000

    Add package django-statici18n/2.0.0/django-statici18n-2.0.0.tar.gz

---
+++ packages/django-statici18n/django-statici18n-2.0.0.tar.gz/django-statici18n-2.0.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-statici18n",
+    version="2.0.0",
+    author="Sebastien Fievet",
+    author_email="zyegfryed@gmail.com",
+    url="http://django-statici18n.readthedocs.org/",
+    description="A Django app that provides helper for generating "
+                "Javascript catalog to static files.",
+    long_description=open('README.rst').read(),
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "Django>=1.8",
+        "django-appconf>=0.6",
+        "six",
+    ],
+    license="BSD",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
