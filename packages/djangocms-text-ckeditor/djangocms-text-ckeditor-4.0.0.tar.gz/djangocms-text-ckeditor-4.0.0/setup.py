30a696690da277a2f822572b0800111c796edbf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:16 +0000

    Add package djangocms-text-ckeditor/4.0.0/djangocms-text-ckeditor-4.0.0.tar.gz

---
+++ packages/djangocms-text-ckeditor/djangocms-text-ckeditor-4.0.0.tar.gz/djangocms-text-ckeditor-4.0.0/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+from djangocms_text_ckeditor import __version__
+
+
+REQUIREMENTS = [
+    "django-cms>=3.6",
+    "html5lib>=1",
+    "Pillow",
+]
+
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Framework :: Django",
+    "Framework :: Django :: 2.2",
+    "Framework :: Django :: 3.0",
+    "Framework :: Django :: 3.1",
+    "Framework :: Django CMS",
+    "Framework :: Django CMS :: 3.6",
+    "Framework :: Django CMS :: 3.7",
+    "Framework :: Django CMS :: 3.8",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries",
+]
+
+
+setup(
+    name="djangocms-text-ckeditor",
+    version=__version__,
+    author="Divio AG",
+    author_email="info@divio.ch",
+    url="https://github.com/divio/djangocms-text-ckeditor",
+    license="BSD-3-Clause",
+    description="Text Plugin for django CMS with CKEditor support",
+    long_description=open("README.rst").read(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite="tests.settings.test",
+)
