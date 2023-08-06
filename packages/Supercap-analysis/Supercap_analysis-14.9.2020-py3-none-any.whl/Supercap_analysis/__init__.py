acc9c61ebc7dc417ad5773b81f15594ad0a1fd08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:45 +0000

    Add package Supercap-analysis/14.9.2020/Supercap_analysis-14.9.2020-py3-none-any.whl

---
+++ packages/Supercap-analysis/Supercap_analysis-14.9.2020-py3-none-any.whl/Supercap_analysis/__init__.py
@@ -0,0 +1,10 @@
+name = 'Supercap_analysis'
+
+from .supercap import *
+from .load_capacitor import *
+from .glob_analysis import *
+from .cccap import *
+from .utilities import*
+
+
+__version__ ='14.09.2020'
