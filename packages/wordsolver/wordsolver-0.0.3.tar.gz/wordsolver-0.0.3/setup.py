859440d1071ca24109f8accac3180848fcf4a37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:15 +0000

    Add package wordsolver/0.0.3/wordsolver-0.0.3.tar.gz

---
+++ packages/wordsolver/wordsolver-0.0.3.tar.gz/wordsolver-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="wordsolver",
+    version="0.0.3",
+    author="Christopher Malcolm",
+    author_email="chris.c.malcolm.96@gmail.com",
+    description="A package to solve word games",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/chrismalcolm/solver",
+    packages=["wordsolver"],
+    classifiers=[
+        "Programming Language :: Python :: 3.6"
+    ],
+    install_requires=["numpy>=1.9.3", "pythonds>=1.2.1"]
+)
