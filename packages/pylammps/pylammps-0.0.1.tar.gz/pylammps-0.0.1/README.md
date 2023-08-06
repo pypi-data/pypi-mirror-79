c4cba3878c8a47dc640e1bf5c7ddb8ebf4b2222f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:35 +0000

    Add package pylammps/0.0.1/pylammps-0.0.1.tar.gz

---
+++ packages/pylammps/pylammps-0.0.1.tar.gz/pylammps-0.0.1/README.md
@@ -0,0 +1,13 @@
+# pyqe
+
+#### Description
+pylammps
+
+#### Installation
+
+1. xxxx
+2. xxxx
+3. xxxx
+
+#### Instructions
+- testing
---
+++ packages/pylammps/pylammps-0.0.1.tar.gz/pylammps-0.0.1/pylammps/constant.py
@@ -0,0 +1,13 @@
+#   coding:utf-8
+#   This file is part of Alkemiems.
+#
+#   Alkemiems is free software: you can redistribute it and/or modify
+#   it under the terms of the MIT License.
+
+__author__ = 'Guanjie Wang'
+__version__ = 1.0
+__maintainer__ = 'Guanjie Wang'
+__email__ = "gjwang@buaa.edu.cn"
+__date__ = '2020/09/17 21:18:48'
+
+ROOT_PATH = './'
