b12c7d155f6e2ac9a9676a032f76bf8f0f1097de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:10 +0000

    Add package apio-django/1.0.1/apio_django-1.0.1.tar.gz

---
+++ packages/apio-django/apio_django-1.0.1.tar.gz/apio_django-1.0.1/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="apio_django",
+    version="1.0.1",
+    description="Application error & performance monitoring",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    author="Apio Team",
+    author_email="apio.monitor@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["apio"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "square=square.__main__:main",
+        ]
+    },
+)
