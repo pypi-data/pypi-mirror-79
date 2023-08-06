d574df35fd983001a29edc249ae98caee83af55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:04 +0000

    Add package gocept.amqprun/2.1/gocept.amqprun-2.1.tar.gz

---
+++ packages/gocept.amqprun/gocept.amqprun-2.1.tar.gz/gocept.amqprun-2.1/setup.py
@@ -0,0 +1,94 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'ZConfig',
+    'kombu',
+    'setuptools',
+    'six',
+    'transaction',
+    'zope.component[zcml]',
+    'zope.configuration',
+    'zope.event',
+    'zope.interface',
+    'zope.schema',
+]
+
+readfiles_require = [
+    'gocept.filestore',
+]
+
+security_require = [
+    'zope.security>=4.0.0dev',
+]
+
+testing_require = [
+    'amqp',
+]
+
+tests_require = (
+    testing_require +
+    readfiles_require +
+    security_require + [
+        'gocept.testing',
+        'mock>=0.8.0, < 4 ; python_version=="2.7"',  # PY2
+        'plone.testing',
+        'zope.testing',
+        'zipp < 2 ; python_version=="2.7"',  # PY2
+    ])
+
+
+setup(
+    name='gocept.amqprun',
+    version='2.1',
+    author='gocept <mail at gocept dot com>',
+    author_email='mail@gocept.com',
+    url='https://github.com/NativeInstruments/gocept.amqprun',
+    description=(
+        "gocept.amqprun helps you writing and running AMQP consumers, and"
+        " sending AMQP messages. It currently only supports AMQP 0-9-1 and"
+        " integrates with the Zope Tool Kit (ZTK) so you can use adapters,"
+        " utilities and all the buzz."
+    ),
+    long_description=(
+        open('README.rst').read()
+        + '\n\n'
+        + open('CHANGES.rst').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Framework :: Zope :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'License :: OSI Approved',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development',
+    ],
+    license='ZPL',
+    namespace_packages=['gocept'],
+    install_requires=install_requires,
+    extras_require=dict(
+        test=tests_require,
+        testing=testing_require,  # use it to use amqprun's test infrastructure
+        readfiles=readfiles_require,
+        security=security_require,
+    ),
+    entry_points=dict(console_scripts=[
+        'server = gocept.amqprun.main:main',
+        'send_files = gocept.amqprun.readfiles:main',
+        'test_sender = gocept.amqprun.tests.basic:send_messages',
+    ]),
+)
---
+++ packages/gocept.amqprun/gocept.amqprun-2.1.tar.gz/gocept.amqprun-2.1/src/gocept/amqprun/tests/test_main.py
@@ -0,0 +1,170 @@
+# coding: utf8
+import gocept.amqprun.interfaces
+import gocept.amqprun.message
+import gocept.amqprun.session
+import gocept.amqprun.testing
+import gocept.amqprun.tests.integration
+import logging
+import zope.component
+import six
+
+try:
+    from unittest import mock
+except ImportError:  # PY2
+    import mock
+
+
+class ReceiveMessages(gocept.amqprun.testing.MainTestCase):
+
+    def setUp(self):
+        super(ReceiveMessages, self).setUp()
+        self.messages_received = []
+        gocept.amqprun.tests.integration.messages_received = (
+            self.messages_received)
+        self.make_config(__name__, 'integration')
+        self._queues.append('test.queue')
+        self._queues.append('test.queue.error')
+        self.start_server()
+
+    def tearDown(self):
+        gocept.amqprun.tests.integration.messages_received = None
+        super(ReceiveMessages, self).tearDown()
+
+    def test_message_should_be_processed(self):
+        self.assertEquals([], self.messages_received)
+        self.send_message('blarf', routing_key='test.routing')
+        self.server.run_once()
+        self.assertEquals(1, len(self.messages_received))
+
+    def test_technical_errors_should_not_crash(self):
+        self.assertEquals([], self.messages_received)
+        self.send_message('blarf', routing_key='test.error')
+        self.server.run_once()
+        self.assertEquals(1, len(self.messages_received))
+
+    def test_exception_handling_with_iresponse_acks_original_message(self):
+        self.assertEquals([], self.messages_received)
+        self.expect_message_on('test.iresponse-error')
+
+        def ack(session):
+            self.ack_called = True
+            original_ack(session)
+        self.ack_called = False
+        original_ack = gocept.amqprun.session.Session.ack_received_message
+
+        def flush(session):
+            if not self.flush_called:
+                self.flush_called = True
+                raise RuntimeError('provoked error')
+            original_flush(session)
+        self.flush_called = False
+        original_flush = gocept.amqprun.session.Session.flush
+
+        with mock.patch.multiple('gocept.amqprun.session.Session',
+                                 ack_received_message=ack, flush=flush):
+            self.send_message('blarf', routing_key='test.iresponse')
+            self.server.run_once()
+            self.wait_for_message()
+            self.assertTrue(self.ack_called)
+            self.assertEquals(1, len(self.messages_received))
+            self.assertTrue(self.messages_received[0]._exception)
+
+
+class ConfigLoadingTest(gocept.amqprun.testing.MainTestCase):
+
+    def setUp(self):
+        super(ConfigLoadingTest, self).setUp()
+        self.patchers = []
+        patcher = mock.patch('gocept.amqprun.server.Server')
+        self.server = patcher.start()
+        self.patchers.append(patcher)
+        patcher = mock.patch('gocept.amqprun.worker.Worker')
+        self.worker = patcher.start()
+        self.patchers.append(patcher)
+        patcher = mock.patch('sys.exit')
+        patcher.start()
+        self.patchers.append(patcher)
+
+    def tearDown(self):
+        for patcher in self.patchers:
+            patcher.stop()
+        super(ConfigLoadingTest, self).tearDown()
+
+    def test_basic_configuration_should_load_zcml(self):
+        import gocept.amqprun.interfaces
+        import gocept.amqprun.main
+        config = self.make_config(__name__, 'basic')
+        gocept.amqprun.main.main(config)
+        self.assertEquals(1, self.server.call_count)
+        utilities = list(zope.component.getUtilitiesFor(
+            gocept.amqprun.interfaces.IHandler))
+        self.assertEquals(1, len(utilities))
+        self.assertEquals('basic', utilities[0][0])
+
+    def test_configuration_should_load_logging(self):
+        import gocept.amqprun.interfaces
+        import gocept.amqprun.main
+        config = self.make_config(
+            __name__, 'logging', mapping={
+                'logger_info_name': 'foo',
+                'logger_debug_name': 'foo.bar'
+            })
+        gocept.amqprun.main.main(config)
+        self.assertEquals(1, self.server.call_count)
+        self.assertEqual(logging.CRITICAL, logging.getLogger().level)
+        self.assertEqual(logging.INFO, logging.getLogger('foo').level)
+        self.assertEqual(logging.DEBUG, logging.getLogger('foo.bar').level)
+
+    def test_settings_should_be_available_through_utility(self):
+        import gocept.amqprun.interfaces
+        import gocept.amqprun.main
+        config = self.make_config(__name__, 'settings')
+        gocept.amqprun.main.main(config)
+        settings = zope.component.getUtility(
+            gocept.amqprun.interfaces.ISettings)
+        self.assertEquals('foo', settings.get('test.setting.1'))
+        self.assertEquals('bar', settings.get('test.setting.2'))
+
+    def test_settings_should_be_unicode(self):
+        import gocept.amqprun.interfaces
+        import gocept.amqprun.main
+        config = self.make_config(__name__, 'settings')
+        gocept.amqprun.main.main(config)
+        settings = zope.component.getUtility(
+            gocept.amqprun.interfaces.ISettings)
+        self.assertIsInstance(settings.get('test.setting.1'), six.text_type)
+        self.assertEquals(u'Ümläuten', settings.get('test.setting.unicode'))
+
+    def test_settings_should_allow_upper_case(self):
+        import gocept.amqprun.interfaces
+        import gocept.amqprun.main
+        config = self.make_config(__name__, 'settings')
+        gocept.amqprun.main.main(config)
+        settings = zope.component.getUtility(
+            gocept.amqprun.interfaces.ISettings)
+        self.assertEquals('qux', settings.get('test.SETTING.__default__'))
+
+    def test_main_should_send_configfinished_event(self):
+        from gocept.amqprun.interfaces import ISender, IConfigFinished
+        from gocept.amqprun.main import main
+        from zope.component import provideHandler, queryUtility
+        config = self.make_config(__name__, 'basic')
+        self.handler_called = False
+
+        def handler(event):
+            self.handler_called = True
+            self.assertTrue(queryUtility(ISender))
+
+        provideHandler(handler, (IConfigFinished,))
+        main(config)
+        self.assertTrue(self.handler_called)
+
+    def test_main_should_register_ISender(self):
+        import gocept.amqprun.interfaces
+        import gocept.amqprun.main
+        import zope.component
+        config = self.make_config(__name__, 'basic')
+        gocept.amqprun.main.main(config)
+        self.assertEqual(
+            self.server(), zope.component.getUtility(
+                gocept.amqprun.interfaces.ISender))
