d1073755af0fb4622a7648e049f772aae96c0ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:40 +0000

    Add package bbd/0.0.4/bbd-0.0.4.tar.gz

---
+++ packages/bbd/bbd-0.0.4.tar.gz/bbd-0.0.4/src/bbd/gis/__init__.py
@@ -0,0 +1,11 @@
+from .make_map import make_map
+from .utils import (
+    are_coordinates_in_shape,
+    get_geojson_bounds,
+)
+
+__all__ = [
+    make_map,
+    are_coordinates_in_shape,
+    get_geojson_bounds,
+]
