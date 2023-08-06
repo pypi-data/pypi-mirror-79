f80bb8f1ca5c941b68ba2080c744cc525caf0408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:25 +0000

    Add package dsnd-probability-wes/0.2/dsnd_probability_wes-0.2.tar.gz

---
+++ packages/dsnd-probability-wes/dsnd_probability_wes-0.2.tar.gz/dsnd_probability_wes-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_wes',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_wes'],
+      author = 'Wesley Siow',
+      author_email = 'myemail@acompany.com',
+      zip_safe=False)
