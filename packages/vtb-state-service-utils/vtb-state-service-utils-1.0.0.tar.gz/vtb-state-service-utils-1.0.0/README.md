7b1c74252778ed6ddc612ef21389789421b09ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:03 +0000

    Add package vtb-state-service-utils/1.0.0/vtb-state-service-utils-1.0.0.tar.gz

---
+++ packages/vtb-state-service-utils/vtb-state-service-utils-1.0.0.tar.gz/vtb-state-service-utils-1.0.0/README.md
@@ -0,0 +1,3 @@
+State service tools
+
+Need STATE_SERVICE_URL & STATE_SERVICE_TOKEN (opt) in env.
---
+++ packages/vtb-state-service-utils/vtb-state-service-utils-1.0.0.tar.gz/vtb-state-service-utils-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vtb-state-service-utils',
+    version='1.0.0',
+    packages=['state_service_utils'],
+    url='https://bitbucket.org/Michail_Shutov/state_service_utils',
+    license='',
+    author=' Mikhail Shutov',
+    author_email='michael-russ@yandex.ru',
+    description=''
+)
