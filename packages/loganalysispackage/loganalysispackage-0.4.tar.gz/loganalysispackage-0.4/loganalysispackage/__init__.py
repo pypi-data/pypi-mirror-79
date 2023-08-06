8d1b4f1c2b2c3dc63b825ad02cab4de43f1a3c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:04 +0000

    Add package loganalysispackage/0.4/loganalysispackage-0.4.tar.gz

---
+++ packages/loganalysispackage/loganalysispackage-0.4.tar.gz/loganalysispackage-0.4/loganalysispackage/__init__.py
@@ -0,0 +1,13 @@
+from . import loganalysis, average, classify, configreader, datasource, elasticsearchconn, model, mongoconn, prediction
+
+__all__ = [
+    'average',
+    'classify',
+    'configreader',
+    'datasource',
+    'elasticsearchconn',
+    'loganalysis',
+    'model',
+    'mongoconn',
+    'prediction'
+]
