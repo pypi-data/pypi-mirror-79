d6beb5ec74336588d8827eddc02d7ec5c55e6585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:15 +0000

    Add package imzmqx/1.2.0/imzmqx-1.2.0.tar.gz

---
+++ packages/imzmqx/imzmqx-1.2.0.tar.gz/imzmqx-1.2.0/imzmqx/__version__.py
@@ -0,0 +1,9 @@
+# populates fields for >>>help(imagezmq) and setup.py
+__title__ = 'imzmqx'
+__description__ = 'Transporting OpenCV images via ZMQ - enhanced.'
+__url__ = 'https://github.com/minlaxz/imzmqx'
+__version__ = '1.2.0'
+__author__ = 'Jeff Bass, Min Latt'
+__author_email__ = 'jeff@yin-yang-ranch.com, minminlaxz@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Jeff Bass'
