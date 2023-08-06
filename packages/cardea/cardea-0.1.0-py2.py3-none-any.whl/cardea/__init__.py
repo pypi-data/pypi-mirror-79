161e578e58de46b03f88a45e00b92f8f4a281b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:39 +0000

    Add package cardea/0.1.0/cardea-0.1.0-py2.py3-none-any.whl

---
+++ packages/cardea/cardea-0.1.0-py2.py3-none-any.whl/cardea/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+__author__ = """MIT Data To AI Lab"""
+__email__ = 'dailabmit@gmail.com'
+__version__ = '0.1.0'
+
+import logging
+
+from cardea.cardea import Cardea
+
+logging.getLogger('cardea').addHandler(logging.NullHandler())
