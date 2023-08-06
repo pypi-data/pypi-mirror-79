08a5855cc3fa53b154c7184dbc29ec184380d604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:29 +0000

    Add package notesplit/1.0.7/notesplit-1.0.7.tar.gz

---
+++ packages/notesplit/notesplit-1.0.7.tar.gz/notesplit-1.0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='notesplit',
+    version='1.0.7',
+    description="Write your private diary in text files, and share parts of it with someone else's diaries.",
+    long_description=long_description,
+    long_description_content_type='text/rst',
+    url='https://github.com/mindey/notesplit',
+    author='Mindey',
+    author_email='~@mindey.com',
+    license='MIT',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires=[],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+            'notesplit=notesplit.split:main',
+            'notesync=notesplit.sync:main'
+        ],
+    }
+)
