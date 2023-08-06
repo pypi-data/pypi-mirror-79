9666de3afd1f35996a910ce6d359a59d85cb918f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:06 +0000

    Add package smpw/1.0.0/smpw-1.0.0.tar.gz

---
+++ packages/smpw/smpw-1.0.0.tar.gz/smpw-1.0.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['smpw']
+setup_kwargs = {
+    'name': 'smpw',
+    'version': '1.0.0',
+    'description': 'my modile is python 3.8 is hack from windows 10 and my name is nick pl. lead the team  please pip install start',
+    'long_description': None,
+    'author': 'John Smith',
+    'author_email': 'azarovnick@bk.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
