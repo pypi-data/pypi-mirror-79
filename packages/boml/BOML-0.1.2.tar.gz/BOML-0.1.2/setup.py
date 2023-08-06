9069b7adf507b92f8ffe79599e26759c02c4616d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:04 +0000

    Add package boml/0.1.2/BOML-0.1.2.tar.gz

---
+++ packages/boml/BOML-0.1.2.tar.gz/BOML-0.1.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup,find_packages
+from os import path
+
+current_directory = path.abspath(path.dirname(__file__))
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+def get_install_requirements():
+    requirements_path = path.join(current_directory, "requirements.txt")
+    with open(requirements_path, encoding="utf-8") as fp:
+        return fp.read().splitlines()
+
+
+setup(
+    name="BOML",
+    version="0.1.2",
+    packages=find_packages(),
+    long_description=long_description,
+    url="https://github.com/dut-media-lab/BOML",
+    license="MIT",
+    keywords=[
+    "meta-learning",
+    "bilevel-optimization",
+    "few-shot-lerning",
+    "python",
+    "Deep learning"],
+    install_requires=get_install_requirements(),
+    classifiers=[
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    ],
+    python_requires='>=3.5,<3.8',
+    author="Yaohua Liu, Risheng Liu",
+    author_email="liuyaohua@mail.dlut.edu.cn",
+    description="A Bilevel Optimizer Library in Python for Meta Learning",
+)
