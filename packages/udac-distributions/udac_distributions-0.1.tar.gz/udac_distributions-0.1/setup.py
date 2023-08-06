271e218352c8a0f2c4fb268e78cc6860f7c5b7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:02 +0000

    Add package udac-distributions/0.1/udac_distributions-0.1.tar.gz

---
+++ packages/udac-distributions/udac_distributions-0.1.tar.gz/udac_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udac_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['udac_distributions'],
+      author = 'Chidimma Nweke',
+      author_email = 'realchidinweke@gmail.com',
+      zip_safe=False)
