6c8c8ab4029a186f285f92a8c5d6037b108063e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:26 +0000

    Add package wapt-tools/1.3.0/wapt_tools-1.3.0.tar.gz

---
+++ packages/wapt-tools/wapt_tools-1.3.0.tar.gz/wapt_tools-1.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wapt_tools',
+    url='https://github.com/lrobinot/wapt-tools',
+    author='Ludovic ROBINOT',
+    author_email='lrobinot@gmail.com',
+    packages=find_packages(),
+    install_requires=['beautifulsoup4', 'requests', 'python_gitlab'],
+    version='1.3.0',
+    license='Apache 2.0',
+    description='WAPT packaging utilities.',
+    include_package_data=True,
+    package_data={'': ['data/*.*']},
+)
