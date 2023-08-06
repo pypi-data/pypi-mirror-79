7cbbc4f148358e4892a0ab6cc62d1dc5912cf2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:26 +0000

    Add package pybashrc/0.0.2/pybashrc-0.0.2.tar.gz

---
+++ packages/pybashrc/pybashrc-0.0.2.tar.gz/pybashrc-0.0.2/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+
+def setup():
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    setuptools.setup(
+        name="pybashrc",
+        version="0.0.2",
+        author="Jelmer Neeven",
+        author_email="author@example.com",
+        description="Register python functions as bash commands",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/jneeven/pybashrc",
+        packages=setuptools.find_packages(),
+        package_data={
+            # If any package contains *.txt or *.rst files, include them:
+            "": [
+                ".pybashrc_aliases",
+                ".pybashrc_execute.py",
+                "alias_template",
+                "bashrc_template",
+            ],
+        },
+        entry_points={"console_scripts": ["pybash=pybashrc.post_setup:post_setup"]},
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: Unix ",
+        ],
+        python_requires=">=3.6",
+    )
+
+
+if __name__ == "__main__":
+    setup()
