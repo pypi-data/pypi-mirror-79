f55ea395f6fac31f87b2cb053a54c17742060e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:55 +0000

    Add package zmipc/0.4/zmipc-0.4.tar.gz

---
+++ packages/zmipc/zmipc-0.4.tar.gz/zmipc-0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='zmipc',
+      version='0.4',
+      description='A zero-copy memory-sharing IPC on the top of mmap',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/mzy2240/zmipc',
+      author='Zeyu Mao',
+      author_email='mao.mzy@gmail.com',
+      license='MIT',
+      packages=['zmipc'],
+      zip_safe=False)
