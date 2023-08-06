d3a65fa0b9d46fa245420341c66ae0b9c01eae75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:29 +0000

    Add package pconvert-rust/0.1.2/pconvert-rust-0.1.2.tar.gz

---
+++ packages/pconvert-rust/pconvert-rust-0.1.2.tar.gz/pconvert-rust-0.1.2/README.md
@@ -0,0 +1,40 @@
+# P(NG)Convert Rust
+
+The [Rust](https://www.rust-lang.org) version of the famous [P(NG)Convert](https://github.com/hivesolutions/pconvert) from Hive Solutions.
+
+## Compiling & Executing
+
+Compile with cargo
+
+```bash
+cargo build
+```
+
+and run binary:
+
+```bash
+./target/debug/pconvert-rust
+```
+
+Or build + run with cargo:
+
+```bash
+cargo run
+```
+
+## Usage
+
+```bash
+pconvert-rust convert <file_in> <file_out>
+```
+
+## License
+
+P(NG)Convert Rust is currently licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/).
+
+## Build Automation
+
+[![Build Status](https://travis-ci.org/ripe-tech/pconvert-rust.svg?branch=master)](https://travis-ci.org/ripe-tech/pconvert-rust)
+[![Build Status GitHub](https://github.com/ripe-tech/pconvert-rust/workflows/Main%20Workflow/badge.svg)](https://github.com/ripe-tech/pconvert-rust/actions)
+[![PyPi Status](https://img.shields.io/pypi/v/pconvert-rust.svg)](https://pypi.python.org/pypi/pconvert-rust)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://www.apache.org/licenses/)
---
+++ packages/pconvert-rust/pconvert-rust-0.1.2.tar.gz/pconvert-rust-0.1.2/src/utils.rs
@@ -0,0 +1,54 @@
+use super::blending::demultiply_image;
+use super::errors::PConvertError;
+use image::io::Reader;
+use image::png::{CompressionType, FilterType, PngEncoder};
+use image::{ColorType, DynamicImage, ImageBuffer, ImageFormat, Rgba};
+use std::fs::File;
+use std::io::BufWriter;
+
+pub fn read_png(
+    file_in: String,
+    demultiply: bool,
+) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>, PConvertError> {
+    let reader = Reader::open(file_in)?;
+    let reader = Reader::with_format(reader.into_inner(), ImageFormat::Png);
+
+    let mut img = match reader.decode() {
+        Ok(DynamicImage::ImageRgba8(img)) => img,
+        _ => return Err(PConvertError::UnsupportedImageTypeError),
+    };
+
+    if demultiply {
+        demultiply_image(&mut img)
+    }
+
+    Ok(img)
+}
+
+pub fn write_png(
+    file_out: String,
+    png: &ImageBuffer<Rgba<u8>, Vec<u8>>,
+    compression: CompressionType,
+    filter: FilterType,
+) -> Result<(), PConvertError> {
+    let file = File::create(&file_out)?;
+    let buff = BufWriter::new(file);
+    let encoder = PngEncoder::new_with_quality(buff, compression, filter);
+    Ok(encoder.encode(&png, png.width(), png.height(), ColorType::Rgba8)?)
+}
+
+pub fn max<T: PartialOrd>(x: T, y: T) -> T {
+    if x > y {
+        x
+    } else {
+        y
+    }
+}
+
+pub fn min<T: PartialOrd>(x: T, y: T) -> T {
+    if x < y {
+        x
+    } else {
+        y
+    }
+}
