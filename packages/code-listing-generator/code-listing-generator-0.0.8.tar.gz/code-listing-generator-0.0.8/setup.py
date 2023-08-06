ea1e3698a945a90425957dcf96dd60b6dfab8211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:54 +0000

    Add package code-listing-generator/0.0.8/code-listing-generator-0.0.8.tar.gz

---
+++ packages/code-listing-generator/code-listing-generator-0.0.8.tar.gz/code-listing-generator-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="code-listing-generator", # Replace with your own username
+    version="0.0.8",
+    author="Ben Kettle",
+    author_email="",
+    description="Creates a LaTeX document with code from all files in the current directory",
+    url="https://github.com/bkettle/code-listing-generator",
+    packages=setuptools.find_packages(),
+    install_requires=['jinja2'],
+    scripts=['scripts/generate-code-listing'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
+)
