110d2d7bc62ec45438543b5a398f6b436242eb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:45 +0000

    Add package marcosdistributions/0.1/marcosdistributions-0.1.tar.gz

---
+++ packages/marcosdistributions/marcosdistributions-0.1.tar.gz/marcosdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='marcosdistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['marcosdistributions'],
+      author = 'Marco Besier',
+      author_email = 'marcobesier@hey.com',
+      zip_safe=False)
