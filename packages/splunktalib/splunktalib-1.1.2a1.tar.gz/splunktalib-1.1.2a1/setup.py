1acef7304997d8d9bdce6b12f782c519ea8375da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:29 +0000

    Add package splunktalib/1.1.2a1/splunktalib-1.1.2a1.tar.gz

---
+++ packages/splunktalib/splunktalib-1.1.2a1.tar.gz/splunktalib-1.1.2a1/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['splunktalib',
+ 'splunktalib.common',
+ 'splunktalib.concurrent',
+ 'splunktalib.conf_manager',
+ 'splunktalib.httplib2_helper',
+ 'splunktalib.httplib2_helper.httplib2_py2',
+ 'splunktalib.httplib2_helper.httplib2_py2.httplib2',
+ 'splunktalib.httplib2_helper.httplib2_py3',
+ 'splunktalib.httplib2_helper.httplib2_py3.httplib2',
+ 'splunktalib.schedule']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httplib2>=0,<1', 'sortedcontainers>=2.2,<3.0']
+
+setup_kwargs = {
+    'name': 'splunktalib',
+    'version': '1.1.2a1',
+    'description': 'Supporting library for Splunk Add-ons',
+    'long_description': None,
+    'author': 'rfaircloth-splunk',
+    'author_email': 'rfaircloth@splunk.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*',
+}
+
+
+setup(**setup_kwargs)
