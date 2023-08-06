1d31555387180a290d30a3740dead6fb7a0fe8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:52 +0000

    Add package igel/0.1.8/igel-0.1.8-py2.py3-none-any.whl

---
+++ packages/igel/igel-0.1.8-py2.py3-none-any.whl/igel/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for igel."""
+from .igel import IgelModel, models_dict, metrics_dict
+
+
+__author__ = "Nidhal Baccouri"
+__email__ = 'nidhalbacc@gmail.com'
+__version__ = '0.1.8'
