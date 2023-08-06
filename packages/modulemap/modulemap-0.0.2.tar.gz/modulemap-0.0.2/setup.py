a526a4804a9065581bfa012bb9bb69765bf26d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:01 +0000

    Add package modulemap/0.0.2/modulemap-0.0.2.tar.gz

---
+++ packages/modulemap/modulemap-0.0.2.tar.gz/modulemap-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="modulemap",
+    version="0.0.2",
+    author="Sam Ingram",
+    author_email="sp_ingram12@yahoo.co.uk",
+    description="A lightweight option for creating json description files of Python modules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/singram12/ModuleMap",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+   'jsondiff>=1.2.0',
+    ],
+    python_requires='>=3.5',
+)
