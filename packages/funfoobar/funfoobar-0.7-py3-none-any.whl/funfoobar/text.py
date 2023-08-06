caa17159e2fc1abaafc6885b9927c26a7a804544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:03 +0000

    Add package funfoobar/0.7/funfoobar-0.7-py3-none-any.whl

---
+++ packages/funfoobar/funfoobar-0.7-py3-none-any.whl/funfoobar/text.py
@@ -0,0 +1,6 @@
+from markdown import markdown
+
+def joke():
+    return markdown('Wenn ist das Nunst fcck git und Slotermeyer?'
+                    'Ja! ... **Beiherhund** das Oder die Flipperwaldt '
+                    'gersput.')
