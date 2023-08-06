92537b94c835f063d3c2bcbd695a61f076c5931b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:37 +0000

    Add package odoo12-addon-account-move-attachment/12.0.0.1.0/odoo12-addon-account_move_attachment-12.0.0.1.0.tar.gz

---
+++ packages/odoo12-addon-account-move-attachment/odoo12-addon-account_move_attachment-12.0.0.1.0.tar.gz/odoo12-addon-account_move_attachment-12.0.0.1.0/odoo/addons/account_move_attachment/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account Move Attachment',
+    'version': '12.0.0.1.0',
+    'category': 'Accounting/Accounting',
+    'summary': 'Adds social network features to account.move',
+    'description': "",
+    'author': 'Coopdevs Treball SCCL',
+    'license': 'AGPL-3',
+    'website': 'https://coopdevs.org/',
+    'depends': ['mail', 'account'],
+    'data': [
+        'account_move_view.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+}
