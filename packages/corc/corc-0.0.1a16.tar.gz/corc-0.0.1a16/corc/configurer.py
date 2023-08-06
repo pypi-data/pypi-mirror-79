cdd3101593c7d4ed65919deb98c4a7bb7fe07d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:06 +0000

    Add package corc/0.0.1a16/corc-0.0.1a16.tar.gz

---
+++ packages/corc/corc-0.0.1a16.tar.gz/corc-0.0.1a16/corc/configurer.py
@@ -0,0 +1,86 @@
+from ansible.parsing.dataloader import DataLoader
+from ansible.inventory.manager import InventoryManager
+from ansible.vars.manager import VariableManager
+from ansible.playbook import Playbook
+from ansible.executor.task_queue_manager import TaskQueueManager
+
+
+def load_playbook_source(path):
+    loader = DataLoader()
+    play_source = loader.load_from_file(path)
+    return play_source
+
+
+def run_playbook(
+    play, variable_manager=None, inventory_manager=None, loader=None, passwords=None
+):
+    tqm = None
+    try:
+        tqm = TaskQueueManager(
+            inventory=inventory_manager,
+            variable_manager=variable_manager,
+            loader=loader,
+            passwords=passwords,
+        )
+        tqm.run(play)
+    finally:
+        if tqm is not None:
+            tqm.cleanup()
+        if loader:
+            loader.cleanup_all_tmp_files()
+
+
+class AnsibleConfigurer:
+    def __init__(self, options):
+        self.options = options
+        self.loader = DataLoader()
+        if "inventory_path" in self.options:
+            sources = self.options["inventory_path"]
+        else:
+            sources = None
+
+        self.inventory_manager = InventoryManager(self.loader, sources=sources)
+        self.inventory_manager.add_group("compute")
+
+        self.variable_manager = VariableManager(
+            loader=self.loader, inventory=self.inventory_manager
+        )
+        for host in self.options["hosts"]:
+            self.inventory_manager.add_host(host, group="compute", port="22")
+
+        self.variable_manager.set_host_variable(host, "ansible_connection", "ssh")
+        self.variable_manager.set_host_variable(host, "ansible_user", "opc")
+        self.variable_manager.set_host_variable(host, "ansible_become", "yes")
+        self.variable_manager.set_host_variable(host, "ansible_become_method", "sudo")
+        self.variable_manager.set_host_variable(
+            host, "ansible_host_key_checking", "False"
+        )
+
+        # Use the provided ssh key
+        self.variable_manager.set_host_variable(
+            host, "ansible_ssh_private_key_file", self.options["ssh_private_key_file"]
+        )
+
+        self.variable_manager.set_host_variable(host, "verbosity", 4)
+
+    def apply(self):
+        playbook_path = self.options["playbook_path"]
+        playbook = Playbook.load(
+            playbook_path, variable_manager=self.variable_manager, loader=self.loader
+        )
+
+        plays = playbook.get_plays()
+        for play in plays:
+            run_playbook(
+                play,
+                variable_manager=self.variable_manager,
+                inventory_manager=self.inventory_manager,
+                loader=self.loader,
+            )
+
+    @classmethod
+    def validate_options(cls, options):
+        if not isinstance(options, dict):
+            raise TypeError("options is not a dictionary")
+
+        # expected_ansible_keys = ["inventory_path", "playbook_path"]
---
+++ packages/corc/corc-0.0.1a16.tar.gz/corc-0.0.1a16/corc/defaults.py
@@ -0,0 +1,71 @@
+# Package name
+PACKAGE_NAME = "corc"
+
+# Providers
+AWS = "AWS"
+AWS_LOWER = "aws"
+
+OCI = "OCI"
+OCI_LOWER = "oci"
+
+PROVIDER = "PROVIDER"
+DEFAULT_PROVIDER = OCI
+DEFAULT_PROVIDER_LOWER = OCI_LOWER
+PROVIDERS = [AWS, OCI]
+PROVIDERS_LOWER = [AWS_LOWER, OCI_LOWER]
+
+PROFILE = "PROFILE"
+
+# Argument group defaults
+ANSIBLE = "ANSIBLE"
+CLUSTER = "CLUSTER"
+NODE = "NODE"
+CLUSTER_NODE = "{}_{}".format(CLUSTER, NODE)
+INSTANCE = "INSTANCE"
+CONFIG = "CONFIG"
+JOB = "JOB"
+META = "META"
+JOB_META = "{}_{}".format(JOB, META)
+RUN = "RUN"
+
+STORAGE = "STORAGE"
+S3 = "S3"
+STORAGE_S3 = "{}_{}".format(STORAGE, S3)
+
+
+# Networking
+SUBNET = "SUBNET"
+INTERNETGATEWAY = "INTERNETGATEWAY"
+ROUTETABLE = "ROUTETABLE"
+ROUTERULES = "ROUTERULES"
+VCN = "VCN"
+VCN_SUBNET = "{}_{}".format(VCN, SUBNET)
+VCN_INTERNETGATEWAY = "{}_{}".format(VCN, INTERNETGATEWAY)
+VCN_ROUTETABLE = "{}_{}".format(VCN, ROUTETABLE)
+VCN_ROUTETABLE_ROUTERULES = "{}_{}".format(VCN_ROUTETABLE, ROUTERULES)
+
+# Action groups
+ACTION_GROUPS = [
+    ANSIBLE,
+    CLUSTER,
+    INSTANCE,
+    CONFIG,
+    JOB,
+    META,
+    NODE,
+    RUN,
+    STORAGE,
+    SUBNET,
+    S3,
+    VCN,
+]
+
+
+# Storage credentials secret name
+STORAGE_CREDENTIALS_NAME = "storage-credentials"
+
+# Kubernetes defaults
+KUBERNETES_NAMESPACE = "default"
+
+# Job defaults
+JOB_DEFAULT_NAME = "job"
