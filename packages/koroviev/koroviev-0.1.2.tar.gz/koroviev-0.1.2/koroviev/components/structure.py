29d260895661c6c324df531ab8786a45221f49f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:23 +0000

    Add package koroviev/0.1.2/koroviev-0.1.2.tar.gz

---
+++ packages/koroviev/koroviev-0.1.2.tar.gz/koroviev-0.1.2/koroviev/components/structure.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+
+from termcolor import cprint
+
+from koroviev.const import (
+    DEFAULT_CONFIG_FILENAME,
+    DEFAULT_CONFIG_PRESET,
+    DEFAULT_TEMPLATES_DIRNAME,
+    SetupLanguage,
+    SetupLanguageExtension,
+)
+
+
+def remove_template_structure(templates_folder: str):
+    templates_folder = os.path.join(os.getcwd(), templates_folder)
+    shutil.rmtree(templates_folder, ignore_errors=True)
+
+
+def create_template_structure(
+    templates: dict, templates_folder: str, default_template_extension: str
+):
+    for template, info in templates.items():
+        type_folder = os.path.join(os.getcwd(), templates_folder, info.type.name)
+
+        if not os.path.isdir(type_folder):
+            cprint(
+                f"Create template type folder '{info.type.name}': '{type_folder}'...",
+                "green",
+            )
+            os.makedirs(type_folder, exist_ok=True)
+
+        template_filepath = os.path.join(
+            os.getcwd(),
+            templates_folder,
+            info.type.name,
+            f"{template}.{default_template_extension if not info.extension else info.extension}",
+        )
+
+        if os.path.isfile(template_filepath):
+            continue
+
+        with open(template_filepath, "w") as f:
+            cprint(
+                f"Create template file '{template}': '{template_filepath}'...", "green",
+            )
+            f.write("\n")
