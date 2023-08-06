6a7355748351613fba298355dc91319ab93ec3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:45 +0000

    Add package aurflux/2.1.1/aurflux-2.1.1.tar.gz

---
+++ packages/aurflux/aurflux-2.1.1.tar.gz/aurflux-2.1.1/aurflux/__init__.py
@@ -0,0 +1,8 @@
+from .command import argh
+from . import errors
+from .aurflux import Aurflux, AurfluxEvent
+from .command import Command
+from .cog import AurfluxCog
+
+__package__ = "aurflux"
+__name__ = "aurflux"
