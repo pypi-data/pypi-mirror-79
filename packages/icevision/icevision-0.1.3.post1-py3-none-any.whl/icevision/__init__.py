c7bdf2309176b1f522c46542a6b78f4017a9ffac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:53 +0000

    Add package icevision/0.1.3.post1/icevision-0.1.3.post1-py3-none-any.whl

---
+++ packages/icevision/icevision-0.1.3.post1-py3-none-any.whl/icevision/__init__.py
@@ -0,0 +1,14 @@
+from icevision.utils import *
+from icevision.core import *
+from icevision import parsers
+from icevision import tfms
+from icevision.data import *
+from icevision import backbones
+from icevision import models
+from icevision.models import *
+from icevision.metrics import *
+from icevision.visualize import *
+
+import icevision.test_utils as test_utils
+
+__version__ = "0.1.1rc2"
