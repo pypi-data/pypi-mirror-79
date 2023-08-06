9ba14aba298dc17d2fdfee91bfff45e9a1e0af2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:13 +0000

    Add package simplesbml/2.0.1/simplesbml-2.0.1.tar.gz

---
+++ packages/simplesbml/simplesbml-2.0.1.tar.gz/simplesbml-2.0.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 16:19:07 2015, 2020
+
+@author: carolc24, Kyle Medley, Hebrert Sauro
+"""
+
+from setuptools import setup
+import os.path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='simplesbml',
+      version='2.0.1',
+      description='A simplified interface for constructing and accessing SBML docs. Version 2.0',
+      author='Caroline Cannistra, Kyle Medley, Hebrert Sauro',
+      author_email='tellurium-discuss@googlegroups.com',
+      url='http://simplesbml.readthedocs.io/en/latest/',
+      long_description=long_description,
+      long_description_content_type='text/markdown',     
+      install_requires=['tesbml>=5.15.0'],
+      license='MIT',
+      packages=['simplesbml'],
+      classifiers=[
+       'License :: OSI Approved :: MIT License',
+       'Programming Language :: Python :: 3.7',
+       'Programming Language :: Python :: 3.8',
+       'Operating System :: OS Independent',
+      ],
+      python_requires='>=3.7',)
---
+++ packages/simplesbml/simplesbml-2.0.1.tar.gz/simplesbml-2.0.1/simplesbml/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 23:13:42 2015, 2020
+
+@author: carolc24, Kyle Medley, hsauro
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .simplesbml import SbmlModel
+from .simplesbml import loadSBMLStr
+from .simplesbml import loadSBMLFile
+
+__version__ = '2.0.1'
+
+
+
