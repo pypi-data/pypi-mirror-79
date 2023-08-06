5d654f70ad271f3e3b13c98ef20d91ae49f03523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:38 +0000

    Add package MFAProblem/1.0.1b2/MFAProblem-1.0.1b2.tar.gz

---
+++ packages/MFAProblem/MFAProblem-1.0.1b2.tar.gz/MFAProblem-1.0.1b2/mfa_problem/tests/unit/test_mfa_problem_format_io.py
@@ -0,0 +1,18 @@
+
+import unittest
+
+
+class MfaProblemFormatIO(unittest.TestCase):
+    def test_intermediate_calculus(self):
+        '''
+        inputs :
+        montecarlo_results,
+        vars_type_dict
+        output:
+        mc_result
+        '''
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
