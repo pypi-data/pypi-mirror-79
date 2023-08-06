506309efd6891424d6f9b2c5698a717218476c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:32 +0000

    Add package starlette-json/20.9.18/starlette_json-20.9.18-py3-none-any.whl

---
+++ packages/starlette-json/starlette_json-20.9.18-py3-none-any.whl/starlette_json/__init__.py
@@ -0,0 +1,4 @@
+from .starlette_orjson import ORJsonResponse, ORJsonMiddleware
+from .starlette_ujson import UJsonResponse, UJsonMiddleware
+from .starlette_rapidjson import RapidJsonResponse, RapidJsonMiddleware
+from .starlette_simplejson import SimpleJsonResponse, SimpleJsonMiddleware
