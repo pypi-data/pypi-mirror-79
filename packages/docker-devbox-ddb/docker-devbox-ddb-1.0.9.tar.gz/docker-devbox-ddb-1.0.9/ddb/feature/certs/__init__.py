f322905a00d9f257f44a289c97dd9404d4412b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:46 +0000

    Add package docker-devbox-ddb/1.0.9/docker-devbox-ddb-1.0.9.tar.gz

---
+++ packages/docker-devbox-ddb/docker-devbox-ddb-1.0.9.tar.gz/docker-devbox-ddb-1.0.9/ddb/feature/certs/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from typing import Iterable, ClassVar
+
+from .actions import GenerateCertAction, RemoveCertAction, GenerateSignerCertificateAction
+from .schema import CertsSchema
+from ..feature import Feature
+from ..schema import FeatureSchema
+from ...action import Action
+
+
+class CertsFeature(Feature):
+    """
+    Generate SSL certificates for your project.
+    """
+
+    @property
+    def name(self) -> str:
+        return "certs"
+
+    @property
+    def schema(self) -> ClassVar[FeatureSchema]:
+        return CertsSchema
+
+    @property
+    def actions(self) -> Iterable[Action]:
+        return (
+            GenerateCertAction(),
+            RemoveCertAction(),
+            GenerateSignerCertificateAction()
+        )
---
+++ packages/docker-devbox-ddb/docker-devbox-ddb-1.0.9.tar.gz/docker-devbox-ddb-1.0.9/ddb/feature/smartcd/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from typing import Iterable, ClassVar
+
+from ddb.action import Action
+from ddb.feature import Feature
+from .actions import SmartcdAction, WindowsProjectActivate
+
+
+class SmartcdFeature(Feature):
+    """
+    Smartcd support (https://github.com/cxreg/smartcd).
+    """
+
+    @property
+    def name(self) -> str:
+        return "smartcd"
+
+    @property
+    def actions(self) -> Iterable[Action]:
+        return (
+            SmartcdAction(),
+            WindowsProjectActivate(),
+        )
