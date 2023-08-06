5b3aeaf7e6b7b149f285bf8c120e1d2b42d1ee29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:56 +0000

    Add package cognito-overlay/0.0.1.dev15/cognito_overlay-0.0.1.dev15-py3-none-any.whl

---
+++ packages/cognito-overlay/cognito_overlay-0.0.1.dev15-py3-none-any.whl/cognito_overlay/_config.py
@@ -0,0 +1,14 @@
+import os
+import boto3
+import json
+
+_client = boto3.client('secretsmanager')
+_secretname = os.getenv('AUTH_SECRET_ID')
+
+if _secretname:
+  record = _client.get_secret_value(SecretId=_secretname)
+  config = json.loads(record['SecretString'])
+
+  CLIENT_ID = config['client_id']
+else:
+  CLIENT_ID = ""
