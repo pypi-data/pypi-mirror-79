d0c2f30a4ce0c455b6e49976c84320d0056337e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:36 +0000

    Add package shenko/0.1.32/shenko-0.1.32-py2.py3-none-any.whl

---
+++ packages/shenko/shenko-0.1.32-py2.py3-none-any.whl/shenko/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for shenko."""
+
+__author__ = """Danny Dowshenko"""
+__email__ = 'dowshenko225@gmail.com'
+__version__ = '0.1.32'
+import shenko
+from shenko import main
