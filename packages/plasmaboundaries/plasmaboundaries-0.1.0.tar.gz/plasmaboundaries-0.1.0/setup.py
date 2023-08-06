bc4444badbd1752572cdee5cb3fd95f26488685d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:22 +0000

    Add package plasmaboundaries/0.1.0/plasmaboundaries-0.1.0.tar.gz

---
+++ packages/plasmaboundaries/plasmaboundaries-0.1.0.tar.gz/plasmaboundaries-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="plasmaboundaries",
+    version="0.1.0",
+    author="Remi Delaporte-Mathurin",
+    author_email="rdelaportemathurin@gmail.com",
+    description="Determine plasma flux functions for various plasma parameters and plasma configurations",
+    url="https://github.com/RemiTheWarrior/plasma-boundaries",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'matplotlib',
+        'scipy',
+        'numpy',
+        'sympy',
+    ],
+    zip_safe=True,
+)
