a58b63de4d66b99a3f9532f92d6adaf26b7a5051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:06 +0000

    Add package rtspbuffercam/0.1.2/rtspbuffercam-0.1.2.tar.gz

---
+++ packages/rtspbuffercam/rtspbuffercam-0.1.2.tar.gz/rtspbuffercam-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rtspbuffercam',
+    version='0.1.2',
+    packages=['rtspcam'],
+    url='https://github.com/lewis-morris/rtspbuffercam',
+    license='MIT',
+    author='Lewis Morris',
+    author_email='lewis.morris@gmail.com',
+    description='RTSP rtspcam grabber with buffer exhaustion'
+)
