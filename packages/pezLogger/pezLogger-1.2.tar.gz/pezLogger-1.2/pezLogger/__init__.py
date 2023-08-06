0d785d4d8d2c4742224f0c6279759bbce0ef7dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:21 +0000

    Add package pezLogger/1.2/pezLogger-1.2.tar.gz

---
+++ packages/pezLogger/pezLogger-1.2.tar.gz/pezLogger-1.2/pezLogger/__init__.py
@@ -0,0 +1,5 @@
+from pezLogger.src.logger import Logger
+from pezLogger.src.mockLogger import MockLogger
+from pezLogger.src.base.enums import LogLevel, LogLocation
+
+__all__ = ["Logger", "MockLogger", "LogLevel", "LogLocation"]
---
+++ packages/pezLogger/pezLogger-1.2.tar.gz/pezLogger-1.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name='pezLogger',
+  packages=['pezLogger'],
+  version='1.2',
+  license='MIT',
+  description='Custom logger',
+  author='Jason \'Perry\' Taylor',
+  url='https://github.com/YesmynameisPerry/pezLogger',
+  keywords=['LOGGER', 'CUSTOM', 'PEZ'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
