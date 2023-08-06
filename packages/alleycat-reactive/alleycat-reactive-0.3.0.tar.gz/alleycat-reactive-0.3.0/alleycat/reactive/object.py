9089f8327e53ea2d8a7fa47c40a456ab04e3ff9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:59 +0000

    Add package alleycat-reactive/0.3.0/alleycat-reactive-0.3.0.tar.gz

---
+++ packages/alleycat-reactive/alleycat-reactive-0.3.0.tar.gz/alleycat-reactive-0.3.0/alleycat/reactive/object.py
@@ -0,0 +1,36 @@
+from typing import TypeVar
+
+from rx import Observable
+from rx.core.typing import Disposable
+
+from alleycat.reactive import functions as rv
+
+T = TypeVar("T")
+
+
+class ReactiveObject(Disposable):
+    disposed = rv.from_value(False)
+
+    def __init__(self):
+        super().__init__()
+
+    def observe(self, name: str) -> Observable:
+        if self.disposed:
+            raise RuntimeError("Cannot observe a disposed object.")
+
+        return rv.observe(self, name)
+
+    def dispose(self) -> None:
+        if self.disposed:
+            raise RuntimeError("The object has already been disposed.")
+
+        self.disposed = True
+
+        rv.dispose(self)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not self.disposed:
+            self.dispose()
