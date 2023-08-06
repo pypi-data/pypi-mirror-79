cee32f5896434441b54b644e5e9570515055f1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:28 +0000

    Add package sentry-plugin-dingding/0.0.8/sentry-plugin-dingding-0.0.8.tar.gz

---
+++ packages/sentry-plugin-dingding/sentry-plugin-dingding-0.0.8.tar.gz/sentry-plugin-dingding-0.0.8/src/sentry_plugin_dingding/valueDict.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+markdownMap = {
+  u"PROJECT_NAME": u'',
+  u"TITLE": u"title",
+  u"MESSAGE": u"message",
+  u"ENV": u"env",
+  u"PLATFORM": u"platform"
+}
+
+defaultMarkdown = u'''
+### {{ PROJECT_NAME }}
+#### {{ TITLE }}
+{{ MESSAGE }}
+[查看详情]({{ URL }})
+'''
