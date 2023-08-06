0bc8d02a383a9ca5635c887e4395de12ebfe3fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:14 +0000

    Add package cdk8s/0.28.0/cdk8s-0.28.0.tar.gz

---
+++ packages/cdk8s/cdk8s-0.28.0.tar.gz/cdk8s-0.28.0/src/cdk8s/_jsii/__init__.py
@@ -0,0 +1,21 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk8s", "0.28.0", __name__[0:-6], "cdk8s@0.28.0.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
