7c0cce0dc51eebfea15a0831b2e8528dba76d857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:54 +0000

    Add package maybach-request/0.5.0/maybach_request-0.5.0-py3-none-any.whl

---
+++ packages/maybach-request/maybach_request-0.5.0-py3-none-any.whl/maybach_request/__init__.py
@@ -0,0 +1,69 @@
+import requests
+import sys
+import json
+from time import sleep
+port = None
+host = None
+def send(data):
+  port = get_port()
+  host = get_host()
+  if (host is None):
+    host = '127.0.0.1'
+  requests.post('http://' + host + ':' + port + '/test', data = data)
+  
+def get_print():
+  sleep(0.2)
+  port = get_port()
+  host = get_host()
+  if (host is None):
+    host = '127.0.0.1'
+  r = requests.get('http://' + host + ':' + port + '/test')
+  if r.status_code == 200:
+    try:
+      info = json.loads(r.text)
+      if info.get('status') == 1:
+        return []
+      return info.get('msg')
+    except Exception as e:
+      print(e)
+      return []
+    
+def send_print(data):
+	port = get_port()
+	host = get_host()
+	if (host is None):
+		host = '127.0.0.1'
+	r = requests.post('http://' + host + ':' + port + '/print', data = { 'print': data })
+  
+def get_port():
+  global port
+  if (port != None):
+    return port
+  match = False
+  for i in sys.argv:
+    if match:
+      port = i
+      break;
+    if i == '--port':
+      match = True
+  return port
+
+def get_host():
+  global host
+  if (host != None):
+    return host
+  match = False
+  for i in sys.argv:
+    if match:
+      host = i
+      break;
+    if i == '--host':
+      match = True
+  return host
+
+def pass_test(msg):
+  send({ 'status': 0, 'msg': msg })
+  
+def no_pass_test(msg):
+  send({ 'status': 1, 'msg': msg })
+  
