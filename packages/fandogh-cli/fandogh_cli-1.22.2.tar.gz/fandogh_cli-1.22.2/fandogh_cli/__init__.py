a453c6a070694f8641b22d87255704099bbf1d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:43 +0000

    Add package fandogh-cli/1.22.2/fandogh_cli-1.22.2.tar.gz

---
+++ packages/fandogh-cli/fandogh_cli-1.22.2.tar.gz/fandogh_cli-1.22.2/fandogh_cli/__init__.py
@@ -0,0 +1,5 @@
+VERSION = '1.22.2'
+NAME = "fandogh_cli"
+
+if __name__ == "__main__":
+    print(VERSION)
---
+++ packages/fandogh-cli/fandogh_cli-1.22.2.tar.gz/fandogh_cli-1.22.2/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+import setuptools
+
+from fandogh_cli import VERSION
+
+setup(
+    name='fandogh_cli',
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    version=VERSION,
+    description='Official CLI for Fandogh PaaS',
+    author='Soroosh Sarabadani',
+    install_requires=["click>=7.0",
+                      "pyOpenSSL",
+                      "requests[security]",
+                      "requests-toolbelt",
+                      "beautifultable",
+                      "pytz",
+                      "tzlocal",
+                      "pyyaml>=3.13se",
+                      "websocket-client"],
+    author_email='soroosh.sarabadani@gmail.com',
+    url='https://github.com/fandoghpaas/fandogh-cli',
+    keywords=['PaaS', 'Fandogh'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    entry_points='''
+        [console_scripts]
+        fandogh=fandogh_cli.fandogh:base
+    ''',
+
+)
