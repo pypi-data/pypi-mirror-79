45efe804f1354e726bc7560968a2bbbb445f110d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:05 +0000

    Add package oasys-barc4ro/1.0.0/oasys-barc4ro-1.0.0.tar.gz

---
+++ packages/oasys-barc4ro/oasys-barc4ro-1.0.0.tar.gz/oasys-barc4ro-1.0.0/barc4ro/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM barc4ro SETUP.PY
+short_version = '1.0.0'
+version = '1.0.0'
+full_version = '1.0.0.dev0+58d7e10'
+git_revision = '58d7e10d8569946d8c1af12d719c4006e2711f3e'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
