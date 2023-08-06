ee6cfc4378b5521cac471e5b23c5a15d5f2d6898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:45 +0000

    Add package keep-alive/0.0.1/keep-alive-0.0.1.tar.gz

---
+++ packages/keep-alive/keep-alive-0.0.1.tar.gz/keep-alive-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="keep-alive",
+    version="0.0.1",
+    author="Multi",
+    author_email="multiidev@gmail.com",
+    description="A package to keep an application online with HTTPS requests.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/codebankco/keep-alive",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
