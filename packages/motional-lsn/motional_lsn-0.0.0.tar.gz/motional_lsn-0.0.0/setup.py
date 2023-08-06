bfd5066d05ea606c1eb5dadff5e9c700d52742c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:41 +0000

    Add package motional-lsn/0.0.0/motional_lsn-0.0.0.tar.gz

---
+++ packages/motional-lsn/motional_lsn-0.0.0.tar.gz/motional_lsn-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="motional_lsn",
+    version="0.0.0",
+    author="Motional",
+    author_email="devops@motional.com",
+    description="motional_lsn",
+    long_description="motional_lsn",
+    long_description_content_type="text/markdown",
+    url="https://motional.com/",
+    packages=[],
+    python_requires='>=5',
+)
