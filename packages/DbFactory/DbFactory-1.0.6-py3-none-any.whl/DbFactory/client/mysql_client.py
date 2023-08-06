5f3b009985ea814e26cc739aac2c15b68ec9962b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:24 +0000

    Add package DbFactory/1.0.6/DbFactory-1.0.6-py3-none-any.whl

---
+++ packages/DbFactory/DbFactory-1.0.6-py3-none-any.whl/DbFactory/client/mysql_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# wechat: chending2012
+
+# python pack
+
+# third part pack
+
+# self pack
+from DbFactory.util.decorator import cost_time
+from .db_base import config
+from .mysql_client_base import MysqlClientBase
+
+
+class MysqlClient:
+    def __init__(self, **kwargs):
+        self.client__ = MysqlClientBase(**kwargs)
+
+    @cost_time(warning_time=config.get("ACTION_WARNING_TIME"))
+    def generation_func(self, method, *args, **kwargs):
+        def action():
+            return getattr(self.client__, method)(*args, **kwargs)
+
+        return action()
