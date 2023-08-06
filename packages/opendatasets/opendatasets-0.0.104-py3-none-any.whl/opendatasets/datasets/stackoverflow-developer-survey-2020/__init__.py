90a1efb90350a454e7a5d9953e0157ba26bf85c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:35 +0000

    Add package opendatasets/0.0.104/opendatasets-0.0.104-py3-none-any.whl

---
+++ packages/opendatasets/opendatasets-0.0.104-py3-none-any.whl/opendatasets/datasets/stackoverflow-developer-survey-2020/__init__.py
@@ -0,0 +1,14 @@
+from opendatasets.utils import download_url, GITHUB_RAW_BASE_URL
+
+FILES = [
+    'survey_results_public.csv',
+    'survey_results_schema.csv',
+    'README.txt'
+]
+
+def download(dataset_id, data_dir, dry_run, **kwargs):
+    base_url = GITHUB_RAW_BASE_URL + dataset_id + "/"
+    for fname in FILES:
+        full_url = base_url + fname
+        download_url(full_url, data_dir, fname, dry_run=dry_run)
+            
