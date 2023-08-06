2dd3c42a8614de79c6ca15e60387b7c1926b745b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:50 +0000

    Add package plotarchive/0.0.5/plotarchive-0.0.5.tar.gz

---
+++ packages/plotarchive/plotarchive-0.0.5.tar.gz/plotarchive-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plotarchive", # Replace with your own username
+    version="0.0.5",
+    author="Chris Buswinka",
+    author_email="buswinka@gmail.com",
+    description="An easy method to save code and data that generates scientific plots.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/buswinka/plotarchive",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
