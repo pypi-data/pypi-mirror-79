574ad627b7e3a850776f8395443489f7f7fec039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:59 +0000

    Add package mrsimulator/0.5.0.dev0/mrsimulator-0.5.0.dev0.tar.gz

---
+++ packages/mrsimulator/mrsimulator-0.5.0.dev0.tar.gz/mrsimulator-0.5.0.dev0/src/c_lib/include/powder_setup.h
@@ -0,0 +1,13 @@
+// -*- coding: utf-8 -*-
+//
+//  powder_setup.h
+//
+//  @copyright Deepansh J. Srivastava, 2019-2020.
+//  Created by Deepansh J. Srivastava, Apr 11, 2019.
+//  Contact email = deepansh2012@gmail.com
+//
+
+#include "config.h"
+
+extern void octahedron_averaging_setup(int nt, void *exp_I_alpha,
+                                       void *exp_I_beta, double *amp);
---
+++ packages/mrsimulator/mrsimulator-0.5.0.dev0.tar.gz/mrsimulator-0.5.0.dev0/src/c_lib/lib/powder_setup.c
@@ -0,0 +1,18 @@
+// -*- coding: utf-8 -*-
+//
+//  powder_setup.c
+//
+//  @copyright Deepansh J. Srivastava, 2019-2020.
+//  Created by Deepansh J. Srivastava, Apr 11, 2019
+//  Contact email = deepansh2012@gmail.com
+//
+
+#include "powder_setup.h"
+#include "octahedron.h"
+
+void octahedron_averaging_setup(int nt, void *exp_I_alpha, void *exp_I_beta,
+                                double *amp) {
+  // octahedronGetPolarAngleTrigOverAnOctant(nt, cos_alpha, cos_beta, amp);
+  octahedronGetPolarAngleCosineAzimuthalAnglePhaseOverOctant(nt, exp_I_alpha,
+                                                             exp_I_beta, amp);
+}
