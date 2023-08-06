c56a9e92aa87e24f0024b601ddd8e5442e5e5eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:44 +0000

    Add package rdptools/0.1.4/rdptools-0.1.4.tar.gz

---
+++ packages/rdptools/rdptools-0.1.4.tar.gz/rdptools-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rdptools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['office365-rest-client==2.2.0', 'pandas']
+
+setup_kwargs = {
+    'name': 'rdptools',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'Baiyue Cao',
+    'author_email': 'caobaiyue@live.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.0.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
