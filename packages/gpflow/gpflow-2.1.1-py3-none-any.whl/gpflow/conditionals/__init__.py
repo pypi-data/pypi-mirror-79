9fdad6e02b1fe0a0ae6207e618a125baf88cc503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:35 +0000

    Add package gpflow/2.1.1/gpflow-2.1.1-py3-none-any.whl

---
+++ packages/gpflow/gpflow-2.1.1-py3-none-any.whl/gpflow/conditionals/__init__.py
@@ -0,0 +1,13 @@
+# noqa: F401
+
+from .dispatch import conditional, sample_conditional
+
+from . import conditionals
+from . import sample_conditionals
+
+from . import multioutput
+
+
+from .util import base_conditional
+
+from .uncertain_conditionals import uncertain_conditional
---
+++ packages/gpflow/gpflow-2.1.1-py3-none-any.whl/gpflow/covariances/__init__.py
@@ -0,0 +1,5 @@
+from .dispatch import Kuf
+from .dispatch import Kuu
+
+from . import kufs, kuus
+from . import multioutput
