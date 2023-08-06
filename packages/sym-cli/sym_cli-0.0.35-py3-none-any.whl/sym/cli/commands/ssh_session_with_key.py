13bc84a4523e2d43e48e6361d4e02ee8d7b48b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:05 +0000

    Add package sym-cli/0.0.35/sym_cli-0.0.35-py3-none-any.whl

---
+++ packages/sym-cli/sym_cli-0.0.35-py3-none-any.whl/sym/cli/commands/ssh_session_with_key.py
@@ -0,0 +1,31 @@
+from datetime import timedelta
+
+import click
+
+from ..decorators import loses_interactivity, require_bins, require_login
+from ..helpers.boto import host_to_instance
+from ..helpers.global_options import GlobalOptions
+from ..helpers.options import resource_argument
+from ..helpers.ssh import ensure_ssh_key, start_tunnel
+from .sym import sym
+
+
+@sym.command(hidden=True, short_help="Start a SSH session")
+@resource_argument
+@click.option("--host", required=True)
+@click.option("--port", default=22, type=int, show_default=True)
+@click.make_pass_decorator(GlobalOptions)
+@loses_interactivity
+@require_bins("aws", "session-manager-plugin")
+@require_login
+def ssh_session_with_key(
+    options: GlobalOptions, resource: str, host: str, port: int
+) -> None:
+    """
+      Use approved creds for RESOURCE to start a SSH session to an EC2 instance,
+      uploading a public key if necessary.
+    """
+    client = options.create_saml_client(resource)
+    instance = host_to_instance(client, host)
+    ensure_ssh_key(client, instance, port, ttl=timedelta(minutes=5))
+    start_tunnel(client, instance, port)
