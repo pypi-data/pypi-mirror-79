94a157908db8f784567fd186d5a9c2e14ff0c726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:40 +0000

    Add package numdatasalva/0.0.5/numdatasalva-0.0.5-py3-none-any.whl

---
+++ packages/numdatasalva/numdatasalva-0.0.5-py3-none-any.whl/numdatasalva/calculate.py
@@ -0,0 +1,15 @@
+# this is the main code file containing 2 methods to calculate square and cube of a number
+
+def calculateSquare(num):
+	square= num * num
+	return square 
+
+
+def calculateCube(num):
+	cube= num * num * num
+	return cube 
+
+
+
+
+
