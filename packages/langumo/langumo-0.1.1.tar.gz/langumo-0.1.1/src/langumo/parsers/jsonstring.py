3966b4911469aa77670cf38ba6dc8d9555f03cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:12 +0000

    Add package langumo/0.1.1/langumo-0.1.1.tar.gz

---
+++ packages/langumo/langumo-0.1.1.tar.gz/langumo-0.1.1/src/langumo/parsers/jsonstring.py
@@ -0,0 +1,42 @@
+import re
+import json
+from langumo.building import Parser
+from langumo.utils import AuxiliaryFile
+from typing import Iterable
+
+
+class EscapedJSONStringParser(Parser):
+    single_quotes_pattern = re.compile('[\x60\xb4\u2018\u2019]')
+    double_quotes_pattern = re.compile('[\u201c\u201d]')
+
+    def extract(self, raw: AuxiliaryFile) -> Iterable[str]:
+        with raw.open('r') as fp:
+            for line in fp:
+                if not line.strip():
+                    continue
+
+                yield line
+
+    def parse(self, text: str) -> str:
+        text, _ = json.decoder.scanstring(text, 1)
+
+        filtered = []
+        for line in text.strip().splitlines():
+            if not line:
+                continue
+
+            # Remove duplicated spaces.
+            line = line.replace('\n', ' ').replace('\t', ' ')
+            while '  ' in line:
+                line = line.replace('  ', ' ')
+
+            # Normalize the quotes by replacing unusual ones to the standard
+            # ones.
+            line = (EscapedJSONStringParser
+                    .single_quotes_pattern.sub('\'', line))
+            line = (EscapedJSONStringParser
+                    .double_quotes_pattern.sub('"', line))
+
+            filtered.append(line)
+
+        return '\n'.join(filtered)
