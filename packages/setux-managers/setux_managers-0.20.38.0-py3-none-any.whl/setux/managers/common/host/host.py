e95e6ecbe3db64350a1409289f37e6166d9a89b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:51 +0000

    Add package setux-managers/0.20.38.0/setux_managers-0.20.38.0-py3-none-any.whl

---
+++ packages/setux-managers/setux_managers-0.20.38.0-py3-none-any.whl/setux/managers/common/host/host.py
@@ -0,0 +1,36 @@
+from setux.core.manage import Manager
+
+
+class Distro(Manager):
+    manager = 'host'
+
+    @property
+    def name(self):
+        attr = '_host_name_'
+        try:
+            val = getattr(self, attr)
+        except AttributeError:
+            ret, out, err = self.run('hostname')
+            val = out[0]
+            setattr(self, attr,  val)
+        return val
+
+    @name.setter
+    def name(self, val):
+        attr = '_host_name_'
+        delattr(self, attr)
+        ret, out, err = self.run(
+            'hostname', val.replace('_', '')
+        )
+        return ret
+
+    @property
+    def fqdn(self):
+        ret, out, err = self.run('hostname -f')
+        return out[0]
+
+    @property
+    def addr(self):
+        ret, out, err = self.run('curl -s https://api.ipify.org')
+        return ret==0 and out[0] or '!'
+
