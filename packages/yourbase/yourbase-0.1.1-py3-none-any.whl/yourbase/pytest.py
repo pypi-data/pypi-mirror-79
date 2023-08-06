21236cb92d731dc8745a12799228e5c4164c8393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:36 +0000

    Add package yourbase/0.1.1/yourbase-0.1.1-py3-none-any.whl

---
+++ packages/yourbase/yourbase-0.1.1-py3-none-any.whl/yourbase/pytest.py
@@ -0,0 +1,48 @@
+# This file sets up YourBase pytest integration.
+#
+# For projects using pytest, the project only needs to add yourbase to its
+# requirements file. This file will be run as an entry point (via setup.py) and
+# will attach to pytest hooks so that tests do not need to be decorated.
+#
+# For projects not using pytest, or when running outside of the YourBase CI,
+# this file does nothing.
+
+import inspect
+import os
+import yourbase
+
+
+if yourbase.ENABLED:
+    try:
+        import pytest
+
+        print("[YB] pytest found, attaching")
+        from . import skip_when_possible
+
+        def pytest_runtest_setup(item: pytest.Item):
+            if yourbase.PLUGIN is None:
+                return
+
+            fname = "%s" % (item.function.__qualname__)
+            fpath = os.getcwd() + os.sep + inspect.getfile(item.function)
+
+            if yourbase.PLUGIN.can_skip(fpath, fname):
+                item.add_marker(
+                    pytest.mark.skip(reason="[YB] No dependencies changed ✨")
+                )
+            else:
+                yourbase.PLUGIN.start_test(fname, fpath)
+
+        def pytest_runtest_teardown(item: pytest.Item):
+            if yourbase.PLUGIN is None:
+                return
+
+            fname = "%s" % (item.function.__qualname__)
+            fpath = os.getcwd() + os.sep + inspect.getfile(item.function)
+
+            yourbase.PLUGIN.end_test(fname, fpath)
+
+    except ModuleNotFoundError:
+        print(
+            "[YB] YourBase pytest code was initiated, but pytest wasn't found. Is pytest installed oddly?"
+        )
