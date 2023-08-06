e195d1eb1fdb3505ea238f916e9403552543e012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:08 +0000

    Add package rsa3/0.0.0/rsa3-0.0.0-py3-none-any.whl

---
+++ packages/rsa3/rsa3-0.0.0-py3-none-any.whl/pyrsa/data/__init__.py
@@ -0,0 +1,7 @@
+from .dataset import Dataset
+from .dataset import load_dataset
+from .dataset import dataset_from_dict
+from .computations import average_dataset
+from .computations import average_dataset_by
+from .noise import cov_from_residuals
+from .noise import prec_from_residuals
---
+++ packages/rsa3/rsa3-0.0.0-py3-none-any.whl/pyrsa/vis/__init__.py
@@ -0,0 +1,3 @@
+from .rdm_plot import show_rdm
+from .mds_plot import mds, rdm_dimension_reduction
+from .model_plot import plot_model_comparison
