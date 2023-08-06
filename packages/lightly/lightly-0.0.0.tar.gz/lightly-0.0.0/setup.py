85637be52df08bf256e6279c88fc453bd553f8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:17 +0000

    Add package lightly/0.0.0/lightly-0.0.0.tar.gz

---
+++ packages/lightly/lightly-0.0.0.tar.gz/lightly-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lightly',
+    version='0.0.0',
+    author='Philipp Wirth',
+    author_email='philipp@whattolabel.com',
+    description='',
+    packages=find_packages(),    
+    install_requires=[],
+)
