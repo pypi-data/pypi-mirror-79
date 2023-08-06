488d0b8203162ef5f670e870937414d4b64533e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:07 +0000

    Add package pytm/1.1.0/pytm-1.1.0.tar.gz

---
+++ packages/pytm/pytm-1.1.0.tar.gz/pytm-1.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name='pytm',
+    version='1.1.0',
+    packages=['pytm'],
+    summary='A Python-based framework for threat modeling.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    url="https://github.com/izar/pytm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Security",
+        "Natural Language :: English",
+    ],
+    python_requires='>=3',
+    package_data={
+        'pytm': ['images/lambda.png', 'threatlib/threats.json'],
+    },
+    exclude_package_data={'': ['report.html']},
+    include_package_data=True,
+)
