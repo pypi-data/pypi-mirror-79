482f9d8455778f16c6e8a2ed7b22035dea8787da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:36 +0000

    Add package numsys/1.0.0/numsys-1.0.0.tar.gz

---
+++ packages/numsys/numsys-1.0.0.tar.gz/numsys-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from numsys.support import version
+from setuptools import setup
+
+setup(name = 'numsys',
+      version = version,
+      description = 'numeral systems - number base conversion module',
+      long_description = open('README.rst').read(),
+      license = 'GPLv3',
+      author = 'Electrostatus',
+      url = 'http://github.com/Electrostatus/numsys',
+      packages = ['numsys'],
+      package_data = {'': ['*.rst']},
+      extra_requires = ['gmpy2'],  # optional, for fast float pt precision
+      keywords = 'number base numeral system mixed radix roman',
+      classifiers = [
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Intended Audience :: Education',
+        ],
+      )
