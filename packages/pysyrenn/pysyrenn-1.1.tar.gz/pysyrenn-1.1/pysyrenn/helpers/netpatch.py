6515d2df942bd467532abba8ad809aa64ba3dbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:33 +0000

    Add package pysyrenn/1.1/pysyrenn-1.1.tar.gz

---
+++ packages/pysyrenn/pysyrenn-1.1.tar.gz/pysyrenn-1.1/pysyrenn/helpers/netpatch.py
@@ -0,0 +1,10 @@
+"""Stubs for netpatch."""
+
+class NetPatcher:
+    """Stubs for NetPatcher."""
+    def __init__(self, network, layer_index, inputs, labels):
+        """Warns that NetPatcher is no longer in this package."""
+        print("Patching and MaskingNetworks are no longer part of pysyrenn.")
+        print("We are working to provide a new patching-specific package.")
+        print("\nIn the meantime, please use pysyrenn==1.0")
+        raise NotImplementedError
