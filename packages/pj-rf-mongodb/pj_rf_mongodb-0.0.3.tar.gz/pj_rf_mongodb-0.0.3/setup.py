b095d42af7bc4ec3bc14e7eab69852e4a38bb707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:54 +0000

    Add package pj-rf-mongodb/0.0.3/pj_rf_mongodb-0.0.3.tar.gz

---
+++ packages/pj-rf-mongodb/pj_rf_mongodb-0.0.3.tar.gz/pj_rf_mongodb-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="pj_rf_mongodb",
+    version="0.0.3",
+    description="Get Data from MongoDB during execution using Robot Framework..",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    author="PJ Gevana",
+    author_email="patdroidz@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["pj_rf_mongodb"],
+    include_package_data=True,
+    # install_requires=["requests"],
+)
