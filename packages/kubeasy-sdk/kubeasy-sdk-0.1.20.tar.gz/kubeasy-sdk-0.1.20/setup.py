3accfb06be734fd7bf7ffc78b599d53ea7cd7b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:32 +0000

    Add package kubeasy-sdk/0.1.20/kubeasy-sdk-0.1.20.tar.gz

---
+++ packages/kubeasy-sdk/kubeasy-sdk-0.1.20.tar.gz/kubeasy-sdk-0.1.20/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import os
+from requirementslib import Lockfile
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kubeasy-sdk",
+    version=os.getenv('KUBEASY_VERSION', "v0.1.20"),
+    author="Dylan Turnbull",
+    author_email="dylanturn@gmail.com",
+    description="Kubernetes made easy!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dylanturn/kubeasy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires = Lockfile.load("Pipfile.lock").as_requirements()
+)
