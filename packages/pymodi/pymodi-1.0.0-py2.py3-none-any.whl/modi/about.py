a67f1d6084fd48ea9d8700bd6e91e1a53e338db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:27 +0000

    Add package pymodi/1.0.0/pymodi-1.0.0-py2.py3-none-any.whl

---
+++ packages/pymodi/pymodi-1.0.0-py2.py3-none-any.whl/modi/about.py
@@ -0,0 +1,8 @@
+__title__ = 'pymodi'
+__version__ = "1.0.0"
+__author__ = 'LUXROBO'
+__email__ = 'tech@luxrobo.com'
+__description__ = 'Easy😆 and fast💨 MODI Python API package.'
+__url__ = 'https://github.com/LUXROBO/pymodi'
+__license__ = 'MIT'
+__summary__ = 'Easy😆 and fast💨 MODI Python API package.'
