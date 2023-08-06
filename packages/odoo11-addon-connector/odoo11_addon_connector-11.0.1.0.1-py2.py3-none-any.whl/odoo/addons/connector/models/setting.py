396839f84f1f842a9607f22e3a9cc076e85bbc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:01 +0000

    Add package odoo11-addon-connector/11.0.1.0.1/odoo11_addon_connector-11.0.1.0.1-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-connector/odoo11_addon_connector-11.0.1.0.1-py2.py3-none-any.whl/odoo/addons/connector/models/setting.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013-2017 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+from odoo import models
+
+
+class ConnectorConfigSettings(models.TransientModel):
+
+    _name = 'connector.config.settings'
+    _description = 'Connector Configuration'
+    _inherit = 'res.config.settings'
