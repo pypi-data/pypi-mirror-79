a516b03647b69816c106d13a75531b9b551e1f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:30 +0000

    Add package pmgwidgets/0.1.3/pmgwidgets-0.1.3.tar.gz

---
+++ packages/pmgwidgets/pmgwidgets-0.1.3.tar.gz/pmgwidgets-0.1.3/pmgwidgets/__init__.py
@@ -0,0 +1,7 @@
+from .table import PMGTableWidget, PMTableView, PMGTableTabWidget
+from .containers import PMFlowArea, PMScrollArea, PMTabWidget, PMFlowLayout, PMFlowLayoutWithGrid
+from .basicwidgets import PMDockObject, PMPushButtonPane, PMToolButton
+from .toolbars import PMToolBar,TopToolBar,ActionWithMessage
+from .sourcemgr import create_icon
+from .normal import SettingsPanel,center_window,set_always_on_top,set_closable,set_minimizable
+from .treeviews import PMGFilesTreeview
