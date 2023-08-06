a63ca84c09ad0db00b1811f277f372271b883965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:06 +0000

    Add package strontium/0.0.13/strontium-0.0.13-py2.py3-none-any.whl

---
+++ packages/strontium/strontium-0.0.13-py2.py3-none-any.whl/strontium/medical/augmentations.py
@@ -0,0 +1,19 @@
+import numpy as np 
+import cv2
+import math
+import random
+import warnings
+from ..utils import *
+
+class Crop():
+ 	def __init__(self, image:np.array, x, y):
+ 		super(Crop, self).__init__()
+ 		self.image = image
+ 		self.x     = x
+ 		self.y     = y
+
+ 	def __call__(self):
+		return self.image[0:self.x, 0:self.y]
+		
+transformation = Crop()
+crop = CoreTransformation(p=0.5, construct_class=transformation)
