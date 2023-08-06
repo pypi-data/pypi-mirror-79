b9ba6775c2a49999884b3c3fff19d3e39c66fe29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:01 +0000

    Add package simple-injection/0.2.1/simple-injection-0.2.1.tar.gz

---
+++ packages/simple-injection/simple-injection-0.2.1.tar.gz/simple-injection-0.2.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="simple-injection",
+    version="0.2.1",
+    description="A simple library for typing-based dependency injection",
+    author="Bradley Lewis",
+    license="MIT",
+    url="https://github.com/BradLewis/simple-injection",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
