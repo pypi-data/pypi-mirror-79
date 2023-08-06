0d7e36f5154ee627e6a4bd6417a7d6786d665e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:08 +0000

    Add package strontium/0.0.14/strontium-0.0.14-py2.py3-none-any.whl

---
+++ packages/strontium/strontium-0.0.14-py2.py3-none-any.whl/strontium/utils/transforms.py
@@ -0,0 +1,36 @@
+class CoreTransformation:
+
+	def __init__(self, p, construct_class):
+		super(CoreTransformation, self).__init__()
+		self.p = p
+		self.construct_class = construct_class
+
+	def __call__(self, *args):
+
+		if self.construct_class == None:
+			raise ValueError("Constructor improperly called, please retry by properly applying a constructor for the transformation.")
+		else:
+			try:
+				return self.construct_class(*args)
+			except Exception:
+				raise ValueError("Your constructor function has an error in it.")
+
+			
+class TwoImageTransformation:
+
+	def __init__(self, p, construct_class):
+		super(TwoImageTransformation, self).__init__()
+		self.p = p
+		self.construct_class = construct_class
+
+	def __call__(self, image1, image2):
+		if image == None:
+			raise ValueError("You have improperly called the image constructor.")
+		else:
+			if self.construct_class == None:
+				raise ValueError("Constructor improperly called, please retry by properly applying a constructor for the transformation.")
+			else:
+				try:
+					return self.construct_class(image1, image2)
+				except Exception:
+					raise ValueError("Your constructor function has an error in it.")		
