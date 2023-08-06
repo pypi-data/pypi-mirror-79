dc02d664d79909a5ea2f798e69b9f97a9c8b9547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:17 +0000

    Add package django-stisla/0.0.7/django_stisla-0.0.7-py3-none-any.whl

---
+++ packages/django-stisla/django_stisla-0.0.7-py3-none-any.whl/django_stisla/admin.py
@@ -0,0 +1,22 @@
+import datetime
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
+from django.contrib import admin
+from django.contrib.admin import AdminSite
+
+# Register your models here.
+
+class StislaAdmin(AdminSite):
+
+    site_short_title = "DJ"
+
+    def each_context(self, request):
+        context = super().each_context(request)
+        context["site_short_title"] = self.site_short_title
+        context["current_time"] = datetime.datetime.utcnow()
+        return context
+
+
+site = StislaAdmin()
+site.register(Group, GroupAdmin)
+site.register(User, UserAdmin)
