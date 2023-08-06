328ec066264a48805abd2fd4a448ae7f6e0ec7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:31 +0000

    Add package setux-managers/0.20.38.1/setux_managers-0.20.38.1.tar.gz

---
+++ packages/setux-managers/setux_managers-0.20.38.1.tar.gz/setux_managers-0.20.38.1/setux/managers/common/host/host.py
@@ -0,0 +1,43 @@
+from urllib.request import urlopen
+
+from setux.core.manage import Manager
+
+
+class Distro(Manager):
+    manager = 'host'
+
+    @property
+    def name(self):
+        attr = '_host_name_'
+        try:
+            val = getattr(self, attr)
+        except AttributeError:
+            ret, out, err = self.run('hostname')
+            val = out[0]
+            setattr(self, attr,  val)
+        return val
+
+    @name.setter
+    def name(self, val):
+        attr = '_host_name_'
+        delattr(self, attr)
+        ret, out, err = self.run(
+            'hostname', val.replace('_', '')
+        )
+        return ret
+
+    @property
+    def fqdn(self):
+        ret, out, err = self.run('hostname -f')
+        return out[0]
+
+    @property
+    def addr(self):
+        with urlopen('https://api.ipify.org', timeout=1) as resp:
+            if resp.status==200:
+                adr = resp.read().decode()
+                return adr
+            else:
+                debug(resp.reason)
+                return '!'
+
