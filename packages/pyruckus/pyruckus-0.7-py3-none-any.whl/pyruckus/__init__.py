c7d16efa5edea76ec50373034e727566a3a9a7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:02 +0000

    Add package pyruckus/0.7/pyruckus-0.7-py3-none-any.whl

---
+++ packages/pyruckus/pyruckus-0.7-py3-none-any.whl/pyruckus/__init__.py
@@ -0,0 +1,67 @@
+import re
+
+from .RuckusSSH import RuckusSSH
+from .const import CLIENTS_REGEX, MESH_NAME_REGEX
+
+
+class Ruckus:
+    """Class for communicating with the device."""
+
+    def __init__(self, host: str, username: str, password: str, login_timeout=15, timeout=10) -> None:
+        """Set runtime configuration."""
+        self.host = host
+        self.username = username
+        self.password = password
+        self.login_timeout = login_timeout
+        self.timeout = timeout
+
+        self.ssh = None
+        self.connect()
+
+    def __del__(self) -> None:
+        """Disconnect on delete."""
+        self.disconnect()
+
+    def connect(self) -> None:
+        """Create SSH connection and login."""
+        ssh = RuckusSSH(encoding="utf-8")
+        ssh.login(self.host, username=self.username, password=self.password, login_timeout=self.login_timeout)
+        self.ssh = ssh
+
+    def disconnect(self) -> None:
+        """Close the SSH session."""
+        if self.ssh and self.ssh.isalive():
+            self.ssh.close()
+
+    def clients(self) -> dict:
+        """Pull active clients from the device."""
+        if not self.ssh.isalive():
+            self.connect()
+
+        result = self.ssh.run_privileged("show current-active-clients all")
+
+        devices = {}
+        for client in re.split("Clients:", result):
+            match = CLIENTS_REGEX.search(client)
+            if match:
+                devices[match.group("mac")] = {
+                    "ip_address": match.group("ip"),
+                    "mac": match.group("mac"),
+                    "name": match.group("name"),
+                }
+
+        return devices
+
+    def mesh_name(self) -> str:
+        """Pull the current mesh name."""
+        if not self.ssh.isalive():
+            self.connect()
+
+        result = self.ssh.run_privileged("show mesh info")
+
+        match = MESH_NAME_REGEX.search(result)
+
+        if match:
+            return match.group("name")
+        else:
+            return "Ruckus Mesh"
