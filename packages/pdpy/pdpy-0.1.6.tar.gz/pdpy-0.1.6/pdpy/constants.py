2b1e726766ff1c1e230be3fbd2714c99bfd18ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:17 +0000

    Add package pdpy/0.1.6/pdpy-0.1.6.tar.gz

---
+++ packages/pdpy/pdpy-0.1.6.tar.gz/pdpy-0.1.6/pdpy/constants.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Package constants."""
+
+# Package settings ------------------------------------------------------------
+
+SETTINGS_API_URL = 'api_url'
+SETTINGS_API_URL_DEFAULT = 'https://api.parliament.uk/sparql'
+
+# API settings ----------------------------------------------------------------
+
+API_PAUSE_TIME = 0.5
+
+# XML ids ---------------------------------------------------------------------
+
+XML_DATE = 'http://www.w3.org/2001/XMLSchema#date'
+
+# Parliamentary Data Platform ids ---------------------------------------------
+
+PDP_ID_HOUSE_OF_COMMONS = '1AFu55Hs'
+PDP_ID_HOUSE_OF_LORDS = 'WkUWUBMx'
---
+++ packages/pdpy/pdpy-0.1.6.tar.gz/pdpy-0.1.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name = 'pdpy',
+    packages = ['pdpy'],
+    version = '0.1.6',
+    description = 'A package for downloading data from the Parliamentary Data Platform',
+    author = 'Oliver Hawkins',
+    author_email = 'oli@olihawkins.com',
+    url = 'https://github.com/houseofcommonslibrary/pdpy',
+    license = 'BSD',
+    keywords = ['Parliament', 'MP', 'House of Commons', 'House of Lords'],
+    install_requires = ['numpy', 'pandas', 'requests'],
+    classifiers = [],
+)
