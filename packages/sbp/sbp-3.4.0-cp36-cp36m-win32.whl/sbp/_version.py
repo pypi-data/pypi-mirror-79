2dcf57084c64d0cc66e6eecec63fd9427e785ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:05 +0000

    Add package sbp/3.4.0/sbp-3.4.0-cp36-cp36m-win32.whl

---
+++ packages/sbp/sbp-3.4.0-cp36-cp36m-win32.whl/sbp/_version.py
@@ -0,0 +1,7 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '3.4.0'
+full_version = '3.4.0'
+git_revision = '6aa29030'
+is_released = True
+if not is_released:
+    version = full_version
