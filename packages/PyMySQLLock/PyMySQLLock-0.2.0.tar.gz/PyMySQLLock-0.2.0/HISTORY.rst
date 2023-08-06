0e199a9d3c5981e3edca23b29bc873c83d110c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:24 +0000

    Add package PyMySQLLock/0.2.0/PyMySQLLock-0.2.0.tar.gz

---
+++ packages/PyMySQLLock/PyMySQLLock-0.2.0.tar.gz/PyMySQLLock-0.2.0/HISTORY.rst
@@ -0,0 +1,21 @@
+=======
+History
+=======
+
+------------------
+0.2.0 (2020-09-13)
+------------------
+
+* Add API to get all acquired locks on given mysql db
+
+------------------
+0.1.0 (2020-09-13)
+------------------
+
+* Add background lock connection refresh
+
+------------------
+0.0.1 (2020-09-12)
+------------------
+
+* First release on PyPI.
---
+++ packages/PyMySQLLock/PyMySQLLock-0.2.0.tar.gz/PyMySQLLock-0.2.0/tests/test_locker.py
@@ -0,0 +1,40 @@
+from PyMySQLLock import Locker
+
+
+def test_get_lock_success(mysql_conn_params, mysql_connectors):
+    for connector in mysql_connectors:
+        mysql_conn_params["mysql_lib_connector"] = connector
+        locker = Locker(**mysql_conn_params)
+        l = locker.lock("test")
+        ret = l.acquire(refresh_interval_secs=1)
+        assert ret
+        assert l.locked()
+
+        # verify the lock returns in get_locks
+        assert "test" in locker.get_all_locks()
+
+        # verify if lock is obtained by trying to acquire again with same name
+        l1 = locker.lock("test")
+        ret1 = l1.acquire(timeout=1)
+        assert not ret1
+        assert not l1.locked()
+
+        # release the lock now
+        l.release()
+
+        # try again to acquire
+        ret1 = l1.acquire(timeout=1)
+        assert ret1
+        assert l1.locked()
+
+        l1.release()
+
+
+def test_mysql_error(mysql_conn_params):
+    mysql_conn_params["password"] = "foo"  # this will cause an error
+    try:
+        locker = Locker(**mysql_conn_params)
+        l = locker.lock("test")
+        ret = l.acquire()
+    except Exception as e:
+        assert "Could not connect to db" in str(e)
