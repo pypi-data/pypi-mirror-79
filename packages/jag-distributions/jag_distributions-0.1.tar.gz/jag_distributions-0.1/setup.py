6f7ddc7015bfdad4e3870b7548c523be697a1b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:33 +0000

    Add package jag-distributions/0.1/jag_distributions-0.1.tar.gz

---
+++ packages/jag-distributions/jag_distributions-0.1.tar.gz/jag_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jag_distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['jag_distributions'],
+      author='Jeroen Goddijn',
+      zip_safe=False)
