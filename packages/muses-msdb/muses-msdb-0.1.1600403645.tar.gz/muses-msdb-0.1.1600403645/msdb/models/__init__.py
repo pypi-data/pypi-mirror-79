20a1182c95be55cb887c578f72cf75409630ae1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:49 +0000

    Add package muses-msdb/0.1.1600403645/muses-msdb-0.1.1600403645.tar.gz

---
+++ packages/muses-msdb/muses-msdb-0.1.1600403645.tar.gz/muses-msdb-0.1.1600403645/msdb/models/__init__.py
@@ -0,0 +1,15 @@
+from .base_model import BaseModel
+from .member import MemberManager, Member
+from .message import MessageManager, Message
+from .site_param import SiteParamManager, SiteParam
+from .section import SectionManager, Section
+from .tag import TagManager, Tag
+from .license import LicenseManager, License
+from .book import BookManager, Book
+from .bookpart import BookPartManager, BookPart
+from .post import PostManager, Post
+from .comment import CommentManager, Comment
+from .rating import Rating, PostRatingManager, PostRating, CommentRatingManager, CommentRating
+from .like import Like, PostLikeManager, PostLike, CommentLikeManager, CommentLike
+
+
