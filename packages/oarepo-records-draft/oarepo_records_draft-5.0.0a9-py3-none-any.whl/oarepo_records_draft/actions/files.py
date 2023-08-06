91bc086e9fd1089779874cee34459947c3cf10ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:39 +0000

    Add package oarepo-records-draft/5.0.0a9/oarepo_records_draft-5.0.0a9-py3-none-any.whl

---
+++ packages/oarepo-records-draft/oarepo_records_draft-5.0.0a9-py3-none-any.whl/oarepo_records_draft/actions/files.py
@@ -0,0 +1,144 @@
+try:
+
+    from functools import wraps
+
+    from flask import jsonify, abort
+    from flask import request
+    from flask.views import MethodView
+    from invenio_db import db
+    from invenio_records_rest.utils import deny_all
+    from invenio_records_rest.views import pass_record
+    from invenio_rest import ContentNegotiatedMethodView
+
+    from invenio_files_rest.signals import file_uploaded, file_downloaded, file_deleted
+    from invenio_files_rest.serializer import json_serializer
+
+
+    def apply_permission(perm_or_factory):
+        def func(*args, **kwargs):
+            if callable(perm_or_factory):
+                return perm_or_factory(*args, **kwargs)
+            return perm_or_factory
+
+        return func
+
+
+    # adopted from invenio-records-rest
+    def verify_file_permission(permission_factory, record, key, missing_ok):
+        if key not in record.files and not missing_ok:
+            abort(404)
+
+        try:
+            file_object = record.files[key]
+        except KeyError:
+            file_object = None
+
+        permission = apply_permission(permission_factory)(record=record, key=key, file_object=file_object)
+
+        if not permission.can():
+            from flask_login import current_user
+            if not current_user.is_authenticated:
+                abort(401)
+            abort(403)
+
+
+    def need_file_permission(factory_name, missing_ok=False):
+        def permission_builder(f):
+            @wraps(f)
+            def permission_decorator(self, record=None, *args, **kwargs):
+                permission_factory = getattr(self, factory_name, deny_all)
+
+                # FIXME use context instead
+                request._methodview = self
+
+                key = kwargs['key']
+                verify_file_permission(permission_factory, record, key, missing_ok)
+
+                return f(self, record=record, *args, **kwargs)
+
+            return permission_decorator
+
+        return permission_builder
+
+
+    class FileResource(MethodView):
+        view_name = '{0}_attachment'
+
+        def __init__(self, get_file_factory=None, put_file_factory=None, delete_file_factory=None,
+                     restricted=True, as_attachment=True, *args, **kwargs):
+            super().__init__(
+                *args,
+                **kwargs
+            )
+            self.put_file_factory = put_file_factory
+            self.get_file_factory = get_file_factory
+            self.delete_file_factory = delete_file_factory
+            self.restricted = restricted
+            self.as_attachment = as_attachment
+
+        @pass_record
+        @need_file_permission('put_file_factory', missing_ok=True)
+        def put(self, pid, record, key):
+            record.files[key] = request.stream
+            record.commit()
+            db.session.commit()
+            ret = jsonify(record.files[key].dumps())
+            file_uploaded.send(record.files[key].get_version())
+            ret.status_code = 201
+            return ret
+
+        @pass_record
+        @need_file_permission('delete_file_factory')
+        def delete(self, pid, record, key):
+            deleted_record = record.files[key]
+            deleted_record_version = deleted_record.get_version()
+            del record.files[key]
+            record.commit()
+            db.session.commit()
+            file_deleted.send(deleted_record_version)
+            ret = jsonify(deleted_record.dumps())
+            ret.status_code = 200
+            return ret
+
+        @pass_record
+        @need_file_permission('get_file_factory')
+        def get(self, pid, record, key):
+            obj = record.files[key]
+            obj = obj.get_version()
+            file_downloaded.send(obj)
+            return obj.send_file(restricted=self.call(self.restricted, record, obj, key),
+                                 as_attachment=self.call(self.as_attachment, record, obj, key))
+
+        def call(self, prop, record, obj, key):
+            if callable(prop):
+                return prop(record=record, obj=obj, key=key)
+            return prop
+
+
+    class FileListResource(ContentNegotiatedMethodView):
+
+        view_name = '{0}_attachments'
+
+        def __init__(self, get_file_factory=None, serializers=None, *args, **kwargs):
+            super().__init__(
+                *args,
+                serializers=serializers or {
+                    'application/json': json_serializer,
+                },
+                default_media_type='application/json',
+                **kwargs
+            )
+            self.get_file_factory = get_file_factory
+
+        @pass_record
+        def get(self, pid, record):
+            return jsonify([
+                file for key, file in record.files.filesmap.items()
+                if apply_permission(self.get_file_factory)(record=record, key=key, file_object=file).can()
+            ])
+
+except:
+    FileResource = None
+    FileListResource = None
+
+__all__ = ('FileResource', 'FileListResource')
