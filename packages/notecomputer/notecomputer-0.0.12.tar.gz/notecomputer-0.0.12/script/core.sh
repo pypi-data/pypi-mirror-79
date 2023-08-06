94a2204bbbddb7f071f102b9632ca3f7c904ffab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:17 +0000

    Add package notecomputer/0.0.12/notecomputer-0.0.12.tar.gz

---
+++ packages/notecomputer/notecomputer-0.0.12.tar.gz/notecomputer-0.0.12/script/core.sh
@@ -0,0 +1,12 @@
+pip install twine notetool
+git config --global user.email "1007530194@qq.com"
+git config --global user.name "niuliangtao"
+
+cd /root
+mkdir -vp /root/workspace
+
+cd /root/workspace
+git clone git@github.com:notechats/notetool.git
+git clone git@github.com:notechats/notekeras.git
+git clone git@github.com:notechats/notedrive.git
+git clone git@github.com:notechats/notecomputer.git
