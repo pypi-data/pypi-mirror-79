fea698f85fab5caabb9bbbe88b5d77807d8da895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:15 +0000

    Add package mab-hakuinadvisors/0.3.1/mab_hakuinadvisors-0.3.1-py3-none-any.whl

---
+++ packages/mab-hakuinadvisors/mab_hakuinadvisors-0.3.1-py3-none-any.whl/mab/ab.py
@@ -0,0 +1,32 @@
+from .mab import MAB
+import random
+
+
+class AB(MAB):
+    """AB testing Class for Multiarm Bandit Application
+
+    Args:
+        MAB (MAB): Muliarm Bandit parent class
+    """
+    def __init__(self, counts=None, values=None, n_arms=None):
+        super().__init__(counts, values, n_arms)
+        self.current_arm = 0
+
+    @property
+    def name(self):
+        return "AB-test"
+
+    def select_arm(self):
+        current_arm = self.current_arm
+        # switch arm to the next position
+        if self.current_arm == self.n_arms - 1:
+            self.current_arm = 0
+        else:
+            self.current_arm += 1
+
+        # return the arm before
+        return current_arm
+
+    def reset(self):
+        super(AB, self).reset()
+        self.current_arm = 0
