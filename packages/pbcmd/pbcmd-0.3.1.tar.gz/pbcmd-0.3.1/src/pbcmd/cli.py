4293b2a5564b48418cc02db71829304ce2d6a50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:21 +0000

    Add package pbcmd/0.3.1/pbcmd-0.3.1.tar.gz

---
+++ packages/pbcmd/pbcmd-0.3.1.tar.gz/pbcmd-0.3.1/src/pbcmd/cli.py
@@ -0,0 +1,19 @@
+"""CLI interface."""
+# pylint: disable=import-self,unused-import,unexpected-keyword-arg
+
+import click_completion
+
+from . import cli
+
+import pbcmd.hello
+import pbcmd.calc
+import pbcmd.proxy
+import pbcmd.timefmt
+import pbcmd.git
+import pbcmd.pyon2json
+import pbcmd.csplit
+import pbcmd.rm_timestamped
+
+if __name__ == "__main__":
+    click_completion.init()
+    cli(prog_name="pb")
