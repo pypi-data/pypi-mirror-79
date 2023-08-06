cd808ce078d2c2863ed63b24e310bdcd67dede3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:58 +0000

    Add package invenio-circulation/1.0.0a27/invenio-circulation-1.0.0a27.tar.gz

---
+++ packages/invenio-circulation/invenio-circulation-1.0.0a27.tar.gz/invenio-circulation-1.0.0a27/run-tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 CERN.
+# Copyright (C) 2018 RERO.
+#
+# Invenio-Circulation is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+pydocstyle invenio_circulation tests docs && \
+isort invenio_circulation tests --check-only --diff && \
+check-manifest --ignore ".travis-*" && \
+sphinx-build -qnNW docs docs/_build/html && \
+python setup.py test
