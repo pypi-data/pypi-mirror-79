f01f5f134826f1c4f71ef9ab91bba120077cce56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:35 +0000

    Add package raiseorlaunch/2.3.4/raiseorlaunch-2.3.4.tar.gz

---
+++ packages/raiseorlaunch/raiseorlaunch-2.3.4.tar.gz/raiseorlaunch-2.3.4/raiseorlaunch/__init__.py
@@ -0,0 +1,10 @@
+from raiseorlaunch.raiseorlaunch import (  # noqa: F401
+    Raiseorlaunch,
+    RaiseorlaunchError,
+    __author__,
+    __description__,
+    __license__,
+    __title__,
+    __version__,
+    check_positive,
+)
---
+++ packages/raiseorlaunch/raiseorlaunch-2.3.4.tar.gz/raiseorlaunch-2.3.4/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+setup.py for raiseorlaunch
+"""
+
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="raiseorlaunch",
+    version="2.3.4",
+    description="A run-or-raise-application-launcher for i3 window manager.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/open-dynaMIX/raiseorlaunch",
+    author="Fabio Ambauen",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords="i3 i3wm launcher run-or-raise navigation workspace scratchpad",
+    install_requires=["i3ipc"],
+    packages=["raiseorlaunch"],
+    entry_points={"console_scripts": ["raiseorlaunch = raiseorlaunch.__main__:main"]},
+)
