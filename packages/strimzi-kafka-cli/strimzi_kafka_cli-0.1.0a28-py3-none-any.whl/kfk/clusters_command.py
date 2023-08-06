47fe1c2455f967e1a74a58158ea889e7fbfdb187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:29 +0000

    Add package strimzi-kafka-cli/0.1.0a28/strimzi_kafka_cli-0.1.0a28-py3-none-any.whl

---
+++ packages/strimzi-kafka-cli/strimzi_kafka_cli-0.1.0a28-py3-none-any.whl/kfk/clusters_command.py
@@ -0,0 +1,30 @@
+import click
+import os
+
+from kfk.command import kfk
+from kfk.option_extensions import NotRequiredIf
+from kfk.commons import print_missing_options_for_command
+from kfk.kubectl_command_builder import Kubectl
+
+
+@click.option('-n', '--namespace', help='Namespace to use')
+@click.option('-o', '--output',
+              help='Output format. One of: json|yaml|name|go-template|go-template-file|template|templatefile|jsonpath'
+                   '|jsonpath-file.')
+@click.option('--describe', help='List details for the given cluster.', is_flag=True)
+@click.option('--list', help='List all available clusters.', required=True, is_flag=True)
+@click.option('--cluster', help='Cluster Name', required=True, cls=NotRequiredIf, not_required_if=['list'])
+@kfk.command()
+def clusters(cluster, list, describe, output, namespace):
+    """The kafka cluster(s) to be created, altered or described. """
+    if list:
+        os.system(Kubectl().get().kafkas().namespace(namespace).build())
+    elif describe:
+        if output is not None:
+            os.system(
+                Kubectl().get().kafkas(cluster).namespace(namespace).output(output).build())
+        else:
+            os.system(
+                Kubectl().describe().kafkas(cluster).namespace(namespace).build())
+    else:
+        print_missing_options_for_command("clusters")
