612cd4463b0592e678719bfd432d0de085e1175c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:22 +0000

    Add package google-flask-oauth/0.0.1.dev1/google-flask-oauth-0.0.1.dev1.tar.gz

---
+++ packages/google-flask-oauth/google-flask-oauth-0.0.1.dev1.tar.gz/google-flask-oauth-0.0.1.dev1/google-flask-oauth/oauth.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, request, jsonify
+from google_auth_oauthlib.flow import Flow
+
+oauth = Blueprint('oauth', __name__)
+
+@oauth.route('/oauth/start', methods=['GET'])
+def oauth_start():
+  req_data = request.get_json(force=True)
+
+  flow = Flow.from_client_config(
+    auth_app.config['AUTH_SA_SECRET_CONTENTS'],
+    ['https://www.googleapis.com/auth/userinfo.email']
+  )
+  flow.redirect_uri = f'https://{req_data.get("auth_root", "")}/oauth/exchange'
+  authorization_url, _ = flow.authorization_url(access_type='offline', include_granted_scopes='true')
+  return jsonify({
+    'redirect_url': authorization_url
+  }), 200
+
+@oauth.route('/oauth/exchange', method=['POST'])
+def oauth_exchange():
+  pass
