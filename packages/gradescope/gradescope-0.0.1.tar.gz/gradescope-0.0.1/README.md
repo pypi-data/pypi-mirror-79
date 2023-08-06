59b08dedae897e9b6dcfaa15e90042e92968589f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:54 +0000

    Add package gradescope/0.0.1/gradescope-0.0.1.tar.gz

---
+++ packages/gradescope/gradescope-0.0.1.tar.gz/gradescope-0.0.1/README.md
@@ -0,0 +1,19 @@
+# gradescope
+
+This is a Python utility library for Gradescope.
+
+This library is a subset of a library created by Jérémie Lumbroso, licensed under the LGPL3 license. You can find this library here: https://github.com/jlumbroso/pylifttk.
+
+## Configuration file
+
+The `config.yaml` file contains Gradescope credentials.
+
+```yaml
+gradescope:
+  username: "" # credentials for https://gradescope.com
+  password: ""
+```
+
+## Example
+
+Coming soon!
---
+++ packages/gradescope/gradescope-0.0.1.tar.gz/gradescope-0.0.1/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "gradescope"
+
+# The text of the README file
+README = open("README.md").read()
+
+# Get the version number without importing our package
+# (which would trigger some ImportError due to missing dependencies)
+
+version_contents = {}
+with open(os.path.join(PACKAGE_NAME, "version.py")) as f:
+    exec(f.read(), version_contents)
+
+# This call to setup() does all the work
+setup(
+    name=PACKAGE_NAME,
+    version=version_contents["__version__"],
+    description="Python utility toolkit for Gradescope",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mooey5775/gradescope",
+    author="Edward Li",
+    author_email="edwardli@andrew.cmu.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "bs4",
+        "confuse",
+        "pywsse",
+        "PyYAML",
+        "requests",
+        "six",
+        # "better_exceptions",
+        # "blessings",
+        # "colorama",
+        # "eliot",
+    ],
+    include_package_data=True,
+)
