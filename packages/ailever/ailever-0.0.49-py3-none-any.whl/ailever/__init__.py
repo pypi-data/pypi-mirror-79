3e478afcb62c4a6883a027411ecd2b23231f21c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:07 +0000

    Add package ailever/0.0.49/ailever-0.0.49-py3-none-any.whl

---
+++ packages/ailever/ailever-0.0.49-py3-none-any.whl/ailever/__init__.py
@@ -0,0 +1,17 @@
+from collections import OrderedDict
+
+class SecurityError(Exception):
+    pass
+
+user = input('Enter your ID : ')
+passwd = input(f"Enter [{user}]'password : ")
+
+if passwd == '123' : pass
+else: SecurityError('Your password is incorrect.')
+    
+
+from .docs import *
+
+
+
+
