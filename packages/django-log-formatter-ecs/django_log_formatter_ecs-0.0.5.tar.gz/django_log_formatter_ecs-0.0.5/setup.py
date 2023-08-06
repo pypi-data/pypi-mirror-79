14ddec22a01d2e888ed63e529eeae4c885269a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:38 +0000

    Add package django-log-formatter-ecs/0.0.5/django_log_formatter_ecs-0.0.5.tar.gz

---
+++ packages/django-log-formatter-ecs/django_log_formatter_ecs-0.0.5.tar.gz/django_log_formatter_ecs-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+import setuptools
+
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="django_log_formatter_ecs",
+    version="0.0.5",
+    packages=setuptools.find_packages(),
+    author="Ross Miller",
+    author_email="ross.miller@digita.trade.gov.uk",
+    url="https://github.com/uktrade/django-log-formatter-ecs",
+    description="ECS log formatter for Django",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    install_requires=[
+        "django-ipware~=3.0",
+        "kubi-ecs-logger~=0.1.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
