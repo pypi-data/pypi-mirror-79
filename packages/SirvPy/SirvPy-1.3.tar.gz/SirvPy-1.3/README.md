a79632cd49147d2acf7a0055af5788988fd02f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:07 +0000

    Add package SirvPy/1.3/SirvPy-1.3.tar.gz

---
+++ packages/SirvPy/SirvPy-1.3.tar.gz/SirvPy-1.3/README.md
@@ -0,0 +1,227 @@
+# SirvPy
+SirvPy is a python library for accessing the Sirv REST API
+
+## Installation:
+
+	pip install SirvPy
+
+Import all SirvPy functions you'll need to use in your app.
+
+	from SirvPy import get_access, upload_files
+	
+## Usage:
+
+SirvPy saves you the pain of dealing with conversions between different types returned by Sirv upon making API calls.
+
+### Endpoints:
+
+**1: Requesting Access tokens from Sirv**
+
+You have to create an API Client on Sirv first https://my.sirv.com/#/account/settings/api. Save the "Client ID" and "Client Secret" somewhere. You'll need this for the next step
+
+Sirv requires both the Client ID and Client Secret while requesting for an access token. In your application, save these in variables like so;
+
+	clientId = 'CLIENT ID'
+	clientSecret = 'CLIENT SECRET'
+
+Next, call the get_access() function and pass your variables as arguments.
+
+	access_token = get_access(clientId, clientSecret)
+
+The return type is a dictionary. In most cases, you'll only need the token, which you can get from the dictionary returned.
+
+	access_token = get_access(clientId, clientSecret)['token']
+
+	NOTE: Sirv REST API access tokens last for only 20 minutes. SirvPy can tell whether a token is expired by use of timestamps. The get_access() method makes an api call IF AND ONLY IF your previous access token has expired, otherwise it returns the previous access token. 
+
+**2: Uploading a file**
+
+SirvPy provides a means for both web apps and simple python apps to interact with the upload API.
+
+The upload_files() function takes 3 arguments;
+i)   Your access token
+ii)  The absolute path to your local file OR the file selected by a web app user
+iii) The path to which you want to upload your file
+
+*example use;*
+
+>a) Simple raw Python app
+
+	local_filename = '/absolute/path/to/file/myfile.jpg'
+	upload_path = '/myfolder/oldman.jpg'
+	upload_files(access_token, local_filename, upload_path)
+
+>b) Django Web App
+
+Your template file (HTML)
+
+	<form method="post" enctype="multipart/form-data">
+		{% csrf_token %}
+		<input type="file" name="file_upload">
+		<button type = "submit">Upload File</button>		
+	</form>
+
+
+views.py
+
+	def my_view(request):
+		if request.method == 'POST':
+			upload_files(access_token, request.FILES['file_upload'], upload_path)
+	
+		return render(request, 'myhtml.html')
+
+**3: Searching for files**
+
+Sirv's rest API offers a very wide range of options to search for files. For this reason, the latest version of SirvPy only supports searching of ALL files in the authenticated user's account. Future versions of SirvPy will allow more flexibility that is already being offered directly by Sirv REST API. You can look up all the other queries that you can use to search for files here; https://api.sirv.com/v2/docs?path=/v2/files/search#POST
+
+To search for files, call the 'search_files()' function and pass the your access token as argument.
+
+*example use;*
+
+	search_files(access_token)
+
+The return type is a dictionary of all files in the authenticated user's account.
+
+**4: Checking Storage stats**
+
+	storage_stats(access_token)
+
+**5: Checking Storage info**
+
+	storage_info(access_token)
+
+**6: Checking account info**
+
+	account_info(access_token)
+
+**7: Getting users**
+	
+	get_users(access_token)
+
+**8: Get API usage limits**
+
+	get_limits(access_token)
+
+**9: Get Billing info**
+
+	billing_info(access_token)
+
+**10: Get Transfer Stats**
+
+	transfer_stats(access_token)
+
+**11: Account Events**
+
+	account_events(access_token)
+
+**12: Get user info**
+
+user_id is one of the parameters and can be acquired by calling the get_users() function
+
+	get_user_info(access_token, user_id)
+
+**13: Get Spins Views**
+
+	get_spins_views(access_token)
+
+**14: Convert Video To Spin**
+
+Specify the video file you want to spin.
+
+	convert_to_spin(access_token, "/path/to/video/file")
+
+**15: Convert spin to video**
+
+	convert_to_video(access_token)
+
+**16: Fetch image from URL**
+
+	fetch_url(access_token, url/to/image, "/path/to/upload/to/filename")
+
+**17: Get folder options**
+
+This feature will work for users with enterprise accounts only
+
+	get_folder_options(access_token, "/path/to/file")
+
+**18: Set folder options**
+
+	set_folder_options(access_token, "/path/to/file")
+
+**19: Get meta description**
+
+	get_meta_description(access_token, "/path/to/file")
+
+**20: Set meta description**
+
+	get_meta_description(access_token, "/path/to/file", description)
+
+**21: Get meta title**
+
+	get_meta_title(access_token, "/path/to/file")
+
+**22: Set meta title**
+
+	get_meta_title(access_token, "/path/to/file", title)
+
+**23: Get file meta tags**
+
+	get_file_meta_tags(access_token, "/path/to/file")
+
+**24: Add file meta tags**
+
+	add_file_meta_tags(access_token, "/path/to/file", ["list", "of", "tags"])
+
+**25: Delete file meta tags**
+
+	delete_file_meta_tags(access_token, "/path/to/file", ["list", "of", "tags"])
+
+**26: Get approval flag**
+
+	get_approval_flag(access_token, "/path/to/file")
+
+**27: Set approval flag**
+	
+	set_approval_flag(access_token, "/path/to/file", approved, comment)
+
+approved is boolean; True or False
+
+**28: Delete file or empty directory**
+
+	delete_file(access_token, "/path/to/file")
+
+**29: Copy file**
+
+	copy_file(access_token, "/file/to/copy", "/path/to/copy/to")
+
+**30: Read folder Contents**
+
+	read_folder_contents(access_token, "/path/to/dir")
+
+**31: Get product meta**
+	
+	get_product_meta(access_token, "/path/to/file")
+
+**32: Set Product meta**
+
+	set_product_meta(access_token, "/path/to/file")
+
+**33: Get file info**
+
+**34: Get JWT protected url**
+	
+	get_jwt_url(access_token, "/path/to/file", expiry)
+
+expiry is number of seconds
+
+**35: Update Account Info**
+
+	update_account_info(access_token, minify)
+
+Minify is boolean; True or False
+
+**36: Remote Fetching**
+
+	Remote_fetching(access_token, enabled)
+
+Enabled is boolean; True or False
---
+++ packages/SirvPy/SirvPy-1.3.tar.gz/SirvPy-1.3/SirvPy/Sirv.py
@@ -0,0 +1,330 @@
+import requests
+import time
+import json #for parsing str into dict
+import ast #for parsing single quote "json" to dict
+
+import re
+
+base_url = "https://api.sirv.com"
+
+#This function converts the response returned by API call to dict
+def convert(response):
+	content_dict = json.loads(response.content)
+	return content_dict
+
+def get_access(clientId, clientSecret):
+
+	from .file_handling import retrieve_time, retrieve_access_token, store_time, store_access_token
+	time_elapsed = (time.time() - retrieve_time())/60#find time elapsed in minutes
+
+	if time_elapsed > 20:
+
+		print("Getting new token")
+		payload = { "clientId": clientId,"clientSecret": clientSecret }
+		headers = {"Content-Type" : "application/json"}
+		endpoint = base_url + "/v2/token"
+		sirv_api_request = convert(requests.post(endpoint, headers = headers, json = payload))#make request and convert response to dict		
+		store_access_token(sirv_api_request)#save the dictionary returned as str
+		store_time(time.time())#store current epoch time
+
+	else:
+		print("Unexpired token present; Retrieving...\n token expires in {} minutes".format(str(20 - time_elapsed)))
+		sirv_api_request = ast.literal_eval(retrieve_access_token())#retrieve the string and convert to dict
+
+	return sirv_api_request
+
+
+def upload_files(access_token, local_file, upload_path):
+	endpoint = base_url + "/v2/files/upload"
+	headers = {"Content-Type" : "image/jpeg", "authorization": "bearer {}".format(access_token)}
+	upload_path = {"filename": upload_path}#The path to which the file will be uploaded TBD
+
+	if str(local_file.__class__) == "<class 'str'>":
+		print("User passed a File Path")
+		open_file = open(local_file, 'rb')
+		sirv_api_request = requests.post(endpoint, headers = headers, data = open_file, params = upload_path)
+	elif re.match("<class 'django.core.files", str(local_file.__class__)):
+		print("User passed a django uploadfile")
+		sirv_api_request = requests.post(endpoint, headers = headers, data = local_file, params = upload_path)
+	else:
+		print("Unsupported file source")
+		return
+
+	if sirv_api_request.status_code == 200:
+		print("Successfully uploaded file")
+	else:
+		print("Error {}. File upload failed".format(sirv_api_request.status_code))
+
+	return sirv_api_request
+
+def search_files(access_token):
+	endpoint = base_url + "/v2/files/search"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"query": "basename:*"}
+	sirv_api_request = requests.post(endpoint, headers = headers, json = payload)
+
+	return sirv_api_request
+
+def storage_info(access_token):
+	endpoint = base_url + "/v2/account/storage"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	sirv_api_request = requests.get(endpoint, headers = headers)
+
+	return sirv_api_request
+
+def storage_stats(access_token):
+	endpoint = base_url + "/v2/stats/storage"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	sirv_api_request = requests.get(endpoint, headers = headers)
+
+	return sirv_api_request
+
+def account_info(access_token):
+	endpoint = base_url + "/v2/account"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	sirv_api_request = requests.get(endpoint, headers = headers)
+
+	return sirv_api_request
+
+def get_users(access_token):
+	endpoint = base_url + "/v2/account/users"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	sirv_api_request = requests.get(endpoint, headers = headers)
+
+	return sirv_api_request
+
+def get_limits(access_token):
+	endpoint = base_url + "/v2/account/limits"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	sirv_api_request = requests.get(endpoint, headers = headers)
+
+	return sirv_api_request
+
+def billing_info(access_token):
+	endpoint = base_url + "/v2/billing/plan"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	sirv_api_request = requests.get(endpoint, headers = headers)
+
+	return sirv_api_request
+
+def transfer_stats(access_token):
+	endpoint = base_url + "/v2/stats/http"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	sirv_api_request = requests.get(endpoint, headers = headers)
+
+	return sirv_api_request
+
+def account_events(access_token):
+	endpoint = base_url + "/v2/account/events/search"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	#payload = {"level" : "info", "module" : "", "level" : "", "type" : ""}
+	payload = {"level" : "info"}
+	sirv_api_request = requests.post(endpoint, headers = headers, json = payload)
+
+	return sirv_api_request
+
+def get_user_info(access_token, user_id):
+	endpoint = base_url + "/v2/user"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"userId": user_id}
+	sirv_api_request = requests.get(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def get_spins_views(access_token):
+	endpoint = base_url + "/v2/stats/spins/views"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	sirv_api_request = requests.get(endpoint, headers = headers)
+
+	return sirv_api_request
+
+def convert_to_spin(access_token, filename):
+	endpoint = base_url + "/v2/files/video2spin"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename": filename, "options": {"start" : 0, "end" : 3, "frames" : 30}}#The name and path of the file to spin
+	sirv_api_request = requests.post(endpoint, headers = headers, json = payload)
+
+	return sirv_api_request
+
+def convert_to_video(access_token, filename):
+	endpoint = base_url + "/v2/files/spin2video"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename": filename, "options": {"width" : 1920, "height" : 1080, "loops" : 10, "row": "single"}}#The name and path of the file to spin
+	sirv_api_request = requests.post(endpoint, headers = headers, json = payload)
+
+	return sirv_api_request
+
+def fetch_url(access_token, url, filename):
+	endpoint = base_url + "/v2/files/fetch"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"url" : url, "filename" : filename}#The name and path of the file to spin
+	sirv_api_request = requests.post(endpoint, headers = headers, json = payload)
+
+	return sirv_api_request
+
+def get_folder_options(access_token, filename):
+	endpoint = base_url + "/v2/files/options"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	sirv_api_request = requests.get(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def set_folder_options(access_token, filename):
+	endpoint = base_url + "/v2/files/options"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	query = {"scanSpins": True, "nameSpinsAfterFolder": True, "allowListing": True}
+	sirv_api_request = requests.post(endpoint, headers = headers, params = payload, json = query)
+
+	return sirv_api_request
+
+def get_meta_description(access_token, filename):
+	endpoint = base_url + "/v2/files/meta/description"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	sirv_api_request = requests.get(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def set_meta_description(access_token, filename, description):
+	endpoint = base_url + "/v2/files/meta/description"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"description" : description}
+	query = {"filename" : filename}
+	sirv_api_request = requests.post(endpoint, headers = headers, params = query, json = payload)
+
+	return sirv_api_request
+
+def get_meta_title(access_token, filename):
+	endpoint = base_url + "/v2/files/meta/title"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	sirv_api_request = requests.get(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def set_meta_title(access_token, filename, title):
+	endpoint = base_url + "/v2/files/meta/title"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"title" : title}
+	query = {"filename" : filename}
+	sirv_api_request = requests.post(endpoint, headers = headers, params = query, json = payload)
+
+	return sirv_api_request
+
+def get_file_meta_tags(access_token, filename):
+	endpoint = base_url + "/v2/files/meta/tags"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	sirv_api_request = requests.get(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def add_file_meta_tags(access_token, filename, tags):
+	endpoint = base_url + "/v2/files/meta/tags"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"tags" : tags}
+	query = {"filename" : filename}
+	sirv_api_request = requests.post(endpoint, headers = headers, params = query, json = payload)
+
+	return sirv_api_request
+
+def delete_file_meta_tags(access_token, filename, tags):
+	endpoint = base_url + "/v2/files/meta/tags"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"tags" : tags}
+	query = {"filename" : filename}
+	sirv_api_request = requests.delete(endpoint, headers = headers, params = query, json = payload)
+
+	return sirv_api_request
+
+def get_approval_flag(access_token, filename):
+	endpoint = base_url + "/v2/files/meta/approval"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	sirv_api_request = requests.get(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def set_approval_flag(access_token, filename, approved, comment):
+	endpoint = base_url + "/v2/files/meta/approval"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"approved" : approved, "comment": comment}
+	query = {"filename" : filename}
+	sirv_api_request = requests.post(endpoint, headers = headers, params = query, json = payload)
+
+	return sirv_api_request
+
+def delete_file(access_token, filename):
+	endpoint = base_url + "/v2/files/delete"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	sirv_api_request = requests.post(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def copy_file(access_token, copy_from, copy_to):
+	endpoint = base_url + "/v2/files/copy"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"from" : copy_from, "to" : copy_to}
+	sirv_api_request = requests.post(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def read_folder_contents(access_token, dirname):
+	endpoint = base_url + "/v2/files/readdir"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"dirname" : dirname}
+	sirv_api_request = requests.get(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def get_product_meta(access_token, filename):
+	endpoint = base_url + "/v2/files/meta/product"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	sirv_api_request = requests.get(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def set_product_meta(access_token, filename, product_id, product_name, brand):
+	endpoint = base_url + "/v2/files/meta/product"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	query = {"id" : product_id, "name" : product_name, "brand" : brand}
+	sirv_api_request = requests.post(endpoint, headers = headers, params = payload, json = query)
+
+	return sirv_api_request
+
+def get_file_info(access_token, filename):
+	endpoint = base_url + "/v2/files/stat"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename}
+	sirv_api_request = requests.get(endpoint, headers = headers, params = payload)
+
+	return sirv_api_request
+
+def get_jwt_url(access_token, filename, expiry):
+	endpoint = base_url + "/v2/files/jwt"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"filename" : filename, "expiresIn" : expiry}
+	sirv_api_request = requests.post(endpoint, headers = headers, json = payload)
+
+	return sirv_api_request
+
+def update_account_info(access_token, minify):
+	endpoint = base_url + "/v2/account"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"minify" :  {"enabled" : minify}} 
+	sirv_api_request = requests.post(endpoint, headers = headers, json = payload)
+
+	return sirv_api_request
+
+def remote_fetching(access_token, enabled):
+	endpoint = base_url + "/v2/account/fetching"
+	headers = {"Content-Type" : "application/json", "authorization": "bearer {}".format(access_token)}
+	payload = {"enabled" :  enabled} 
+	sirv_api_request = requests.post(endpoint, headers = headers, json = payload)
+
+	return sirv_api_request
