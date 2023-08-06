a487dc57164909d0f30ece535847e5e118d94cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:14 +0000

    Add package psdr/0.3.8/psdr-0.3.8.tar.gz

---
+++ packages/psdr/psdr-0.3.8.tar.gz/psdr-0.3.8/setup.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from setuptools import setup
+
+install_requires = [
+		'numpy>=1.15', 
+		'redis',
+		'cvxpy',
+		'cvxopt',
+		'tqdm',
+		'dask',
+		'distributed',
+		'sobol_seq',
+		'satyrn>=0.3.2',
+		'iterprinter',
+	]
+
+install_requires += [
+	'matplotlib',
+	'scipy>=1.1.0',
+	]
+
+test_requires = ['pytest']
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='psdr',
+	version = '0.3.8',
+	description = 'Parameter Space Dimension Reduction Toolbox',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+	author = 'Jeffrey M. Hokanson',
+	author_email = 'jeffrey@hokanson.us',
+	url = 'https://github.com/jeffrey-hokanson/PSDR',
+	packages = ['psdr', 'psdr.demos', 'psdr.domains', 'psdr.sample', 'psdr.geometry'],
+	install_requires = install_requires,
+	test_requires = test_requires,
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: GNU Affero General Public License v3',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		'Topic :: Scientific/Engineering :: Mathematics'
+		]
+	)
