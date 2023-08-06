ca2f263240e78235e6961509b0e72bfe4845a4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:41 +0000

    Add package motional-ml-evaluate/0.0.0/motional_ml_evaluate-0.0.0.tar.gz

---
+++ packages/motional-ml-evaluate/motional_ml_evaluate-0.0.0.tar.gz/motional_ml_evaluate-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="motional_ml_evaluate",
+    version="0.0.0",
+    author="Motional",
+    author_email="devops@motional.com",
+    description="motional_ml_evaluate",
+    long_description="motional_ml_evaluate",
+    long_description_content_type="text/markdown",
+    url="https://motional.com/",
+    packages=[],
+    python_requires='>=5',
+)
