6422e8c69ae54704d0b47dfd2bf9b7274be80e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:22 +0000

    Add package google-flask-oauth/0.0.1.dev1/google_flask_oauth-0.0.1.dev1-py3-none-any.whl

---
+++ packages/google-flask-oauth/google_flask_oauth-0.0.1.dev1-py3-none-any.whl/google-flask-oauth/_auth.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from .oauth import oauth
+from ._defaults import Defaults
+
+def auth(secrets_file_contents, **kwArgs):
+  auth_app = Flask('google-flask-oauth')
+  auth_app.config.from_object(Defaults)
+  auth_app.config['AUTH_SA_SECRET_CONTENTS'] = secrets_file_contents
+  for key in kwArgs:
+    auth_app.config['CUSTOM_{}'.format(key.upper())] = kwArgs[key]
+
+  auth_app.register_blueprint(oauth)
+
+  return auth_app
+
+def aws_auth(**kwArgs):
+  from ._aws_bootstrap import CLIENT_SECRET
+  return auth(CLIENT_SECRET, **kwArgs)
