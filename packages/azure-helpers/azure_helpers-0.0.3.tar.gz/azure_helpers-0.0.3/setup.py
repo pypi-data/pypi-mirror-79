6f4b099ebfaa9f2028e82a94eac54a97f82d2716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:05 +0000

    Add package azure-helpers/0.0.3/azure_helpers-0.0.3.tar.gz

---
+++ packages/azure-helpers/azure_helpers-0.0.3.tar.gz/azure_helpers-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="azure_helpers",
+    version="0.0.3",
+    author="Sanath Manavarte",
+    author_email="msanath@gmail.com",
+    description="A package which uses azure SDK for creating simple resources and managing them.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/msanath/azure-helpers.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'azure-cli-core',
+        'azure-mgmt-compute==13.0.0'
+    ]
+)
