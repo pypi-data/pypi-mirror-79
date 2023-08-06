a7aea8cfc7580e0e9f3ffa3f6c331e323c877070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:46 +0000

    Add package aim-cli/2.0.13rc1/aim-cli-2.0.13rc1.tar.gz

---
+++ packages/aim-cli/aim-cli-2.0.13rc1.tar.gz/aim-cli-2.0.13rc1/aim/sdk/select.py
@@ -0,0 +1,48 @@
+from typing import Optional
+
+from aim.engine.repo import AimRepo
+from aim.ql.grammar.statement import Statement
+from aim.ql.grammar.expression import Expression
+
+
+def select_metrics(search_statement: str, repo_path: Optional[str] = None):
+    if repo_path is not None:
+        repo = AimRepo(repo_full_path=repo_path)
+    else:
+        repo = AimRepo.get_working_repo(mode=AimRepo.READING_MODE)
+
+    if not repo:
+        return None
+
+    parser = Statement()
+    parsed_stmt = parser.parse(search_statement.strip())
+    statement_select = parsed_stmt.node['select']
+    statement_expr = parsed_stmt.node['expression']
+
+    if 'run.archived' not in search_statement:
+        default_expression = 'run.archived is not True'
+    else:
+        default_expression = None
+
+    return repo.select_metrics(statement_select,
+                               statement_expr, default_expression)
+
+
+def select_runs(expression: str, repo_path: Optional[str] = None):
+    if repo_path is not None:
+        repo = AimRepo(repo_full_path=repo_path)
+    else:
+        repo = AimRepo.get_working_repo(mode=AimRepo.READING_MODE)
+
+    if not repo:
+        return None
+
+    parser = Expression()
+    parsed_expr = parser.parse(expression.strip())
+
+    if 'run.archived' not in parsed_expr:
+        default_expression = 'run.archived is not True'
+    else:
+        default_expression = None
+
+    return repo.select_runs(parsed_expr.node, default_expression)
