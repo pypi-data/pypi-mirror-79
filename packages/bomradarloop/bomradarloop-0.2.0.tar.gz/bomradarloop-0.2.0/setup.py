1d549e24ea4644343b06fc0bccd7a18d489f353f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:37 +0000

    Add package bomradarloop/0.2.0/bomradarloop-0.2.0.tar.gz

---
+++ packages/bomradarloop/bomradarloop-0.2.0.tar.gz/bomradarloop-0.2.0/setup.py
@@ -0,0 +1,23 @@
+# pylint: disable=C0114
+
+import setuptools
+
+with open("README", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="bomradarloop",
+    version="0.2.0",
+    author="Paul Madden",
+    author_email="maddenp@colorado.edu",
+    description="Create animated GIFs from BOM radar imagery",
+    install_requires=["Pillow", "requests"],
+    long_description=long_description,
+    url="https://github.com/maddenp/bomradarloop",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
