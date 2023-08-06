e0124df8be65ed41763c3a0ca5530eebed6c51fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:24 +0000

    Add package coover-thumbor-plugins/0.3/coover_thumbor_plugins-0.3-py2.7.egg

---
+++ packages/coover-thumbor-plugins/coover_thumbor_plugins-0.3-py2.7.egg/coover_thumbor_plugins/url_signers/base64_hmac_sha256.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import base64
+import hashlib
+import hmac
+
+from six import text_type
+
+from libthumbor.url_signers import BaseUrlSigner
+from thumbor.utils import logger
+
+class UrlSigner(BaseUrlSigner):
+    """Validate urls and sign them using base64 hmac-sha1
+    """
+
+    def signature(self, url):
+        logger.warn(url)
+
+        return base64.urlsafe_b64encode(
+            hmac.new(
+                self.security_key, text_type(url).encode("utf-8"), hashlib.sha256
+            ).digest()
+        )
