a55119cded5281086a5d6b593dbd93be14907182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:20 +0000

    Add package UP-Tree/0.0.2/UP-Tree-0.0.2.tar.gz

---
+++ packages/UP-Tree/UP-Tree-0.0.2.tar.gz/UP-Tree-0.0.2/src/__init__.py
@@ -0,0 +1,10 @@
+from UPTree import UPTree
+
+
+upTree, headerTable, trans_1 = UPTree(
+    'D:\mithil ghinaiya\data_proj_1.txt', 'D:\mithil ghinaiya\profit1.txt')
+
+
+# upTree.disp()
+# print(headerTable)
+# print(trans_1)
