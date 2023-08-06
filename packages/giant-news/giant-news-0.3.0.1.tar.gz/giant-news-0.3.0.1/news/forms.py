4cdae90ad0cf562d9fbbd452361a91ece33cf51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:34 +0000

    Add package giant-news/0.3.0.1/giant-news-0.3.0.1.tar.gz

---
+++ packages/giant-news/giant-news-0.3.0.1.tar.gz/giant-news-0.3.0.1/news/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+from news.models import ArticleTag
+
+
+class NewsSearchForm(forms.Form):
+    """
+    Form to provide search filtering for events.
+    """
+
+    tags = forms.ModelMultipleChoiceField(
+        queryset=ArticleTag.objects.all(),
+        required=False,
+        widget=forms.CheckboxSelectMultiple(),
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.queryset = kwargs.pop("queryset")
+        super().__init__(*args, **kwargs)
+
+    def process(self):
+        """
+        Additionally apply tags filter to the results.
+        """
+        tags = self.cleaned_data.get("tags")
+        if tags:
+            self.queryset = self.queryset.filter(tags__in=tags)
+        return self.queryset
