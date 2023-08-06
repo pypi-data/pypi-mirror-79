92977b98f1b867eaf2d4d256f894e2435d564f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:28 +0000

    Add package getcohorts/0.1.4/getcohorts-0.1.4.tar.gz

---
+++ packages/getcohorts/getcohorts-0.1.4.tar.gz/getcohorts-0.1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="getcohorts",  # Replace with your own username
+    version="0.1.4",
+    author="Tom Waterman",
+    author_email="tjwaterman99@gmail.com",
+    description="Utilities for randomizing A/B tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    url="http://docs.getcohorts.com/"
+)
