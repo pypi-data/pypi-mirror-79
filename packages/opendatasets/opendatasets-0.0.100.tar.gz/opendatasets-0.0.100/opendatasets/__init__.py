f6c7e7b231a7b0f469fc0d9cef5434c17a994c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:26 +0000

    Add package opendatasets/0.0.100/opendatasets-0.0.100.tar.gz

---
+++ packages/opendatasets/opendatasets-0.0.100.tar.gz/opendatasets-0.0.100/opendatasets/__init__.py
@@ -0,0 +1,14 @@
+import importlib
+import os
+from opendatasets._version import __version__
+
+def download(dataset_id, data_dir='.', dry_run=False, **kwargs):
+    data_dir = os.path.join(data_dir, dataset_id)
+    os.makedirs(data_dir, exist_ok=True)
+    dataset = importlib.import_module('opendatasets.datasets.' + dataset_id)
+    if dry_run:
+        print('This is a dry run. URLs will be displayed but the files will not be downloaded.')
+    return dataset.download(dataset_id=dataset_id, data_dir=data_dir, dry_run=dry_run, **kwargs)
+
+def version():
+    return __version__
