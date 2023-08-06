a6a46b11d29c55741c4c2a7b18ed072d85a1f637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:18 +0000

    Add package purplship-python/2020.8.1/purplship_python-2020.8.1-py3-none-any.whl

---
+++ packages/purplship-python/purplship_python-2020.8.1-py3-none-any.whl/purplship/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from purplship.api.addresses import AddressesApi
+from purplship.api.carriers import CarriersApi
+from purplship.api.customs import CustomsApi
+from purplship.api.parcels import ParcelsApi
+from purplship.api.rates import RatesApi
+from purplship.api.shipments import ShipmentsApi
+from purplship.api.shipping import ShippingApi
+from purplship.api.tracking import TrackingApi
+from purplship.api.utils import UtilsApi
