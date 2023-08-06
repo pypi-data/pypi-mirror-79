57225f3bc6f20a4abe778a96e6d569c11b0a1c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:50 +0000

    Add package cookgame/0.0.6/cookgame-0.0.6.tar.gz

---
+++ packages/cookgame/cookgame-0.0.6.tar.gz/cookgame-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cookgame",
+    version="0.0.6",
+    author="piglite",
+    author_email="piglite@vip.sina.com",
+    description="kcode tutoring package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/piglite",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3',
+    packages=['code'],
+    package_data={
+        'code':
+        ['sound/*.wav', 'sound/*.mp3', 'textures/*.png', 'textures/*.jpg'],
+    },
+    install_requires=['arcade'],
+    include_package_data=True)
