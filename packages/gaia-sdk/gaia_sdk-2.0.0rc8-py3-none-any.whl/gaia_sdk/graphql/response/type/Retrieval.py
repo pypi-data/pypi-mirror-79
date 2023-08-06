35153b397bfa05b0e8437fda0458f9e77479ec22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:10 +0000

    Add package gaia-sdk/2.0.0rc8/gaia_sdk-2.0.0rc8-py3-none-any.whl

---
+++ packages/gaia-sdk/gaia_sdk-2.0.0rc8-py3-none-any.whl/gaia_sdk/graphql/response/type/Retrieval.py
@@ -0,0 +1,41 @@
+from gaia_sdk.graphql.response.type.Experience import Experience
+from gaia_sdk.graphql.response.type.Knowledge import Knowledge
+
+Uuid = str
+String = str
+ISO8601 = str
+Struct = dict
+Float = float
+Int = int
+Boolean = bool
+
+
+class Retrieval:
+    dictionary: dict
+
+    def __init__(self, dictionary: dict):
+        self.dictionary = dictionary
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.dictionary == other.dictionary
+        return False
+
+    def __repr__(self):
+        return {'dictionary': self.dictionary}
+
+    """
+    Container element which collects all information static data
+    """
+
+    @property
+    def knowledge(self) -> Knowledge:
+        return Knowledge(self.dictionary.get("knowledge"))
+
+    """
+    Container element which collects all information about runtime data
+    """
+
+    @property
+    def experience(self) -> Experience:
+        return Experience(self.dictionary.get("experience"))
