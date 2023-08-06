e66e9d413e2b815eb6033ebb24db2193b4a57979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:11 +0000

    Add package track-linearization/2.0.0/track_linearization-2.0.0.tar.gz

---
+++ packages/track-linearization/track_linearization-2.0.0.tar.gz/track_linearization-2.0.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = ['numpy', 'scipy', 'matplotlib',
+                    'pandas', 'dask', 'networkx']
+TESTS_REQUIRE = ['pytest >= 2.7.1']
+
+setup(
+    name='track_linearization',
+    version='2.0.0',
+    license='MIT',
+    description=(''),
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    python_requires='>=3.6',
+)
