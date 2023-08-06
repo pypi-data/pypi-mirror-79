3c5317b9faad674a1ef17789226db13e9588d018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:49 +0000

    Add package codingame/0.2.1/codingame-0.2.1.tar.gz

---
+++ packages/codingame/codingame-0.2.1.tar.gz/codingame-0.2.1/codingame/client.py
@@ -0,0 +1,80 @@
+import requests
+import re
+
+from .codingamer import CodinGamer
+from .clash_of_code import ClashOfCode
+from .endpoints import Endpoints
+from .exceptions import CodinGamerNotFound, ClashOfCodeNotFound
+from .utils import validate_args
+
+
+class Client:
+    """CodinGame API client."""
+
+    _CODINGAMER_HANDLE_REGEX = re.compile(r"[0-9a-f]{32}[0-9]{7}")
+    _CLASH_OF_CODE_HANDLE_REGEX = re.compile(r"[0-9]{7}[0-9a-f]{32}")
+
+    def __init__(self):
+        self._session = requests.Session()
+
+    @validate_args
+    def get_codingamer(self, codingamer_handle: str) -> CodinGamer:
+        """Get a CodinGamer from his public handle.
+
+        Parameters
+        -----------
+            codingamer_handle: :class:`str`
+                The CodinGamer's public handle. 39 character long hexadecimal string (regex: ``[0-9a-f]{32}[0-9]{7}``).
+
+        Raises
+        ------
+            :exc:`ValueError`
+                The CodinGamer handle isn't in the good format.
+
+            :exc:`.CodinGamerNotFound`
+                The CodinGamer with the given public handle isn't found.
+
+        Returns
+        --------
+            :class:`CodinGamer`
+                The CodinGamer.
+        """
+
+        if not self._CODINGAMER_HANDLE_REGEX.match(codingamer_handle):
+            raise ValueError(f"CodinGamer handle {codingamer_handle!r} isn't in the good format (regex: [0-9a-f]{{32}}[0-9]{{7}}).")
+
+        r = self._session.post(Endpoints.CodinGamer, json=[codingamer_handle])
+        if r.json() is None:
+            raise CodinGamerNotFound(f"No CodinGamer with handle {codingamer_handle!r}")
+        return CodinGamer(**r.json()["codingamer"])
+
+    @validate_args
+    def get_clash_of_code(self, clash_of_code_handle: str) -> ClashOfCode:
+        """Get a Clash of Code from its public handle.
+
+        Parameters
+        -----------
+            clash_of_code_handle: :class:`str`
+                The Clash of Code's public handle. 39 character long hexadecimal string (regex: ``[0-9]{7}[0-9a-f]{32}``).
+
+        Raises
+        ------
+            :exc:`ValueError`
+                The Clash of Code handle isn't in the good format.
+
+            :exc:`.ClashOfCodeNotFound`
+                The Clash of Code with the given public handle isn't found.
+
+        Returns
+        --------
+            :class:`ClashOfCode`
+                The ClashOfCode.
+        """
+
+        if not self._CLASH_OF_CODE_HANDLE_REGEX.match(clash_of_code_handle):
+            raise ValueError(f"CodinGamer handle {clash_of_code_handle!r} isn't in the good format (regex: [0-9]{{7}}[0-9a-f]{{32}}).")
+
+        r = self._session.post(Endpoints.ClashOfCode, json=[clash_of_code_handle])
+        if r.json() is None:
+            raise ClashOfCodeNotFound(f"No CodinGamer with handle {clash_of_code_handle!r}")
+        return ClashOfCode(session=self._session, **r.json())
---
+++ packages/codingame/codingame-0.2.1.tar.gz/codingame-0.2.1/codingame/codingamer.py
@@ -0,0 +1,87 @@
+class CodinGamer:
+    """Represents a CodinGamer.
+
+    Do not create this class yourself. Only get it through :meth:`Client.get_codingamer()`.
+
+    Attributes
+    -----------
+        public_handle: :class:`str`
+            Public handle of the CodinGamer (hexadecimal str).
+
+        id: :class:`int`
+            ID of the CodinGamer. Last 7 digits of the :attr:`public_handle` reversed.
+
+        rank: :class:`int`
+            Worldwide rank of the CodinGamer.
+
+        level: :class:`int`
+            Level of the CodinGamer.
+
+        country_id: :class:`str`
+            Country ID of the CodinGamer.
+
+        category: Optional[:class:`str`]
+            Category of the CodinGamer. Can be ``STUDENT`` or ``PROFESSIONAL``.
+
+            .. note::
+                You can use :attr:`student` and :attr:`professional` to get a :class:`bool` that describes the CodinGamer's category.
+
+        student: :class:`bool`
+            If the CodinGamer is a student.
+
+        professional: :class:`bool`
+            If the CodinGamer is a professional.
+
+        pseudo: Optional[:class:`str`]
+            Pseudo of the CodinGamer, if set else `None`.
+
+        tagline: Optional[:class:`str`]
+            Tagline of the CodinGamer, if set else `None`.
+
+        biography: Optional[:class:`str`]
+            Biography of the CodinGamer, if set else `None`.
+
+        company: Optional[:class:`str`]
+            Company of the CodinGamer, if set else `None`.
+
+        school: Optional[:class:`str`]
+            School of the CodinGamer, if set else `None`.
+
+        avatar: Optional[:class:`int`]
+            Avatar ID of the CodinGamer, if set else `None`. You can get the avatar url with :attr:`avatar_url`.
+
+        cover: Optional[:class:`int`]
+            Cover ID of the CodinGamer, if set else `None`. You can get the cover url with :attr:`cover_url`.
+
+        avatar_url: Optional[:class:`str`]
+            Avatar URL of the CodinGamer, if set else `None`.
+
+        cover_url: Optional[:class:`str`]
+            Cover URL of the CodinGamer, if set else `None`.
+    """
+
+    def __init__(self, **data):
+        self.public_handle: str = data["publicHandle"]
+        self.id: int = data["userId"]
+        self.rank: int = data["rank"]
+        self.level: int = data["level"]
+        self.country_id: str = data["countryId"]
+
+        self.category: str or None = data["category"] if data["category"] != "UNKNOWN" else None
+        self.student: bool = self.category == "STUDENT"
+        self.professional: bool = self.category == "PROFESSIONAL"
+
+        self.pseudo: str or None = data.get("pseudo", None) or None
+        self.tagline: str or None = data.get("tagline", None) or None
+        self.biography: str or None = data.get("biography", None) or None
+        self.company: str or None = data.get("company", None) or None
+        self.school: str or None = data["formValues"].get("school", None) or None
+
+        self.avatar: int or None = data.get("avatar", None)
+        self.cover: int or None = data.get("cover", None)
+
+        self.avatar_url: str or None = f"https://static.codingame.com/servlet/fileservlet?id={self.avatar}" if self.avatar else None
+        self.cover_url: str or None = f"https://static.codingame.com/servlet/fileservlet?id={self.cover}" if self.cover else None
+
+    def __repr__(self):
+        return "<CodinGamer public_handle={0.public_handle!r} id={0.id!r}>".format(self)
