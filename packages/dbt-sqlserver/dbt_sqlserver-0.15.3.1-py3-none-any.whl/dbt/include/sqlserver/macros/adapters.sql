735315b91fc09059427f96c84c151ba20527a2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:39 +0000

    Add package dbt-sqlserver/0.15.3.1/dbt_sqlserver-0.15.3.1-py3-none-any.whl

---
+++ packages/dbt-sqlserver/dbt_sqlserver-0.15.3.1-py3-none-any.whl/dbt/include/sqlserver/macros/adapters.sql
@@ -0,0 +1,167 @@
+{% macro sqlserver__list_relations_without_caching(information_schema, schema) %}
+  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+    select
+      table_catalog as [database],
+      table_name as [name],
+      table_schema as [schema],
+      case when table_type = 'BASE TABLE' then 'table'
+           when table_type = 'VIEW' then 'view'
+           else table_type
+      end as table_type
+    from {{ information_schema }}.tables
+    where table_schema like '{{ schema }}'
+      and table_catalog like '{{ information_schema.database }}'
+  {% endcall %}
+  {{ return(load_result('list_relations_without_caching').table) }}
+{% endmacro %}
+
+{% macro sqlserver__list_schemas(database) %}
+  {% call statement('list_schemas', fetch_result=True, auto_begin=False) -%}
+    select  name as [schema]
+    from sys.schemas
+  {% endcall %}
+  {{ return(load_result('list_schemas').table) }}
+{% endmacro %}
+
+{% macro sqlserver__create_schema(database_name, schema_name) -%}
+  {% call statement('create_schema') -%}
+    USE {{ database_name }}
+    IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = {{ schema_name | replace('"', "'") }})
+    BEGIN
+    EXEC('CREATE SCHEMA {{ schema_name | replace('"', "") }}')
+    END
+  {% endcall %}
+{% endmacro %}
+
+{% macro sqlserver__drop_schema(database_name, schema_name) -%}
+  {% call statement('drop_schema') -%}
+    drop schema if exists {{database_name}}.{{schema_name}}
+  {% endcall %}
+{% endmacro %}
+
+{% macro sqlserver__drop_relation(relation) -%}
+  {% call statement('drop_relation', auto_begin=False) -%}
+    drop {{ relation.type }} if exists {{ relation.schema }}.{{ relation.identifier }}
+  {%- endcall %}
+{% endmacro %}
+
+{% macro sqlserver__drop_relation_script(relation) -%}
+    drop {{ relation.type }} if exists {{ relation.schema }}.{{ relation.identifier }}
+{% endmacro %}
+
+{% macro sqlserver__check_schema_exists(database, schema) -%}
+  {% call statement('check_schema_exists', fetch_result=True, auto_begin=False) -%}
+    --USE {{ database_name }}
+    SELECT count(*) as schema_exist FROM sys.schemas WHERE name = '{{ schema }}'
+  {%- endcall %}
+  {{ return(load_result('check_schema_exists').table) }}
+{% endmacro %}
+
+{% macro sqlserver__create_view_as(relation, sql) -%}
+  create view {{ relation.schema }}.{{ relation.identifier }} as
+    {{ sql }}
+{% endmacro %}
+
+
+{% macro sqlserver__rename_relation(from_relation, to_relation) -%}
+  {% call statement('rename_relation') -%}
+    EXEC sp_rename '{{ from_relation.schema }}.{{ from_relation.identifier }}', '{{ to_relation.identifier }}'
+    IF EXISTS(
+    SELECT *
+    FROM sys.indexes
+    WHERE name='{{ from_relation.schema }}_{{ from_relation.identifier }}_cci' and object_id = OBJECT_ID('{{ from_relation.schema }}.{{ to_relation.identifier }}'))
+    EXEC sp_rename N'{{ from_relation.schema }}.{{ to_relation.identifier }}.{{ from_relation.schema }}_{{ from_relation.identifier }}_cci', N'{{ from_relation.schema }}_{{ to_relation.identifier }}_cci', N'INDEX'
+  {%- endcall %}
+{% endmacro %}
+
+{% macro sqlserver__create_clustered_columnstore_index(relation) -%}
+  {%- set cci_name = relation.schema ~ '_' ~ relation.identifier ~ '_cci' -%}
+  {%- set relation_name = relation.schema ~ '_' ~ relation.identifier -%}
+  {%- set full_relation = relation.schema ~ '.' ~ relation.identifier -%}
+  DROP INDEX IF EXISTS {{relation_name}}.{{cci_name}}
+  CREATE CLUSTERED COLUMNSTORE INDEX {{cci_name}}
+    ON {{full_relation}}
+{% endmacro %}
+
+{% macro sqlserver__create_table_as(temporary, relation, sql) -%}
+   {%- set as_columnstore = config.get('as_columnstore', default=true) -%}
+   {% set tmp_relation = relation.incorporate(
+   path={"identifier": relation.identifier.replace("#", "") ~ '_temp_view'},
+   type='view')-%}
+   {%- set temp_view_sql = sql.replace("'", "''") -%}
+
+   {{ sqlserver__drop_relation_script(tmp_relation) }}
+
+   {{ sqlserver__drop_relation_script(relation) }}
+
+   EXEC('create view {{ tmp_relation.schema }}.{{ tmp_relation.identifier }} as
+    {{ temp_view_sql }}
+    ');
+
+   SELECT * INTO {{ relation.schema }}.{{ relation.identifier }} FROM
+    {{ tmp_relation.schema }}.{{ tmp_relation.identifier }}
+
+   {{ sqlserver__drop_relation_script(tmp_relation) }}
+    
+   {% if not temporary and as_columnstore -%}
+   {{ sqlserver__create_clustered_columnstore_index(relation) }}
+   {% endif %}
+
+{% endmacro %}_
+
+{% macro sqlserver__insert_into_from(to_relation, from_relation) -%}
+  {%- set full_to_relation = to_relation.schema ~ '.' ~ to_relation.identifier -%}
+  {%- set full_from_relation = from_relation.schema ~ '.' ~ from_relation.identifier -%}
+
+  SELECT * INTO {{full_to_relation}} FROM {{full_from_relation}}
+
+{% endmacro %}
+
+{% macro sqlserver__current_timestamp() -%}
+  getdate()
+{%- endmacro %}
+
+{% macro sqlserver__get_columns_in_relation(relation) -%}
+  {% call statement('get_columns_in_relation', fetch_result=True) %}
+      SELECT
+          column_name,
+          data_type,
+          character_maximum_length,
+          numeric_precision,
+          numeric_scale
+      FROM
+          (select
+              ordinal_position,
+              column_name,
+              data_type,
+              character_maximum_length,
+              numeric_precision,
+              numeric_scale
+          from INFORMATION_SCHEMA.COLUMNS
+          where table_name = '{{ relation.identifier }}'
+            and table_schema = '{{ relation.schema }}'
+          UNION ALL
+          select
+              ordinal_position,
+              column_name,
+              data_type,
+              character_maximum_length,
+              numeric_precision,
+              numeric_scale
+          from tempdb.INFORMATION_SCHEMA.COLUMNS
+          where table_name like '{{ relation.identifier }}%') cols
+      order by ordinal_position
+
+
+  {% endcall %}
+  {% set table = load_result('get_columns_in_relation').table %}
+  {{ return(sql_convert_columns_in_relation(table)) }}
+{% endmacro %}
+
+{% macro sqlserver__make_temp_relation(base_relation, suffix) %}
+    {% set tmp_identifier = '#' ~  base_relation.identifier ~ suffix %}
+    {% set tmp_relation = base_relation.incorporate(
+                                path={"identifier": tmp_identifier}) -%}
+
+    {% do return(tmp_relation) %}
+{% endmacro %}
