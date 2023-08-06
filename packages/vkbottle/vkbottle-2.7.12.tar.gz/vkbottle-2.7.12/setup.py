3d1e916e543a8ed94355d21beea02889505276b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:00 +0000

    Add package vkbottle/2.7.12/vkbottle-2.7.12.tar.gz

---
+++ packages/vkbottle/vkbottle-2.7.12.tar.gz/vkbottle-2.7.12/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+try:
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+except UnicodeDecodeError:
+    with open("README.md", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+with open("vkbottle/const.py") as f:
+    exec(f.read())
+
+setuptools.setup(
+    name="vkbottle",
+    version=locals()["__version__"],
+    author=locals()["__author__"],
+    description="Homogenic! Customizable VK API framework implementing comfort and speed",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/timoniq/vkbottle",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["aiohttp", "pydantic", "contextvars", "vbml==0.5.93", "watchgod", "loguru"],
+)
