6da78fc6d33b49b7ef5a72335febcdfc460fd71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:23 +0000

    Add package swh.objstorage/0.2.0/swh.objstorage-0.2.0-py3-none-any.whl

---
+++ packages/swh.objstorage/swh.objstorage-0.2.0-py3-none-any.whl/swh/objstorage/multiplexer/__init__.py
@@ -0,0 +1,5 @@
+from swh.objstorage.multiplexer.multiplexer_objstorage import MultiplexerObjStorage
+from swh.objstorage.multiplexer.striping_objstorage import StripingObjStorage
+
+
+__all__ = ["MultiplexerObjStorage", "StripingObjStorage"]
