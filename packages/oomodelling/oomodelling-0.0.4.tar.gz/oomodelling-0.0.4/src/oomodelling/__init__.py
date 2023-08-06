d63288a2cca2e66acbb539a69183a41d70ca1022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:01 +0000

    Add package oomodelling/0.0.4/oomodelling-0.0.4.tar.gz

---
+++ packages/oomodelling/oomodelling-0.0.4.tar.gz/oomodelling-0.0.4/src/oomodelling/__init__.py
@@ -0,0 +1,8 @@
+# if somebody does "from somepackage import *", this is what they will
+# be able to access:
+__all__ = [
+    'Model',
+    'ModelSolver',
+    'TrackingSimulator',
+    'examples',
+]
