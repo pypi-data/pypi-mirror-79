51d0ee58ee60d65d46afce8d2c90b47b4fec31ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:14 +0000

    Add package course-packt/1.0.0/course-packt-1.0.0.tar.gz

---
+++ packages/course-packt/course-packt-1.0.0.tar.gz/course-packt-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+    name="course-packt",
+    version= "1.0.0",
+    author="Teri Eyenike",
+    author_email="teyenike1@gmail.com",
+    description="useful to display reels of photos in a stream.",
+    long_description="create and be intuitive about the photos you take and share it with the world, non-stop.",
+    py_modules=["teri"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
