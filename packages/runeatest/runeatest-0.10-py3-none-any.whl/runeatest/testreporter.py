96dbef6f5b49ae18a07c26a566f4a9a2a22c84ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:26 +0000

    Add package runeatest/0.10/runeatest-0.10-py3-none-any.whl

---
+++ packages/runeatest/runeatest-0.10-py3-none-any.whl/runeatest/testreporter.py
@@ -0,0 +1,10 @@
+data = []
+
+def addtestcase(name,issuccess):
+    return {'test':name,'issuccess':str(issuccess), 'result':(getresult(issuccess))}
+
+def getresult (issuccess):
+  if issuccess:
+    return str('success')
+  else:
+    return str('failure')
