ff27aebb911574815a21083826b2597de5da0b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:32 +0000

    Add package pconvert-rust/0.1.3/pconvert-rust-0.1.3.tar.gz

---
+++ packages/pconvert-rust/pconvert-rust-0.1.3.tar.gz/pconvert-rust-0.1.3/src/main.rs
@@ -0,0 +1,27 @@
+use pconvert_rust::errors::PConvertError;
+use std::env;
+
+fn main() -> Result<(), PConvertError> {
+    let mut args = env::args();
+
+    // skips program name, facilitating the parsing
+    // of the extra argument from command line
+    args.next();
+
+    match args.next() {
+        Some(action) => match &action[..] {
+            "convert" => pconvert_rust::pconvert(&mut args)?,
+            "compose" => pconvert_rust::pcompose(&mut args)?,
+            "benchmark" => pconvert_rust::pbenchmark(&mut args)?,
+            "version" => pconvert_rust::pversion()?,
+            _ => print_usage(),
+        },
+        None => print_usage(),
+    };
+
+    Ok(())
+}
+
+fn print_usage() {
+    println!("Usage: pconvert-rust <command> [args...]\nwhere command can be one of the following: compose, convert, benchmark, version");
+}
