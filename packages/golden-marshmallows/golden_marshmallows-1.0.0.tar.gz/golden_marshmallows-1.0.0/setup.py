f68666657f33c91c6b6299af83dcf0eb44a32ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:42 +0000

    Add package golden-marshmallows/1.0.0/golden_marshmallows-1.0.0.tar.gz

---
+++ packages/golden-marshmallows/golden_marshmallows-1.0.0.tar.gz/golden_marshmallows-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='golden_marshmallows',
+    version='1.0.0',
+    author='Guillaume Chorn',
+    author_email='guillaume.chorn@gmail.com',
+    packages=['golden_marshmallows'],
+    url='https://github.com/gchorn/golden-marshmallows',
+    download_url='https://github.com/gchorn/golden-marshmallows/archive/v1.0.0.tar.gz',
+    description='Marshmallow Schema subclass that auto-defines fields based on'
+                ' SQLAlchemy classes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['marshmallow', 'SQLAlchemy']
+)
