0d2e3319c45375eb49ec31f62124ab753e6f47f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:35 +0000

    Add package toga-dummy/0.3.0.dev23/toga_dummy-0.3.0.dev23-py3-none-any.whl

---
+++ packages/toga-dummy/toga_dummy-0.3.0.dev23-py3-none-any.whl/toga_dummy/widgets/tree.py
@@ -0,0 +1,27 @@
+from .base import Widget
+
+
+class Tree(Widget):
+    def create(self):
+        self._action('create Tree')
+
+    def change_source(self, source):
+        self._action('change source', source=source)
+
+    def insert(self, parent, index, item):
+        self._action('insert node', parent=parent, index=index, item=item)
+
+    def change(self, item):
+        self._action('change node', item=item)
+
+    def remove(self, item):
+        self._action('remove node', item=item)
+
+    def clear(self):
+        self._action('clear')
+
+    def set_on_select(self, handler):
+        self._set_value('on_select', handler)
+
+    def set_on_double_click(self, handler):
+        self._set_value('on_double_click', handler)
