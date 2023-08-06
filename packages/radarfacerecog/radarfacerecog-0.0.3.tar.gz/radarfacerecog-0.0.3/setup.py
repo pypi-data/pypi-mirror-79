b3c0386de28174bcbe66b76b44b08792e38274d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:53 +0000

    Add package radarfacerecog/0.0.3/radarfacerecog-0.0.3.tar.gz

---
+++ packages/radarfacerecog/radarfacerecog-0.0.3.tar.gz/radarfacerecog-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Extension
+
+setup(
+    name='radarfacerecog',
+    version='0.0.3',
+    description="Radar Face recogniton, is library to recognize Brazilian politians face's",
+    author='Heitor Sampaio',
+    author_email='horlando.heitor@gmail.com',
+    license='MIT',
+    packages=['radarfacerecog'],
+    include_package_data=True,
+    package_dir={'radarfacerecog': 'radarfacerecog'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=['pandas','beautifulsoup4', 'lxml', 'face_recognition', 'numpy', 'opencv-python'],
+    python_requires='>=3',
+)
