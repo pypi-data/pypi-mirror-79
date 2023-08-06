0eee715518b4b0a0a96732d2001501b878eef9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:46 +0000

    Add package opentrons/3.21.0b0/opentrons-3.21.0b0-py2.py3-none-any.whl

---
+++ packages/opentrons/opentrons-3.21.0b0-py2.py3-none-any.whl/opentrons/hardware_control/util.py
@@ -0,0 +1,50 @@
+""" Utility functions and classes for the hardware controller"""
+import asyncio
+import logging
+from enum import Enum
+from typing import Dict, Any, Optional, List, Tuple
+
+from .types import CriticalPoint
+from opentrons.types import Point
+
+mod_log = logging.getLogger(__name__)
+
+
+def _handle_loop_exception(loop: asyncio.AbstractEventLoop,
+                           context: Dict[str, Any]):
+    mod_log.error(f"Caught exception: {context}")
+
+
+def use_or_initialize_loop(loop: Optional[asyncio.AbstractEventLoop]
+                           ) -> asyncio.AbstractEventLoop:
+    checked_loop = loop or asyncio.get_event_loop()
+    checked_loop.set_exception_handler(_handle_loop_exception)
+    return checked_loop
+
+
+def plan_arc(
+        origin_point: Point,
+        dest_point: Point,
+        z_height: float,
+        origin_cp: CriticalPoint = None,
+        dest_cp: CriticalPoint = None,
+        extra_waypoints: List[Tuple[float, float]] = None)\
+        -> List[Tuple[Point, Optional[CriticalPoint]]]:
+
+    assert z_height >= max(origin_point.z, dest_point.z)
+    checked_wp = extra_waypoints or []
+    return [(origin_point._replace(z=z_height), origin_cp)]\
+        + [(Point(x=wp[0], y=wp[1], z=z_height), dest_cp)
+           for wp in checked_wp]\
+        + [(dest_point._replace(z=z_height), dest_cp),
+           (dest_point, dest_cp)]
+
+
+class DeckTransformState(Enum):
+    OK = "OK"
+    IDENTITY = "IDENTITY"
+    BAD_CALIBRATION = "BAD_CALIBRATION"
+    SINGULARITY = "SINGULARITY"
+
+    def __str__(self):
+        return self.name
