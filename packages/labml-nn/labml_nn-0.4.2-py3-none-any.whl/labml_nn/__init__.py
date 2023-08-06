40861146f4328b30550eea6ca4416a1bb56f2c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:24 +0000

    Add package labml-nn/0.4.2/labml_nn-0.4.2-py3-none-any.whl

---
+++ packages/labml-nn/labml_nn-0.4.2-py3-none-any.whl/labml_nn/__init__.py
@@ -0,0 +1,11 @@
+"""
+# LabML Models
+
+* [Transformers](transformers/index.html)
+* [Recurrent Highway Networks](recurrent_highway_networks/index.html)
+* [LSTM](lstm/index.html)
+* [Capsule Networks](capsule_networks/index.html)
+
+If you have any suggestions for other new implementations,
+please create a [Github Issue](https://github.com/lab-ml/labml_nn/issues).
+"""
