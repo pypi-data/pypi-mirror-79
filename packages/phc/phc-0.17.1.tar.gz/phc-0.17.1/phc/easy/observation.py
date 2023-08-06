9be6b7c10bfac500e4f6b936f762e12779234e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:24 +0000

    Add package phc/0.17.1/phc-0.17.1.tar.gz

---
+++ packages/phc/phc-0.17.1.tar.gz/phc-0.17.1/phc/easy/observation.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+from phc.easy.frame import Frame
+from phc.easy.patient_item import PatientItem
+
+
+class Observation(PatientItem):
+    @staticmethod
+    def table_name():
+        return "observation"
+
+    @staticmethod
+    def code_keys():
+        return [
+            "meta.tag",
+            "code.coding",
+            "component.code.coding",
+            "valueCodeableConcept.coding",
+            "category.coding",
+        ]
+
+    @staticmethod
+    def transform_results(data_frame: pd.DataFrame, **expand_args):
+        args = {
+            **expand_args,
+            "code_columns": [*expand_args.get("code_columns", []), "component"],
+            "custom_columns": [
+                *expand_args.get("custom_columns", []),
+                Frame.codeable_like_column_expander("subject"),
+                Frame.codeable_like_column_expander("related"),
+                Frame.codeable_like_column_expander("performer"),
+            ],
+        }
+
+        return Frame.expand(data_frame, **args)
