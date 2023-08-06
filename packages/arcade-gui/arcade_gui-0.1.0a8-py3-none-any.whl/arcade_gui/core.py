1b5617f42d6d2e2dac2455190185b17f09881cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:16 +0000

    Add package arcade-gui/0.1.0a8/arcade_gui-0.1.0a8-py3-none-any.whl

---
+++ packages/arcade-gui/arcade_gui-0.1.0a8-py3-none-any.whl/arcade_gui/core.py
@@ -0,0 +1,187 @@
+import warnings
+from typing import List, Dict, Optional
+
+import arcade
+
+MOUSE_PRESS = 'MOUSE_PRESS'
+MOUSE_RELEASE = 'MOUSE_RELEASE'
+MOUSE_SCROLL = 'MOUSE_SCROLL'
+KEY_PRESS = 'KEY_PRESS'
+KEY_RELEASE = 'KEY_RELEASE'
+
+TEXT_INPUT = 'TEXT_INPUT'
+TEXT_MOTION = 'TEXT_MOTION'
+TEXT_MOTION_SELECTION = 'TEXT_MOTION_SELECTION'
+
+warnings.warn(
+    "You are using UI Components from arcade-gui, which are now included in arcade itself.",
+    DeprecationWarning
+)
+
+
+class UIEvent:
+    def __init__(self, type: str, **kwargs):
+        self.type = type
+        self.__dict__.update(**kwargs)
+
+        self._repr_keys = tuple(kwargs.keys())
+
+    def __str__(self):
+        return ' '.join([f'{self.type} ', *[f'{key}={getattr(self, key)}' for key in self._repr_keys]])
+
+
+class UIElement:
+    view: 'UIView'
+
+    def __init__(self, id=None, **kwargs):
+        self.id = id
+
+    def on_event(self, event: UIEvent):
+        pass
+
+    def on_draw(self):
+        pass
+
+    def on_focus(self):
+        """
+        Callback if the element gets focused
+        """
+        pass
+
+    def on_unfocus(self):
+        """
+        Callback if the element gets unfocused aka is not focused any more
+        """
+        pass
+
+    def on_update(self, dt: float):
+        pass
+
+    def hover_point(self, hover_x: float, hover_y: float) -> bool:
+        """
+        Test if a given point counts as 'hovering' this UI element. Normally that is a
+        straightforward matter of seeing if a point is inside the rectangle. Occasionally it
+        will also check if we are in a wider zone around a UI element once it is already active,
+        this makes it easier to move scroll bars and the like.
+
+        :param hover_x: The x (horizontal) position of the point.
+        :param hover_y: The y (vertical) position of the point.
+
+        :return: Returns True if we are hovering this element.
+
+        """
+        return False
+
+
+class UIException(Exception):
+    pass
+
+
+class UIView(arcade.View):
+    def __init__(self, *args, **kwargs):
+        super().__init__()  # Here happens a lot of stuff we don't need
+        self.focused_element = None
+
+        self._ui_elements: List[UIElement] = []
+        self._id_cache: Dict[str, UIElement] = {}
+
+    def purge_ui_elements(self):
+        self._ui_elements = []
+        self._id_cache = {}
+
+    def add_ui_element(self, ui_element: UIElement):
+        ui_element.view = self
+        self._ui_elements.append(ui_element)
+
+        if not hasattr(ui_element, 'id'):
+            raise UIException('UIElement seems not to be properly setup, please check if you'
+                              ' overwrite the constructor and forgot "super().__init__(**kwargs)"')
+
+        if ui_element.id is not None:
+            if ui_element.id in self._id_cache:
+                raise UIException(f'duplicate id "{ui_element.id}"')
+
+            self._id_cache[ui_element.id] = ui_element
+
+    def update(self, delta_time: float):
+        """
+        Deprecated, use on_update
+        """
+        pass
+
+    def on_update(self, delta_time: float):
+        for ui_element in self._ui_elements:
+            ui_element.on_update(delta_time)
+
+    def on_draw(self):
+        """
+        Renders ui elements, already calls `arcade.start_render()`
+        :return:
+        """
+        arcade.start_render()
+        for ui_element in self._ui_elements:
+            ui_element.on_draw()
+
+    def on_event(self, event: UIEvent):
+        """
+        Processes UIEvents, forward events to registered elements and manages focused element
+        """
+        for ui_element in self._ui_elements:
+            if event.type == MOUSE_PRESS:
+                if ui_element.hover_point(event.x, event.y):
+                    ui_element.on_focus()
+                    self.focused_element = ui_element
+
+                elif ui_element is self.focused_element:
+                    if self.focused_element:
+                        self.focused_element.on_unfocus()
+                    self.focused_element = None
+
+            ui_element.on_event(event)
+
+    def find_by_id(self, ui_element_id: str) -> Optional[UIElement]:
+        return self._id_cache.get(ui_element_id)
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        super().on_mouse_press(x, y, button, modifiers)
+        self.on_event(UIEvent(MOUSE_PRESS, x=x, y=y, button=button, modifiers=modifiers))
+
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
+        self.on_event(UIEvent(MOUSE_RELEASE, x=x, y=y, button=button, modifiers=modifiers))
+
+    def on_mouse_scroll(self, x: int, y: int, scroll_x: int, scroll_y: int):
+        self.on_event(UIEvent(MOUSE_SCROLL,
+                              x=x,
+                              y=y,
+                              scroll_x=scroll_x,
+                              scroll_y=scroll_y,
+                              ))
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        super().on_key_press(symbol, modifiers)
+        self.on_event(UIEvent(KEY_PRESS,
+                              symbol=symbol,
+                              modifiers=modifiers
+                              ))
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        super().on_key_release(symbol, modifiers)
+        self.on_event(UIEvent(KEY_RELEASE,
+                              symbol=symbol,
+                              modifiers=modifiers
+                              ))
+
+    def on_text(self, text):
+        self.on_event(UIEvent(TEXT_INPUT,
+                              text=text,
+                              ))
+
+    def on_text_motion(self, motion):
+        self.on_event(UIEvent(TEXT_MOTION,
+                              motion=motion,
+                              ))
+
+    def on_text_motion_selection(self, selection):
+        self.on_event(UIEvent(TEXT_MOTION_SELECTION,
+                              selection=selection,
+                              ))
---
+++ packages/arcade-gui/arcade_gui-0.1.0a8-py3-none-any.whl/arcade_gui/label.py
@@ -0,0 +1,51 @@
+import warnings
+
+import arcade
+
+from arcade_gui import UIElement
+
+warnings.warn(
+    "You are using UI Components from arcade-gui, which are now included in arcade itself.",
+    DeprecationWarning
+)
+
+
+class UILabel(UIElement):
+    def __init__(self, text, x, y,
+                 color=arcade.color.BLACK,
+                 font_size=22,
+                 anchor_x="center",
+                 anchor_y="center",
+                 width: int = 0,
+                 align="center",
+                 font_name=('Calibri', 'Arial'),
+                 bold: bool = False,
+                 italic: bool = False,
+                 rotation=0, **kwargs):
+        super().__init__(**kwargs)
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.font_size = font_size
+        self.anchor_x = anchor_x
+        self.anchor_y = anchor_y
+        self.width = width
+        self.align = align
+        self.font_name = font_name
+        self.bold = bold
+        self.italic = italic
+        self.rotation = rotation
+        self.active = True
+
+    def on_draw(self):
+        arcade.draw_text(self.text,
+                         self.x,
+                         self.y,
+                         self.color,
+                         font_size=self.font_size,
+                         anchor_x=self.anchor_x,
+                         anchor_y=self.anchor_y,
+                         width=self.width, align=self.align,
+                         font_name=self.font_name, bold=self.bold,
+                         italic=self.italic, rotation=self.rotation)
