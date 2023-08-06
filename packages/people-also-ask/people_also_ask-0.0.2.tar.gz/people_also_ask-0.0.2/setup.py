f006eaa51277f0ee806f485504765418bb510f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:51 +0000

    Add package people-also-ask/0.0.2/people_also_ask-0.0.2.tar.gz

---
+++ packages/people-also-ask/people_also_ask-0.0.2.tar.gz/people_also_ask-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+version = {}
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="people_also_ask",
+    version="0.0.2",
+    author="LE Van Tuan",
+    author_email="leavantuan2312@gmail.com",
+    packages=setuptools.find_packages(),
+    #long_description=long_description,
+    download_url="https://github.com/lagranges/people_also_ask/archive/0.0.2.tar.gz",
+    install_requires=[
+        "beautifulsoup4",
+        "requests",
+    ],
+    python_requires=">=3.6"
+)
