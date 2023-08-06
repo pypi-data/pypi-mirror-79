69c775f62a73f801bc20dbf5e96f3fe0ea83e30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:20 +0000

    Add package pybsc/0.0.3/pybsc-0.0.3.tar.gz

---
+++ packages/pybsc/pybsc-0.0.3.tar.gz/pybsc-0.0.3/pybsc/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+import pkg_resources
+
+
+__version__ = pkg_resources.get_distribution("pybsc").version
+
+
+from pybsc.split import nsplit
+
+from pybsc.iter_utils import pairwise
+from pybsc.iter_utils import triplewise
