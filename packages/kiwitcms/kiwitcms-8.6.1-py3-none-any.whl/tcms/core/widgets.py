68b7ad8b08b399d78d0de2cedad7ba7b6aba0031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:54 +0000

    Add package kiwitcms/8.6.1/kiwitcms-8.6.1-py3-none-any.whl

---
+++ packages/kiwitcms/kiwitcms-8.6.1-py3-none-any.whl/tcms/core/widgets.py
@@ -0,0 +1,63 @@
+#   Copyright (c) 2018 Kiwi TCMS project. All rights reserved.
+#   Author: Alexander Todorov <info@kiwitcms.org>
+
+"""
+Custom widgets for Django
+"""
+from django import forms
+
+
+class SimpleMDE(forms.Textarea):
+    """
+        SimpleMDE widget for Django
+    """
+    def render(self, name, value, attrs=None, renderer=None):
+        rendered_string = super().render(name, value, attrs, renderer)
+        rendered_string += """
+<input id="simplemde-file-upload" type="file" style="display: none">
+<script>
+var simplemde = new SimpleMDE({
+    element: document.getElementById("%s"),
+    autoDownloadFontAwesome: false,
+    renderingConfig: {
+        codeSyntaxHighlighting: true,
+    },
+    toolbar: [
+        "heading", "bold", "italic", "strikethrough", "|",
+        "ordered-list", "unordered-list", "table", "horizontal-rule", "code", "quote", "|",
+        "link",
+        {
+            // todo: standard shortcut is (Ctrl-Alt-I) but I can't find a way
+            // to assign shortcuts to customized buttons
+            name: "image",
+            action: function handler(editor){
+                $('#simplemde-file-upload').click();
+            },
+            className: "fa fa-picture-o",
+            title: "Insert Image",
+        },
+        {
+            name: "file",
+            action: function handler(editor){
+                $('#simplemde-file-upload').click();
+            },
+            className: "fa fa-paperclip",
+            title: "Attach File",
+        },
+        "|", "preview", "side-by-side", "fullscreen", "|", "guide"]
+});
+</script>
+""" % attrs['id']
+
+        return rendered_string
+
+    class Media:
+        css = {
+            'all': ['simplemde/dist/simplemde.min.css',
+                    'prismjs/themes/prism.css']
+        }
+        js = ['simplemde/dist/simplemde.min.js',
+              'marked/marked.min.js',
+              'prismjs/prism.js',
+              'prismjs/plugins/autoloader/prism-autoloader.min.js',
+              'js/simplemde_security_override.js']
