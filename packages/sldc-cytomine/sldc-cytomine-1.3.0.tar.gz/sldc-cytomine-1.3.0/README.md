2f843181e62e095db9287b348db3a0a94e296a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:13 +0000

    Add package sldc-cytomine/1.3.0/sldc-cytomine-1.3.0.tar.gz

---
+++ packages/sldc-cytomine/sldc-cytomine-1.3.0.tar.gz/sldc-cytomine-1.3.0/README.md
@@ -0,0 +1,17 @@
+
+# SLDC-Cytomine
+
+## Install
+
+From PyPi: `pip install sldc-cytomine`
+
+## Documentation
+
+This folder contains a binding between Cytomine and SLDC.
+
+Classes:
+
+* `CytomineSlide`: encapsulating an `ImageInstance` and `SliceInstance`
+* `CytomineTile`
+* `CytomineTileBuilder`
+* `TileCache`: to avoid downloading the same tile several times. 
---
+++ packages/sldc-cytomine/sldc-cytomine-1.3.0.tar.gz/sldc-cytomine-1.3.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__version__ = "1.3.0"
+
+setup(
+    name="sldc-cytomine",
+    version=__version__,
+    description="Cytomine-SLDC, a SLDC binding to Cytomine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["sldc_cytomine"],
+    url='https://github.com/waliens/sldc',
+    author="Romain Mormont",
+    author_email="romain.mormont@gmail.com",
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    install_requires=["sldc"]
+)
