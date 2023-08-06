92544ae55126c6ab90ac0707e7b74a2dc4be87a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:10 +0000

    Add package greenponik-bh1750/0.0.6/greenponik_bh1750-0.0.6-py3-none-any.whl

---
+++ packages/greenponik-bh1750/greenponik_bh1750-0.0.6-py3-none-any.whl/GreenPonik_BH1750/BH1750.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+"""
+####################################################################
+####################################################################
+####################### GreenPonik_BH1750 ##########################
+####################### Read BH1750 sensor #########################
+#################### with Python3 through i2c ######################
+####################################################################
+####################################################################
+"""
+
+from adafruit_extended_bus import ExtendedI2C as I2C
+import adafruit_bh1750
+
+
+class BH1750:
+
+    DEFAULT_ADDR = 0x23
+    DEFAULT_BUS = 1
+
+    def __init__(self, bus=DEFAULT_BUS, addr=DEFAULT_ADDR):
+        self._bus = bus
+        self._addr = addr
+
+    @property
+    def bus(self):
+        return self._bus
+
+    @property
+    def address(self):
+        return self._addr
+
+    def read_bh1750(self):
+        """
+        @brief Read bh1750 raspberry pi i2c bus
+        Get lux
+        """
+        try:
+            with I2C(self._bus) as i2c:
+                sensor = adafruit_bh1750.BH1750(i2c, address=self._addr)
+                lux = sensor.lux
+                print('Light: %.3f lx' % lux)
+                return lux
+        except Exception as e:
+            print('cannot read bh1750, An exception occurred: {}'.format(e))
