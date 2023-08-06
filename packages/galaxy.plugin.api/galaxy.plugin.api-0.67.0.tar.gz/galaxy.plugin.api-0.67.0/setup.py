954cc6a23ed0145e404268822d761498ff0df93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:17 +0000

    Add package galaxy.plugin.api/0.67.0/galaxy.plugin.api-0.67.0.tar.gz

---
+++ packages/galaxy.plugin.api/galaxy.plugin.api-0.67.0.tar.gz/galaxy.plugin.api-0.67.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="galaxy.plugin.api",
+    version="0.67.0",
+    description="GOG Galaxy Integrations Python API",
+    author='Galaxy team',
+    author_email='galaxy@gog.com',
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    install_requires=[
+        "aiohttp>=3.5.4",
+        "certifi>=2019.3.9",
+        "psutil>=5.6.6; sys_platform == 'darwin'"
+    ]
+)
