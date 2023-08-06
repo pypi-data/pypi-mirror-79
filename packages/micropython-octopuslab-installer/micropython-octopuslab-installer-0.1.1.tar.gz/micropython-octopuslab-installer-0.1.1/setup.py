d1dd4ec05a316fc77b2880ffd3edee665ec6e256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:27 +0000

    Add package micropython-octopuslab-installer/0.1.1/micropython-octopuslab-installer-0.1.1.tar.gz

---
+++ packages/micropython-octopuslab-installer/micropython-octopuslab-installer-0.1.1.tar.gz/micropython-octopuslab-installer-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="micropython-octopuslab-installer",
+    version="0.1.1",
+    author="OctopusLAB",
+    author_email="info@octopuslab.cz",
+    description="OctopusLab installer for MicroPython",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/octopusengine/octopuslab-installer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    install_requires=[
+        'shutil',
+    ],
+    python_requires='>=3.6',
+)
