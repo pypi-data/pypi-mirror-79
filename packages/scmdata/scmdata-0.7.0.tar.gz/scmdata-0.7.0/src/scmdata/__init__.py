35f19c554df5fc4ccf056670c54e2e2d13fe71a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:27 +0000

    Add package scmdata/0.7.0/scmdata-0.7.0.tar.gz

---
+++ packages/scmdata/scmdata-0.7.0.tar.gz/scmdata-0.7.0/src/scmdata/__init__.py
@@ -0,0 +1,12 @@
+"""
+scmdata, simple data handling for simple climate model data
+"""
+from ._version import get_versions  # isort:skip
+
+__version__ = get_versions()["version"]
+del get_versions
+
+REQUIRED_COLS = ["model", "scenario", "region", "variable", "unit"]
+"""Minimum metadata columns required by an ScmRun"""
+
+from scmdata.run import ScmRun, run_append  # noqa: F401, E402
