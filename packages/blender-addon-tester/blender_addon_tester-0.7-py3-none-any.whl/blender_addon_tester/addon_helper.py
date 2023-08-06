b57ba5444f798ad3f3ef711ff8ea085c6f0b7958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:26 +0000

    Add package blender-addon-tester/0.7/blender_addon_tester-0.7-py3-none-any.whl

---
+++ packages/blender-addon-tester/blender_addon_tester-0.7-py3-none-any.whl/blender_addon_tester/addon_helper.py
@@ -0,0 +1,160 @@
+import os
+import sys
+import re
+import time
+import zipfile
+import shutil
+import bpy
+
+def clean_file(filename):
+    """ Cleans up addons files so that any blender version code is strip to be compliant with addon guidelines
+    :param filename     Path to addon file
+    :return None
+    """
+    f = open(filename, "r")
+    lines = f.readlines()
+    f.close()
+
+    unique_blender = True
+    fix_fstrings = False
+    if bpy.app.version <= (2, 79, 0):
+        fix_fstrings = True
+
+    trim_code = False
+    active_print0 = None
+    f = open(filename, "w")
+    for line in lines:
+        if unique_blender:
+            line2 = line
+            if re.search("^\s+else", line) and re.search("else bpy.app.version", line):
+                active_print0 = not (active_print0)
+                line2 = "\n"
+
+            if re.search("endif", line):
+                active_print0 = None
+                trim_code = False
+                line2 = "\n"
+
+            h = re.search("^\s+[el]?if \((\d+), (\d+), (\d+)\) < bpy.app.version", line)
+            if h:
+                current_blender_rev = (
+                    int(h.group(1)),
+                    int(h.group(2)),
+                    int(h.group(3)),
+                )
+                trim_code = True
+                active_print0 = current_blender_rev <= bpy.app.version
+                line2 = "\n"
+
+            if trim_code:
+                line2 = re.sub("^    ", "", line2)
+            if not active_print0 and not active_print0 == None:
+                line2 = "\n"
+            line = line2
+
+        k = re.search('"blender":\s\(\d+, \d+, \d+\)', line)
+        if k:
+            line = '    "blender": {0},\n'.format(bpy.app.version)
+
+        if re.search('print\(f"', line) and fix_fstrings:
+            line = re.sub("print", "pass ; # print", line)
+        f.write(line)
+    f.close()
+
+def zip_addon(addon, addon_dir):
+    """ Zips 'addon' dir or '.py' file to 'addon.zip' if not yet zipped, then moves the archive to 'addon_dir'.
+    :param addon     Absolute or relative path to a directory to zip or a .zip file.
+    :param addon_dir Path to Blender's addon directory to move the zipped archive to.
+    :return (bpy_module, zip_file) Tuple of strings - an importable module name, an addon zip file path.
+    """
+    already_zipped = False
+
+    addon_path = os.path.realpath(addon)
+    addon_basename = os.path.basename(addon_path)
+
+    if addon_basename.endswith(".zip"):
+        already_zipped = True
+
+    if os.path.isdir(addon_dir):
+        shutil.rmtree(addon_dir)
+    os.mkdir(addon_dir)
+
+    print("Addon dir is - {0}".format(os.path.realpath(addon_dir)))
+    if not already_zipped:
+        bpy_module = re.sub(".py", "", addon_basename)
+        zfile = os.path.realpath(bpy_module + ".zip")
+
+        print("Future zip path is - {0}".format(zfile))
+
+        print("Zipping addon - {0}".format(bpy_module))
+
+
+        zf = zipfile.ZipFile(zfile, "w")
+        if os.path.isdir(addon):
+            cwd = os.getcwd()
+            temp_dir = "tmp"
+            if os.path.isdir(temp_dir):
+                shutil.rmtree(temp_dir)
+        
+            shutil.copytree(addon, temp_dir + "/" + addon)
+            os.chdir(temp_dir)
+            if os.path.isdir("__pycache__"):
+                shutil.rmtree("__pycache__")
+            for dirname, subdirs, files in os.walk(addon):
+                zf.write(dirname)
+                for filename in files:
+                    filename = os.path.join(dirname, filename)
+                    clean_file(filename)
+                    zf.write(filename)
+            os.chdir(cwd)
+            shutil.rmtree(temp_dir)
+        else:
+            clean_file(addon)
+            zf.write(addon)
+        zf.close()
+    else:
+        zfile = addon_path
+        print("Detected zip path is - {0}. No need to zip the addon beforehand.".format(zfile))
+
+        bpy_module = addon_basename.split(".zip")[0]
+
+    brev = "{0}.{1}".format(bpy.app.version[0], bpy.app.version[1])
+    bfile = re.sub(".zip", "_{}.zip".format(brev), zfile)
+    shutil.copy(zfile, bfile)
+    return (bpy_module, bfile)
+
+
+def change_addon_dir(bpy_module, zfile, addon_dir):
+    print("Change addon dir - {0}".format(addon_dir))
+
+    if (2, 80, 0) < bpy.app.version:
+        # https://docs.blender.org/api/current/bpy.types.PreferencesFilePaths.html#bpy.types.PreferencesFilePaths.script_directory
+        # requires restart
+        bpy.context.preferences.filepaths.script_directory = addon_dir
+        bpy.utils.refresh_script_paths()
+        bpy.ops.preferences.addon_install(overwrite=True, filepath=zfile)
+        bpy.ops.preferences.addon_enable(module=bpy_module)
+    else:
+        bpy.context.user_preferences.filepaths.script_directory = addon_dir
+        bpy.utils.refresh_script_paths()
+        bpy.ops.wm.addon_install(overwrite=True, filepath=zfile)
+        bpy.ops.wm.addon_enable(module=bpy_module)
+
+
+def cleanup(addon, bpy_module, addon_dir):
+    print("Cleaning up - {}".format(bpy_module))
+    if (2, 80, 0) < bpy.app.version:
+        bpy.ops.preferences.addon_disable(module=bpy_module)
+    else:
+        bpy.ops.wm.addon_disable(module=bpy_module)
+    if os.path.isdir(addon_dir):
+        shutil.rmtree(addon_dir)
+
+
+def get_version(bpy_module):
+    mod = sys.modules[bpy_module]
+    return mod.bl_info.get("version", (-1, -1, -1))
+
+
+def get_bl_version():
+    return bpy.app.version
---
+++ packages/blender-addon-tester/blender_addon_tester-0.7-py3-none-any.whl/blender_addon_tester/run_blender.py
@@ -0,0 +1,153 @@
+import os
+import sys
+import subprocess
+import re
+import shutil
+import zipfile
+from glob import glob
+from .get_blender import get_blender_from_suffix
+
+CURRENT_MODULE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
+BUILTIN_BLENDER_LOAD_TESTS_SCRIPT = os.path.join(CURRENT_MODULE_DIRECTORY, "blender_load_pytest.py")
+
+def _run_blender_with_python_script(blender, blender_python_script):
+    blender_python_script = blender_python_script
+    local_python = CURRENT_MODULE_DIRECTORY
+    os.environ["LOCAL_PYTHONPATH"] = local_python
+
+    cmd = f'{blender} -b --python "{blender_python_script}"'
+    print(f"Will run the following command: {cmd}")
+    result = int(os.system(cmd))
+    if 0 == result:
+        return 0
+    else:
+        return 1
+
+def test_exisiting_addons(blender_revision, addon_path, blender):
+    addon = addon_path
+    rev = re.sub("[a-z]$", "", blender_revision)
+    if "darwin" == sys.platform:
+        loc = os.path.realpath(f"{blender}/../../Resources/{rev}/scripts")
+    else:
+        loc = os.path.realpath(f"{blender}/../{rev}/scripts")
+
+    loc = f"{loc}/*/{addon}"
+    files = glob(loc)
+    for addon in files:
+        zfile = f"{addon}.zip"
+        zf = zipfile.ZipFile(zfile, "w")
+        if os.path.isdir(addon):
+            for dirname, subdirs, files in os.walk(addon):
+                zf.write(dirname)
+                for filename in files:
+                    zf.write(os.path.join(dirname, filename))
+        else:
+            zf.write(addon)
+        zf.close()
+        
+        if os.path.isdir(addon):
+            shutil.rmtree(os.path.realpath(addon))
+        else:
+            os.unlink(os.path.realpath(addon))
+
+def run_blender_version_for_addon_with_pytest_suite(addon_path, blender_revision=None, config={}):
+    """
+    Run tests for "blender_revision" x "addon" using the builtin "blender_load_pytest.py" script or "custom_blender_load_tests_script"
+
+    :param addon: Addon path to test, can be a path to a directory (will be zipped for you) or to a .zip file. The Python module name will be that of the (directory or) zip file without extension, try to make it as pythonic as possible for Blender's Python importer to work properly with it: letters, digits, underscores.
+    :param blender_revision: Version of Blender3d. Default: "2.82"
+    :param config: A options dictionary, its keys allow to override some defaults:
+                    "blender_load_tests_script": str: absolute or CWD-relative path to the Blender Python scripts that loads and runs tests. Default: "blender_load_tests_script.py" (packaged with this module)
+                    "coverage": bool: whether or not run coverage evaluation along tests; Default: False (no coverage evaluation) 
+                    "tests": str: absolute or CWD-relative path to a directory of tests or test script that the blender_load_tests_script can use. Default: "tests/" (CWD-relative)
+                    "blender_cache": str: absolute or CWD-relative path to a directory where to download and extract Blender3d releases.
+    :return: None, will sys-exit with 1 on failure
+    """
+    if None == blender_revision:
+        blender_revision = "2.82"
+        print("No blender_revision given, defaulting to {blender_revision}.")
+
+    print("testing addon_path:", addon_path, "under blender_revision:", blender_revision, "with config dict:", config)
+
+    # Get Blender for the given version in a cached way
+    downloaded_blender_dir = get_blender_from_suffix(blender_revision)
+    print("Downloaded Blender is expected in this directory: ", downloaded_blender_dir)
+
+    # Tune configuration
+    DEFAULT_CONFIG = {"blender_load_tests_script": BUILTIN_BLENDER_LOAD_TESTS_SCRIPT, "coverage": False}
+    # Let the provided config dict override the default one
+    config = dict(DEFAULT_CONFIG, **config)
+
+    if "win32" == sys.platform or "win64" == sys.platform or "cygwin" == sys.platform:
+        ext = ".exe"
+    else:
+        ext = ""
+
+    if "darwin" == sys.platform:
+        blender_executable_root = f"{downloaded_blender_dir}/MacOS/*lender"
+    else:
+        blender_executable_root = f"{downloaded_blender_dir}/blender{ext}"
+
+    files = glob(blender_executable_root)
+    if not 1 == len(files):
+        if len(files) == 0:
+            raise Exception(f"No blenders found in directory {blender_executable_root}: {files}")
+        else:
+            raise Exception(f"Too many blenders found in directory {blender_executable_root}: {files}")
+    
+    blender = os.path.realpath(files[0])
+
+    os.environ["BLENDER_ADDON_TO_TEST"] = addon_path
+
+    if config.get("blender_cache", None):
+        os.environ["BLENDER_CACHE"] = config["blender_cache"]
+#     else:
+#         os.environ["BLENDER_CACHE"] = ".."
+    config_keys = [
+        "blender_load_tests_script",
+        "coverage",
+        "tests",
+        "pytest_args",
+    ]
+    for c in config.keys():
+        if not c in config_keys:
+            raise Exception(f"Unknown key for config:\n\t{c}")
+
+    if not config["blender_load_tests_script"]:
+        test_file = BUILTIN_BLENDER_LOAD_TESTS_SCRIPT
+    else:
+        test_file = config["blender_load_tests_script"]
+
+    if not config.get("coverage"):
+        if os.environ.get("BLENDER_ADDON_COVERAGE_REPORTING"):
+            del os.environ["BLENDER_ADDON_COVERAGE_REPORTING"]
+    else:
+        os.environ["BLENDER_ADDON_COVERAGE_REPORTING"] = "y"
+
+    if not config.get("tests"):
+        if os.environ.get("BLENDER_ADDON_TESTS_PATH"):
+            del os.environ["BLENDER_ADDON_TESTS_PATH"]
+    else:
+        os.environ["BLENDER_ADDON_TESTS_PATH"] = config["tests"] 
+
+    if not config.get("pytest_args"):
+        if os.environ.get("BLENDER_PYTEST_ARGS"):
+            del os.environ["BLENDER_PYTEST_ARGS"]
+    else:
+        os.environ["BLENDER_PYTEST_ARGS"] = config["pytest_args"] 
+
+    test_exisiting_addons(blender_revision, addon_path, blender)
+        
+    # Run tests with the proper Blender version and configured tests
+    return _run_blender_with_python_script(blender, test_file)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        blender_rev = sys.argv[1]
+        addon = sys.argv[2]
+        sys.exit(run_blender_version_for_addon_with_pytest_suite(blender_revision=blender_rev, addon_path=addon))
+    else:
+        print("Usage:", sys.argv[0], "blender_rev addon")
+        sys.exit(1)
+
