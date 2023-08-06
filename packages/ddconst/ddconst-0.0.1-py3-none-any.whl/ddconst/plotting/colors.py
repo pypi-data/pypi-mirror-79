f0b0edc38212e26a2eeaf02ac1a190722301d9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:45 +0000

    Add package ddconst/0.0.1/ddconst-0.0.1-py3-none-any.whl

---
+++ packages/ddconst/ddconst-0.0.1-py3-none-any.whl/ddconst/plotting/colors.py
@@ -0,0 +1,12 @@
+blue = '#002D72'
+light_blue = '#0085CA'
+
+blacks = ['#2C2A29', '#333F48', '#A2AAAD', '#D0D3D4', '#FFFFFF']
+
+yellow = '#F1C400'
+orange = '#ED8B00'
+orange_dark = '#DC582A'
+red = '#AB2328'
+magenta = '#8A1B61'
+green_pine = '#005151'
+green_light = '#43B02A'
