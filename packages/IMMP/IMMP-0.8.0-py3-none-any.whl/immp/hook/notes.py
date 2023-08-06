e0aa6e0d208372a0bd9defd534a26271d23b7c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:28 +0000

    Add package IMMP/0.8.0/IMMP-0.8.0-py3-none-any.whl

---
+++ packages/IMMP/IMMP-0.8.0-py3-none-any.whl/immp/hook/notes.py
@@ -0,0 +1,215 @@
+"""
+Recallable per-channel lists of text items.
+
+Commands:
+    note-add <text>:
+        Add a new note for this channel.
+    note-edit <num> <text>:
+        Update an existing note from this channel with new text.
+    note-remove <num>:
+        Delete an existing note from this channel by its position.
+    note-show <num>:
+        Recall a single note in this channel.
+    note-list:
+        Recall all notes for this channel.
+
+.. note::
+    This hook requires an active :class:`.DatabaseHook` to store data.
+"""
+
+import time
+
+from peewee import CharField, IntegerField
+
+import immp
+from immp.hook.command import BadUsage, CommandParser, command
+from immp.hook.database import BaseModel, DatabaseHook
+
+
+CROSS = "\N{CROSS MARK}"
+TICK = "\N{WHITE HEAVY CHECK MARK}"
+
+
+class Note(BaseModel):
+    """
+    Representation of a single note.
+
+    Attributes:
+        timestamp (int):
+            Creation time of the note.
+        network (str):
+            Network identifier for the channel's plug.
+        channel (str):
+            Channel identifier where the note was created.
+        user (str):
+            User identifier of the note's author.
+        text (str):
+            Note content.
+    """
+
+    timestamp = IntegerField(default=lambda: int(time.time()))
+    network = CharField()
+    channel = CharField(null=True)
+    user = CharField(null=True)
+    text = CharField()
+
+    @classmethod
+    def select_channel(cls, channel):
+        return (cls.select().where(cls.network == channel.plug.network_id,
+                                   cls.channel == channel.source)
+                            .order_by(cls.timestamp))
+
+    @classmethod
+    def select_position(cls, channel, num):
+        if num < 1:
+            raise ValueError
+        try:
+            # ModelSelect.get() ignores the offset clause, use an index instead.
+            return cls.select_channel(channel).limit(1).offset(num - 1)[0]
+        except IndexError:
+            raise Note.DoesNotExist from None
+
+    @classmethod
+    def select_position_multi(cls, channel, *nums):
+        if any(num < 1 for num in nums):
+            raise ValueError
+        notes = list(cls.select_channel(channel))
+        try:
+            return [notes[num - 1] for num in nums]
+        except IndexError:
+            raise Note.DoesNotExist from None
+
+    @property
+    def ago(self):
+        diff = int(time.time()) - self.timestamp
+        for step, unit in ((60, "s"), (60, "m"), (24, "h")):
+            if diff < step:
+                return "{}{}".format(diff, unit)
+            diff //= step
+        return "{}d".format(diff)
+
+    def __repr__(self):
+        return "<{}: #{} {} @ {}: {}>".format(self.__class__.__name__, self.id, self.ago,
+                                              repr(self.channel), repr(self.text))
+
+
+class NotesHook(immp.Hook):
+    """
+    Hook for managing and recalling notes in channels.
+    """
+
+    schema = None
+
+    def __init__(self, name, config, host):
+        super().__init__(name, config, host)
+        self.db = None
+
+    async def start(self):
+        self.db = self.host.resources[DatabaseHook].db
+        self.db.create_tables([Note], safe=True)
+
+    async def channel_migrate(self, old, new):
+        count = (Note.update(network=new.plug.network_id, channel=new.source)
+                     .where(Note.network == old.plug.network_id,
+                            Note.channel == old.source).execute())
+        return count > 0
+
+    @command("note-add", parser=CommandParser.none)
+    async def add(self, msg, text):
+        """
+        Add a new note for this channel.
+        """
+        Note.create(network=msg.channel.plug.network_id,
+                    channel=msg.channel.source,
+                    user=(msg.user.id or msg.user.username) if msg.user else None,
+                    text=text.raw())
+        count = Note.select_channel(msg.channel).count()
+        await msg.channel.send(immp.Message(text="{} Added #{}".format(TICK, count)))
+
+    @command("note-edit", parser=CommandParser.hybrid)
+    async def edit(self, msg, num, text):
+        """
+        Update an existing note from this channel with new text.
+        """
+        try:
+            note = Note.select_position(msg.channel, int(num))
+        except ValueError:
+            raise BadUsage from None
+        except Note.DoesNotExist:
+            text = "{} Does not exist".format(CROSS)
+        else:
+            note.text = text.raw()
+            note.save()
+            text = "{} Edited".format(TICK)
+        await msg.channel.send(immp.Message(text=text))
+
+    @command("note-remove")
+    async def remove(self, msg, *nums):
+        """
+        Delete one or more notes from this channel by their positions.
+        """
+        if not nums:
+            raise BadUsage
+        try:
+            nums = [int(num) for num in nums]
+            notes = Note.select_position_multi(msg.channel, *nums)
+        except ValueError:
+            raise BadUsage from None
+        except Note.DoesNotExist:
+            text = "{} Does not exist".format(CROSS)
+        else:
+            count = Note.delete().where(Note.id.in_(tuple(note.id for note in notes))).execute()
+            text = "{} Removed {} note{}".format(TICK, count, "" if count == 1 else "s")
+        await msg.channel.send(immp.Message(text=text))
+
+    @command("note-show")
+    async def show(self, msg, num):
+        """
+        Recall a single note in this channel.
+        """
+        try:
+            note = Note.select_position(msg.channel, int(num))
+        except ValueError:
+            raise BadUsage from None
+        except Note.DoesNotExist:
+            text = "{} Does not exist".format(CROSS)
+        else:
+            text = immp.RichText([immp.Segment("{}.".format(num), bold=True),
+                                  immp.Segment("\t"),
+                                  *immp.RichText.unraw(note.text, self.host),
+                                  immp.Segment("\t"),
+                                  immp.Segment(note.ago, italic=True)])
+        await msg.channel.send(immp.Message(text=text))
+
+    @command("note-list")
+    async def list(self, msg, query=None):
+        """
+        Recall all notes for this channel, or search for text across all notes.
+        """
+        notes = Note.select_channel(msg.channel)
+        if query:
+            matches = notes.where(Note.text.contains(query))
+            count = len(matches)
+        else:
+            count = len(notes)
+        title = ("{}{} note{} in this channel{}"
+                 .format(count, " matching" if query else "",
+                         "" if count == 1 else "s", ":" if count else "."))
+        text = immp.RichText([immp.Segment(title, bold=bool(notes))])
+        for num, note in enumerate(notes, 1):
+            if query and note not in matches:
+                continue
+            text.append(immp.Segment("\n"),
+                        immp.Segment("{}.".format(num), bold=True),
+                        immp.Segment("\t"),
+                        *immp.RichText.unraw(note.text, self.host),
+                        immp.Segment("\t"),
+                        immp.Segment(note.ago, italic=True))
+        target = None
+        if msg.user:
+            target = await msg.user.private_channel()
+        if target:
+            await target.send(immp.Message(text=text))
+            await msg.channel.send(immp.Message(text="{} Sent".format(TICK)))
+        else:
+            await msg.channel.send(immp.Message(text=text))
---
+++ packages/IMMP/IMMP-0.8.0-py3-none-any.whl/immp/plug/github.py
@@ -0,0 +1,243 @@
+"""
+Listen for incoming GitHub webhooks.
+
+Config:
+    route (str):
+        Path to expose the webhook request handler.
+    secret (str):
+        Shared string between the plug and GitHub's servers.  Optional but recommended, must be
+        configured to match the webhook on GitHub.
+
+Go to your repository > Settings > Webhooks > Add webhook, set the URL to match the configured
+route, and choose the events you wish to handle.  Message summaries for each event will be emitted
+on channels matching the full name of each repository (e.g. ``user/repo``).
+
+.. note::
+    This plug requires an active :class:`.WebHook` to receive incoming messages.
+"""
+
+import hashlib
+import hmac
+import logging
+
+from aiohttp import web
+
+import immp
+from immp.hook.web import WebHook
+
+
+log = logging.getLogger(__name__)
+
+
+class _Schema:
+
+    config = immp.Schema({"route": str,
+                          immp.Optional("secret"): immp.Nullable(str)})
+
+    event = immp.Schema({"repository": {"full_name": str},
+                         "sender": {"id": int,
+                                    "login": str,
+                                    "avatar_url": str,
+                                    "html_url": str}})
+
+
+class GitHubUser(immp.User):
+    """
+    User present in GitHub.
+    """
+
+    @classmethod
+    def from_sender(cls, github, sender):
+        return cls(id_=sender["id"],
+                   plug=github,
+                   username=sender["login"],
+                   avatar=sender["avatar_url"],
+                   raw=sender)
+
+    @property
+    def link(self):
+        return "https://github.com/{}".format(self.username)
+
+    @link.setter
+    def link(self, value):
+        pass
+
+
+class GitHubMessage(immp.Message):
+    """
+    Repository event originating from GitHub.
+    """
+
+    @classmethod
+    def from_event(cls, github, type_, id_, event):
+        """
+        Convert a `GitHub webhook <https://developer.github.com/webhooks/>`_ payload to a
+        :class:`.Message`.
+
+        Args:
+            github (.GitHubPlug):
+                Related plug instance that provides the event.
+            type (str):
+                Event type name from the ``X-GitHub-Event`` header.
+            id (str):
+                GUID of the event delivery from the ``X-GitHub-Delivery`` header.
+            event (dict):
+                GitHub webhook payload.
+
+        Returns:
+            .GitHubMessage:
+                Parsed message object.
+        """
+        repo = event["repository"]["full_name"]
+        channel = immp.Channel(github, repo)
+        user = GitHubUser.from_sender(github, event["sender"])
+        text = None
+        if type_ == "push":
+            count = len(event["commits"])
+            desc = "{} commits".format(count) if count > 1 else event["after"][:7]
+            ref = event["ref"].split("/")[1:]
+            target = "/".join(ref[1:])
+            if ref[0] == "tags":
+                action, join = "tagged", "as"
+            elif ref[0] == "heads":
+                action, join = "pushed", "to"
+            else:
+                raise NotImplementedError
+            text = immp.RichText([immp.Segment("{} ".format(action)),
+                                  immp.Segment(desc, link=event["compare"]),
+                                  immp.Segment(" {} {} {}".format(join, repo, target))])
+            for commit in event["commits"]:
+                text.append(immp.Segment("\n* "),
+                            immp.Segment(commit["id"][:7], code=True),
+                            immp.Segment(" - {}".format(commit["message"].split("\n")[0])))
+        elif type_ == "release":
+            release = event["release"]
+            desc = ("{} ({} {})".format(release["name"], repo, release["tag_name"])
+                    if release["name"] else release["tag_name"])
+            text = immp.RichText([immp.Segment("{} release ".format(event["action"])),
+                                  immp.Segment(desc, link=release["html_url"])])
+        elif type_ == "issues":
+            issue = event["issue"]
+            desc = "{} ({}#{})".format(issue["title"], repo, issue["number"])
+            text = immp.RichText([immp.Segment("{} issue ".format(event["action"])),
+                                  immp.Segment(desc, link=issue["html_url"])])
+        elif type_ == "issue_comment":
+            issue = event["issue"]
+            comment = event["comment"]
+            desc = "{} ({}#{})".format(issue["title"], repo, issue["number"])
+            text = immp.RichText([immp.Segment("{} a ".format(event["action"])),
+                                  immp.Segment("comment", link=comment["html_url"]),
+                                  immp.Segment(" on issue "),
+                                  immp.Segment(desc, link=issue["html_url"])])
+        elif type_ == "pull_request":
+            pull = event["pull_request"]
+            desc = "{} ({}#{})".format(pull["title"], repo, pull["number"])
+            text = immp.RichText([immp.Segment("{} pull request ".format(event["action"])),
+                                  immp.Segment(desc, link=pull["html_url"])])
+        elif type_ == "pull_request_review":
+            pull = event["pull_request"]
+            review = event["review"]
+            desc = "{} ({}#{})".format(pull["title"], repo, pull["number"])
+            text = immp.RichText([immp.Segment("{} a ".format(event["action"])),
+                                  immp.Segment("review", link=review["html_url"]),
+                                  immp.Segment(" on pull request "),
+                                  immp.Segment(desc, link=pull["html_url"])])
+        elif type_ == "pull_request_review_comment":
+            pull = event["pull_request"]
+            comment = event["comment"]
+            desc = "{} ({}#{})".format(pull["title"], repo, pull["number"])
+            text = immp.RichText([immp.Segment("{} a ".format(event["action"])),
+                                  immp.Segment("comment", link=comment["html_url"]),
+                                  immp.Segment(" on pull request "),
+                                  immp.Segment(desc, link=pull["html_url"])])
+        elif type_ == "project":
+            project = event["project"]
+            desc = "{} ({}#{})".format(project["name"], repo, project["number"])
+            text = immp.RichText([immp.Segment("{} project ".format(event["action"])),
+                                  immp.Segment(desc, link=project["html_url"])])
+        elif type_ == "project_card":
+            card = event["project_card"]
+            text = immp.RichText([immp.Segment("{} ".format(event["action"])),
+                                  immp.Segment("card", link=card["html_url"]),
+                                  immp.Segment(" in project:\n"),
+                                  immp.Segment(card["note"])])
+        elif type_ == "gollum":
+            text = immp.RichText()
+            for i, page in enumerate(event["pages"]):
+                if i:
+                    text.append(immp.Segment(", "))
+                text.append(immp.Segment("{} {} wiki page ".format(page["action"], repo)),
+                            immp.Segment(page["title"], link=page["html_url"]))
+        elif type_ == "fork":
+            fork = event["forkee"]
+            text = immp.RichText([immp.Segment("forked {} to ".format(repo)),
+                                  immp.Segment(fork["full_name"], link=fork["html_url"])])
+        elif type_ == "watch":
+            text = immp.RichText([immp.Segment("starred {}".format(repo))])
+        if text:
+            return immp.SentMessage(id_=id_,
+                                    channel=channel,
+                                    text=text,
+                                    user=user,
+                                    action=True,
+                                    raw=event)
+        else:
+            raise NotImplementedError
+
+
+class GitHubPlug(immp.Plug):
+    """
+    Plug for incoming `GitHub <https://github.com>`_ notifications.
+    """
+
+    schema = _Schema.config
+
+    network_name = "GitHub"
+    network_id = "github"
+
+    def __init__(self, name, config, host):
+        super().__init__(name, config, host)
+        self.ctx = None
+
+    def on_load(self):
+        log.debug("Registering webhook route")
+        self.ctx = self.host.resources[WebHook].context(self.config["route"], __name__)
+        self.ctx.route("POST", "", self.handle)
+
+    async def channel_title(self, channel):
+        return channel.source
+
+    async def handle(self, request):
+        if self.config["secret"]:
+            try:
+                body = await request.read()
+            except ValueError:
+                raise web.HTTPBadRequest
+            try:
+                alg, sig = request.headers["X-Hub-Signature"].split("=", 1)
+            except (KeyError, ValueError):
+                log.warning("No signature on event, secret needs configuring on webhook")
+                raise web.HTTPUnauthorized
+            match = hmac.new(self.config["secret"].encode("utf-8"), body, hashlib.sha1).hexdigest()
+            if alg != "sha1" or sig != match:
+                log.warning("Bad signature on event")
+                raise web.HTTPUnauthorized
+        try:
+            data = await request.json()
+        except ValueError:
+            log.warning("Bad content type, webhook needs configuring as JSON")
+            raise web.HTTPBadRequest
+        try:
+            type_ = request.headers["X-GitHub-Event"]
+            id_ = request.headers["X-GitHub-Delivery"]
+            event = _Schema.event(data)
+        except (KeyError, ValueError):
+            raise web.HTTPBadRequest
+        if type_ == "ping":
+            log.debug("Received ping event for %s", event["repository"]["full_name"])
+        else:
+            try:
+                self.queue(GitHubMessage.from_event(self, type_, id_, event))
+            except NotImplementedError:
+                log.debug("Ignoring unrecognised event type %r", type_)
+        return web.Response()
