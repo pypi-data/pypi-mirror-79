6e8a58374b2cc65921412b9731b98277f7ff685b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:29 +0000

    Add package perip/0.1.4/perip-0.1.4-py3-none-any.whl

---
+++ packages/perip/perip-0.1.4-py3-none-any.whl/perip/device_am2315.py
@@ -0,0 +1,12 @@
+from aosong import am2315
+import time
+import redis
+
+r = redis.Redis(host='ec2-75-101-199-232.compute-1.amazonaws.com', port=7599, password='p525f81fc5bf9a369922112084c158114e3978bc3042d9a76172a8508c7cd26ce')
+sensor = am2315.Sensor()
+
+def get_data():
+    while True:
+        r.set('data', 'hello')
+        print(sensor.data())
+        time.sleep(2.00)
