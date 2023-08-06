f8cc38d6a8e1594d944effff3228316304118da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:59 +0000

    Add package idom/0.12.0/idom-0.12.0-py3-none-any.whl

---
+++ packages/idom/idom-0.12.0-py3-none-any.whl/idom/widgets/__init__.py
@@ -0,0 +1,14 @@
+from .utils import hotswap, multiview
+from .module import Module, Import
+from .html import html, image, Input
+
+__all__ = [
+    "node",
+    "hotswap",
+    "multiview",
+    "html",
+    "Module",
+    "Import",
+    "image",
+    "Input",
+]
