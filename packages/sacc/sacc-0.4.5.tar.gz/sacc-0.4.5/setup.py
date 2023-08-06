d053471c164daa07af9e0783b5bde3bf58a972f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:27 +0000

    Add package sacc/0.4.5/sacc-0.4.5.tar.gz

---
+++ packages/sacc/sacc-0.4.5.tar.gz/sacc-0.4.5/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+description = "SACC - the LSST/DESC summary statistic data format library"
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name="sacc", 
+      version="0.4.5",
+      description=description,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url="https://github.com/LSSTDESC/sacc",
+      author="LSST DESC",
+      author_email="joezuntz@googlemail.com",
+      install_requires=requirements,
+      packages=['sacc'],
+)
