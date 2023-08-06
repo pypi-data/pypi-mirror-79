cc6a349e2c4139cd9d0148b506d0751ff4048040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:39 +0000

    Add package zoom-audio-transcribe/0.1.0/zoom_audio_transcribe-0.1.0.tar.gz

---
+++ packages/zoom-audio-transcribe/zoom_audio_transcribe-0.1.0.tar.gz/zoom_audio_transcribe-0.1.0/README.md
@@ -0,0 +1,32 @@
+# zoom_audio_transcribe
+![GH](https://github.com/jdvala/zoom-audio-transcribe/workflows/GH/badge.svg)
+[![Code Coverage](https://codecov.io/gh/jdvala/zoom_audio_transcribe/branch/master/graph/badge.svg)](https://codecov.io/gh/jdvala/zoom_audio_transcribe)
+
+Transcribe zoom meetings audio locally.
+
+Transcribe zoom meetings audio locally, using ![vosk-api](https://github.com/alphacep/vosk-api).
+
+
+## Install
+
+Supported python version >=3.6. Supported OS, Linux. Support for Windows and OSX will come in second version.
+
+```bash
+pip install zoom_audio_transcribe
+```
+
+
+## Usage
+
+```bash
+$ zoom_audio_transcribe
+```
+
+![init](https://github.com/jdvala/zoom-audio-transcribe/blob/master/screenshots/init.png)
+
+Once you are at the menu, select the appropiate meeting and let it run.
+
+![Done](https://github.com/jdvala/zoom-audio-transcribe/blob/master/screenshots/done.png)
+
+Once the transcription is done the, file will be saved in the same folder as the zoom meetings recording. For linux users its `/home/$USER/Documents/Zoom/{meeting_which_was_selected}
+---
---
+++ packages/zoom-audio-transcribe/zoom_audio_transcribe-0.1.0.tar.gz/zoom_audio_transcribe-0.1.0/setup.py
@@ -0,0 +1,56 @@
+import io
+import os.path as path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+requirements_path = path.join(here, "requirements", "prod.txt")
+
+
+def read_requirements(path):
+    try:
+        with io.open(path, mode="rt", encoding="utf-8") as fp:
+            return list(filter(None, (line.split("#")[0].strip() for line in fp)))
+    except IndexError:
+        raise RuntimeError("{} is broken".format(path))
+
+
+setup(
+    name="zoom_audio_transcribe",
+    python_requires=">=3.6",
+    setup_requires=["setuptools_scm"],
+    install_requires=read_requirements(requirements_path),
+    use_scm_version={
+        "version_scheme": "guess-next-dev",
+        "local_scheme": "dirty-tag",
+        "write_to": "src/zoom_audio_transcribe/_repo_version.py",
+        "write_to_template": 'version = "{version}"\n',
+        "relative_to": __file__,
+    },
+    include_package_data=True,
+    package_data={},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": ["zoom_audio_transcribe = zoom_audio_transcribe.cli:main"]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Archiving :: Packaging",
+        "Topic :: System :: Systems Administration",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: Utilities",
+    ],
+    author="Jay Vala",
+    author_email="jay.vala@msn.com",
+    url="https://github.com/jdvala/zoom_audio_transcribe",
+)
