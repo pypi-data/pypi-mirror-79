69d6f009c3e62fc0100659a91a403bf32045de87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:37 +0000

    Add package MCSC/0.1/MCSC-0.1.tar.gz

---
+++ packages/MCSC/MCSC-0.1.tar.gz/MCSC-0.1/MCSC/FalsePostionMethod.py
@@ -0,0 +1,27 @@
+from prettytable import PrettyTable
+
+class FalsePostionMethod:
+    def __init__(self,initial_approx,b,function_fpmethod,steps=20,tolerance=0.00005):
+        self.a=initial_approx
+        self.b=b
+        self.function=function_fpmethod
+        self.tolerance=tolerance
+        self.steps=steps
+        self.table=PrettyTable(['n','a','b','x','f(x)'])
+
+    def __repr__(self):
+        return "FalsePositionMethod(a:{},b:{},tolerance:{})".format(self.a,self.b,self.tolerance)
+
+    def calc(self):
+        a,b=self.a,self.b  
+        for i in range(self.steps):    
+            x=(a*self.function(b)-b*self.function(a))/(self.function(b)-self.function(a))
+            self.table.add_row([i+1,a,b,round(x,4),round(self.function(x),4)])
+            if self.function(x)*self.function(a)<0:
+                b=round(x,4)
+            else:
+                a=round(x,4)
+            if round(abs(self.function(x)),5)<self.tolerance:
+                break
+        
+        return self.table
