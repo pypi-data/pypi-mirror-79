34f39f805458fbeb2b46a81e4e65296929e78a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:40 +0000

    Add package rulecheck/0.6.1/rulecheck-0.6.1-py3-none-any.whl

---
+++ packages/rulecheck/rulecheck-0.6.1-py3-none-any.whl/rulecheck/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+__version__ = '0.6.1'
+
+if sys.version_info.major < 3:
+    sys.exit('Python < 3.6 is unsupported.')
+if sys.version_info.minor < 6:
+    sys.exit('Python < 3.6 is unsupported.')
