c96f7725357d06ac29adbc6d9237bf3c732e9557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:13 +0000

    Add package derobertis-cv/0.5.11/derobertis-cv-0.5.11.tar.gz

---
+++ packages/derobertis-cv/derobertis-cv-0.5.11.tar.gz/derobertis-cv-0.5.11/derobertis_cv/pldata/constants/authors.py
@@ -0,0 +1,11 @@
+
+AT_WARRINGTON = '@warrington.ufl.edu'
+ASSISTANT_PROF = 'Assistant Professor'
+
+# Keys to reference authors
+ANDY = 'andy'
+NIMAL = 'nimal'
+NITISH = 'nitish'
+SUGATA = 'sugata'
+CORBIN = 'corbin'
+JIMMY = 'jimmy'
