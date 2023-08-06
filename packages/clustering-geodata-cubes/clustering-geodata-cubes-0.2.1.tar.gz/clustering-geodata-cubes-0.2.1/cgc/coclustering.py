d5fe30b345fa2516cc781bfbad4b5ec8e4da38bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:38 +0000

    Add package clustering-geodata-cubes/0.2.1/clustering-geodata-cubes-0.2.1.tar.gz

---
+++ packages/clustering-geodata-cubes/clustering-geodata-cubes-0.2.1.tar.gz/clustering-geodata-cubes-0.2.1/cgc/coclustering.py
@@ -0,0 +1,204 @@
+import concurrent.futures
+import dask.distributed
+import logging
+
+from concurrent.futures import ThreadPoolExecutor
+from dask.distributed import Client
+
+from . import coclustering_dask
+from . import coclustering_numpy
+
+from .results import Results
+
+logger = logging.getLogger(__name__)
+
+
+class CoclusteringResults(Results):
+    """
+    Contains results and metadata of a co-clustering calculation
+    """
+    def reset(self):
+        self.row_clusters = None
+        self.col_clusters = None
+        self.error = None
+        self.nruns_completed = 0
+        self.nruns_converged = 0
+
+
+class Coclustering(object):
+    """
+    Perform the co-clustering analysis of a 2D array
+    """
+    def __init__(self,
+                 Z,
+                 nclusters_row,
+                 nclusters_col,
+                 conv_threshold=1.e-5,
+                 max_iterations=1,
+                 nruns=1,
+                 epsilon=1.e-8,
+                 output_filename='',
+                 row_clusters_init=None,
+                 col_clusters_init=None):
+        """
+        Initialize the object
+
+        :param Z: m x n data matrix
+        :param nclusters_row: number of row clusters
+        :param nclusters_col: number of column clusters
+        :param conv_threshold: convergence threshold for the objective function
+        :param max_iterations: maximum number of iterations
+        :param nruns: number of differently-initialized runs
+        :param epsilon: numerical parameter, avoids zero arguments in log
+        :param output_filename: name of the file where to write the clusters
+        :param row_clusters_init: initial row clusters
+        :param col_clusters_init: initial column clusters
+        """
+        # Input parameters -----------------
+        self.nclusters_row = nclusters_row
+        self.nclusters_col = nclusters_col
+        self.conv_threshold = conv_threshold
+        self.max_iterations = max_iterations
+        self.nruns = nruns
+        self.epsilon = epsilon
+        self.output_filename = output_filename
+        self.row_clusters_init = row_clusters_init
+        self.col_clusters_init = col_clusters_init
+        # Input parameters end -------------
+
+        # store input parameters in results object
+        self.results = CoclusteringResults(**self.__dict__)
+
+        assert Z.ndim == 2, 'Incorrect dimensionality for Z matrix'
+        self.Z = Z
+
+        if row_clusters_init is not None and col_clusters_init is not None:
+            assert nruns == 1, 'Only nruns = 1 for given initial clusters'
+            assert Z.shape == (len(row_clusters_init), len(col_clusters_init))
+
+        self.client = None
+
+    def run_with_dask(self, client=None, low_memory=False):
+        """
+        Run the co-clustering with Dask
+
+        :param client: Dask client
+        :param low_memory: if true, use a memory-conservative algorithm
+        :return: co-clustering results
+        """
+        self.client = client if client is not None else Client()
+
+        if low_memory:
+            self._dask_runs_memory()
+        else:
+            self._dask_runs_performance()
+
+        self.results.write(filename=self.output_filename)
+        return self.results
+
+    def run_with_threads(self,
+                         nthreads=1,
+                         low_memory=False,
+                         numba_jit=False):
+        """
+        Run the co-clustering using an algorithm based on numpy + threading
+        (only suitable for local runs)
+
+        :param nthreads: number of threads
+        :param low_memory: if true, use a memory-conservative algorithm
+        :param numba_jit: if true, and low_memory is true, then use Numba
+                          just-in-time compilation to improve performance
+        :return: co-clustering results
+        """
+        with ThreadPoolExecutor(max_workers=nthreads) as executor:
+            futures = [
+                executor.submit(coclustering_numpy.coclustering,
+                                self.Z,
+                                self.nclusters_row,
+                                self.nclusters_col,
+                                self.conv_threshold,
+                                self.max_iterations,
+                                self.epsilon,
+                                low_memory,
+                                numba_jit,
+                                row_clusters_init=self.row_clusters_init,
+                                col_clusters_init=self.col_clusters_init)
+                for _ in range(self.nruns)
+            ]
+            for future in concurrent.futures.as_completed(futures):
+                logger.info(f'Retrieving run {self.results.nruns_completed}')
+                converged, niters, row, col, e = future.result()
+                logger.info(f'Error = {e}')
+                if converged:
+                    logger.info(f'Run converged in {niters} iterations')
+                    self.results.nruns_converged += 1
+                else:
+                    logger.warning(f'Run not converged in {niters} iterations')
+                if self.results.error is None or e < self.results.error:
+                    self.results.row_clusters = row
+                    self.results.col_clusters = col
+                    self.results.error = e
+                self.results.nruns_completed += 1
+        self.results.write(filename=self.output_filename)
+        return self.results
+
+    def _dask_runs_memory(self):
+        """ Memory efficient Dask implementation: sequential runs """
+        for r in range(self.nruns):
+            logger.info(f'Run {self.results.nruns_completed}')
+            converged, niters, row, col, e = coclustering_dask.coclustering(
+                self.Z,
+                self.nclusters_row,
+                self.nclusters_col,
+                self.conv_threshold,
+                self.max_iterations,
+                self.epsilon,
+                row_clusters_init=self.row_clusters_init,
+                col_clusters_init=self.col_clusters_init
+            )
+            logger.info(f'Error = {e}')
+            if converged:
+                logger.info(f'Run converged in {niters} iterations')
+                self.results.nruns_converged += 1
+            else:
+                logger.warning(f'Run not converged in {niters} iterations')
+            if self.results.error is None or e < self.results.error:
+                self.results.row_clusters = row.compute()
+                self.results.col_clusters = col.compute()
+                self.results.error = e
+            self.results.nruns_completed += 1
+
+    def _dask_runs_performance(self):
+        """
+        Faster but memory-intensive Dask implementation: all runs are
+        simultaneosly submitted to the scheduler
+        """
+        Z = self.client.scatter(self.Z)
+        futures = [self.client.submit(
+                       coclustering_dask.coclustering,
+                       Z,
+                       self.nclusters_row,
+                       self.nclusters_col,
+                       self.conv_threshold,
+                       self.max_iterations,
+                       self.epsilon,
+                       row_clusters_init=self.row_clusters_init,
+                       col_clusters_init=self.col_clusters_init,
+                       run_on_worker=True,
+                       pure=False)
+                   for _ in range(self.nruns)]
+        for future, result in dask.distributed.as_completed(
+                futures, with_results=True, raise_errors=False):
+            logger.info(f'Retrieving run {self.results.nruns_completed} ..')
+            converged, niters, row, col, e = result
+            logger.info(f'Error = {e}')
+            if converged:
+                logger.info(f'Run converged in {niters} iterations')
+                self.results.nruns_converged += 1
+            else:
+                logger.warning(f'Run not converged in {niters} iterations')
+            if self.results.error is None or e < self.results.error:
+                self.results.row_clusters = row.compute()
+                self.results.col_clusters = col.compute()
+                self.results.error = e
+            self.results.nruns_completed += 1
---
+++ packages/clustering-geodata-cubes/clustering-geodata-cubes-0.2.1.tar.gz/clustering-geodata-cubes-0.2.1/cgc/coclustering_dask.py
@@ -0,0 +1,120 @@
+import logging
+
+import numpy as np
+
+import dask.array as da
+from dask.distributed import get_client, rejoin, secede
+
+logger = logging.getLogger(__name__)
+
+
+def _distance(Z, Y, epsilon):
+    """ Distance function """
+    Y = Y + epsilon
+    # The first term below is equal to: da.dot(da.ones(m, n), Y)
+    # with Z.shape = (m, n) and Y.shape = (n, k)
+    d = (Y.sum(axis=0, keepdims=True).repeat(Z.shape[0], axis=0)
+         - da.dot(Z, da.log(Y)))
+    return d
+
+
+def _initialize_clusters(n_el, n_clusters):
+    """ Initialize cluster array """
+    cluster_idx = da.mod(da.arange(n_el), n_clusters)
+    return da.random.permutation(cluster_idx)
+
+
+def _setup_cluster_matrix(n_clusters, cluster_idx):
+    """ Set cluster occupation matrix """
+    return da.eye(n_clusters, dtype=np.bool)[cluster_idx]
+
+
+def coclustering(Z, nclusters_row, nclusters_col, errobj, niters, epsilon,
+                 col_clusters_init=None, row_clusters_init=None,
+                 run_on_worker=False):
+    """
+    Run the co-clustering, Dask implementation
+
+    :param Z: m x n data matrix
+    :param nclusters_row: num row clusters
+    :param nclusters_col: number of column clusters
+    :param errobj: convergence threshold for the objective function
+    :param niters: maximum number of iterations
+    :param epsilon: numerical parameter, avoids zero arguments in log
+    :param row_clusters_init: initial row cluster assignment
+    :param col_clusters_init: initial column cluster assignment
+    :param run_on_worker: whether the function is submitted to a Dask worker
+    :return: has converged, number of iterations performed. final row and
+    column clustering, error value
+    """
+    client = get_client()
+
+    [m, n] = Z.shape
+
+    row_clusters = da.array(row_clusters_init) \
+        if row_clusters_init is not None \
+        else _initialize_clusters(m, nclusters_row)
+    col_clusters = da.array(col_clusters_init) \
+        if col_clusters_init is not None \
+        else _initialize_clusters(n, nclusters_col)
+    R = _setup_cluster_matrix(nclusters_row, row_clusters)
+    C = _setup_cluster_matrix(nclusters_col, col_clusters)
+
+    e, old_e = 2 * errobj, 0
+    s = 0
+    converged = False
+
+    Gavg = Z.mean()
+
+    while (not converged) & (s < niters):
+        logger.debug(f'Iteration # {s} ..')
+        # Calculate cluster based averages
+        # nel_clusters is a matrix with the number of elements per co-cluster
+        # originally computed as:  da.dot(da.dot(R.T, da.ones((m, n))), C)
+        nel_row_clusters = da.bincount(row_clusters, minlength=nclusters_row)
+        nel_col_clusters = da.bincount(col_clusters, minlength=nclusters_col)
+        logger.debug('num of populated clusters: row {}, col {}'.format(
+                        da.sum(nel_row_clusters > 0).compute(),
+                        da.sum(nel_col_clusters > 0).compute()))
+        nel_clusters = da.outer(nel_row_clusters, nel_col_clusters)
+        CoCavg = (da.dot(da.dot(R.T, Z), C) + Gavg * epsilon) / \
+                 (nel_clusters + epsilon)
+
+        # Calculate distance based on row approximation
+        d_row = _distance(Z, da.dot(C, CoCavg.T), epsilon)
+        # Assign to best row cluster
+        row_clusters = da.argmin(d_row, axis=1)
+        R = _setup_cluster_matrix(nclusters_row, row_clusters)
+
+        # Calculate distance based on column approximation
+        d_col = _distance(Z.T, da.dot(R, CoCavg), epsilon)
+        # Assign to best column cluster
+        col_clusters = da.argmin(d_col, axis=1)
+        C = _setup_cluster_matrix(nclusters_col, col_clusters)
+
+        # Error value (actually just the column components really)
+        old_e = e
+        minvals = da.min(d_col, axis=1)
+        # power 1 divergence, power 2 euclidean
+        e = da.sum(da.power(minvals, 1))
+        row_clusters, R, col_clusters, C, e = client.persist([row_clusters, R,
+                                                              col_clusters, C,
+                                                              e])
+        if run_on_worker:
+            # this is workaround for e.compute() for a function that runs
+            # on a worker with multiple threads
+            # https://github.com/dask/distributed/issues/3827
+            e = client.compute(e)
+            secede()
+            e = e.result()
+            rejoin()
+        else:
+            e = e.compute()
+        logger.debug(f'Error = {e:+.15e}, dE = {e - old_e:+.15e}')
+        converged = abs(e - old_e) < errobj
+        s = s + 1
+    if converged:
+        logger.debug(f'Coclustering converged in {s} iterations')
+    else:
+        logger.debug(f'Coclustering not converged in {s} iterations')
+    return converged, s, row_clusters, col_clusters, e
