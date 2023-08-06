0416530018ad3dfc8e151a353e5e5e66493c91fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:20 +0000

    Add package datasette-atom/0.8/datasette-atom-0.8.tar.gz

---
+++ packages/datasette-atom/datasette-atom-0.8.tar.gz/datasette-atom-0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+
+VERSION = "0.8"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="datasette-atom",
+    description="Datasette plugin that adds a .atom output format",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Simon Willison",
+    url="https://github.com/simonw/datasette-atom",
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["datasette_atom"],
+    entry_points={"datasette": ["atom = datasette_atom"]},
+    install_requires=["datasette>=0.49", "feedgen", "bleach"],
+    extras_require={"test": ["pytest", "pytest-asyncio", "httpx"]},
+    tests_require=["datasette-atom[test]"],
+)
