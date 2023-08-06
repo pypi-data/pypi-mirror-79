60bdb8a68353a2a11cb1decbbc76b466392a551e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:55 +0000

    Add package django-tabular-permissions/2.8/django-tabular-permissions-2.8.tar.gz

---
+++ packages/django-tabular-permissions/django-tabular-permissions-2.8.tar.gz/django-tabular-permissions-2.8/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-tabular-permissions',
+    version='2.8',
+    packages=['tabular_permissions'],
+    include_package_data=True,
+    license='BSD License',
+    description='Display django permissions in a tabular format that is user friendly, and highly customisable',
+    long_description=README,
+    url='https://github.com/RamezIssac/django-tabular-permissions',
+    author='Ramez Ashraf',
+    author_email='ramez@rasystems.io',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
