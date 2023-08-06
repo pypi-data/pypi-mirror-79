391f5b968017257cd903b87b8e7e96f5b7d55b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:30 +0000

    Add package semanticscholar/0.1.6/semanticscholar-0.1.6.tar.gz

---
+++ packages/semanticscholar/semanticscholar-0.1.6.tar.gz/semanticscholar-0.1.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='semanticscholar',
+    version='0.1.6',
+    description='A python library that aims to retrieve data from Semantic Scholar API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://danielnsilva.com/semanticscholar',
+    author='Daniel Silva',
+    author_email='danielnsilva@gmail.com',
+    license='MIT',
+    packages=['semanticscholar'],
+    install_requires=['requests', 'tenacity'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    zip_safe=False
+)
