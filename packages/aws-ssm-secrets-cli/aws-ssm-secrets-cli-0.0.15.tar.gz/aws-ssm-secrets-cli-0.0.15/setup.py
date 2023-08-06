f04c4a43d6cdab6c4939f12402afb4a8292e9cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:52 +0000

    Add package aws-ssm-secrets-cli/0.0.15/aws-ssm-secrets-cli-0.0.15.tar.gz

---
+++ packages/aws-ssm-secrets-cli/aws-ssm-secrets-cli-0.0.15.tar.gz/aws-ssm-secrets-cli-0.0.15/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+install_requirements = [
+    "boto3",
+    "click"
+]
+
+setup(
+    name="aws-ssm-secrets-cli",
+    version='0.0.15',
+    license='MIT',
+    description = 'AWS Secret CLI for manage SSM SecureString and SecretsManager',
+    long_description = open("README.md", "r").read(),
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+    long_description_content_type='text/markdown',
+    author = 'Lucas Vieira',
+    author_email = 'lucas.vieira94@outlook.com',
+    url = 'https://github.com/lucasvieirasilva/aws-ssm-secrets-cli',
+    keywords = ['AWS', 'SECRETS', 'SSM', 'MANAGER'],
+    install_requires=install_requirements,
+    entry_points={
+        "console_scripts": [
+            'aws-secrets=aws_secrets.cli.cli:cli'
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ]
+)
