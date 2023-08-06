26f95d387ec401403a901bcfcd7739655ec2bd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:46 +0000

    Add package take-satisfaction/0.3.14/take_satisfaction-0.3.14-py3-none-any.whl

---
+++ packages/take-satisfaction/take_satisfaction-0.3.14-py3-none-any.whl/take_satisfaction/utils/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'Gabriel Salgado, Juliana Guamá, Milo Utsch e Rogers Damas'
+__version__ = '0.1.3'
+
+from .load_params import load_params, load_query
+from .sparksql_ops import *
+from .formatting import *
+from .iterable_ops import *
+
+from .mathematical_utils import weighted_mean, scale
