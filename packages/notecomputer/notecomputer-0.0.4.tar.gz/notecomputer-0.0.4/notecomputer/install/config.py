cb301aa88954bb0133097fd5081bd8ec63c6bc60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:02 +0000

    Add package notecomputer/0.0.4/notecomputer-0.0.4.tar.gz

---
+++ packages/notecomputer/notecomputer-0.0.4.tar.gz/notecomputer-0.0.4/notecomputer/install/config.py
@@ -0,0 +1,12 @@
+from .base import run_cmd
+
+
+def config_all():
+    config_ssh()
+    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/' '~/'")
+
+
+def config_ssh():
+    # run_cmd("cp -r '/root/.ssh' '/content/gdrive/My Drive/core/configs/ssh'")
+    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/ssh/rsa' '~/.ssh/'")
+    run_cmd("cp -rf '/content/gdrive/My Drive/core/configs/ssh/rsa' '~/.ssh/'")
