782ea74385434a43a4622a113a15df56cc17e5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:33 +0000

    Add package adafruit-circuitpython-bno08x-rvc/1.0.0/adafruit-circuitpython-bno08x-rvc-1.0.0.tar.gz

---
+++ packages/adafruit-circuitpython-bno08x-rvc/adafruit-circuitpython-bno08x-rvc-1.0.0.tar.gz/adafruit-circuitpython-bno08x-rvc-1.0.0/examples/bno08x_rvc_simpletest.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2020 Bryan Siepert, written for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
+import time
+import board
+import busio
+from adafruit_bno08x_rvc import BNO08x_RVC
+
+uart = busio.UART(board.TX, board.RX, baudrate=115200, receiver_buffer_size=2048)
+rvc = BNO08x_RVC(uart)
+while True:
+    roll, pitch, yaw, x_accel, y_accel, z_accel = rvc.heading
+    print("Roll: %2.2f Pitch: %2.2f Yaw: %2.2f Degrees" % (roll, pitch, yaw))
+    print("Acceleration X: %2.2f Y: %2.2f Z: %2.2f m/s^2" % (x_accel, y_accel, z_accel))
+    print("")
+    time.sleep(0.1)
