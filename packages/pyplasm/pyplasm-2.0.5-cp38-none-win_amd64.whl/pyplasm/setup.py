bd70e1643217246066e43d8f2f3ea6b5e3026fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:55 +0000

    Add package pyplasm/2.0.5/pyplasm-2.0.5-cp38-none-win_amd64.whl

---
+++ packages/pyplasm/pyplasm-2.0.5-cp38-none-win_amd64.whl/pyplasm/setup.py
@@ -0,0 +1,36 @@
+import os, sys, setuptools
+import shutil
+
+#increase this number for PIP/conda
+VERSION="2.0.5"
+this_dir="."
+
+shutil.rmtree('./build', ignore_errors=True)
+shutil.rmtree('./dist', ignore_errors=True)
+shutil.rmtree('./pyplasm.egg-info', ignore_errors=True)
+shutil.rmtree('./__pycache__', ignore_errors=True)
+shutil.rmtree('./examples/__pycache__', ignore_errors=True)
+
+# findFilesInCurrentDirectory
+def findFilesInCurrentDirectory():
+	ret=[]
+	for dirpath, __dirnames__, filenames in os.walk("."):
+	  for filename in filenames:
+	    file= os.path.abspath(os.path.join(dirpath, filename))
+	    ret.append(file)
+	return ret
+
+setuptools.setup(
+  name = "pyplasm",
+  description = "Plasm is a design language for geometric and solid parametric design, developed by the CAD Group at the Universities La Sapienza and Roma Tre in Italy",
+  version=VERSION,
+  url="https://github.com/plasm-language/pyplasm",
+  author="Giorgio Scorzelli",
+  author_email="scorzell@dia.uniroma3.it",
+  packages=["pyplasm"],
+  package_dir={"pyplasm":'.'},
+  package_data={"pyplasm": findFilesInCurrentDirectory()},
+  platforms=['Linux', 'OS-X', 'Windows'],
+  license = "GPL",
+  install_requires=['numpy'],
+)
