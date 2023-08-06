c94f47ffa38cd154ddf81385ead2642ebb601eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:02 +0000

    Add package t5-podcast-summariser/0.0.5/t5_podcast_summariser-0.0.5.tar.gz

---
+++ packages/t5-podcast-summariser/t5_podcast_summariser-0.0.5.tar.gz/t5_podcast_summariser-0.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(name = 't5_podcast_summariser', 
+    version = '0.0.5', 
+    description="Podcast Transcript Summarisation", 
+    packages = ['t5_podcast_summariser'],
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    author = 'Paul Owoicho',
+    author_email = 'paulowoicho@gmail.com',
+    url='https://github.com/paulowoicho/msc_project',
+    zip_safe=False)
