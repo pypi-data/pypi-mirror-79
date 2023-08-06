b9fce1c1927d307ff7f0b650cc09880d74579438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:09 +0000

    Add package humilis-batch/0.1.4/humilis-batch-0.1.4.tar.gz

---
+++ packages/humilis-batch/humilis-batch-0.1.4.tar.gz/humilis-batch-0.1.4/setup.py
@@ -0,0 +1,44 @@
+"""Setuptools entry point."""
+
+import os
+import codecs
+from setuptools import setup, find_packages
+
+from humilis_batch import __version__, __author__
+
+dirname = os.path.dirname(__file__)
+description = "Humilis plug-in to deploy AWS Batch resources"
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError, RuntimeError):
+    if os.path.isfile("README.md"):
+        long_description = codecs.open(os.path.join(dirname, "README.md"),
+                                       encoding="utf-8").read()
+    else:
+        long_description = description
+
+setup(
+    name="humilis-batch",
+    include_package_data=True,
+    package_data={
+        "": ["*.j2", "*.yaml"]},
+    packages=find_packages(include=['humilis_batch',
+                                    'humilis_batch.*']),
+    version=__version__,
+    author=__author__,
+    author_email="german@findhotel.net",
+    url="https://github.com/humilis/humilis-batch",
+    license="MIT",
+    description=description,
+    long_description=long_description,
+    install_requires=[
+        "humilis>=1.5.10"],
+    classifiers=[
+        "Programming Language :: Python :: 3"],
+    zip_safe=False,
+    entry_points={
+        "humilis.layers": [
+            "batch=humilis_batch.plugin:get_layer_path"]}
+)
