875fedc01741c7b92f0e8e812d2de76b917c0120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:47 +0000

    Add package FetchCord/2.6.2/FetchCord-2.6.2-py3-none-any.whl

---
+++ packages/FetchCord/FetchCord-2.6.2-py3-none-any.whl/fetch_cord/out.py
@@ -0,0 +1,335 @@
+from fetch_cord.bash import exec_bash, BashError
+import sys
+import os
+import re
+import subprocess
+import fetch_cord.__init__ as __init__
+from fetch_cord.args import parse_args
+from fetch_cord.update import update
+from fetch_cord.debugger import run_debug
+from fetch_cord.checks import get_amdgpurender, check_primeoffload, get_gpuinfo, get_gpu_vendors, get_dewm, get_deid,\
+    get_wmid, set_laptop, check_batteryline, check_theme, check_fontline, check_termid, check_res, get_win_gpu,\
+    get_cpumodel, get_cpuinfo, check_memline, check_diskline, check_laptop, get_long_os
+
+
+args = parse_args()
+
+if args.update:
+    update()
+
+if args.version:
+    print("FetchCord version:", __init__.VERSION)
+    sys.exit(0)
+
+
+if args.time:
+    if int(args.time) < 15:
+        print("ERROR: Invalid time set, must be > 15 seconds, cannot continue.")
+        sys.exit(1)
+    else:
+        print("setting custom time %s seconds" % args.time)
+
+try:
+    if args.help:
+        sys.exit(0)
+except AttributeError:
+    pass
+
+loop = 0
+
+
+def XDG_Symlink(home):
+    try:
+        print("Symlinking XDG_RUNTIME_DIR path for Flatpak Discord.")
+        exec_bash(
+            "cd %s/.var && ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-0 " % home)
+    except BashError as e:
+        print("Could not symlink XDG_RUNTIME_DIR Error: %s" % str(e))
+        return
+
+
+def check_neofetch_scoop():
+    return subprocess.run(["neofetch", "--stdout", "--config none" if args.noconfig else ""], encoding="utf-8", stdout=subprocess.PIPE, shell=(os.name=="nt")).stdout
+
+def check_neofetchwin():
+    return subprocess.run(["neofetch", "--noart"], check=True, encoding='utf-8', stdout=subprocess.PIPE).stdout
+
+
+def neofetch(loop):
+    global cpuline, nvidiagpuline, amdgpuline, termline, fontline, wmline, intelgpuline, radgpuline, \
+        vmwaregpuline, virtiogpuline, shell_line, kernelline, sysosline, moboline, \
+        deline, batteryline, resline, themeline, hostline, memline, packagesline, diskline,\
+        cirrusgpuline, baseinfo, neofetchwin
+    neofetchwin = False
+    if os.name == "nt":
+        try:
+            neofetchwin = check_neofetchwin()
+        except (FileNotFoundError, subprocess.CalledProcessError) as e:
+            pass
+
+        if not neofetchwin:
+            try:
+                baseinfo = check_neofetch_scoop()
+            except (FileNotFoundError, subprocess.CalledProcessError) as e:
+                print(
+                    "ERROR: Neofetch not found, please install it or check installation and that neofetch is in PATH.")
+                sys.exit(1)
+
+    else:
+        home = os.getenv('HOME')
+        flatpak_discord_path = os.path.isdir(
+            "%s/.var/app/com.discordapp.Discord" % home)
+        package_path = os.path.isfile("/usr/bin/discord")
+        manual_install_path = os.path.isdir("/opt/Discord")
+        if loop == 0 and flatpak_discord_path and not package_path and not manual_install_path:
+            XDG_Symlink(home)
+        try:
+            baseinfo = check_neofetch_scoop()
+        except (FileNotFoundError, subprocess.CalledProcessError) as e:
+            print("ERROR: Neofetch not found, please install it or check installation and that neofetch is in PATH.")
+            sys.exit(1)
+
+    # make lists
+    cpu = "CPU:"
+    cpuline = []
+    nvidiagpu = "GPU: NVIDIA"
+    nvidiagpuline = []
+    amdgpu = "GPU: AMD"
+    amdgpuline = []
+    intelgpu = "GPU: Intel"
+    intelgpuline = []
+    vmwaregpu = "GPU: VMware"
+    vmwaregpuline = []
+    virtiogpu = "GPU: 00:02.0 Red"
+    virtiogpuline = []
+    cirrusgpu = "GPU: 00:02.0 Cirrus"
+    cirrusgpuline = []
+    term = "Terminal:"
+    termline = []
+    font = "Font:"
+    fontline = []
+    wm = "WM:"
+    wmline = []
+    disk = "Disk"
+    diskline = []
+    de = "DE:"
+    deline = []
+    shell = "Shell:"
+    shell_line = []
+    kernel = "Kernel:"
+    kernelline = []
+    sysos = "OS:"
+    sysosline = []
+    mem = "Memory:"
+    memline = []
+    mobo = "Motherboard:"
+    moboline = []
+    packages = "Packages:"
+    packagesline = []
+    host = "Host:"
+    hostline = []
+    res = "Resolution:"
+    resline = []
+    theme = "Theme:"
+    themeline = []
+    battery = "Battery"
+    batteryline = []
+
+    if neofetchwin:
+        nvidiagpu = "NVIDIA"
+        radgpu = "Radeon"
+        intelgpu = "Intel"
+        radgpuline = []
+        filepath = "tmp.txt"
+
+        with open(filepath, 'w') as f:
+            print(neofetchwin, file=f)
+
+        with open(filepath, 'rt') as f:
+            lines = f.readlines()
+            for i in range(len(lines)):
+                line = lines[i]
+                if line.find(cpu) != -1:
+                    cpuline.append(line.rstrip('\n'))
+                if line.find(nvidiagpu) != -1:
+                    nvidiagpuline.append(
+                        line[line.find(nvidiagpu):].rstrip('\n'))
+                if line.find(amdgpu) != -1:
+                    amdgpuline.append(line.rstrip('\n'))
+                if line.find(intelgpu) != -1 and line.find(cpu) == -1:
+                    intelgpuline.append(
+                        line[line.find(intelgpu):].rstrip('\n'))
+                if line.find(vmwaregpu) != -1:
+                    vmwaregpuline.append(line.rstrip('\n'))
+                if line.find(sysos) != -1:
+                    sysosline.append(line.rstrip('\n'))
+                if line.find(mem) != -1:
+                    memline.append(line.rstrip('\n'))
+                if line.find(mobo) != -1:
+                    moboline.append(line.rstrip('\n'))
+                if line.find(radgpu) != -1:
+                    radgpuline.append(line[line.find(radgpu):].rstrip('\n'))
+                if line.find(disk) != -1:
+                    diskline.append(line[line.find(disk):].rstrip('\n'))
+                    i += 1
+                    while(i < len(lines)):
+                        line = lines[i]
+                        if line != "\n":
+                            diskline.append(
+                                "Disk: "+line.lstrip().rstrip('\n'))
+                        i += 1
+                    break
+
+    elif not neofetchwin:
+        filepath = "tmp.txt"
+        with open(filepath, 'w') as f:
+            print(baseinfo, file=f)
+        with open(filepath, "rt") as f:
+            for line in f:
+                if line.find(cpu) != -1:
+                    cpuline.append(line.rstrip('\n'))
+                if line.find(nvidiagpu) != -1:
+                    nvidiagpuline.append(line.rstrip('\n'))
+                if line.find(amdgpu) != -1:
+                    amdgpuline.append(line.rstrip('\n'))
+                if line.find(intelgpu) != -1:
+                    intelgpuline.append(line.rstrip('\n'))
+                if line.find(vmwaregpu) != -1:
+                    vmwaregpuline.append(line.rstrip('\n'))
+                if line.find(virtiogpu) != -1:
+                    virtiogpuline.append(line.rstrip('\n'))
+                if line.find(term) != -1:
+                    termline.append(line.rstrip('\n'))
+                if line.find(font) != -1:
+                    fontline.append(line.rstrip('\n'))
+                if line.find(de) != -1:
+                    deline.append(line.rstrip('\n'))
+                if line.find(wm) != -1:
+                    wmline.append(line.rstrip('\n'))
+                if line.find(shell) != -1:
+                    shell_line.append(line.rstrip('\n'))
+                if line.find(packages) != -1:
+                    packagesline.append(line.rstrip('\n'))
+                if line.find(kernel) != -1:
+                    kernelline.append(line.rstrip('\n'))
+                if line.find(sysos) != -1:
+                    sysosline.append(line.rstrip('\n'))
+                if line.find(host) != -1:
+                    hostline.append(line.rstrip('\n'))
+                if line.find(res) != -1:
+                    resline.append(line.rstrip('\n'))
+                if line.find(mem) != -1 and args.memtype == "gb":
+                    memgb = line.rstrip('\n')
+                    memgb = memgb.split()
+                    used = float(memgb[1].replace("MiB", ""))
+                    total = float(memgb[3].replace("MiB", ""))
+                    memline.append(
+                        "Memory: " + str(round(used / 1024, 2)) + "GiB / " + str(
+                            round(total / 1024, 2)) + "GiB")
+                elif line.find(mem) != -1:
+                    memline.append(line.rstrip('\n'))
+                if line.find(theme) != -1:
+                    themeline.append(line.rstrip('\n'))
+                if line.find(disk) != -1:
+                    diskline.append(line.rstrip('\n'))
+                if line.find(battery) != -1:
+                    batteryline.append(line.rstrip('\n'))
+                if line.find(cirrusgpu) != -1:
+                    cirrusgpuline.append(line.rstrip('\n'))
+
+    try:
+        if os.path.isfile(filepath):
+            os.remove(filepath)
+    except FileNotFoundError:
+        pass
+
+    return (memline, packagesline, diskline, batteryline, cpuline)
+
+
+baseinfo = False
+neofetchwin = False
+neofetch(loop)
+
+sysosid = sysosline[0].split()[1]
+
+# I don't know if macOS has the same path linux does to check power_supply
+if sysosid.lower() != "macos" and os.name != "nt":
+    laptop = check_laptop()
+else:
+    laptop = False
+
+amdgpurenderlist = []
+gpuvendor = ""
+
+
+if amdgpuline and os.name != "nt":
+    amdgpurenderlist = get_amdgpurender(amdgpuline, intelgpuline, laptop)
+if sysosid.lower() not in ["windows", "macos"]:
+    primeoffload = check_primeoffload(laptop, loop)
+else:
+    primeoffload = False
+
+
+if baseinfo:
+    gpuinfo = get_gpuinfo(cirrusgpuline, vmwaregpuline, virtiogpuline, amdgpuline, nvidiagpuline,
+                          intelgpuline, primeoffload, amdgpurenderlist, sysosid, loop)
+    gpuvendor = get_gpu_vendors(cirrusgpuline, vmwaregpuline, virtiogpuline, amdgpuline,
+                                nvidiagpuline, intelgpuline, primeoffload, sysosid)
+
+    if gpuinfo == "":
+        gpuinfo = "GPU: N/A"
+
+    if not hostline:
+        hostline = ["Host: N/A"]
+
+    if not kernelline:
+        kernelline = ["Kernel: N/A"]
+
+    if not shell_line:
+        shell_line = ["Shell: N/A"]
+
+    if not packagesline:
+        packagesline = ["Packages: N/A"]
+
+    dewmid = get_dewm(deline, wmline)
+    deid = get_deid(deline)
+    wmid = get_wmid(wmline)
+
+    lapordesk = set_laptop(laptop, sysosid)
+    batteryline = check_batteryline(batteryline, hostline)
+
+    themeline = check_theme(themeline)
+    fontline = check_fontline(fontline)
+
+    termid = check_termid(termline)
+
+    if not termline:
+        termline = ["Terminal: N/A"]
+
+    shellid = shell_line[0].split()[1]
+
+    resline = check_res(resline)
+
+elif neofetchwin:
+
+    if not moboline:
+        moboline = "Motherboard: N/A"
+    gpuinfo, gpuvendor = get_win_gpu(nvidiagpuline, radgpuline, intelgpuline)
+    deid = False
+    wmid = False
+    termid = False
+    hostline = False
+
+
+if sysosid.lower() in ['windows', 'linux', 'opensuse']:
+    sysosid = get_long_os(sysosline)
+
+cpuvendor = cpuline[0].split()[1].replace("Intel(R)", "Intel")
+cpumodel = get_cpumodel(cpuline, cpuvendor, baseinfo)
+cpuinfo = get_cpuinfo(cpuline, baseinfo)
+memline = check_memline(memline)
+diskline = check_diskline(diskline, cpuinfo)
+
+
+if args.debug:
+    run_debug()
---
+++ packages/FetchCord/FetchCord-2.6.2-py3-none-any.whl/fetch_cord/run_rpc.py
@@ -0,0 +1,364 @@
+# Import cool new rpc module that gives us more control and gets rid of headaches :)
+from pypresence import Presence, exceptions
+import time
+import sys
+import os
+import psutil
+# import info about system
+from fetch_cord.args import parse_args
+from fetch_cord.bash import BashError, exec_bash
+from fetch_cord.testing import gpuid, cpuappid, appid
+from fetch_cord.debugger import run_rpc_debug
+from fetch_cord.out import gpuinfo, sysosline, sysosid, memline, cpuinfo, \
+        neofetch, diskline, neofetchwin, baseinfo, hostline
+if baseinfo:
+    from fetch_cord.testing import desktopid, termappid, hostappid, shellid
+    from fetch_cord.out import packagesline, kernelline, shell_line, fontline, \
+        termline, lapordesk, resline, themeline, batteryline, \
+        gpuinfo, dewmid
+elif neofetchwin:
+    from fetch_cord.out import moboline, check_neofetchwin
+    from fetch_cord.testing import moboid
+
+
+uptime = psutil.boot_time()
+args = parse_args()
+
+
+def main():
+    if baseinfo and not hostline and args.nodistro and args.noshell and args.nohardware:
+        print("ERROR: no hostline is available!")
+        sys.exit(1)
+    # printing info with debug switch
+    if args.debug:
+        if baseinfo:
+            run_rpc_debug(uptime=uptime, appid=appid, cpuappid=cpuappid, termappid=termappid, packagesline=packagesline, hostline=hostline, hostappid=hostappid)
+        else:
+            run_rpc_debug(uptime=uptime, appid=appid, cpuappid=cpuappid)
+    loop = 0
+    if neofetchwin:
+        wandowz(loop)
+    else:
+        loonix(loop)
+
+def first_connect():
+    try:
+        client_id = appid
+        RPC = Presence(client_id)
+        RPC.connect()
+        print("RPC Connection Successful.")
+    except ConnectionRefusedError:
+        rpc_tryconnect(RPC)
+
+print("Connecting")
+try:
+    time.sleep(5)
+except KeyboardInterrupt:
+    print("Stopping connection.")
+    sys.exit(0)
+# discord uses unix time to interpret time for rich presnse, this is uptime in unix time
+start_time = float(uptime)
+
+
+def rpc_tryconnect(RPC):
+    while True:
+        try:
+            RPC.connect()
+            break
+        except ConnectionRefusedError:
+            print("RPC connection refused (is Discord open?); trying again in 30 seconds")
+            time.sleep(30)
+
+
+
+def rpc_tryclear(RPC):
+    try:
+        RPC.clear(pid=os.getpid())
+    except exceptions.InvalidID:
+        pass
+
+
+def rpc_tryupdate(RPC, state, details, large_image, large_text, small_image, small_text, start):
+    try:
+        RPC.update(state=state, details=details, large_image=large_image,
+                    large_text=large_text, small_image=small_image, small_text=small_text,
+                    start=start)
+    # ConnectionResetError is here to avoid crashing if Discord is still just starting
+    except (ConnectionResetError, exceptions.InvalidID):
+        pass
+
+
+def runmac():
+    from fetch_cord.testing import devicetype, product, bigicon, ver
+    client_id = '740822755376758944'  # macos appid for discord rpc
+    if args.debug:
+        print("runmac")
+        print("devicetype: %s" % devicetype)
+        print("product %s" % product)
+        print("bigicon: %s" % bigicon)
+        print("ver: %s" % ver)
+        print("uptime: %s" % uptime)
+        print("client_id: %s" % client_id)
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+                state=packagesline[0],  # update state as packages
+                details=kernelline[0],  # update details as kernel
+                large_image=bigicon,  # set icon
+                large_text=sysosline[0],  # set large icon text
+                small_image=devicetype,  # set small image icon
+                small_text=product,  # set small image text
+                start=start_time)
+    if args.time:
+        custom_time()
+    elif args.nohost and args.nohardware and args.noshell:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+def custom_time():
+    ctime = int(args.time)
+    time.sleep(ctime)
+
+
+# cycle
+
+
+def cycle0():
+    if args.debug:
+        print("cycle 0")
+    client_id = appid
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+               state=packagesline[0],
+               details=kernelline[0],
+               large_image="big",
+               large_text=sysosline[0],
+               small_image=desktopid,
+               small_text=dewmid,
+               start=start_time)
+    if args.debug:
+        print("appid: %s" % client_id)
+
+    if args.time:
+        custom_time()
+    elif args.nohost and args.nohardware and args.noshell:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+
+# cycle
+
+
+def cycle1():
+    if args.debug:
+        print("cycle 1")
+    client_id = cpuappid
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+               state=diskline,
+               details=memline,
+               large_image="big",
+               large_text=cpuinfo,
+               small_image=gpuid,
+               small_text=gpuinfo,
+               start=start_time)
+    if args.debug:
+        print("appid: %s" % client_id)
+    if args.time:
+        custom_time()
+    elif args.nodistro and args.noshell and args.nohost:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+
+# cycle
+
+
+def cycle2():
+    if args.debug:
+        print("cycle 2")
+    client_id = termappid
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+               state=themeline,
+               details=fontline,
+               large_image="big",
+               large_text=termline[0],
+               small_image=shellid,
+               small_text=shell_line[0],
+               start=start_time)
+    if args.debug:
+        print("appid: %s" % client_id)
+    if args.time:
+        custom_time()
+    elif args.nodistro and args.nohardware and args.nohost:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+
+def cycle3():
+    # if not then forget it
+    if hostline != ["Host: N/A"]:
+        if args.debug:
+            print("cycle 3")
+        client_id = hostappid
+        RPC = Presence(client_id)
+        rpc_tryconnect(RPC)
+        rpc_tryupdate(RPC,
+                state=resline,
+                details=batteryline,
+                large_image="big",
+                large_text=hostline[0],
+                small_image=lapordesk,
+                small_text=lapordesk,
+                start=start_time)
+        if args.debug:
+            print("appid: %s" % client_id)
+        if args.time:
+            custom_time()
+        elif args.nodistro and args.nohardware and args.noshell:
+            time.sleep(9999)
+        else:
+            time.sleep(30)
+    # back from whence you came
+    else:
+        loop = 1
+        loonix(loop)
+    rpc_tryclear(RPC)
+
+
+def pause():
+    if args.debug:
+        print("pause_cycle")
+    if args.time:
+        custom_time()
+    else:
+        time.sleep(30)
+
+
+def windows():
+    if args.debug:
+        print("w_cycle 0")
+    client_id = appid
+    RPC = Presence(client_id)
+    rpc_tryconnect(RPC)
+    rpc_tryupdate(RPC,
+               state=sysosline[0],
+               details=memline,
+               large_image="big",
+               large_text=sysosline[0],
+               small_image=moboid,
+               small_text=moboline[0],
+               start=start_time)
+    if args.debug:
+        print("appid: %s" % client_id)
+    if args.time:
+        custom_time()
+    elif args.nohardware:
+        time.sleep(9999)
+    else:
+        time.sleep(30)
+    rpc_tryclear(RPC)
+
+
+def check_change(loop):
+
+    neofetch(loop)
+
+    from fetch_cord.out import diskline, nvidiagpuline, \
+            memline, cpuline, gpuinfo
+    from fetch_cord.checks import get_cpuinfo, check_diskline, check_batteryline, check_memline
+    if baseinfo:
+        from fetch_cord.checks import check_batteryline
+        from fetch_cord.out import lapordesk, batteryline, packagesline, check_batteryline, get_gpuinfo, cirrusgpuline, virtiogpuline, vmwaregpuline, intelgpuline, amdgpuline, primeoffload, sysosid, amdgpurenderlist
+
+    global packagesline, cpuinfo, gpuinfo, memline, diskline, batteryline
+
+    memline = check_memline(memline)
+    cpuinfo = get_cpuinfo(cpuline, baseinfo)
+    diskline = check_diskline(diskline, cpuinfo)
+    if baseinfo:
+        batteryline = check_batteryline(batteryline, hostline)
+        packagesline = packagesline
+
+    if baseinfo and nvidiagpuline and sysosid.lower() != "macos":
+        from fetch_cord.out import gpuinfo
+        gpuinfo = get_gpuinfo(cirrusgpuline, vmwaregpuline, virtiogpuline, amdgpuline, nvidiagpuline,\
+        intelgpuline, primeoffload, amdgpurenderlist,sysosid, loop)
+
+    loop = 1
+
+    if not neofetchwin:
+        return loonix(loop)
+    else:
+        return wandowz(loop)
+
+def loonix(loop):
+    try:
+        if args.poll_rate:
+            rate = int(args.poll_rate)
+        else:
+            rate = 3
+        if loop == 0:
+            first_connect()
+        while loop < rate:
+            if not args.nodistro and sysosid.lower() != "macos":
+                cycle0()
+            if sysosid.lower() == "macos":
+                runmac()
+            if not args.nohardware:
+                cycle1()
+            if not args.noshell:
+                cycle2()
+            if not args.nohost and sysosid.lower() != "macos":
+                cycle3()
+            if args.pause_cycle:
+                pause()
+            loop += 1
+        if not args.nohardware or not args.nodistro or not args.nohost:
+            loop = 1
+            check_change(loop)
+        else:
+            loop = 1
+            loonix(loop)
+    except (KeyboardInterrupt, ConnectionResetError):
+        if KeyboardInterrupt:
+            print("Closing connection.")
+            sys.exit(0)
+        else:
+            rpc_tryconnect(RPC)
+
+
+def wandowz(loop):
+    try:
+        if loop == 0:
+            first_connect()
+        while loop < 3:
+            if not args.nodistro:
+                windows()
+            if not args.nohardware:
+                cycle1()
+            loop += 1
+        if not args.nohardware:
+            loop = 1
+            check_change(loop)
+        else:
+            loop = 1
+            wandowz(loop)
+    except (KeyboardInterrupt, ConnectionResetError):
+        if KeyboardInterrupt:
+            print("Closing connection.")
+            sys.exit(0)
+        else:
+            rpc_tryconnect(RPC)
