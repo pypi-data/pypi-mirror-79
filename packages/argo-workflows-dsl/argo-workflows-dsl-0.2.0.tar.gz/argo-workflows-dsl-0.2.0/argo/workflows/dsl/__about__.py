b5147201d772520748dbfd1a7c0af2b88e2dbaa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:06 +0000

    Add package argo-workflows-dsl/0.2.0/argo-workflows-dsl-0.2.0.tar.gz

---
+++ packages/argo-workflows-dsl/argo-workflows-dsl-0.2.0.tar.gz/argo-workflows-dsl-0.2.0/argo/workflows/dsl/__about__.py
@@ -0,0 +1,24 @@
+"""About this package."""
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "argo-workflows-dsl"
+__summary__ = "Python DSL for Argo Workflows"
+__uri__ = "https://github.com/argoproj-labs/argo-client-python"
+
+__version__ = "0.2.0"
+
+__author__ = "Yudi Xue, Marek Cermak"
+__email__ = "binarycrayon@gmail.com, macermak@redhat.com"
+
+__license__ = "Apache Software License"
+__copyright__ = "Copyright 2020 {0}".format(__author__)
