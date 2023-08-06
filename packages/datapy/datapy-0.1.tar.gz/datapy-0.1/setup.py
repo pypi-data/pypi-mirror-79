77de27699ece48a81b45ca2311b9b4a5d1cc3dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:35 +0000

    Add package datapy/0.1/datapy-0.1.tar.gz

---
+++ packages/datapy/datapy-0.1.tar.gz/datapy-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name='datapy',
+    packages=['datapy'],
+    version='0.1',
+    license='apache-2.0',
+    description='An intuitive lightweight file based database and query tool.',
+    author='Corey McCarty',
+    author_email='corey@mccarty.dev',
+    url='https://github.com/xanderyzwich/datapy',
+    download_url='https://github.com/xanderyzwich/datapy/archive/v_01.tar.gz',
+    keywords=['file', 'database', 'query'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Database',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+)
