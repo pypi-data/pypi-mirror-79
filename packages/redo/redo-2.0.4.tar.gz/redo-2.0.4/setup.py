9f038bd926d2a1fcce37ab355270fae80665d74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:27 +0000

    Add package redo/2.0.4/redo-2.0.4.tar.gz

---
+++ packages/redo/redo-2.0.4.tar.gz/redo-2.0.4/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    from distutils.core import find_packages, setup
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name="redo",
+    version="2.0.4",
+    description="Utilities to retry Python callables.",
+    long_description=long_description,
+    author="Ben Hearsum",
+    author_email="ben@hearsum.ca",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    entry_points={"console_scripts": ["retry = redo.cmd:main"]},
+    url="https://github.com/mozilla-releng/redo",
+)
