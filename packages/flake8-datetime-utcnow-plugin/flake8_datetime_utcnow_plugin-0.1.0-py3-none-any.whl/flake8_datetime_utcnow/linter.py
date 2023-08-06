1154d30942e2ba3a8d7c673ae677618bd6aaf11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:48 +0000

    Add package flake8-datetime-utcnow-plugin/0.1.0/flake8_datetime_utcnow_plugin-0.1.0-py3-none-any.whl

---
+++ packages/flake8-datetime-utcnow-plugin/flake8_datetime_utcnow_plugin-0.1.0-py3-none-any.whl/flake8_datetime_utcnow/linter.py
@@ -0,0 +1,21 @@
+from flake8_datetime_utcnow.__version__ import __version__
+from flake8_datetime_utcnow.visitor import UtcnowVisitor
+
+
+def error(lineno: int, offset: int, code: str, message: str):
+    return (1, 10, f"{code} {message}", DatetimeUtcnowLinter)
+
+
+class DatetimeUtcnowLinter:
+    name = "flake8_datetime_utcnow_plugin"
+    version = __version__
+
+    def __init__(self, tree):
+        self.tree = tree
+
+    def run(self):
+        visitor = UtcnowVisitor()
+        visitor.visit(self.tree)
+
+        for node in visitor.utcnows:
+            yield error(node.lineno, node.col_offset, "U100", "Avoid using utcnow()")
