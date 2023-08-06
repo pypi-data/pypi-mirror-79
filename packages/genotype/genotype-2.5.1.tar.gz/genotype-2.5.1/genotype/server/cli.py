7354351dec1f211856e84d3c17afb69836f1cb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:37 +0000

    Add package genotype/2.5.1/genotype-2.5.1.tar.gz

---
+++ packages/genotype/genotype-2.5.1.tar.gz/genotype-2.5.1/genotype/server/cli.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import logging
+
+import click
+
+from genotype.compat import iteritems
+from genotype.server.app import create_app
+
+log = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option("--debug", is_flag=True)
+@click.option("--port", default=5000)
+@click.option("--host", default="localhost")
+@click.pass_context
+def serve(context, debug, port, host):
+    """Start up the web interface."""
+    flask_config = {
+        "GENOTYPE_{}".format(key.upper()): value for key, value in iteritems(context.obj)
+    }
+    flask_config["SQLALCHEMY_DATABASE_URI"] = context.obj["database"]
+    flask_config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app = create_app(config_obj=flask_config)
+    app.run(debug=debug, port=port, host=host)
