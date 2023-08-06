1e0386a56533b0d2b754bb960e9974640ea0a443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:21 +0000

    Add package dfauditor/0.0.1/dfauditor-0.0.1-py3-none-any.whl

---
+++ packages/dfauditor/dfauditor-0.0.1-py3-none-any.whl/dfauditor/about.py
@@ -0,0 +1,14 @@
+import dfauditor.__init__
+
+
+name = "dfauditor"
+description = (
+    "A dataframe auditor that extracts descriptive statistics from dataframe columns"
+)
+version = dfauditor.__init__.__version__
+author = "Jacques du Toit"
+email = ""
+uri = "https://github.com/jackdotwa/dataframe-auditor"
+keywords = 'pandas, analytics, utilities'
+classifiers = []
+
