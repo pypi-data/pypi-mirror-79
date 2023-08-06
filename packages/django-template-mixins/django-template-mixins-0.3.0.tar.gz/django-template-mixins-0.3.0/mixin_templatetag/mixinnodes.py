e2e88b878e33d27f52e939dcda64640701af9330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:43 +0000

    Add package django-template-mixins/0.3.0/django-template-mixins-0.3.0.tar.gz

---
+++ packages/django-template-mixins/django-template-mixins-0.3.0.tar.gz/django-template-mixins-0.3.0/mixin_templatetag/mixinnodes.py
@@ -0,0 +1,25 @@
+from django import template
+
+
+class MixinNode(template.Node):
+    def render(self, context):
+        return ''
+
+
+class MixNode(template.Node):
+    def __init__(self, nodelist, *args, extra_context=None, isolated_context=False, **kwargs):
+        self.nodelist = nodelist
+        self.extra_context = extra_context or {}
+        self.isolated_context = isolated_context
+        super().__init__(*args, **kwargs)
+
+    def render(self, context):
+        values = {
+            name: var.resolve(context)
+            for name, var in self.extra_context.items()
+        }
+
+        if self.isolated_context:
+            return self.nodelist.render(context.new(values))
+        with context.push(**values):
+            return self.nodelist.render(context)
