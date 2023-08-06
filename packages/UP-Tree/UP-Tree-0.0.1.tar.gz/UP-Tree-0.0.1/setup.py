600bf55d4c16ea67997d0c1fb01060dee9f4bc09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:15 +0000

    Add package UP-Tree/0.0.1/UP-Tree-0.0.1.tar.gz

---
+++ packages/UP-Tree/UP-Tree-0.0.1.tar.gz/UP-Tree-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="UP-Tree",  # Replace with your own username
+    version="0.0.1",
+    author="Mithil Ghinaiya",
+    author_email="mghinaiya@gmail.com",
+    description="This is a simple module which takes input of transactions and provides it's UP-Tree, HeaderTable, and transactions dictionary",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/miths/UPTree",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
