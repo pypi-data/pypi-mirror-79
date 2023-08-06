b7dcb5a8b0f2df911c29fb896c135d815a6775e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:21 +0000

    Add package sym-cli/0.0.29b4/sym_cli-0.0.29b4-py3-none-any.whl

---
+++ packages/sym-cli/sym_cli-0.0.29b4-py3-none-any.whl/sym/cli/helpers/keywords_to_options.py
@@ -0,0 +1,32 @@
+from typing import List, Mapping, Sequence, Union
+
+Options = Mapping[str, Union[str, bool]]
+Argument = Union[str, Options]
+
+
+def keywords_to_options(args: Sequence[Argument]) -> List[str]:
+    """
+    Converts any dicts in the given arguments into a sequence of
+    options. The keys correspond to option names (with underscores
+    replaced with hyphens). String values correspond to option
+    arguments; boolean values correspond to the presence or
+    absence of the option, with no argument.
+    """
+
+    result = []
+    for arg in args:
+        if isinstance(arg, str):
+            result.append(arg)
+            continue
+        for (key, value) in arg.items():
+            if value is False:
+                continue
+            prefix = "-" if len(key) == 1 else "--"
+            option = prefix + key.replace("_", "-")
+            if not isinstance(value, list):
+                value = [value]
+            for val in value:
+                result.append(option)
+                if isinstance(val, str):
+                    result.append(val)
+    return result
