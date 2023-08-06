4a8e9305ac6bdd3179737d658d93805d825ccfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:31 +0000

    Add package madewithml/0.0.1/madewithml-0.0.1.tar.gz

---
+++ packages/madewithml/madewithml-0.0.1.tar.gz/madewithml-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='madewithml',
+    packages=['madewithml'],
+    version='0.0.1',
+    license='MIT',
+    description="Share what you've made with ML",
+    author='Made With ML',
+    author_email='hello@madewithml.com',
+    url='https://madewithml.com/',
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords=[
+        'machine-learning',
+        'artificial-intelligence',
+        'computer-science'
+    ],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+)
