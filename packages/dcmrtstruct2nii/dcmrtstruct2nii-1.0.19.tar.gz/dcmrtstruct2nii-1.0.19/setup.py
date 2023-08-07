638cb3390105050dd52fe0442b585a7d2b3fdc16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:25 +0000

    Add package dcmrtstruct2nii/1.0.19/dcmrtstruct2nii-1.0.19.tar.gz

---
+++ packages/dcmrtstruct2nii/dcmrtstruct2nii-1.0.19.tar.gz/dcmrtstruct2nii-1.0.19/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import sys
+
+if sys.version_info < (3, 6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='dcmrtstruct2nii',
+    version='1.0.19',
+    description='Convert DICOM RT-Struct to nii',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Thomas Phil',
+    author_email='thomas@tphil.nl',
+    url='https://github.com/Sikerdebaard/dcmrtstruct2nii',
+    python_requires=">=3.6",
+    packages=find_packages(),  #same as name
+    install_requires=[
+        'numpy>=1.15.4',
+        'pydicom>=1.2.1',
+        'scikit-image>=0.14.1',
+        'scipy>=1.2.0',
+        'SimpleITK>=1.2.0',
+        'cleo>=0.7.2'
+    ], #external packages as dependencies
+    #scripts=[
+    #    'bin/dcmrtstruct2nii'
+    #]
+    entry_points={
+        'console_scripts': [
+            'dcmrtstruct2nii=dcmrtstruct2nii.cli.dcmrtstruct2nii:run',
+        ],
+    },
+)
